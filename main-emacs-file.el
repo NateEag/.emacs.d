19175d21fc1efb2887c3639fb7e9be44de20e9b2
Nate Eagleson <nate@ishness.net>
2011-08-10 13:14:57 -0400

    Add default settings for editing tab-indented files.

--- main-emacs-file.el
+++ main-emacs-file.el
@@ -29,6 +29,12 @@
 ;; I do not use double-spaces after sentences. Neither should you.
 (setq sentence-end-double-space nil)
 
+;; I dislike using tabs for indentation, but adhering to project styles is
+;; more important than my personal likes and dislikes. Therefore, here are my
+;; default settings for tab-indented code.
+(setq-default tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
+
 ;; GNU-style indentation on C-like languages is stupid. Linux style is much
 ;; more sane.
 (setq c-default-style "linux"

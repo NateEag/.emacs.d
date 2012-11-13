998501bfb4a70ef0c82e7f47979c933fb3495fd9
Nate Eagleson <nate@nateeag.com>
2012-11-12 22:10:04 -0500

    Work around a freeze on my work setup.

--- tabs.dir-locals.el
+++ tabs.dir-locals.el
@@ -1,9 +1,13 @@
 ;; This is a sample .dir-locals file for working with projects that use
 ;; Tabs for indentation.
 ;; The 'nil' configuration applies to all modes.
+;; DEBUG I originally had '(eval . (highlight-regexp "^ *"))' as the third
+;; line of my nil config, but it caused memory exhaustion errors on my work
+;; setup. I think it has to do with the hack I'm using to load .dir-locals.el
+;; over TRAMP/plink, but until I have it solved, I'm removing it.
 ((nil . ((indent-tabs-mode . t)
          (tab-width . 4)
-         (eval . (highlight-regexp "^ *")))))
+         )))
 
 ;; On rare occasions I might work on a project that uses smart tabs - tabs
 ;; indicate indentation level, spaces indicate alignment level. In that case,

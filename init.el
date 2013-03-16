134a65cc39e3ed0296c9bfef16da67c4216f5edd
Nate Eagleson <nate@nateeag.com>
2013-03-16 05:17:40 -0400

    Turn on autocomplete and autopair in JS mode
    Perhaps someday I'll get a really good JS mode going, but this should
    make things a bit less painful for low effort.

--- init.el
+++ init.el
@@ -259,10 +259,14 @@
   (autoload 'javascript-mode "javascript" nil t)
   (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode)))
 
+(autoload 'autopair-init "autopair-init.el")
+(autoload 'auto-complete-init "auto-complete-init.el")
 (defadvice javascript-mode (after load-smart-dash-mode)
   "Load smart dash mode, since javascript-mode doesn't have a hook."
   (smart-dash-mode t)
-  (comment-auto-fill))
+  (comment-auto-fill)
+  (autopair-init)
+  (auto-complete-init))
 (ad-activate 'javascript-mode)
 
 ;; If we're running in a window system, start an emacs server, so emacsclient

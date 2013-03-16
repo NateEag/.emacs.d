bd5620d20392c87280314a573b8f7423f0c46571
Nate Eagleson <nate@nateeag.com>
2013-03-16 04:43:25 -0400

    Import PHP_CodeSniffer to aid Flymake's PHP powers
    yes, it's a PHP library, not an elisp one, but my major goal for this
    repo is to keep it as close to working in one checkout as I possibly
    can. Hence, why keep this dependency anywhere else?

---
+++ php-cs-1.4.4/CodeSniffer/Standards/Squiz/Tests/WhiteSpace/OperatorSpacingUnitTest.js
@@ -0,0 +1,53 @@
+
+
+result = 1 + 2;
+result = 1  + 2;
+result = 1  +   2;
+result = 1 +2;
+result = 1+ 2;
+result = 1+2;
+
+result = 1 - 2;
+result = 1  - 2;
+result = 1  -   2;
+result = 1 -2;
+result = 1- 2;
+result = 1-2;
+
+result = 1 * 2;
+result = 1  * 2;
+result = 1  *   2;
+result = 1 *2;
+result = 1* 2;
+result = 1*2;
+
+result = 1 / 2;
+result = 1  / 2;
+result = 1  /   2;
+result = 1 /2;
+result = 1/ 2;
+result = 1/2;
+
+result = 1 % 2;
+result = 1  % 2;
+result = 1  %   2;
+result = 1 %2;
+result = 1% 2;
+result = 1%2;
+result = '100%';
+
+result += 4;
+result+=4;
+result -= 4;
+result-=4;
+result /= 4;
+result/=4;
+result *=4;
+result*=4;
+
+$.localScroll({offset: {top: -32}});
+
+switch (result) {
+	case -1:
+		break;
+}

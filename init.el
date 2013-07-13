eb3fa85c6db8d3cd07a6830ad35281d603cd5d2f
Nate Eagleson <nate@nateeag.com>
2013-07-13 10:50:53 -0400

    Default to Symfony2 coding style in php-mode
    Because it's more-or-less PSR-2, which is what we use at work, and what
    I'm starting to use in my stuff at home, too.

--- init.el
+++ init.el
@@ -283,7 +283,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((eval highlight-regexp "^ *")))))
+ '(safe-local-variable-values (quote ((eval highlight-regexp "^ *"))))
+ '(php-mode-custom-coding-style "Symfony2"))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.

ae13ed2e86a0621668574ca2832409dcbabeb973
Nate Eagleson <nate@nateeag.com>
2013-08-26 23:42:11 -0400

    Sort-of working jedi-mode.
    A few seconds of experimentation confirm two things:
    
    1) This is much better autocomplete than rope-mode provides.
    2) It will take some tweaking to make this do what I want.

--- python-mode-init.el
+++ python-mode-init.el
@@ -14,90 +14,24 @@
 
 (autoload 'autopair-init "autopair-init.el")
 
-;; The somewhat-convoluted setup for ropemacs/autocomplete.el integration
-;; in python-mode follows.
-(defvar pymacs-initialized nil)
-(defun initialize-pymacs ()
-  "Load pymacs if it is not already loaded."
-  (interactive)
-  (if (not pymacs-initialized)
-      (do-initialize-pymacs)))
-
-(defun do-initialize-pymacs ()
-  "Does the work of loading pymacs."
-  (autoload 'pymacs-apply "pymacs")
-  (autoload 'pymacs-call "pymacs")
-  (autoload 'pymacs-eval "pymacs" nil t)
-  (autoload 'pymacs-exec "pymacs" nil t)
-  (autoload 'pymacs-load "pymacs" nil t)
-  (setq pymacs-initialized 't))
-
-(defvar rope-initialized nil)
-(defun initialize-rope ()
-  "Loads and configures rope as I like to use it."
-  (if (not rope-initialized)
-      ((lambda ()
-        (pymacs-load "ropemacs" "rope-")
-        (setq ropemacs-enable-autoimport t)
-        (setq rope-initialized 't)))))
-
-(defvar auto-complete-python-initialized nil)
-(defvar ac-initialized nil)
-(defun initialize-auto-complete-python ()
-  "Loads and configures auto-complete for Python hacking."
-
-  (autoload 'auto-complete-init "auto-complete-init.el")
-
-  (auto-complete-init)
-
-  (ac-ropemacs-init)
-  (setq ac-sources (append '(ac-source-ropemacs
-                             ac-source-ropemacs-dot) ac-sources)))
-
-;; Mostly based on
-;; https://github.com/gabrielelanaro/emacs-for-python/blob/master/epy-completion.el
-;; with some shades of the old auto-complete-config.el still hanging on.
-;; Hybridization brought on by vague feeling that it used to run faster than
-;; Gabriel's code when I tried his (but I might well just be crazy, and I know
-;; virtually nothing about elisp).
-(defun ac-ropemacs-init ()
-  (defvar ac-ropemacs-completions-cache nil)
-
-  (ac-define-source ropemacs
-    '((init
-       . (fill-ac-ropemacs-completions-cache))
-      (candidates . ac-ropemacs-completions-cache)
-      (prefix . "[_a-zA-Z0-9]+")))
-
-  (ac-define-source ropemacs-dot
-    '((init
-       . (fill-ac-ropemacs-completions-cache))
-      (candidates . ac-ropemacs-completions-cache)
-      (prefix . c-dot)
-      (requires . 0))))
-
-(defun fill-ac-ropemacs-completions-cache ()
-  (setq ac-ropemacs-completions-cache
-      (mapcar
-          (lambda (completion)
-      	          (concat ac-prefix completion))
-          	  (ignore-errors (rope-completions)))))
+;; Load Jedi. Note that for this to work, you'll have to install Jedi in a
+;; virtualenv, using the Makefile.
+(autoload 'emacs-epc-init "emacs-epc-init.el") ;; Need epc for jedi.
+(add-to-list 'load-path "~/.emacs.d/emacs-jedi")
+(autoload 'jedi:setup "jedi" nil t)
 
 ;; Load my python-mode accessories only when python-mode kicks in.
 (defun load-python-mode-accessories ()
   "Loads all the libraries/tools I want to have when I'm in python-mode."
-  ;; GRIPE Commenting these out because they're just breaking things.
-  ;; I'm hoping to ditch it for Jedi soon anyway, as I work on moving to Emacs
-  ;; 24.
-  ;; (initialize-pymacs)
-  ;; (initialize-rope)
-  ;; (initialize-auto-complete-python)
   (smart-dash-mode t)
   (comment-auto-fill)
   (autopair-init)
   (setq autopair-handle-action-fns
         (list #'autopair-default-handle-action
-              #'autopair-python-triple-quote-action)))
+              #'autopair-python-triple-quote-action))
+  (emacs-epc-init)
+  (jedi:setup)
+  (setq jedi:complete-on-dot t))
 (add-hook 'python-mode-hook 'load-python-mode-accessories)
 
 (provide 'python-mode-init)

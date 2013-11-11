154fe8353c82eb958d3e21e1bad168598a889d6f
Nate Eagleson <nate@nateeag.com>
2013-11-11 13:51:32 -0500

    Only turn on comment-auto-fill in current buffer
    Since it's meant for use in mode hooks, it's the right thing to do.

--- comment-auto-fill.el
+++ comment-auto-fill.el
@@ -1,8 +1,8 @@
 ;; Auto-fill in comments is something I have wanted for a very long time.
 ;; It looks like it may actually be simpler than I thought.
 (defun comment-auto-fill ()
-  "Call to activate auto-filling comments. Not perfect, but beats nothing."
-  (setq comment-auto-fill-only-comments t)
+  "Call to activate auto-filling comments for the current buffer."
+  (setq-local comment-auto-fill-only-comments t)
   (auto-fill-mode 1))
 
 (provide 'comment-auto-fill)

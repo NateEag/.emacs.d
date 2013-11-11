201af2434b2b719163f1afab456d0d1107a3ec8a
Nate Eagleson <nate@nateeag.com>
2013-11-10 21:42:47 -0500

    Add elisp-slime-nav mode
    It's pretty nifty.

--- emacs-lisp-init.el
+++ emacs-lisp-init.el
@@ -3,4 +3,6 @@
 (defun emacs-lisp-init ()
   "My emacs lisp editing config."
   (autopair-init)
-  (comment-auto-fill))
+  (comment-auto-fill)
+  (elisp-slime-nav-mode)
+  (diminish 'elisp-slime-nav-mode))

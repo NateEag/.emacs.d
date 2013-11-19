9828e0b27e3e0366742d514b06895091690179df
Nate Eagleson <nate@nateeag.com>
2013-11-19 18:42:02 -0500

    Progress on adding lilypond-mode
    It's not in ELPA and lilypond-words.el has to be built with each
    Lilypond release, which makes it sort of tricky to find.
    
    I ripped it out of an RPM.

--- init.el
+++ init.el
@@ -299,6 +299,12 @@
 ;; Everyone needs text-mode.
 (add-hook 'text-mode-hook 'text-mode-init)
 
+;; lilypond-mode - ripped from
+(autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
+(add-to-list 'auto-mode-alist '("\\.ly$" . LilyPond-mode))
+(add-to-list 'auto-mode-alist '("\\.ily$" . LilyPond-mode))
+(add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
+
 ;; Commit message mode.
 (defun git-commit-mode-hook ()
   "My settings for writing commit messages."

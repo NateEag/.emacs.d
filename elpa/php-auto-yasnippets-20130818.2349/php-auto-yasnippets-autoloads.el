c4faa8a9dca54e837602ef0d93845f917bfa2e5d
Nate Eagleson <nate@nateeag.com>
2013-12-01 01:53:38 -0500

    Add basic php-auto-yasnippets setup
    I hope to get it to auto-trigger on selecting a function from the
    auto-complete menu.
    
    That will have to wait for a time other than 2 AM on a Sunday morning,
    however, and may prove to just not be feasible.

---
+++ elpa/php-auto-yasnippets-20130818.2349/php-auto-yasnippets-autoloads.el
@@ -0,0 +1,40 @@
+;;; php-auto-yasnippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (yas/create-php-snippet yas/initialize) "php-auto-yasnippets"
+;;;;;;  "php-auto-yasnippets.el" (21146 54819 0 0))
+;;; Generated autoloads from php-auto-yasnippets.el
+
+(autoload 'yas/initialize "php-auto-yasnippets" "\
+Setup yasnippet hook for php-auto-yasnippet.
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(yas/initialize))
+
+(autoload 'yas/create-php-snippet "php-auto-yasnippets" "\
+Creates and expands a snippet for the PHP function at point.
+
+If called with the universal prefix then it prompts the user for
+the name of a PHP class and treats the name at point as the name
+of a method for that class.
+
+\(fn PREFIX)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("php-auto-yasnippets-pkg.el") (21146 54819
+;;;;;;  299472 0))
+
+;;;***
+
+(provide 'php-auto-yasnippets-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; php-auto-yasnippets-autoloads.el ends here
---
+++ elpa/php-auto-yasnippets-20130818.2349/php-auto-yasnippets-pkg.el
@@ -0,0 +1,3 @@
+(define-package "php-auto-yasnippets" "20130818.2349" "Creates snippets for PHP functions"
+  '((php-mode "1.11")
+    (yasnippet "0.8.0")))

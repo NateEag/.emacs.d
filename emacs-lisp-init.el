297dd071011acc5853ca92a71c769e914369c4ca
Nate Eagleson <nate@nateeag.com>
2013-12-03 17:04:54 -0500

    Make my life better when editing Emacs Lisp
    because it seems like I'm headed inexorably towards writing more of it,
    not less.

--- emacs-lisp-init.el
+++ emacs-lisp-init.el
@@ -1,6 +1,9 @@
 (defun emacs-lisp-init ()
   "My emacs lisp editing config."
+
   (autopair-init)
   (comment-auto-fill)
   (elisp-slime-nav-mode)
-  (diminish 'elisp-slime-nav-mode))
+  (diminish 'elisp-slime-nav-mode)
+  (ac-emacs-lisp-mode-setup)
+  (eldoc-mode t))

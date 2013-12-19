4ab838bcacc353977773d00e56a5d7e9262cd22d
Nate Eagleson <nate@nateeag.com>
2013-12-19 07:24:00 -0500

    Store customized variables in their own file
    This way it'll be slightly less of a pain to juggle different variable
    settings between machines.

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(php-mode-custom-coding-style "Symfony2")
+ '(safe-local-variable-values (quote ((eval highlight-regexp "^ *")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -381,16 +381,5 @@
 (when (display-graphic-p)
     (server-start))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(php-mode-custom-coding-style "Symfony2")
- '(safe-local-variable-values (quote ((eval highlight-regexp "^ *")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)

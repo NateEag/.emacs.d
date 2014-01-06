e912bce61c844568cd23eb9a5794d3daf80a313f
Nate Eagleson <nate@nateeag.com>
2014-01-06 12:54:37 -0500

    Make php-cs-1.4.4/scripts/phpcs.bat work
    It looks like it's meant to built by some install process.
    
    I just hardcoded a few values that work on my current Windows box, and
    seem like they should work fine on future Windows boxes too.

--- php-cs-1.4.4/scripts/phpcs.bat
+++ php-cs-1.4.4/scripts/phpcs.bat
@@ -1,9 +1,9 @@
 @echo off
 REM PHP_CodeSniffer tokenises PHP code and detects violations of a
 REM defined set of coding standards.
-REM 
+REM
 REM PHP version 5
-REM 
+REM
 REM @category  PHP
 REM @package   PHP_CodeSniffer
 REM @author    Greg Sherwood <gsherwood@squiz.net>
@@ -13,4 +13,4 @@ REM @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.
 REM @version   CVS: $Id: phpcs.bat,v 1.3 2007-11-04 22:02:16 squiz Exp $
 REM @link      http://pear.php.net/package/PHP_CodeSniffer
 
-"@php_bin@" -d auto_append_file="" -d auto_prepend_file="" -d include_path="'@php_dir@'" -f "@bin_dir@\phpcs" -- %*
+"php" -d auto_append_file="" -d auto_prepend_file="" -d include_path="'%HOMEPATH%\.emacs.d\php-cs-1.4.4'" -f "%HOMEPATH%\.emacs.d\php-cs-1.4.4\scripts\phpcs" -- %*

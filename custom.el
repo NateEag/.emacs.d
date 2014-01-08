a84d98e7a2c658e60d32292a57138290a6227a2b
Nate Eagleson <nate@nateeag.com>
2014-01-08 17:23:56 -0500

    Make PHP class snippet brace placement PSR-2 compliant

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-trigger-key "TAB")
+ '(php-executable "/Program Files (x86)/PHP/php")
  '(php-mode-custom-coding-style "Symfony2")
  '(safe-local-variable-values (quote ((eval highlight-regexp "^ *")))))
 (custom-set-faces

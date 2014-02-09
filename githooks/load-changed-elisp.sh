a8ba8e8d7a3ab3d7e9adcb05dc68863f57370c65
Nate Eagleson <nate@nateeag.com>
2014-02-08 23:03:34 -0500

    Only load updated .el files if a server's running

--- githooks/load-changed-elisp.sh
+++ githooks/load-changed-elisp.sh
@@ -6,8 +6,14 @@
 # This is probably only useful after git pull, to load latest config changes
 # into a long-running instance.
 
-updated_files=$(git diff --name-only --diff-filter=[ACM] $1 $2 | grep '\.el$')
-for file in $updated_files
-do
-    emacsclient --eval "(load-file \"~/.emacs.d/$file\")"
-done
+emacs --batch --eval "(require 'server)" \
+    --eval "(if (not (server-running-p)) (kill-emacs 1))"
+is_server_running=$?
+if [ $is_server_running -eq 0 ]; then
+    updated_files=$(git diff --name-only --diff-filter=[ACM] $1 $2 | grep '\.el$')
+    for file in $updated_files
+    do
+        emacsclient --eval \
+            "(if (server-running-p) (load-file \"~/.emacs.d/$file\"))"
+    done
+fi

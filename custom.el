55b7bfde181713c53994f08f1af68a82f03ac392
Nate Eagleson <nate@nateeag.com>
2014-02-11 23:33:43 -0500

    Make smart-dash and auto-complete play nicely

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-trigger-commands-on-completing (quote (delete-backward-char backward-delete-char backward-delete-char-untabify autopair-backspace paredit-backward-delete paredit-backward-delete-word smart-dash-insert)))
  '(ac-trigger-key "TAB")
  '(php-mode-custom-coding-style "Symfony2")
  '(safe-local-variable-values (quote ((eval highlight-regexp "^ *")))))

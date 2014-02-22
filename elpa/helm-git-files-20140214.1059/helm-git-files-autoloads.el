98024e64a1117889ce24e6d0598478e5c5dae673
Nate Eagleson <nate@nateeag.com>
2014-02-22 14:40:43 -0500

    Add helm-git-files from MELPA

---
+++ elpa/helm-git-files-20140214.1059/helm-git-files-autoloads.el
@@ -0,0 +1,46 @@
+;;; helm-git-files-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-git-files helm-git-files:submodule-sources
+;;;;;;  helm-git-files:git-p) "helm-git-files" "helm-git-files.el"
+;;;;;;  (21256 64301 0 0))
+;;; Generated autoloads from helm-git-files.el
+
+(autoload 'helm-git-files:git-p "helm-git-files" "\
+
+
+\(fn &optional ROOT)" nil nil)
+
+(defvar helm-git-files:modified-source nil)
+
+(defvar helm-git-files:untracked-source nil)
+
+(defvar helm-git-files:all-source nil)
+
+(autoload 'helm-git-files:submodule-sources "helm-git-files" "\
+
+
+\(fn KINDS &optional ROOT)" nil nil)
+
+(autoload 'helm-git-files "helm-git-files" "\
+`helm' for opening files managed by Git.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-git-files-pkg.el") (21256 64301
+;;;;;;  356367 0))
+
+;;;***
+
+(provide 'helm-git-files-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-git-files-autoloads.el ends here
---
+++ elpa/helm-git-files-20140214.1059/helm-git-files-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-git-files" "20140214.1059" "helm for git files" (quote ((helm "1.5.9"))))

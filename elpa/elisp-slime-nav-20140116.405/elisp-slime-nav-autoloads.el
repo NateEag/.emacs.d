f99b80aa7d9fdd0822384324dfd95e9d86acec3d
Nate Eagleson <nate@nateeag.com>
2014-02-23 09:21:29 -0500

    Update elisp-slime-nav from MELPA

--- elpa/elisp-slime-nav-20140116.405/elisp-slime-nav-autoloads.el
+++ elpa/elisp-slime-nav-20140116.405/elisp-slime-nav-autoloads.el
@@ -1,10 +1,12 @@
 ;;; elisp-slime-nav-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
 
-;;;### (autoloads nil "elisp-slime-nav" "elisp-slime-nav.el" (21120
-;;;;;;  17258 0 0))
+;;;### (autoloads (elisp-slime-nav-describe-elisp-thing-at-point
+;;;;;;  elisp-slime-nav-find-elisp-thing-at-point turn-on-elisp-slime-nav-mode
+;;;;;;  elisp-slime-nav-mode) "elisp-slime-nav" "elisp-slime-nav.el"
+;;;;;;  (21258 879 0 0))
 ;;; Generated autoloads from elisp-slime-nav.el
 
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav" "\
@@ -18,25 +20,39 @@ Explicitly enable `elisp-slime-nav-mode'.
 \(fn)" nil nil)
 
 (autoload 'elisp-slime-nav-find-elisp-thing-at-point "elisp-slime-nav" "\
-Jump to the elisp thing at point, be it a function, variable, library or face.
-With a prefix arg, prompt for the symbol to jump to.
-Argument SYM-NAME thing to find.
+Find the elisp thing at point, be it a function, variable, library or face.
+
+With a prefix arg, or if there is no thing at point, prompt for
+the symbol to jump to.
+
+Argument SYM-NAME is the thing to find.
 
 \(fn SYM-NAME)" t nil)
 
 (autoload 'elisp-slime-nav-describe-elisp-thing-at-point "elisp-slime-nav" "\
 Display the full documentation of the elisp thing at point.
+
 The named subject may be a function, variable, library or face.
-With a prefix arg, prompt for the symbol to jump to.
-Argument SYM-NAME thing to find.
+
+With a prefix arg, or if there is not \"thing\" at point, prompt
+for the symbol to jump to.
+
+Argument SYM-NAME is the thing to find.
 
 \(fn SYM-NAME)" t nil)
 
 ;;;***
 
+;;;### (autoloads nil nil ("elisp-slime-nav-pkg.el") (21258 880 161357
+;;;;;;  0))
+
+;;;***
+
+(provide 'elisp-slime-nav-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; elisp-slime-nav-autoloads.el ends here

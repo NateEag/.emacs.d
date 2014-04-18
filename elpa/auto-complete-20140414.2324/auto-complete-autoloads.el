0043e52a79325954c915f5182aaf44886664475c
Nate Eagleson <nate@nateeag.com>
2014-04-18 12:38:54 -0400

    Update auto-complete from MELPA

--- elpa/auto-complete-20140414.2324/auto-complete-autoloads.el
+++ elpa/auto-complete-20140414.2324/auto-complete-autoloads.el
@@ -1,10 +1,10 @@
 ;;; auto-complete-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (global-auto-complete-mode auto-complete-mode auto-complete)
-;;;;;;  "auto-complete" "auto-complete.el" (21296 34015 0 0))
+;;;### (autoloads nil "auto-complete" "auto-complete.el" (21329 21748
+;;;;;;  0 0))
 ;;; Generated autoloads from auto-complete.el
 
 (autoload 'auto-complete "auto-complete" "\
@@ -41,15 +41,13 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 ;;;***
 
 ;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el")
-;;;;;;  (21296 34015 639000 0))
+;;;;;;  (21329 21749 158000 0))
 
 ;;;***
 
-(provide 'auto-complete-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; auto-complete-autoloads.el ends here
---
+++ elpa/auto-complete-20140414.2324/auto-complete-pkg.el
@@ -0,0 +1,1 @@
+(define-package "auto-complete" "20140414.2324" "Auto Completion for GNU Emacs" '((popup "0.5.0")))

9fa88d093674503a8a0225cec678b8963cb730ac
Nate Eagleson <nate@nateeag.com>
2014-04-18 12:49:16 -0400

    Update puppet-mode from MELPA

--- elpa/puppet-mode-20140417.1040/puppet-mode-autoloads.el
+++ elpa/puppet-mode-20140417.1040/puppet-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; puppet-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (puppet-mode) "puppet-mode" "puppet-mode.el" (21315
-;;;;;;  7575 0 0))
+;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (21329 22380
+;;;;;;  0 0))
 ;;; Generated autoloads from puppet-mode.el
 
 (autoload 'puppet-mode "puppet-mode" "\
@@ -16,16 +16,9 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("puppet-mode-pkg.el") (21315 7575 180000
-;;;;;;  0))
-
-;;;***
-
-(provide 'puppet-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; puppet-mode-autoloads.el ends here

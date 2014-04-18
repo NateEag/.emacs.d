75737d3820d7118f868ae4c2ab828e42b1b0db87
Nate Eagleson <nate@nateeag.com>
2014-04-18 12:57:31 -0400

    Update web-mode from MELPA

--- elpa/web-mode-20140413.1309/web-mode-autoloads.el
+++ elpa/web-mode-20140413.1309/web-mode-autoloads.el
@@ -1,10 +1,9 @@
 ;;; web-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21299 11559
-;;;;;;  0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (21329 22881 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
@@ -14,16 +13,14 @@ Major mode for editing web templates (HTML documents with embedded parts and blo
 
 ;;;***
 
-;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21299
-;;;;;;  11559 377000 0))
+;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21329
+;;;;;;  22881 375000 0))
 
 ;;;***
 
-(provide 'web-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; web-mode-autoloads.el ends here

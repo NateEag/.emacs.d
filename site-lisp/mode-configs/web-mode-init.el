528bf16f61b998f60dad8cacc538d0d284a0497e
Nate Eagleson <nate@nateeag.com>
2014-04-19 10:04:33 -0400

    Prefer server-side comments in web-mode
    this variable name changed and I never knew.

--- site-lisp/mode-configs/web-mode-init.el
+++ site-lisp/mode-configs/web-mode-init.el
@@ -6,7 +6,7 @@
   (setq web-mode-css-indent-offset 4)
   (setq web-mode-code-indent-offset 4)
   (setq web-mode-indent-style 2)
-  (setq web-mode-prefer-server-commenting t)
+  (setq web-mode-comment-style 2)
 
   (setq web-mode-style-padding 0)
   (setq web-mode-script-padding 0)

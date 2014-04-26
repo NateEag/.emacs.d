94526a74f127d41a7545a4c1596e37d0bbf0f39d
Nate Eagleson <nate@nateeag.com>
2014-04-25 23:59:45 -0400

    Add focus-mode for a minimalist editing setup
    Who needs to pay $10 for WriteRoom?

--- custom.el
+++ custom.el
@@ -3,13 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ac-trigger-commands-on-completing (quote (delete-backward-char backward-delete-char backward-delete-char-untabify autopair-backspace paredit-backward-delete paredit-backward-delete-word smart-dash-insert)))
+ '(ac-trigger-commands-on-completing
+   (quote
+    (delete-backward-char backward-delete-char backward-delete-char-untabify autopair-backspace paredit-backward-delete paredit-backward-delete-word smart-dash-insert)))
  '(ac-trigger-key "TAB")
  '(php-mode-custom-coding-style "Symfony2")
- '(safe-local-variable-values (quote ((php-auto-yasnippet-required-files (list "~/github/gitlist/vendor/autoload.php")) (eval guess-style-guess-all) (eval highlight-regexp "^ *")))))
+ '(safe-local-variable-values
+   (quote
+    ((php-auto-yasnippet-required-files
+      (list "~/github/gitlist/vendor/autoload.php"))
+     (eval guess-style-guess-all)
+     (eval highlight-regexp "^ *")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe ((t nil))))

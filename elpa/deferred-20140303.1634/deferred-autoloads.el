0ef270cfd565bc9c8f5e59ab613110534dd75e19
Nate Eagleson <nate@nateeag.com>
2014-05-07 21:41:06 -0400

    Update deferred from MELPA

--- elpa/deferred-20140303.1634/deferred-autoloads.el
+++ elpa/deferred-20140303.1634/deferred-autoloads.el
@@ -1,18 +1,15 @@
 ;;; deferred-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("deferred-pkg.el" "deferred.el") (21236
-;;;;;;  53992 685654 0))
+;;;### (autoloads nil nil ("deferred.el") (21354 57493 663193 0))
 
 ;;;***
 
-(provide 'deferred-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; deferred-autoloads.el ends here
---
+++ elpa/deferred-20140303.1634/deferred-pkg.el
@@ -0,0 +1,1 @@
+(define-package "deferred" "20140303.1634" "Simple asynchronous functions for emacs lisp" 'nil :url "https://github.com/kiwanami/emacs-deferred" :keywords ("deferred" "async"))

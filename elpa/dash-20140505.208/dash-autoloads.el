755ec0aa0d1e44254995c8fc319c8796b2bcf66b
Nate Eagleson <nate@nateeag.com>
2014-05-07 21:40:09 -0400

    Update dash from MELPA

--- elpa/dash-20140505.208/dash-autoloads.el
+++ elpa/dash-20140505.208/dash-autoloads.el
@@ -1,18 +1,15 @@
 ;;; dash-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21172 13150
-;;;;;;  885731 0))
+;;;### (autoloads nil nil ("dash.el") (21354 57435 822376 0))
 
 ;;;***
 
-(provide 'dash-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; dash-autoloads.el ends here

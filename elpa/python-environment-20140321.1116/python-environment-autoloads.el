8771f5b94ed1a4114cc285aade6f3d9d82e5ca51
Nate Eagleson <nate@nateeag.com>
2014-05-07 22:22:02 -0400

    Add python-environment from ELPA

---
+++ elpa/python-environment-20140321.1116/python-environment-autoloads.el
@@ -0,0 +1,16 @@
+;;; python-environment-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("python-environment-pkg.el" "python-environment.el"
+;;;;;;  "test-python-environment.el") (21354 57663 788476 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-environment-autoloads.el ends here

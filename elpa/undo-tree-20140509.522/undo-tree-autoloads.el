6feb8bb17fdefd94706b3c906aa382c2c7082183
Nate Eagleson <nate@nateeag.com>
2014-05-12 23:52:50 -0400

    Update undo-tree from MELPA

--- elpa/undo-tree-20140509.522/undo-tree-autoloads.el
+++ elpa/undo-tree-20140509.522/undo-tree-autoloads.el
@@ -1,10 +1,9 @@
 ;;; undo-tree-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (global-undo-tree-mode undo-tree-mode) "undo-tree"
-;;;;;;  "undo-tree.el" (21228 30393 0 0))
+;;;### (autoloads nil "undo-tree" "undo-tree.el" (21361 38655 0 0))
 ;;; Generated autoloads from undo-tree.el
 
 (autoload 'undo-tree-mode "undo-tree" "\
@@ -50,16 +49,9 @@ See `undo-tree-mode' for more information on Undo-Tree mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("undo-tree-pkg.el") (21228 30393 754165
-;;;;;;  0))
-
-;;;***
-
-(provide 'undo-tree-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; undo-tree-autoloads.el ends here

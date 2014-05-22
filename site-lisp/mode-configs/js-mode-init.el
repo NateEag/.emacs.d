c7d59e4cb229ba8257a149b8d058e053b517d8da
Nate Eagleson <nate@nateeag.com>
2014-05-21 23:36:04 -0400

    Rebind Tern's completion-at-point to auto-complete
    I wish there was just a proper auto-complete source.

--- site-lisp/mode-configs/js-mode-init.el
+++ site-lisp/mode-configs/js-mode-init.el
@@ -1,3 +1,7 @@
+(defun tern-complete-fallback-to-tab ()
+  "If tern finds completions at point, try them. Else, usual tab behavior."
+  )
+
 (defun js-mode-init ()
   "My mode hook for JS editing modes."
 
@@ -15,4 +19,8 @@
   (eval-after-load 'tern
     '(progn
        (require 'tern-auto-complete)
-       (tern-ac-setup))))
+       (tern-ac-setup)
+       ;; Override Tern's completion-at-point keybinding with auto-complete.
+       ;; I really wish I could just press Tab for this, but the current
+       ;; tern-auto-complete package does not have a general-case ac-source.
+       (define-key tern-mode-keymap (kbd "M-TAB") 'tern-ac-complete))))

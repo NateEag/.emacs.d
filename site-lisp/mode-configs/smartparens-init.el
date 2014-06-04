9f8c693431b4e3452981d47f06cfe30cc037165d
Nate Eagleson <nate@nateeag.com>
2014-06-04 11:08:50 -0400

    Tweak smartparen keybindings a bit
    It should be easier for me to jump to beginning/end of something this
    way, which is something I'm currently missing.

--- site-lisp/mode-configs/smartparens-init.el
+++ site-lisp/mode-configs/smartparens-init.el
@@ -16,10 +16,10 @@
   (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
   (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
 
-  (define-key sp-keymap (kbd "C-M-d") 'sp-down-sexp)
-  (define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
-  (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
-  (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
+  (define-key sp-keymap (kbd "C-S-d") 'sp-down-sexp)
+  (define-key sp-keymap (kbd "C-S-a") 'sp-backward-down-sexp)
+  (define-key sp-keymap (kbd "C-M-a") 'sp-beginning-of-sexp)
+  (define-key sp-keymap (kbd "C-M-d") 'sp-end-of-sexp)
 
   (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
   (define-key emacs-lisp-mode-map (kbd ")") 'sp-up-sexp)

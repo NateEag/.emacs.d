0ee2e5416997b548f352f65734ec11b9725a6a2f
Nate Eagleson <nate@nateeag.com>
2014-06-19 23:22:30 -0400

    Use my-prog-mode-init in python-mode-hook

--- site-lisp/mode-configs/python-mode-accessories-init.el
+++ site-lisp/mode-configs/python-mode-accessories-init.el
@@ -3,17 +3,13 @@
 ;; Load my python-mode accessories only when python-mode kicks in.
 (defun python-mode-accessories-init ()
   "Loads all the libraries/tools I want to have when I'm in python-mode."
-  (smart-dash-mode t)
-  (comment-auto-fill)
-  (smartparens-mode)
-  (auto-complete-mode)
+
+  (my-prog-mode-init)
 
   ;; Initialize Jedi. Note that for this to work, you'll have to install Jedi
   ;; in a virtualenv, using the Makefile.
   (setq jedi:setup-keys t)
   (jedi:setup)
-  (setq jedi:complete-on-dot t)
-
-  (flycheck-mode))
+  (setq jedi:complete-on-dot t))
 
 (provide 'python-mode-accessories-init)

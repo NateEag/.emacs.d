b22856216223c531a665e6e56b07261b91c8c608
Nate Eagleson <nate@nateeag.com>
2014-06-19 23:35:26 -0400

    require_once snippet not overridden by once in php-mode
    computers do what you tell them, mostly.
    
    though technically somebody else told mine to do this.

--- site-lisp/mode-configs/yasnippet-init.el
+++ site-lisp/mode-configs/yasnippet-init.el
@@ -9,6 +9,12 @@
       (progn
         ;; Set up all snippet dirs to lazy-load.
         (yas-reload-all)
+
+        ;; Give keys_with_underscores priority over non-underscored-keys.
+        ;; This lets things like require_once in php-mode not be overridden by
+        ;; 'once' from cc-mode, php-mode's parent mode.
+        (setq yas-key-syntaxes (list "w_" "w" "w_." "w_.()" "^ "))
+
         ;; GRIPE For reasons I don't understand, I need this invocation in
         ;; order to avoid a never-ending recursion of defining keybindings. I
         ;; think it's some interaction between yasnippet and auto-complete, but

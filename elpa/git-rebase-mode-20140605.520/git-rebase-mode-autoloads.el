b6ba2143b24d6ec48705c36d597eff380d060409
Nate Eagleson <nate@nateeag.com>
2014-07-21 09:02:37 -0400

    Update package git-rebase-mode-20140313.1504

--- elpa/git-rebase-mode-20140605.520/git-rebase-mode-autoloads.el
+++ elpa/git-rebase-mode-20140605.520/git-rebase-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; git-rebase-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (git-rebase-mode) "git-rebase-mode" "git-rebase-mode.el"
-;;;;;;  (21286 63350 0 0))
+;;;### (autoloads nil "git-rebase-mode" "git-rebase-mode.el" (21453
+;;;;;;  3947 0 0))
 ;;; Generated autoloads from git-rebase-mode.el
 
 (autoload 'git-rebase-mode "git-rebase-mode" "\
@@ -21,16 +21,9 @@ running 'man git-rebase' at the command line) for details.
 
 ;;;***
 
-;;;### (autoloads nil nil ("git-rebase-mode-pkg.el") (21286 63350
-;;;;;;  345000 0))
-
-;;;***
-
-(provide 'git-rebase-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; git-rebase-mode-autoloads.el ends here

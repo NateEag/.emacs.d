38248474e000b32dd5081a1b51a918069f2c81bc
Nate Eagleson <nate@nateeag.com>
2014-07-21 08:29:51 -0400

    Update package epc-20140323.2054

--- elpa/epc-20140609.2234/epc-autoloads.el
+++ elpa/epc-20140609.2234/epc-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (21354
-;;;;;;  57239 607414 0))
+;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (21453
+;;;;;;  1978 565427 0))
 
 ;;;***
 

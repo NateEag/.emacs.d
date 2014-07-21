956fd20ab0b2ce13f79f979aea0fa1c790a84c7d
Nate Eagleson <nate@nateeag.com>
2014-07-21 08:28:34 -0400

    Update package pkg-info-20131101.408

--- elpa/pkg-info-20140610.630/pkg-info-autoloads.el
+++ elpa/pkg-info-20140610.630/pkg-info-autoloads.el
@@ -1,12 +1,31 @@
 ;;; pkg-info-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (pkg-info-version-info pkg-info-package-version
-;;;;;;  pkg-info-defining-library-version pkg-info-library-version)
-;;;;;;  "pkg-info" "pkg-info.el" (21191 39529 0 0))
+;;;### (autoloads nil "pkg-info" "pkg-info.el" (21453 1904 0 0))
 ;;; Generated autoloads from pkg-info.el
+
+(autoload 'pkg-info-library-original-version "pkg-info" "\
+Get the original version in the header of LIBRARY.
+
+The original version is stored in the X-Original-Version header.
+This header is added by the MELPA package archive to preserve
+upstream version numbers.
+
+LIBRARY is either a symbol denoting a named feature, or a library
+name as string.
+
+If SHOW is non-nil, show the version in the minibuffer.
+
+Return the version from the header of LIBRARY as list.  Signal an
+error if the LIBRARY was not found or had no X-Original-Version
+header.
+
+See Info node `(elisp)Library Headers' for more information
+about library headers.
+
+\(fn LIBRARY &optional SHOW)" t nil)
 
 (autoload 'pkg-info-library-version "pkg-info" "\
 Get the version in the header of LIBRARY.
@@ -24,6 +43,27 @@ about library headers.
 
 \(fn LIBRARY &optional SHOW)" t nil)
 
+(autoload 'pkg-info-defining-library-original-version "pkg-info" "\
+Get the original version of the library defining FUNCTION.
+
+The original version is stored in the X-Original-Version header.
+This header is added by the MELPA package archive to preserve
+upstream version numbers.
+
+If SHOW is non-nil, show the version in mini-buffer.
+
+This function is mainly intended to find the version of a major
+or minor mode, i.e.
+
+   (pkg-info-defining-library-version 'flycheck-mode)
+
+Return the version of the library defining FUNCTION.  Signal an
+error if FUNCTION is not a valid function, if its defining
+library was not found, or if the library had no proper version
+header.
+
+\(fn FUNCTION &optional SHOW)" t nil)
+
 (autoload 'pkg-info-defining-library-version "pkg-info" "\
 Get the version of the library defining FUNCTION.
 
@@ -34,10 +74,10 @@ or minor mode, i.e.
 
    (pkg-info-defining-library-version 'flycheck-mode)
 
-Return the version of the library defining FUNCTION (as by
-`pkg-info-locate-library-version').  Signal an error if FUNCTION
-is not a valid function, if its defining library was not found,
-or if the library had no proper version header.
+Return the version of the library defining FUNCTION.  Signal an
+error if FUNCTION is not a valid function, if its defining
+library was not found, or if the library had no proper version
+header.
 
 \(fn FUNCTION &optional SHOW)" t nil)
 
@@ -73,16 +113,9 @@ version.
 
 ;;;***
 
-;;;### (autoloads nil nil ("pkg-info-pkg.el") (21191 39529 811226
-;;;;;;  0))
-
-;;;***
-
-(provide 'pkg-info-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; pkg-info-autoloads.el ends here

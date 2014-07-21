d843fa2afce180fad6587a881d6426117693e9ef
Nate Eagleson <nate@nateeag.com>
2014-07-21 08:28:41 -0400

    Update package s-20131223.944

--- elpa/s-20140714.707/s-autoloads.el
+++ elpa/s-20140714.707/s-autoloads.el
@@ -1,18 +1,15 @@
 ;;; s-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21191 39531 260813
-;;;;;;  0))
+;;;### (autoloads nil nil ("s.el") (21453 1910 843443 0))
 
 ;;;***
 
-(provide 's-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; s-autoloads.el ends here

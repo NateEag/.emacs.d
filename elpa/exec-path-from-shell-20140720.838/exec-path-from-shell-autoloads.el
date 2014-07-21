df7b324b5634b6bec55b05dc2736854d71a159ae
Nate Eagleson <nate@nateeag.com>
2014-07-21 08:28:06 -0400

    Update package exec-path-from-shell-20140219.104

--- elpa/exec-path-from-shell-20140720.838/exec-path-from-shell-autoloads.el
+++ elpa/exec-path-from-shell-20140720.838/exec-path-from-shell-autoloads.el
@@ -1,11 +1,10 @@
 ;;; exec-path-from-shell-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (exec-path-from-shell-initialize exec-path-from-shell-copy-env
-;;;;;;  exec-path-from-shell-copy-envs) "exec-path-from-shell" "exec-path-from-shell.el"
-;;;;;;  (21254 17903 0 0))
+;;;### (autoloads nil "exec-path-from-shell" "exec-path-from-shell.el"
+;;;;;;  (21453 1876 0 0))
 ;;; Generated autoloads from exec-path-from-shell.el
 
 (autoload 'exec-path-from-shell-copy-envs "exec-path-from-shell" "\
@@ -37,16 +36,9 @@ values used in the user's shell.
 
 ;;;***
 
-;;;### (autoloads nil nil ("exec-path-from-shell-pkg.el") (21254
-;;;;;;  17903 658000 0))
-
-;;;***
-
-(provide 'exec-path-from-shell-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; exec-path-from-shell-autoloads.el ends here

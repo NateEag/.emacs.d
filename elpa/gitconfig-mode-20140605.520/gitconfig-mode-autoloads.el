770f2beb9119e2a52c5741654fc04e7acbf418fa
Nate Eagleson <nate@nateeag.com>
2014-07-21 08:28:47 -0400

    Update package gitconfig-mode-20140313.1504

--- elpa/gitconfig-mode-20140605.520/gitconfig-mode-autoloads.el
+++ elpa/gitconfig-mode-20140605.520/gitconfig-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; gitconfig-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (gitconfig-mode) "gitconfig-mode" "gitconfig-mode.el"
-;;;;;;  (21286 63470 0 0))
+;;;### (autoloads nil "gitconfig-mode" "gitconfig-mode.el" (21453
+;;;;;;  1918 0 0))
 ;;; Generated autoloads from gitconfig-mode.el
 
 (autoload 'gitconfig-mode "gitconfig-mode" "\
@@ -16,16 +16,9 @@ A major mode for editing .gitconfig files.
 
 ;;;***
 
-;;;### (autoloads nil nil ("gitconfig-mode-pkg.el") (21286 63470
-;;;;;;  55000 0))
-
-;;;***
-
-(provide 'gitconfig-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; gitconfig-mode-autoloads.el ends here
---
+++ elpa/gitconfig-mode-20140605.520/gitconfig-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gitconfig-mode" "20140605.520" "Major mode for editing .gitconfig files" 'nil :url "https://github.com/magit/git-modes" :keywords ("convenience" "vc" "git"))

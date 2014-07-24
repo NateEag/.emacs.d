e6b608b4971d705a9c71e9ffba628cd6973ed5bc
Nate Eagleson <nate@nateeag.com>
2014-07-24 06:47:51 -0400

    Update package helm-20140722.535

--- elpa/helm-20140722.535/helm-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm" "20140722.535" "Helm is an Emacs incremental and narrowing framework" 'nil)
---
+++ elpa/helm-20140723.1000/helm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm" "20140723.1000" "Helm is an Emacs incremental and narrowing framework" 'nil)

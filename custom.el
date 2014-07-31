d1ae49a9617b4728cfc870a4d1f7ff0576d33154
Nate Eagleson <nate@nateeag.com>
2014-07-31 17:05:43 -0400

    Turn on delete-selection-mode
    I don't actually use this much, but I prefer it to the default behavior.

--- custom.el
+++ custom.el
@@ -16,6 +16,7 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(delete-selection-mode t)
  '(fill-column 79 nil nil "Code should generally be under 80 columns wide for readability.")
  '(global-font-lock-mode t nil nil "Everyone likes syntax coloration.")
  '(guess-style-guesser-alist

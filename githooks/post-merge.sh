cd39a332004cf78fe04ebc203a3a5050d674826e
Nate Eagleson <nate@nateeag.com>
2014-07-31 22:43:50 -0400

    post-merge hook does `git clean -fd elpa/`
    which should make the ghost directories just go away after a pull.

--- githooks/post-merge.sh
+++ githooks/post-merge.sh
@@ -7,3 +7,6 @@ hooks_dir=$(dirname $0)
 $hooks_dir/compile-changed-elisp.sh ORIG_HEAD HEAD
 $hooks_dir/load-changed-elisp.sh ORIG_HEAD HEAD
 $hooks_dir/rm-stale-bytecode-files.sh ORIG_HEAD HEAD
+
+# Clean out any untracked/unignored files.
+git clean -fd elpa/

ddd572d8cc6c836c4d0d42758fe786e724176667
Nate Eagleson <nate@nateeag.com>
2014-07-31 20:47:23 -0400

    Update package web-mode-20140730.1414

--- elpa/web-mode-20140730.1414/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20140730.1414" "major mode for editing html templates" 'nil :url "http://web-mode.org" :keywords '("html" "template" "php" "javascript" "js" "css" "web" "django" "jsp" "asp" "erb" "twig" "jinja" "blade" "dust" "closure" "freemarker" "mustache" "velocity" "cheetah" "smarty"))

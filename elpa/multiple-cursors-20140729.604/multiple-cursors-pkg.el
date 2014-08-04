31f519f14469708ea870b67c78fd24f7df056b9e
Nate Eagleson <nate@nateeag.com>
2014-08-04 08:11:54 -0400

    Update package multiple-cursors-20140729.604

--- elpa/multiple-cursors-20140729.604/multiple-cursors-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "multiple-cursors" "20140729.604" "Multiple cursors for Emacs." 'nil)
---
+++ elpa/multiple-cursors-20140802.828/multiple-cursors-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "multiple-cursors" "20140802.828" "Multiple cursors for Emacs." 'nil)

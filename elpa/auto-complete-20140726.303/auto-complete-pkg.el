74e8501c85ff88987d62e60dffb0c1a76a10db94
Nate Eagleson <nate@nateeag.com>
2014-08-04 08:10:43 -0400

    Update package auto-complete-20140726.303

--- elpa/auto-complete-20140726.303/auto-complete-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auto-complete" "20140726.303" "Auto Completion for GNU Emacs" '((popup "0.5.0")))
--- elpa/auto-complete-20140803.2118/auto-complete-autoloads.el
+++ elpa/auto-complete-20140803.2118/auto-complete-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-complete" "auto-complete.el" (21460 28764
+;;;### (autoloads nil "auto-complete" "auto-complete.el" (21471 30782
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-complete.el
 
@@ -41,7 +41,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 ;;;***
 
 ;;;### (autoloads nil "auto-complete-config" "auto-complete-config.el"
-;;;;;;  (21460 28764 0 0))
+;;;;;;  (21471 30782 0 0))
 ;;; Generated autoloads from auto-complete-config.el
 
 (autoload 'ac-config-default "auto-complete-config" "\
@@ -51,7 +51,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("auto-complete-pkg.el") (21460 28764 836085
+;;;### (autoloads nil nil ("auto-complete-pkg.el") (21471 30782 986348
 ;;;;;;  0))
 
 ;;;***

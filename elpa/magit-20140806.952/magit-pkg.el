d1d704fa62cc799a75b35580b4d003c908b85baf
Nate Eagleson <nate@nateeag.com>
2014-08-07 07:57:12 -0400

    Update package magit-20140804.720

---
+++ elpa/magit-20140806.952/magit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "magit" "20140806.952" "control Git from Emacs" '((cl-lib "0.3") (git-commit-mode "0.14.0") (git-rebase-mode "0.14.0")) :keywords '("vc" "tools"))

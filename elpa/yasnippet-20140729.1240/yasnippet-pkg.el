2d27a6756c91c46577b7ea4cca04fe323e2b9a8b
Nate Eagleson <nate@nateeag.com>
2014-08-12 08:56:16 -0400

    Update package yasnippet-20140729.1240

--- elpa/yasnippet-20140729.1240/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet" "20140729.1240" "Yet another snippet extension for Emacs." 'nil :url "http://github.com/capitaomorte/yasnippet" :keywords '("convenience" "emulation"))

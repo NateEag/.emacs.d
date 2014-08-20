81f0eaa61165cfda902b757de0a1fe63aec0d88a
Nate Eagleson <nate@nateeag.com>
2014-08-20 08:38:33 -0400

    Update package markdown-mode-20131210.700

--- elpa/markdown-mode-20140819.602/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20140819.602/markdown-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; markdown-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (gfm-mode markdown-mode) "markdown-mode" "markdown-mode.el"
-;;;;;;  (21258 2141 0 0))
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (21492 38597
+;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
 (autoload 'markdown-mode "markdown-mode" "\
@@ -22,16 +22,9 @@ Major mode for editing GitHub Flavored Markdown files.
 
 ;;;***
 
-;;;### (autoloads nil nil ("markdown-mode-pkg.el") (21258 2141 712294
-;;;;;;  0))
-
-;;;***
-
-(provide 'markdown-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; markdown-mode-autoloads.el ends here

f5bd7d6116ca01f29457c3ab7bf8626b0e5280b7
Nate Eagleson <nate@nateeag.com>
2014-08-20 08:38:52 -0400

    Update package web-mode-20140817.1315

--- elpa/web-mode-20140817.1315/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20140817.1315" "major mode for editing html templates" 'nil :url "http://web-mode.org" :keywords ("html" "template" "javascript" "css" "web" "php" "django" "erb" "jsp"))

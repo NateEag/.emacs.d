e7fd43e7f67d47e4750ea3280b54c589a2ae10bb
Nate Eagleson <nate@nateeag.com>
2014-08-25 13:24:47 -0400

    Call my-prog-mode-init from css-mode-init

--- site-lisp/mode-configs/css-mode-init.el
+++ site-lisp/mode-configs/css-mode-init.el
@@ -1,12 +1,11 @@
 (defun css-mode-init ()
   "Set up css-mode the way I like it."
 
-  (smartparens-mode)
+  (my-prog-mode-init)
+
   (skewer-reload-stylesheets-mode)
   (rainbow-mode)
-  (auto-complete-mode)
   (ac-css-mode-setup)
-  (flycheck-mode)
   (emmet-mode)
 
   (setq ac-sources '(ac-source-css-property-names ac-source-css-property)))

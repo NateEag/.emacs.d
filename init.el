fab2792de0d745740c8558ace355ce29c7a7da8d
Nate Eagleson <nate@nateeag.com>
2014-08-25 12:16:06 -0400

    Activate scss-mode for *.scss files

--- init.el
+++ init.el
@@ -558,6 +558,9 @@
 (setq httpd-port 9000)
 (add-hook 'css-mode-hook 'css-mode-init)
 
+(add-auto-mode 'scss-mode
+               "\\.scss\\'")
+
 ;; Web mode.
 ;; For editing web templates of various stripes.
 (add-auto-mode 'web-mode

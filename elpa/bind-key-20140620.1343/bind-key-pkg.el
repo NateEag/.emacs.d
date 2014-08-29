93a16a206f42c7e0c3c4b1a1b952510117fae786
Nate Eagleson <nate@nateeag.com>
2014-08-29 07:27:05 -0400

    Add use-package package from MELPA

---
+++ elpa/bind-key-20140620.1343/bind-key-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bind-key" "20140620.1343" "A simple way to manage personal keybindings" 'nil :url "https://github.com/jwiegley/use-package" :keywords ("keys" "keybinding" "config" "dotemacs"))

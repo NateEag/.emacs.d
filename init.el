c41c9b709c3210c3c661b23520c2fa4b988a5087
Nate Eagleson <nate@nateeag.com>
2014-09-01 08:23:10 -0400

    Handle evil-mode config via use-package
    Slightly cleaner this way.

--- init.el
+++ init.el
@@ -324,57 +324,6 @@
 (global-auto-revert-mode)
 (diminish 'auto-revert-mode)
 
-(defun evil-mode-init ()
-  "My personal evil-mode configuration."
-
-  ;; Use regular emacs keybindings for insert-mode.
-  (setcdr evil-insert-state-map nil)
-
-  ;; Use 'jk' to go from insert-state to normal-state. It's easier to type than
-  ;; Escape.
-  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-
-  ;; Use ',' as my leader key.
-  (global-evil-leader-mode)
-  (evil-leader/set-leader ",")
-
-  ;; Turn on surround everywhere.
-  (global-evil-surround-mode)
-
-  ;; Set up my leader shortcuts.
-  ;;
-  ;; I'm not really sure what I'll want here long-term. I'm starting with a few
-  ;; of my regular shortcuts.
-  (evil-leader/set-key
-   "g" 'magit-status
-   "b" 'my-helm-for-files
-   "s" 'helm-swoop
-   "r" 'er/expand-region
-   "f" 'find-file
-   "w" 'save-buffer
-   "x" 'execute-extended-command)
-
-  ;; Some modes it's better to start in insert-state for.
-  (require 'cl)
-  (loop for (mode . state) in '((git-commit-mode . insert))
-        do (evil-set-initial-state mode state))
-
-  ;; Set up evil-nerd-commenter.
-  ;;
-  ;; This is a lame workaround. Requiring evil-nerd-commenter if evil-mode is
-  ;; not active causes a crash. Thus, we require it whenever evil-mode is
-  ;; activated, since it'll be a no-op after the first time.
-  ;;
-  ;; DEBUG Commented out for now, because I still haven't solved the weird
-  ;; crashes it's causing me.
-  ;;
-  ;; (add-hook 'evil-local-mode-hook '(lambda ()
-  ;;                                    (require 'evil-nerd-commenter)
-  ;;                                    (evilnc-default-hotkeys)))
-  )
-
-(eval-after-load 'evil (lambda () (evil-mode-init)))
-
 (add-hook 'magit-auto-revert-mode-hook
           (lambda () (diminish 'magit-auto-revert-mode)))
 
@@ -400,6 +349,7 @@
   ;; "Emacs is a great OS, but a terrible text editor.
   ;; Fortunately, it's possible to write a great text editor for a great OS..."
   ;; -- some wag discussing evil-mode
+
   (require 'evil)
   (evil-local-mode)
 

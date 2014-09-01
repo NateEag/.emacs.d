c9c7be4d09620d7d134188ab8376807852627a23
Nate Eagleson <nate@nateeag.com>
2014-09-01 10:42:08 -0400

    Update package git-commit-mode-20140814.1435

--- elpa/git-commit-mode-20140814.1435/git-commit-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit-mode" "20140814.1435" "Major mode for editing git commit messages" 'nil :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))

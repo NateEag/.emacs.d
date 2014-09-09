9df322a4fc549c766b98b10d79c0a324db4d484e
Nate Eagleson <nate@nateeag.com>
2014-09-09 14:44:26 -0400

    Update package evil-20140908.3

--- elpa/evil-20140908.2328/evil-autoloads.el
+++ elpa/evil-20140908.2328/evil-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "evil-core" "evil-core.el" (21517 45447 0 0))
+;;;### (autoloads nil "evil-core" "evil-core.el" (21519 19060 0 0))
 ;;; Generated autoloads from evil-core.el
  (autoload 'evil-mode "evil" "Toggle evil in all buffers" t)
 
@@ -13,7 +13,7 @@
 ;;;;;;  "evil-common.el" "evil-digraphs.el" "evil-ex.el" "evil-integration.el"
 ;;;;;;  "evil-macros.el" "evil-maps.el" "evil-pkg.el" "evil-repeat.el"
 ;;;;;;  "evil-search.el" "evil-states.el" "evil-types.el" "evil-vars.el"
-;;;;;;  "evil.el") (21517 45447 612847 0))
+;;;;;;  "evil.el") (21519 19060 790516 0))
 
 ;;;***
 

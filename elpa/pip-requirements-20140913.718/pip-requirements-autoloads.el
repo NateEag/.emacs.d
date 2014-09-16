78a865592458d57d140943f77df314d5a7b5bca7
Nate Eagleson <nate@nateeag.com>
2014-09-16 16:11:12 -0400

    Add pip-requirements major-mode from MELPA

---
+++ elpa/pip-requirements-20140913.718/pip-requirements-autoloads.el
@@ -0,0 +1,26 @@
+;;; pip-requirements-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pip-requirements" "pip-requirements.el" (21528
+;;;;;;  12537 0 0))
+;;; Generated autoloads from pip-requirements.el
+
+(add-to-list 'auto-mode-alist `(,(rx ".pip" string-end) . pip-requirements-mode))
+
+(add-to-list 'auto-mode-alist `(,(rx "requirements" (zero-or-more anything) ".txt" string-end) . pip-requirements-mode))
+
+(autoload 'pip-requirements-mode "pip-requirements" "\
+Major mode for editing pip requirements files.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pip-requirements-autoloads.el ends here

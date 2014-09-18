18b5019c62a167841149b490ad6f6825e99d8c88
Nate Eagleson <nate@nateeag.com>
2014-09-18 12:41:55 -0400

    Update package groovy-mode-20131129.409

--- elpa/groovy-mode-20140915.802/groovy-mode-autoloads.el
+++ elpa/groovy-mode-20140915.802/groovy-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "grails-mode" "grails-mode.el" (21426 64919
+;;;### (autoloads nil "grails-mode" "grails-mode.el" (21531 2894
 ;;;;;;  0 0))
 ;;; Generated autoloads from grails-mode.el
 
@@ -20,8 +20,8 @@ Grails Minor Mode.
 
 ;;;***
 
-;;;### (autoloads nil "groovy-electric" "groovy-electric.el" (21426
-;;;;;;  64919 0 0))
+;;;### (autoloads nil "groovy-electric" "groovy-electric.el" (21531
+;;;;;;  2894 0 0))
 ;;; Generated autoloads from groovy-electric.el
 
 (autoload 'groovy-electric-mode "groovy-electric" "\
@@ -39,11 +39,10 @@ have Font Lock enabled. ${ } is expanded when in a GString
 
 ;;;***
 
-;;;### (autoloads nil "groovy-mode" "groovy-mode.el" (21426 64919
+;;;### (autoloads nil "groovy-mode" "groovy-mode.el" (21531 2894
 ;;;;;;  0 0))
 ;;; Generated autoloads from groovy-mode.el
-
-(add-to-list 'auto-mode-alist '("\\.groovy\\'" . groovy-mode))
+ (add-to-list 'auto-mode-alist '("\\.groovy\\'" . groovy-mode))
 
 (defvar groovy-mode-hook nil "\
 *Hook called by `groovy-mode'.")
@@ -61,11 +60,9 @@ Key bindings:
 
 \(fn)" t nil)
 
-(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
-
 ;;;***
 
-;;;### (autoloads nil "inf-groovy" "inf-groovy.el" (21426 64919 0
+;;;### (autoloads nil "inf-groovy" "inf-groovy.el" (21531 2894 0
 ;;;;;;  0))
 ;;; Generated autoloads from inf-groovy.el
 
@@ -125,7 +122,7 @@ of `groovy-program-name').  Runs the hooks `inferior-groovy-mode-hook'
 
 ;;;***
 
-;;;### (autoloads nil nil ("groovy-mode-pkg.el") (21426 64919 705589
+;;;### (autoloads nil nil ("groovy-mode-pkg.el") (21531 2894 786199
 ;;;;;;  0))
 
 ;;;***
---
+++ elpa/groovy-mode-20140915.802/groovy-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "groovy-mode" "20140915.802" "Groovy mode derived mode" 'nil)

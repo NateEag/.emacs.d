358c407b55a830dc7f54fcb11c1a9e4a6032fdab
Nate Eagleson <nate@nateeag.com>
2014-09-29 17:15:25 -0400

    Update package yasnippet-20140922.1402

--- elpa/yasnippet-20140929.240/yasnippet-autoloads.el
+++ elpa/yasnippet-20140929.240/yasnippet-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (21536 41417 0 0))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (21545 52204 0 0))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -45,7 +45,7 @@ See `yas-minor-mode' for more information on Yas minor mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (21536 41417 712807
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (21545 52204 755019
 ;;;;;;  0))
 
 ;;;***
---
+++ elpa/yasnippet-20140929.240/yasnippet-pkg.el
@@ -0,0 +1,1 @@
+(define-package "yasnippet" "20140929.240" "Yet another snippet extension for Emacs." 'nil :url "http://github.com/capitaomorte/yasnippet" :keywords ("convenience" "emulation"))

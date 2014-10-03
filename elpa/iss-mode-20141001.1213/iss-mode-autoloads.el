57e1c7bc6e099db6214d8970fd7531705650ef7b
Nate Eagleson <nate@nateeag.com>
2014-10-03 07:55:07 -0400

    Add iss-mode from MELPA
    Since I have a project that uses InnoSetup, when I saw this pop up in my
    package-list, I thought "oooo, I should install that."
    
    So I did.

---
+++ elpa/iss-mode-20141001.1213/iss-mode-autoloads.el
@@ -0,0 +1,21 @@
+;;; iss-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "iss-mode" "iss-mode.el" (21550 36478 0 0))
+;;; Generated autoloads from iss-mode.el
+
+(autoload 'iss-mode "iss-mode" "\
+Major mode for editing InnoSetup script files.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; iss-mode-autoloads.el ends here
---
+++ elpa/iss-mode-20141001.1213/iss-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "iss-mode" "20141001.1213" "Mode for InnoSetup install scripts" 'nil)

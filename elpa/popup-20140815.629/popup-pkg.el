42607ccd101e3acaa3c32f96c4a4b59176ed3e9b
Nate Eagleson <nate@nateeag.com>
2014-10-03 07:57:10 -0400

    Update package popup-20140815.629

--- elpa/popup-20140815.629/popup-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "popup" "20140815.629" "Visual Popup User Interface" '((cl-lib "0.3")) :keywords '("lisp"))
--- elpa/popup-20141002.320/popup-autoloads.el
+++ elpa/popup-20141002.320/popup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("popup.el") (21488 8154 495612 0))
+;;;### (autoloads nil nil ("popup.el") (21550 36629 602862 0))
 
 ;;;***
 

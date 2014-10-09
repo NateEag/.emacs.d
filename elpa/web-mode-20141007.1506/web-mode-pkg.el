e50efbd7b8e85fb2f9e73831c0db40b060497129
Nate Eagleson <nate@nateeag.com>
2014-10-09 09:09:08 -0400

    Update package web-mode-20141006.2323

--- elpa/web-mode-20141007.1506/web-mode-pkg.el
+++ elpa/web-mode-20141007.1506/web-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "web-mode" "20141006.2323" "major mode for editing html templates" 'nil :url "http://web-mode.org" :keywords
+(define-package "web-mode" "20141007.1506" "major mode for editing html templates" 'nil :url "http://web-mode.org" :keywords
   '("html" "template" "javascript" "css" "web" "php" "django" "erb" "jsp"))
 ;; Local Variables:
 ;; no-byte-compile: t

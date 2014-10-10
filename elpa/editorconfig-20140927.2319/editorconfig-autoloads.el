3f7ab46c3b23d9e6afdd41904904f1589c1b3206
Nate Eagleson <nate@nateeag.com>
2014-10-10 16:25:26 -0400

    Add editorconfig plugin from MELPA
    Since it has a binary dependency, I'm not sure how this'll go...

---
+++ elpa/editorconfig-20140927.2319/editorconfig-autoloads.el
@@ -0,0 +1,24 @@
+;;; editorconfig-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "editorconfig" "editorconfig.el" (21560 16550
+;;;;;;  0 0))
+;;; Generated autoloads from editorconfig.el
+
+(autoload 'edconf-find-file-hook "editorconfig" "\
+
+
+\(fn)" nil nil)
+
+(add-hook 'find-file-hook 'edconf-find-file-hook)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; editorconfig-autoloads.el ends here
---
+++ elpa/editorconfig-20140927.2319/editorconfig-pkg.el
@@ -0,0 +1,1 @@
+(define-package "editorconfig" "20140927.2319" "EditorConfig Emacs extension" 'nil :url "http://github.com/editorconfig/editorconfig-emacs#readme")

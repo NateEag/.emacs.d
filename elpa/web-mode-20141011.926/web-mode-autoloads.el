3ebf04c92093b864c9eff9be27e46baef36930f9
Nate Eagleson <nate@nateeag.com>
2014-10-13 07:13:53 -0400

    Update package web-mode-20141007.1506

--- elpa/web-mode-20141011.926/web-mode-autoloads.el
+++ elpa/web-mode-20141011.926/web-mode-autoloads.el
@@ -3,18 +3,13 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (21559 48472 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (21563 46064 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
 Major mode for editing web templates.
 
 \(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21559
-;;;;;;  48472 886098 0))
 
 ;;;***
 

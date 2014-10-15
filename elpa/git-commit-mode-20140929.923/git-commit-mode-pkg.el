449efe8fd2cb41d17d6d41a3f5a90a828a970b8f
Nate Eagleson <nate@nateeag.com>
2014-10-15 09:08:47 -0400

    Update package git-commit-mode-20140929.923

--- elpa/git-commit-mode-20140929.923/git-commit-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "git-commit-mode" "20140929.923" "Major mode for editing git commit messages" 'nil :url "https://github.com/magit/git-modes" :keywords ("convenience" "vc" "git"))
--- elpa/git-commit-mode-20141014.1634/git-commit-mode-autoloads.el
+++ elpa/git-commit-mode-20141014.1634/git-commit-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit-mode" "git-commit-mode.el" (21545
-;;;;;;  52209 0 0))
+;;;### (autoloads nil "git-commit-mode" "git-commit-mode.el" (21566
+;;;;;;  29150 0 0))
 ;;; Generated autoloads from git-commit-mode.el
 
 (autoload 'git-commit-mode "git-commit-mode" "\
@@ -16,7 +16,9 @@ basic structure of and errors in git commit messages.
 
 \(fn)" t nil)
 
-(dolist (pattern '("/COMMIT_EDITMSG\\'" "/NOTES_EDITMSG\\'" "/MERGE_MSG\\'" "/TAG_EDITMSG\\'" "/PULLREQ_EDITMSG\\'")) (add-to-list 'auto-mode-alist (cons pattern 'git-commit-mode)))
+(add-to-list 'auto-mode-alist '("/MERGE_MSG\\'" . git-commit-mode))
+
+(add-to-list 'auto-mode-alist '("/\\(?:COMMIT\\|NOTES\\|TAG\\|PULLREQ\\)_EDITMSG\\'" . git-commit-mode))
 
 ;;;***
 

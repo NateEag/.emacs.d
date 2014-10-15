5faf6674a315d3a48707de0c2c3c7369f134f0eb
Nate Eagleson <nate@nateeag.com>
2014-10-15 15:26:45 -0400

    Add literate-coffee-mode package from MELPA

---
+++ elpa/literate-coffee-mode-20140621.2041/literate-coffee-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; literate-coffee-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "literate-coffee-mode" "literate-coffee-mode.el"
+;;;;;;  (21566 50327 0 0))
+;;; Generated autoloads from literate-coffee-mode.el
+
+(autoload 'litcoffee-mode "literate-coffee-mode" "\
+Major mode for editing Literate CoffeeScript.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.litcoffee\\'" . litcoffee-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; literate-coffee-mode-autoloads.el ends here

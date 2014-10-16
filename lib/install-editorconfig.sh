b2f9439d553971e9e81962d7e774a3c929182c98
Nate Eagleson <nate@nateeag.com>
2014-10-16 08:13:34 -0400

    Move bin/ and lib/ to my dotfiles repo
    They don't really need to live in here. I just need bin on my $PATH.

--- lib/install-editorconfig.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Install editorconfig locally.
-
-# For now, a simple hack is to install it via homebrew. If I ever move to
-# Linux, I'll have to change this to use the appropriate package manager.
-brew install editorconfig

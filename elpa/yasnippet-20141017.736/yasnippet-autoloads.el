1954eba3ec79336e8ebfbf9687be96bd601ec803
Nate Eagleson <nate@nateeag.com>
2014-10-20 10:06:41 -0400

    Update package yasnippet-20141008.2316

--- elpa/yasnippet-20141017.736/yasnippet-autoloads.el
+++ elpa/yasnippet-20141017.736/yasnippet-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (21559 48470 0 0))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (21573 5872 0 0))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -45,7 +45,7 @@ See `yas-minor-mode' for more information on Yas minor mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (21559 48470 542084
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (21573 5872 59894
 ;;;;;;  0))
 
 ;;;***

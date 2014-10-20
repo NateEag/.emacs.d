474be6fea9ea24166b05c8a515dba2e14b208254
Nate Eagleson <nate@nateeag.com>
2014-10-20 10:05:07 -0400

    Add sane-term package from MELPA
    We'll see if it's an improvement on multi-term, as it claims to be.

---
+++ elpa/sane-term-20141019.943/sane-term-autoloads.el
@@ -0,0 +1,26 @@
+;;; sane-term-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "sane-term" "sane-term.el" (21573 5711 0 0))
+;;; Generated autoloads from sane-term.el
+
+(autoload 'sane-term-create "sane-term" "\
+Create new term buffer.
+
+\(fn)" t nil)
+
+(autoload 'sane-term "sane-term" "\
+Cycle through term buffers, creating if necessary.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sane-term-autoloads.el ends here
---
+++ elpa/sane-term-20141019.943/sane-term.el
@@ -0,0 +1,92 @@
+;;; sane-term.el --- Multi Term is crazy. This is not.
+
+;; Copyright (C) 2014 Adam Patterson
+
+;; Author: Adam Patterson <adam@adamrt.com>
+;; URL: http://github.com/adamrt/sane-term
+;; Version: 20141019.943
+;; X-Original-Version: 0.1
+;; Package-Requires: ((emacs "24.1"))
+
+;;; Commentary:
+
+;; You can set it up like this:
+
+;;     (require 'sane-term)
+;;     (global-set-key (kbd "C-x t") 'sane-term)
+;;     (global-set-key (kbd "C-x T") 'sane-term-create)
+
+;;; Code:
+
+(defgroup sane-term nil
+  "Multi Term is crazy. This is not."
+  :group 'term)
+
+(defcustom sane-term-shell-command "/bin/bash"
+  "Specify which shell to use."
+  :type 'string
+  :group 'sane-term)
+
+(defcustom sane-term-initial-create t
+  "Creates a term if one doesn't exist."
+  :type 'boolean
+  :group 'sane-term)
+
+(defcustom sane-term-kill-on-exit t
+  "Kill term buffer on exit (C-d or `exit`)."
+  :type 'boolean
+  :group 'sane-term)
+
+(defcustom sane-term-next-on-kill t
+  "When killing a term buffer, go to the next one.
+Depends on sane-term-kill-on-exit."
+  :type 'boolean
+  :group 'sane-term)
+
+(defun sane-term-buffer-exists-p ()
+  "Boolean if term-mode buffers exist."
+  (catch 'loop
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (derived-mode-p 'term-mode)
+          (throw 'loop t))))))
+
+(defun sane-term-next ()
+  "Cycle through term buffers."
+  (interactive)
+  (when (derived-mode-p 'term-mode)
+    (bury-buffer))
+  (catch 'loop
+    (dolist (buf (buffer-list))
+      (when (with-current-buffer buf (derived-mode-p 'term-mode))
+        (switch-to-buffer buf)
+        (throw 'loop nil)))))
+
+;;;###autoload
+(defun sane-term-create ()
+  "Create new term buffer."
+  (interactive)
+  (ansi-term sane-term-shell-command))
+
+;;;###autoload
+(defun sane-term ()
+  "Cycle through term buffers, creating if necessary."
+  (interactive)
+  (when sane-term-initial-create
+    (unless (sane-term-buffer-exists-p)
+      (sane-term-create)))
+  (sane-term-next))
+
+(defadvice term-handle-exit
+    (after term-kill-buffer-on-exit activate)
+  "Kill term buffers on exiting term (C-d or `exit`).
+Optionally go to next term buffer."
+  (when sane-term-kill-on-exit
+    (kill-buffer)
+    (when sane-term-next-on-kill
+      (sane-term-next))))
+
+
+(provide 'sane-term)
+
+;;; sane-term.el ends here

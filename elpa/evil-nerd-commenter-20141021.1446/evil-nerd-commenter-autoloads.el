4e46a5cbe451422facb67682cd49c824d5f0b472
Nate Eagleson <nate@nateeag.com>
2014-10-22 09:28:00 -0400

    Update package evil-nerd-commenter-20141021.354

--- elpa/evil-nerd-commenter-20141021.1446/evil-nerd-commenter-autoloads.el
+++ elpa/evil-nerd-commenter-20141021.1446/evil-nerd-commenter-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "evil-nerd-commenter" "evil-nerd-commenter.el"
-;;;;;;  (21574 21725 0 0))
+;;;;;;  (21575 45279 0 0))
 ;;; Generated autoloads from evil-nerd-commenter.el
 
 (autoload 'evilnc-comment-or-uncomment-paragraphs "evil-nerd-commenter" "\
@@ -75,8 +75,8 @@ Set the hotkeys of evil-nerd-comment
 
 ;;;***
 
-;;;### (autoloads nil nil ("evil-nerd-commenter-pkg.el") (21574 21725
-;;;;;;  423658 0))
+;;;### (autoloads nil nil ("evil-nerd-commenter-pkg.el") (21575 45279
+;;;;;;  163331 0))
 
 ;;;***
 

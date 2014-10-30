a063206e1d417e3ecef73e8331eee77e5db0073d
Nate Eagleson <nate@nateeag.com>
2014-10-29 20:32:33 -0400

    Add cygwin-mount package from MELPA

---
+++ elpa/cygwin-mount-20131111.2146/cygwin-mount-autoloads.el
@@ -0,0 +1,16 @@
+;;; cygwin-mount-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("cygwin-mount.el") (21585 34397 777400
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cygwin-mount-autoloads.el ends here
---
+++ elpa/cygwin-mount-20131111.2146/cygwin-mount-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cygwin-mount" "20131111.2146" "Teach EMACS about cygwin styles and mount points." 'nil :keywords '("files" "mount" "cygwin"))

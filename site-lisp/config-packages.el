34b17589e03993ba10c6aeb5e7c32165b47b79f1
Nate Eagleson <nate@nateeag.com>
2014-10-31 09:31:34 -0400

    Indent evil use-package config correctly

--- site-lisp/config-packages.el
+++ site-lisp/config-packages.el
@@ -98,56 +98,56 @@
   :commands evil-local-mode
   :config
   (progn
-   ;; Use regular emacs keybindings for insert-mode.
-   (setcdr evil-insert-state-map nil)
+    ;; Use regular emacs keybindings for insert-mode.
+    (setcdr evil-insert-state-map nil)
 
-   ;; Use 'jk' to go from insert-state to normal-state. It's easier to type than
-   ;; Escape.
-   (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-   (key-chord-define evil-motion-state-map "jk" 'evil-normal-state)
-   (key-chord-define evil-visual-state-map "jk" 'evil-normal-state)
-   (key-chord-define evil-emacs-state-map "jk" 'evil-normal-state)
+    ;; Use 'jk' to go from insert-state to normal-state. It's easier to type than
+    ;; Escape.
+    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+    (key-chord-define evil-motion-state-map "jk" 'evil-normal-state)
+    (key-chord-define evil-visual-state-map "jk" 'evil-normal-state)
+    (key-chord-define evil-emacs-state-map "jk" 'evil-normal-state)
 
-   ;; Use ',' as my leader key.
-   (global-evil-leader-mode)
-   (evil-leader/set-leader ",")
+    ;; Use ',' as my leader key.
+    (global-evil-leader-mode)
+    (evil-leader/set-leader ",")
 
-   ;; Turn on surround everywhere.
-   (global-evil-surround-mode)
+    ;; Turn on surround everywhere.
+    (global-evil-surround-mode)
 
-   ;; Set up my leader shortcuts.
-   ;;
-   ;; I'm not really sure what I'll want here long-term. I'm starting with a few
-   ;; of my regular shortcuts.
-   (evil-leader/set-key
-     "g" 'magit-status
-     "b" 'my-helm-for-files
-     "s" 'helm-swoop
-     "r" 'er/expand-region
-     "f" 'find-file
-     "w" 'save-buffer
-     "x" 'execute-extended-command
-     "m" 'multi-term-dedicated-toggle)
+    ;; Set up my leader shortcuts.
+    ;;
+    ;; I'm not really sure what I'll want here long-term. I'm starting with a few
+    ;; of my regular shortcuts.
+    (evil-leader/set-key
+      "g" 'magit-status
+      "b" 'my-helm-for-files
+      "s" 'helm-swoop
+      "r" 'er/expand-region
+      "f" 'find-file
+      "w" 'save-buffer
+      "x" 'execute-extended-command
+      "m" 'multi-term-dedicated-toggle)
 
-   ;; Some modes it's better to start in insert-state for.
-   (require 'cl)
-   (loop for (mode . state) in '((git-commit-mode . insert))
-         do (evil-set-initial-state mode state))
+    ;; Some modes it's better to start in insert-state for.
+    (require 'cl)
+    (loop for (mode . state) in '((git-commit-mode . insert))
+          do (evil-set-initial-state mode state))
 
-   ;; Set up evil-nerd-commenter.
-   ;;
-   ;; This is a lame workaround. Requiring evil-nerd-commenter if evil-mode is
-   ;; not active causes a crash. Thus, we require it whenever evil-mode is
-   ;; activated, since it'll be a no-op after the first time.
-   ;;
-   ;; DEBUG Commented out for now, because I still haven't solved the weird
-   ;; crashes it's causing me.
-   ;;
-   ;; (add-hook 'evil-local-mode-hook '(lambda ()
-   ;;                                    (require 'evil-nerd-commenter)
-   ;;                                    (evilnc-default-hotkeys)))
+    ;; Set up evil-nerd-commenter.
+    ;;
+    ;; This is a lame workaround. Requiring evil-nerd-commenter if evil-mode is
+    ;; not active causes a crash. Thus, we require it whenever evil-mode is
+    ;; activated, since it'll be a no-op after the first time.
+    ;;
+    ;; DEBUG Commented out for now, because I still haven't solved the weird
+    ;; crashes it's causing me.
+    ;;
+    ;; (add-hook 'evil-local-mode-hook '(lambda ()
+    ;;                                    (require 'evil-nerd-commenter)
+    ;;                                    (evilnc-default-hotkeys)))
 
-   ))
+    ))
 
 (use-package glasses
   :commands glasses-mode

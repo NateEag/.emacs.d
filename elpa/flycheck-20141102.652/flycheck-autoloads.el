72a69e84d49544a15a6f0b46439593c73047175f
Nate Eagleson <nate@nateeag.com>
2014-11-03 12:13:57 -0500

    Update package flycheck-20141031.343

--- elpa/flycheck-20141102.652/flycheck-autoloads.el
+++ elpa/flycheck-20141102.652/flycheck-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (21587 34988 0 0))
+;;;### (autoloads nil "flycheck" "flycheck.el" (21591 47060 0 0))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-mode "flycheck" "\
@@ -134,7 +134,7 @@ The following PROPERTIES constitute an error level:
 
 ;;;***
 
-;;;### (autoloads nil nil ("flycheck-pkg.el") (21587 34988 553044
+;;;### (autoloads nil nil ("flycheck-pkg.el") (21591 47060 840062
 ;;;;;;  0))
 
 ;;;***

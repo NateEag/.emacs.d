f99cd76a875458d74efeea1f79f270ebe354a5ae
Nate Eagleson <nate@nateeag.com>
2014-11-05 14:08:52 -0500

    Update package js2-mode-20141101.1305

--- elpa/js2-mode-20141105.154/js2-mode-autoloads.el
+++ elpa/js2-mode-20141105.154/js2-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (21589
-;;;;;;  30433 0 0))
+;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (21594
+;;;;;;  30147 0 0))
 ;;; Generated autoloads from js2-imenu-extras.el
 
 (autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
@@ -19,7 +19,7 @@ Toggle Imenu support for frameworks and structural patterns.
 
 ;;;***
 
-;;;### (autoloads nil "js2-mode" "js2-mode.el" (21589 30433 0 0))
+;;;### (autoloads nil "js2-mode" "js2-mode.el" (21594 30147 0 0))
 ;;; Generated autoloads from js2-mode.el
 
 (autoload 'js2-minor-mode "js2-mode" "\
@@ -37,7 +37,7 @@ Major mode for editing JavaScript code.
 
 ;;;***
 
-;;;### (autoloads nil nil ("js2-mode-pkg.el") (21589 30433 372103
+;;;### (autoloads nil nil ("js2-mode-pkg.el") (21594 30147 234045
 ;;;;;;  0))
 
 ;;;***

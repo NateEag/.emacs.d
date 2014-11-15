e5ee973159f9f60d8c20755192876ebea1465f1f
Nate Eagleson <nate@nateeag.com>
2014-11-15 07:48:30 -0500

    Update package auto-complete-20141103.105

--- elpa/auto-complete-20141111.2346/auto-complete-autoloads.el
+++ elpa/auto-complete-20141111.2346/auto-complete-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-complete" "auto-complete.el" (21591 47079
+;;;### (autoloads nil "auto-complete" "auto-complete.el" (21607 19355
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-complete.el
 
@@ -41,7 +41,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 ;;;***
 
 ;;;### (autoloads nil "auto-complete-config" "auto-complete-config.el"
-;;;;;;  (21591 47080 0 0))
+;;;;;;  (21607 19356 0 0))
 ;;; Generated autoloads from auto-complete-config.el
 
 (autoload 'ac-config-default "auto-complete-config" "\
@@ -51,7 +51,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("auto-complete-pkg.el") (21591 47080 408582
+;;;### (autoloads nil nil ("auto-complete-pkg.el") (21607 19356 839338
 ;;;;;;  0))
 
 ;;;***

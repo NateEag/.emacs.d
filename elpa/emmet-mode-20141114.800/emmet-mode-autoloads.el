0f511efa21172fa2c40be5014df2d6fab787ffcc
Nate Eagleson <nate@nateeag.com>
2014-11-15 07:48:31 -0500

    Update package emmet-mode-20141025.1540

--- elpa/emmet-mode-20141114.800/emmet-mode-autoloads.el
+++ elpa/emmet-mode-20141114.800/emmet-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "emmet-mode" "emmet-mode.el" (21582 10999 0
+;;;### (autoloads nil "emmet-mode" "emmet-mode.el" (21607 19358 0
 ;;;;;;  0))
 ;;; Generated autoloads from emmet-mode.el
 

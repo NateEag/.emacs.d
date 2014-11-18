b08010329c7db16a7a10e98f5d615daabb3fbce9
Nate Eagleson <nate@nateeag.com>
2014-11-17 19:32:21 -0500

    Add bats-mode package from MELPA

---
+++ elpa/bats-mode-20141115.701/bats-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; bats-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "bats-mode" "bats-mode.el" (21610 37759 0 0))
+;;; Generated autoloads from bats-mode.el
+
+(autoload 'bats-mode "bats-mode" "\
+Major mode for editing and running Bats tests.
+
+See URL `https://github.com/sstephenson/bats'.
+
+\\{bats-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.bats\\'" . bats-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bats-mode-autoloads.el ends here
---
+++ elpa/bats-mode-20141115.701/bats-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bats-mode" "20141115.701" "Emacs mode for editing and running Bats tests" 'nil :url "https://github.com/dougm/bats-mode" :keywords '("bats" "tests"))

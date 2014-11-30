52ffc35f4dc1cf321674871b032ebc7da488096d
Nate Eagleson <nate@nateeag.com>
2014-11-29 22:36:05 -0500

    Update package flycheck-20141123.1016

--- elpa/flycheck-20141128.1328/flycheck-pkg.el
+++ elpa/flycheck-20141128.1328/flycheck-pkg.el
@@ -1,9 +1,9 @@
-(define-package "flycheck" "20141123.1016" "Modern on-the-fly syntax checking for GNU Emacs"
+(define-package "flycheck" "20141128.1328" "Modern on-the-fly syntax checking for GNU Emacs"
   '((dash "2.4.0")
     (pkg-info "0.4")
     (cl-lib "0.3")
     (emacs "24.1"))
-  :url "https://flycheck.readthedocs.org" :keywords
+  :url "https://www.flycheck.org" :keywords
   '("convenience" "languages" "tools"))
 ;; Local Variables:
 ;; no-byte-compile: t

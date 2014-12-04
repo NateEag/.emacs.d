947d0cf82cfbaf927f2ebabce5675e05b78b0786
Nate Eagleson <nate@nateeag.com>
2014-12-04 09:14:23 -0500

    Update package multi-term-20140827.704

--- elpa/multi-term-20141203.1658/multi-term-autoloads.el
+++ elpa/multi-term-20141203.1658/multi-term-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "multi-term" "multi-term.el" (21520 48273 0
+;;;### (autoloads nil "multi-term" "multi-term.el" (21632 27711 0
 ;;;;;;  0))
 ;;; Generated autoloads from multi-term.el
 

e77ca63529711b27357576dfee02e2533cd91bf8
Nate Eagleson <nate@nateeag.com>
2014-12-04 22:37:11 -0500

    Update package web-mode-20141203.1214

--- elpa/web-mode-20141204.1436/web-mode-autoloads.el
+++ elpa/web-mode-20141204.1436/web-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (21632 27713 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (21633 10342 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\

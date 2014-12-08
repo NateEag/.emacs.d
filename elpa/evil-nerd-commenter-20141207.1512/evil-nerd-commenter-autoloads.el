ccb8d3e52b8765b50a68e91d95102445bd91ea2c
Nate Eagleson <nate@nateeag.com>
2014-12-08 09:15:29 -0500

    Update package evil-nerd-commenter-20141031.316

--- elpa/evil-nerd-commenter-20141207.1512/evil-nerd-commenter-autoloads.el
+++ elpa/evil-nerd-commenter-20141207.1512/evil-nerd-commenter-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "evil-nerd-commenter" "evil-nerd-commenter.el"
-;;;;;;  (21587 34986 0 0))
+;;;;;;  (21637 45695 0 0))
 ;;; Generated autoloads from evil-nerd-commenter.el
 
 (autoload 'evilnc-comment-or-uncomment-paragraphs "evil-nerd-commenter" "\
@@ -76,7 +76,7 @@ Set the hotkeys of evil-nerd-comment
 ;;;***
 
 ;;;### (autoloads nil nil ("evil-nerd-commenter-operator.el" "evil-nerd-commenter-pkg.el")
-;;;;;;  (21587 34986 584206 0))
+;;;;;;  (21637 45695 859305 0))
 
 ;;;***
 
---
+++ elpa/evil-nerd-commenter-20141207.1512/evil-nerd-commenter-pkg.el
@@ -0,0 +1,4 @@
+(define-package "evil-nerd-commenter" "20141207.1512" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

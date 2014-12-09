3fbea35eb019b87f7754207b8c2f8606c73d50b0
Nate Eagleson <nate@nateeag.com>
2014-12-09 13:39:44 -0500

    Update package helm-20141207.2228

--- elpa/helm-20141208.2347/helm-pkg.el
+++ elpa/helm-20141208.2347/helm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm" "20141207.2228" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20141208.2347" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
     (async "1.2")))

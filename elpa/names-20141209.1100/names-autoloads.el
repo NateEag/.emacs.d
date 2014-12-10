277f5e44a7906a37e11a1f66debbb1893987affe
Nate Eagleson <nate@nateeag.com>
2014-12-10 12:45:49 -0500

    Update package names-20141119

--- elpa/names-20141209.1100/names-autoloads.el
+++ elpa/names-20141209.1100/names-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "names" "names.el" (21638 4671 0 0))
+;;;### (autoloads nil "names" "names.el" (21640 34507 0 0))
 ;;; Generated autoloads from names.el
 
 (defvar names--inside-make-autoload nil "\
@@ -77,8 +77,8 @@ Use the `names--inside-make-autoload' variable to indicate to
 
 ;;;***
 
-;;;### (autoloads nil nil ("names-dev.el" "names-pkg.el") (21638
-;;;;;;  4671 859367 0))
+;;;### (autoloads nil nil ("names-dev.el" "names-pkg.el") (21640
+;;;;;;  34507 875494 0))
 
 ;;;***
 

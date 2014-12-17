b2fd769372942721f8c88a20022b669edbe2382e
Nate Eagleson <nate@nateeag.com>
2014-12-17 08:41:22 -0500

    Update package web-mode-20141215.1123

--- elpa/web-mode-20141215.1123/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20141215.1123" "major mode for editing html templates" 'nil :url "http://web-mode.org" :keywords '("html" "template" "javascript" "css" "web" "php" "django" "erb" "jsp"))

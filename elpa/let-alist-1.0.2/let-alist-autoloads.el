4fefb926b46f469820f7d940a6253a6c5fb417d8
Nate Eagleson <nate@nateeag.com>
2014-12-22 09:11:43 -0500

    Update package let-alist-1.0.1

--- elpa/let-alist-1.0.2/let-alist-autoloads.el
+++ elpa/let-alist-1.0.2/let-alist-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "let-alist" "let-alist.el" (21649 34825 0 0))
+;;;### (autoloads nil "let-alist" "let-alist.el" (21656 9886 0 0))
 ;;; Generated autoloads from let-alist.el
 
 (autoload 'let-alist "let-alist" "\
@@ -16,16 +16,24 @@ For instance, the following code
   (let-alist alist
     (if (and .title .body)
         .body
-      .site))
+      .site
+      .site.contents))
 
-expands to
+essentially expands to
 
   (let ((.title (cdr (assq 'title alist)))
-        (.body (cdr (assq 'body alist)))
-        (.site (cdr (assq 'site alist))))
+        (.body  (cdr (assq 'body alist)))
+        (.site  (cdr (assq 'site alist)))
+        (.site.contents (cdr (assq 'contents (cdr (assq 'site alist))))))
     (if (and .title .body)
         .body
-      .site))
+      .site
+      .site.contents))
+
+If you nest `let-alist' invocations, the inner one can't access
+the variables of the outer one. You can, however, access alists
+inside the original alist by using dots inside the symbol, as
+displayed in the example above.
 
 \(fn ALIST &rest BODY)" nil t)
 

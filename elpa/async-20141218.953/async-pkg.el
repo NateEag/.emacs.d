665e23f926da6fd66ba8ab2c661141d8c85a2e33
Nate Eagleson <nate@nateeag.com>
2014-12-22 09:11:46 -0500

    Update package async-20141218.953

--- elpa/async-20141218.953/async-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "async" "20141218.953" "Asynchronous processing in Emacs" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/async-20141220.2333/async-autoloads.el
+++ elpa/async-20141220.2333/async-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "async" "async.el" (21651 10324 0 0))
+;;;### (autoloads nil "async" "async.el" (21656 9889 0 0))
 ;;; Generated autoloads from async.el
 
 (autoload 'async-start-process "async" "\
@@ -69,7 +69,7 @@ returns nil.  It can still be useful, however, as an argument to
 ;;;***
 
 ;;;### (autoloads nil nil ("async-bytecomp.el" "async-pkg.el" "dired-async.el"
-;;;;;;  "smtpmail-async.el") (21651 10324 152813 0))
+;;;;;;  "smtpmail-async.el") (21656 9889 74586 0))
 
 ;;;***
 

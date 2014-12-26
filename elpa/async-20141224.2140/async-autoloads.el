0ffd3c95a7e2a6f3222c995bbd26a0c21bae7ef2
Nate Eagleson <nate@nateeag.com>
2014-12-26 09:47:49 -0500

    Update package async-20141222.2207

--- elpa/async-20141224.2140/async-autoloads.el
+++ elpa/async-20141224.2140/async-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "async" "async.el" (21657 37753 0 0))
+;;;### (autoloads nil "async" "async.el" (21661 29971 0 0))
 ;;; Generated autoloads from async.el
 
 (autoload 'async-start-process "async" "\
@@ -69,7 +69,7 @@ returns nil.  It can still be useful, however, as an argument to
 ;;;***
 
 ;;;### (autoloads nil nil ("async-bytecomp.el" "async-pkg.el" "dired-async.el"
-;;;;;;  "smtpmail-async.el") (21657 37753 398447 0))
+;;;;;;  "smtpmail-async.el") (21661 29971 855948 0))
 
 ;;;***
 

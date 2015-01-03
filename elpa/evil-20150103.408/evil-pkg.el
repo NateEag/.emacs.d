2fd8df27f5c6897f6d230e92c303cc62bc9653bc
Nate Eagleson <nate@nateeag.com>
2015-01-03 08:29:14 -0500

    Update package evil-20141228.901

--- elpa/evil-20150103.408/evil-pkg.el
+++ elpa/evil-20150103.408/evil-pkg.el
@@ -1,4 +1,4 @@
-(define-package "evil" "20141228.901" "Extensible Vi layer for Emacs."
+(define-package "evil" "20150103.408" "Extensible Vi layer for Emacs."
   '((undo-tree "0.6.3")
     (goto-chg "1.6")))
 ;; Local Variables:

c3066e75906035ee6c1c7a85dec2284fe20aaa30
Nate Eagleson <nate@nateeag.com>
2015-01-04 22:51:49 -0500

    Update package coffee-mode-20150101.1816

--- elpa/coffee-mode-20150104.3/coffee-mode-autoloads.el
+++ elpa/coffee-mode-20150104.3/coffee-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21670 43055
+;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21674 2642
 ;;;;;;  0 0))
 ;;; Generated autoloads from coffee-mode.el
 
--- elpa/coffee-mode-20150104.3/coffee-mode.el
+++ elpa/coffee-mode-20150104.3/coffee-mode.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2010 Chris Wanstrath
 
-;; Version: 20150101.1816
-;; X-Original-Version: 0.5.7
+;; Version: 20150104.3
+;; X-Original-Version: 0.5.8
 ;; Keywords: CoffeeScript major mode
 ;; Author: Chris Wanstrath <chris@ozmm.org>
 ;; URL: http://github.com/defunkt/coffee-mode
@@ -138,7 +138,7 @@
 ;; Customizable Variables
 ;;
 
-(defconst coffee-mode-version "0.5.7"
+(defconst coffee-mode-version "0.5.8"
   "The version of `coffee-mode'.")
 
 (defgroup coffee nil

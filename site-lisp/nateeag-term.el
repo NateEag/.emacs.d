ea7dec89bd815523721c7ebf25da569e697d38ac
Nate Eagleson <nate@nateeag.com>
2015-01-13 10:27:45 -0500

    Get nateeag-term.el much closer to working
    not sure how I got onto this tangent, but I'm stopping, because it's
    really not relevant to my current work.

--- site-lisp/nateeag-term.el
+++ site-lisp/nateeag-term.el
@@ -21,9 +21,25 @@ If one is already displayed, move to the next term-mode buffer."
     (interactive)
 
     (let ((shell-window (nateeag/get-term-window)))
-      (if (equalp shell-window nil)
-          (sane-term-create)
-        (sane-term-next)))
+      (if (not shell-window)
+          (progn
+            (print shell-window)
+            (if (not (nateeag/get-term-buffers))
+                ;; sane-term dependency
+                (sane-term-create))
+            ; Need to create a term window. For now I'm trying popwin.
+            (popwin:popup-buffer (first (nateeag/get-term-buffers))
+                                 :height 24 :stick t :tail t))
+
+        (progn
+          ;; popwin dependency
+          (popwin:select-popup-window)
+          (popwin:display-buffer (nateeag/next-term-buffer (popwin:get-buffer)))
+          ;; TODO sane-term-next calls bury-buffer, which makes popwin close
+          ;; the buffer (reasonably enough). Instead, let's loop through our
+          ;; term buffers and pick the one following our current buffer.
+          ;; (sane-term-next)
+          )))
     )
 
 (defun nateeag/get-term-window ()
@@ -31,7 +47,10 @@ If one is already displayed, move to the next term-mode buffer."
 
 Return nil if no such window can be found."
 
-  (--find '(get-buffer-window it) (nateeag/get-term-buffers)))
+  (let ((term-buffers (nateeag/get-term-buffers)))
+    (if term-buffers
+        (get-buffer-window
+         (--find '(and (get-buffer-window it) it) (nateeag/get-term-buffers))))))
 
 (defun nateeag/get-term-buffers ()
   "Return a list of all term-mode buffers.

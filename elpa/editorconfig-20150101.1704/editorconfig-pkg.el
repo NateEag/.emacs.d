e7bba4ef012ec151691becce26d4904606266661
Nate Eagleson <nate@nateeag.com>
2015-01-14 09:24:27 -0500

    Update package editorconfig-20150101.1704

--- elpa/editorconfig-20150101.1704/editorconfig-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "editorconfig" "20150101.1704" "EditorConfig Emacs extension" 'nil :url "http://github.com/editorconfig/editorconfig-emacs#readme")
---
+++ elpa/editorconfig-20150113.1135/editorconfig-pkg.el
@@ -0,0 +1,1 @@
+(define-package "editorconfig" "20150113.1135" "EditorConfig Emacs extension" 'nil :url "http://github.com/editorconfig/editorconfig-emacs#readme")

6b2491796d08911029a15e47f4eabff9fecce1c6
Nate Eagleson <nate@nateeag.com>
2015-01-14 09:22:18 -0500

    Add python-mode from ELPA
    and remove my patched version.
    
    I may wind up rolling this back, depending on how the current release
    handles triple-quoted strings.
    
    Or just re-starting the discussion about the original patch.

---
+++ elpa/python-mode-20150113.931/python-mode-autoloads.el
@@ -0,0 +1,16 @@
+;;; python-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
+;;;;;;  (21686 31434 748671 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-mode-autoloads.el ends here
---
+++ elpa/python-mode-20150113.931/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20150113.931" "Python major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

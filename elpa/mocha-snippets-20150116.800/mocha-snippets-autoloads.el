a68f96910037305f2ad12a13d37f33a0f1c7a59a
Nate Eagleson <nate@nateeag.com>
2015-01-20 10:47:58 -0500

    Update package mocha-snippets-20150114.752

--- elpa/mocha-snippets-20150116.800/mocha-snippets-autoloads.el
+++ elpa/mocha-snippets-20150116.800/mocha-snippets-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "mocha-snippets" "mocha-snippets.el" (21686
-;;;;;;  45428 0 0))
+;;;### (autoloads nil "mocha-snippets" "mocha-snippets.el" (21694
+;;;;;;  30892 0 0))
 ;;; Generated autoloads from mocha-snippets.el
 
 (autoload 'mocha-snippets-initialize "mocha-snippets" "\
@@ -16,8 +16,8 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("mocha-snippets-pkg.el") (21686 45428
-;;;;;;  950920 0))
+;;;### (autoloads nil nil ("mocha-snippets-pkg.el") (21694 30892
+;;;;;;  827405 0))
 
 ;;;***
 
--- elpa/mocha-snippets-20150116.800/mocha-snippets-pkg.el
+++ elpa/mocha-snippets-20150116.800/mocha-snippets-pkg.el
@@ -1,4 +1,4 @@
-(define-package "mocha-snippets" "20150114.752" "Yasnippets for the Mocha JS Testing Framework"
+(define-package "mocha-snippets" "20150116.800" "Yasnippets for the Mocha JS Testing Framework"
   '((yasnippet "0.8.0"))
   :keywords
   '("test" "javascript"))

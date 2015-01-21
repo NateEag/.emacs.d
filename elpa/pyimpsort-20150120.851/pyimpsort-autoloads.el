6b7bbe2ea95311e2a5270b9e49597f1f08aee6aa
Nate Eagleson <nate@nateeag.com>
2015-01-21 09:17:47 -0500

    Add pyimpsort package from MELPA

---
+++ elpa/pyimpsort-20150120.851/pyimpsort-autoloads.el
@@ -0,0 +1,31 @@
+;;; pyimpsort-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pyimpsort" "pyimpsort.el" (21695 46316 0 0))
+;;; Generated autoloads from pyimpsort.el
+
+(autoload 'pyimpsort-region "pyimpsort" "\
+Sort python imports from region BEGIN to END points.
+
+\(fn BEGIN END)" t nil)
+
+(autoload 'pyimpsort-buffer "pyimpsort" "\
+Sort python imports from current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pyimpsort-pkg.el") (21695 46316 973491
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pyimpsort-autoloads.el ends here
---
+++ elpa/pyimpsort-20150120.851/pyimpsort-pkg.el
@@ -0,0 +1,7 @@
+(define-package "pyimpsort" "20150120.851" "Sort python imports."
+  '((emacs "24.3"))
+  :url "https://github.com/emacs-pe/pyimpsort.el" :keywords
+  '("convenience"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

dd6f03d2c48fd5eeb815f4669700c2fdd757342a
Nate Eagleson <nate@nateeag.com>
2015-01-27 09:05:57 -0500

    Update package evil-commentary-20141220.252

--- elpa/evil-commentary-20141220.252/evil-commentary-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; evil-commentary-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "evil-commentary" "evil-commentary.el" (21653
-;;;;;;  45483 0 0))
-;;; Generated autoloads from evil-commentary.el
-
-(autoload 'evil-commentary-default-setup "evil-commentary" "\
-Default key bindings for `evil-commentary`.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; evil-commentary-autoloads.el ends here

f412fc2320705e8fa534be75bc820d45a58db6a8
Nate Eagleson <nate@nateeag.com>
2015-02-06 19:28:14 -0500

    Update package deferred-20141208.1613

--- elpa/deferred-20150205.616/deferred-autoloads.el
+++ elpa/deferred-20150205.616/deferred-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("deferred.el") (21639 16886 687777 0))
+;;;### (autoloads nil nil ("deferred.el") (21717 23579 855881 0))
 
 ;;;***
 

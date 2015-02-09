f55c7abadd1969670626138c6f31dd79660099e6
Nate Eagleson <nate@nateeag.com>
2015-02-09 07:15:41 -0500

    Update package python-mode-20150117.955

--- elpa/python-mode-20150206.703/python-mode-autoloads.el
+++ elpa/python-mode-20150206.703/python-mode-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (21695 49478 770902 0))
+;;;;;;  (21720 42217 192960 0))
 
 ;;;***
 

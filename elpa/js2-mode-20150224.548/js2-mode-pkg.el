ae7a6cda6f719e8ba9f197a7ac87b4fa6991c432
Nate Eagleson <nate@nateeag.com>
2015-02-24 21:16:50 -0500

    Update package js2-mode-20150220.543

--- elpa/js2-mode-20150224.548/js2-mode-pkg.el
+++ elpa/js2-mode-20150224.548/js2-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "js2-mode" "20150220.543" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20150224.548" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
   :url "https://github.com/mooz/js2-mode/" :keywords

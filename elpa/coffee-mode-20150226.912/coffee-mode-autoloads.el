ca95e9e2ae1ee12c964dbb9e818766e2d81e872a
Nate Eagleson <nate@nateeag.com>
2015-02-28 11:16:20 -0500

    Update package coffee-mode-20150222.1753

--- elpa/coffee-mode-20150226.912/coffee-mode-autoloads.el
+++ elpa/coffee-mode-20150226.912/coffee-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21739 63152
+;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21745 59857
 ;;;;;;  0 0))
 ;;; Generated autoloads from coffee-mode.el
 

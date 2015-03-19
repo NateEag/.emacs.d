5a71d2d0cae4f94addca16a93838a14b661783b0
Nate Eagleson <nate@nateeag.com>
2015-03-18 22:48:03 -0400

    Update package jedi-core-20150305.212

--- elpa/jedi-core-20150305.212/jedi-core-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "jedi-core" "20150305.212" "Common code of jedi.el and company-jedi.el"
-  '((epc "0.1.0")
-    (python-environment "0.0.2")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/jedi-core-20150318.1613/jedi-core-autoloads.el
+++ elpa/jedi-core-20150318.1613/jedi-core-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "jedi-core" "jedi-core.el" (21758 53161 0 0))
+;;;### (autoloads nil "jedi-core" "jedi-core.el" (21770 14560 0 0))
 ;;; Generated autoloads from jedi-core.el
 
 (autoload 'jedi:start-dedicated-server "jedi-core" "\
@@ -94,7 +94,7 @@ Blocking version `jedi:install-server'.
 
 ;;;***
 
-;;;### (autoloads nil nil ("jedi-core-pkg.el") (21758 53161 174783
+;;;### (autoloads nil nil ("jedi-core-pkg.el") (21770 14560 871292
 ;;;;;;  0))
 
 ;;;***

8e716d5a9f177a258af0f1b41f08299c079f313e
Nate Eagleson <nate@nateeag.com>
2015-03-21 00:59:50 -0400

    Update package virtualenvwrapper-20141026.1301

--- elpa/virtualenvwrapper-20150316.1607/virtualenvwrapper-autoloads.el
+++ elpa/virtualenvwrapper-20150316.1607/virtualenvwrapper-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "virtualenvwrapper" "virtualenvwrapper.el"
-;;;;;;  (21582 11039 0 0))
+;;;;;;  (21772 64196 0 0))
 ;;; Generated autoloads from virtualenvwrapper.el
 
 (autoload 'venv-deactivate "virtualenvwrapper" "\

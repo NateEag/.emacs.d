092c2fc761d92f2755b44148ff43a44a0204696e
Nate Eagleson <nate@nateeag.com>
2015-03-30 16:29:49 -0400

    Update package flycheck-20150328.532

--- elpa/flycheck-20150330.925/flycheck-autoloads.el
+++ elpa/flycheck-20150330.925/flycheck-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (21784 40934 0 0))
+;;;### (autoloads nil "flycheck" "flycheck.el" (21785 45625 0 0))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-info "flycheck" "\
@@ -209,8 +209,8 @@ Use this together with the `option', `option-list' and
 
 ;;;***
 
-;;;### (autoloads nil nil ("flycheck-ert.el" "flycheck-pkg.el") (21784
-;;;;;;  40934 5242 0))
+;;;### (autoloads nil nil ("flycheck-ert.el" "flycheck-pkg.el") (21785
+;;;;;;  45625 721436 0))
 
 ;;;***
 

39dc053455c5b658fe33cd569d3af554fe123902
Nate Eagleson <nate@nateeag.com>
2015-04-07 16:40:26 -0400

    Update package gitattributes-mode-20150330.1048

--- elpa/gitattributes-mode-20150330.1048/gitattributes-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "gitattributes-mode" "20150330.1048" "Major mode for editing .gitattributes files" 'nil :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))

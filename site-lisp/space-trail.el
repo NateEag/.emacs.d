d3ca930f006aaa0910478b096a0c37c6d948c752
Nate Eagleson <nate@nateeag.com>
2015-04-18 11:05:46 -0400

    Do not strip trailing whitespace on current line
    Without this workaround, it often happens when you've just created an
    empty block then save:
    
        if (foo) {
            |
        }
    
    where | is the cursor.

--- site-lisp/space-trail.el
+++ site-lisp/space-trail.el
@@ -21,28 +21,107 @@
 ;;; Code:
 
 
-(defvar space-trail-stop-whitespace-removal-predicates
-  '(space-trail-ignored-mode-p)
-  "A list of functions. If any return true, strip passed buffer.")
-
-;; STUB
-(defvar space-trail-strip-line-whitespace-predicates
-  "A list of functions. If any return true, strip passed line.")
-
 (defvar space-trail-ignored-modes
   '(diff-mode)
   "A list of modes that should not have trailing whitespace stripped.")
 
-(defun space-trail-delete-trailing-whitespace ()
+(defvar space-trail-stop-whitespace-removal-predicates
+  '(space-trail-ignored-mode-p)
+  "A list of functions. If any return true, strip passed buffer.")
+
+(defvar space-trail-strip-whitespace-on-current-line-p
+  t
+  ;; TODO Make this description coherent. Probably the variable name too...
+  "Whether to strip trailing whitespace on the cursor's current line.")
+
+;; STUB
+(defvar space-trail-prevent-line-stripping-predicates
+  '(space-trail-point-on-line-p)
+  "A list of functions that decide whether to strip a line's whitespace.
+
+When stripping a buffer's whitespace, each one is called, passing the
+current line number and the value of (point) as the user sees it.
+
+If any function returns true, that line's trailing whitespace won't
+be stripped.")
+
+(defun space-trail-point-on-line-p (line-num orig-point)
+  "Return true if ORIG-POINT is on LINE-NUM of current buffer."
+  (= line-num (line-number-at-pos orig-point)))
+
+(defun space-trail-delete-trailing-whitespace (&optional start end)
+  "Delete trailing whitespace between START and END.
+If called interactively, START and END are the start/end of the
+region if the mark is active, or of the buffer's accessible
+portion if the mark is inactive.
+
+This command deletes whitespace characters after the last
+non-whitespace character in each line between START and END, if none
+of the functions in `space-trail-should-strip-line-whitespace-predicates'
+return true for that line.  It does not consider formfeed characters to be
+whitespace.
+
+If this command acts on the entire buffer (i.e. if called
+interactively with the mark inactive, or called from Lisp with
+END nil), it also deletes all trailing lines at the end of the
+buffer if the variable `delete-trailing-lines' is non-nil.
+
+This is largely `delete-trailing-whitespace', modified
+to give space-trail.el a hook point."
+  (interactive (progn
+                 (barf-if-buffer-read-only)
+                 (if (use-region-p)
+                     (list (region-beginning) (region-end))
+                   (list nil nil))))
+  (save-match-data
+    (save-excursion
+      (let ((end-marker (copy-marker (or end (point-max))))
+            (start (or start (point-min)))
+            (orig-point (point)))
+        (goto-char start)
+        (while (re-search-forward "\\s-$" end-marker t)
+          (skip-syntax-backward "-" (line-beginning-position))
+          (if (or
+               ;; Don't delete formfeeds, even if they are considered whitespace.
+               (looking-at-p ".*\f")
+               ;; Don't delete lines that have protection via space-trail.
+               ;; TODO This whole mess should be abstracted.
+               ;; TODO Document or fix dep on cl for `some`.
+               (some
+                (lambda (x) x)
+                ;; I'm trying to create a closure around the line we're
+                ;; currently on and orig-point. I'm not sure this has the
+                ;; effect I want, and it's pretty hard to read to boot.
+                (mapcar (lambda (func)
+                          (funcall func
+                                   (line-number-at-pos)
+                                   orig-point))
+                        space-trail-prevent-line-stripping-predicates)))
+              (progn
+                (goto-char (match-end 0)))
+            (delete-region (point) (match-end 0))))
+
+        ;; Delete trailing empty lines.
+        (goto-char end-marker)
+        (when (and (not end)
+		   delete-trailing-lines
+                   ;; Really the end of buffer.
+		   (= (point-max) (1+ (buffer-size)))
+                   (<= (skip-chars-backward "\n") -2))
+          (delete-region (1+ (point)) end-marker))
+        (set-marker end-marker nil))))
+  ;; Return nil for the benefit of `write-file-functions'.
+  nil)
+
+(defun space-trail-maybe-delete-trailing-whitespace ()
   "Delete trailing whitespace in current buffer if appropriate."
 
   ;; TODO declare cl-lib dependency for `some`.
   (unless (some
            (lambda (x) x)
+           ;; TODO change `stop` to `prevent`.
            (mapcar 'funcall space-trail-stop-whitespace-removal-predicates))
-        ;; TODO Replace this with something that checks whether a given line
-        ;; should be stripped.
-        (delete-trailing-whitespace)))
+        (space-trail-delete-trailing-whitespace)))
 
 (defun space-trail-ignored-mode-p (&optional buffer-or-string)
   "Return true if `BUFFER-OR-STRING's `major-mode' should not be stripped.
@@ -56,13 +135,13 @@
   "Remove meaningless trailing whitespace before saving."
 
   (interactive)
-  (add-hook 'before-save-hook 'space-trail-delete-trailing-whitespace))
+  (add-hook 'before-save-hook 'space-trail-maybe-delete-trailing-whitespace))
 
 (defun space-trail-deactivate ()
   "Stop removing meaningless trailing whitespace before saving."
 
   (interactive)
-  (remove-hook 'before-save-hook 'space-trail-delete-trailing-whitespace))
+  (remove-hook 'before-save-hook 'space-trail-maybe-delete-trailing-whitespace))
 
 (provide 'space-trail)
 ;;; space-trail.el ends here

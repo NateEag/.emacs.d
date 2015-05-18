75cc6de7339e054ddac751654a8b5efd54224e11
Nate Eagleson <nate@nateeag.com>
2015-05-18 19:33:42 -0400

    Update package jedi-20150413.2208

--- elpa/jedi-20150413.2208/jedi-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "jedi" "20150413.2208" "a Python auto-completion for Emacs" '((emacs "24") (jedi-core "0.2.2") (auto-complete "1.4")))
--- elpa/jedi-20150517.212/jedi.el
+++ elpa/jedi-20150517.212/jedi.el
@@ -4,8 +4,8 @@
 
 ;; Author: Takafumi Arakaki <aka.tkf at gmail.com>
 ;; Package-Requires: ((emacs "24") (jedi-core "0.2.2") (auto-complete "1.4"))
-;; Version: 0.2.3
-;; Package-Version: 20150413.2208
+;; Version: 0.2.4
+;; Package-Version: 20150517.212
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

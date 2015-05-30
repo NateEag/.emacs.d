8a35937e93de7e01e6747a056b10e779b9eb7d5e
Nate Eagleson <nate@nateeag.com>
2015-05-29 20:46:58 -0400

    Update package multiple-cursors-20150307.2322

--- elpa/multiple-cursors-20150528.2303/multiple-cursors-autoloads.el
+++ elpa/multiple-cursors-20150528.2303/multiple-cursors-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "mc-edit-lines" "mc-edit-lines.el" (21758 53342
+;;;### (autoloads nil "mc-edit-lines" "mc-edit-lines.el" (21865 2168
 ;;;;;;  0 0))
 ;;; Generated autoloads from mc-edit-lines.el
 
@@ -34,11 +34,11 @@ Add one cursor to the beginning of each line in the active region.
 ;;;***
 
 ;;;### (autoloads nil "mc-hide-unmatched-lines-mode" "mc-hide-unmatched-lines-mode.el"
-;;;;;;  (21758 53342 0 0))
+;;;;;;  (21865 2168 0 0))
 ;;; Generated autoloads from mc-hide-unmatched-lines-mode.el
 
 (autoload 'mc-hide-unmatched-lines-mode "mc-hide-unmatched-lines-mode" "\
-Minor mode when enabled hides all lines where no cursos (and
+Minor mode when enabled hides all lines where no cursors (and
 also hum/lines-to-expand below and above) To make use of this
 mode press \"C-'\" while multiple-cursor-mode is active. You can
 still edit lines while you are in mc-hide-unmatched-lines
@@ -48,7 +48,7 @@ mode. To leave this mode press <return> or \"C-g\"
 
 ;;;***
 
-;;;### (autoloads nil "mc-mark-more" "mc-mark-more.el" (21758 53342
+;;;### (autoloads nil "mc-mark-more" "mc-mark-more.el" (21865 2169
 ;;;;;;  0 0))
 ;;; Generated autoloads from mc-mark-more.el
 
@@ -134,7 +134,7 @@ Find and mark all the parts of the buffer matching the currently active region
 (autoload 'mc/mark-all-in-region "mc-mark-more" "\
 Find and mark all the parts in the region matching the given search
 
-\(fn BEG END)" t nil)
+\(fn BEG END &optional SEARCH)" t nil)
 
 (autoload 'mc/mark-all-in-region-regexp "mc-mark-more" "\
 Find and mark all the parts in the region matching the given regexp
@@ -209,7 +209,7 @@ Mark the tag we're in and its pair for renaming.
 
 ;;;***
 
-;;;### (autoloads nil "mc-mark-pop" "mc-mark-pop.el" (21758 53342
+;;;### (autoloads nil "mc-mark-pop" "mc-mark-pop.el" (21865 2168
 ;;;;;;  0 0))
 ;;; Generated autoloads from mc-mark-pop.el
 
@@ -222,7 +222,7 @@ to the popped mark.
 ;;;***
 
 ;;;### (autoloads nil "mc-separate-operations" "mc-separate-operations.el"
-;;;;;;  (21758 53342 0 0))
+;;;;;;  (21865 2169 0 0))
 ;;; Generated autoloads from mc-separate-operations.el
 
 (autoload 'mc/insert-numbers "mc-separate-operations" "\
@@ -240,10 +240,22 @@ Insert increasing numbers for each cursor, starting at 0 or ARG.
 
 \(fn)" t nil)
 
+(autoload 'mc/vertical-align "mc-separate-operations" "\
+Aligns all cursors vertically with a given CHARACTER to the one with the
+highest colum number (the rightest).
+Might not behave as intended if more than one cursors are on the same line.
+
+\(fn CHARACTER)" t nil)
+
+(autoload 'mc/vertical-align-with-space "mc-separate-operations" "\
+Aligns all cursors with whitespace like `mc/vertical-align' does
+
+\(fn)" t nil)
+
 ;;;***
 
 ;;;### (autoloads nil "multiple-cursors-core" "multiple-cursors-core.el"
-;;;;;;  (21758 53342 0 0))
+;;;;;;  (21865 2168 0 0))
 ;;; Generated autoloads from multiple-cursors-core.el
 
 (autoload 'multiple-cursors-mode "multiple-cursors-core" "\
@@ -254,7 +266,7 @@ Mode while multiple cursors are active.
 ;;;***
 
 ;;;### (autoloads nil "rectangular-region-mode" "rectangular-region-mode.el"
-;;;;;;  (21758 53342 0 0))
+;;;;;;  (21865 2168 0 0))
 ;;; Generated autoloads from rectangular-region-mode.el
 
 (autoload 'set-rectangular-region-anchor "rectangular-region-mode" "\
@@ -273,7 +285,7 @@ A mode for creating a rectangular region to edit
 ;;;***
 
 ;;;### (autoloads nil nil ("mc-cycle-cursors.el" "multiple-cursors-pkg.el"
-;;;;;;  "multiple-cursors.el") (21758 53342 981175 0))
+;;;;;;  "multiple-cursors.el") (21865 2169 132376 0))
 
 ;;;***
 

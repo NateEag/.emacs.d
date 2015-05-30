45f8aa4e6c22becda915cf2b4a2f56edf5a2ff7a
Nate Eagleson <nate@nateeag.com>
2015-05-29 20:45:29 -0400

    Update package emacs-eclim-20150502.516

--- elpa/emacs-eclim-20150528.1637/emacs-eclim-pkg.el
+++ elpa/emacs-eclim-20150528.1637/emacs-eclim-pkg.el
@@ -1,4 +1,4 @@
-(define-package "emacs-eclim" "20150502.516" "An interface to the Eclipse IDE."
+(define-package "emacs-eclim" "20150528.1637" "An interface to the Eclipse IDE."
   '((json "1.2")
     (s "1.9.0")))
 ;; Local Variables:

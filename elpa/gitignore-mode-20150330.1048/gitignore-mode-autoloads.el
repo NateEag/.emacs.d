94f45026c784c7280356fde57d8b64711986ca2e
Nate Eagleson <nate@nateeag.com>
2015-07-07 07:10:57 -0400

    Update package gitignore-mode-20150405.1627

--- elpa/gitignore-mode-20150330.1048/gitignore-mode-autoloads.el
+++ elpa/gitignore-mode-20150330.1048/gitignore-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "gitignore-mode" "gitignore-mode.el" (21796
-;;;;;;  16558 0 0))
+;;;### (autoloads nil "gitignore-mode" "gitignore-mode.el" (21915
+;;;;;;  46016 0 0))
 ;;; Generated autoloads from gitignore-mode.el
 
 (autoload 'gitignore-mode "gitignore-mode" "\

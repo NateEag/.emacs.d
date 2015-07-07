80de95b1f292a11f2bc528ce6b38ec263af56205
Nate Eagleson <nate@nateeag.com>
2015-07-07 07:10:47 -0400

    Update package git-timemachine-20150513.1457

--- elpa/git-timemachine-20150624.1446/git-timemachine-autoloads.el
+++ elpa/git-timemachine-20150624.1446/git-timemachine-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-timemachine" "git-timemachine.el" (21850
-;;;;;;  30401 0 0))
+;;;### (autoloads nil "git-timemachine" "git-timemachine.el" (21915
+;;;;;;  46005 0 0))
 ;;; Generated autoloads from git-timemachine.el
 
 (autoload 'git-timemachine-toggle "git-timemachine" "\

a833efab9a5bb240fb8a44e17ef0e2bef8f514cc
Nate Eagleson <nate@nateeag.com>
2015-08-16 16:30:17 -0400

    Update package notmuch-20150630.1237

--- elpa/notmuch-20150630.1237/notmuch-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "notmuch" "20150630.1237" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/notmuch-20150807.1233/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20150807.1233" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

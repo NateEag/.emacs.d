cd01d65f289b961e75dc10fb9aafdb30977a5eae
Nate Eagleson <nate@nateeag.com>
2015-08-16 16:30:57 -0400

    Update package solarized-theme-20150714.1020

--- elpa/solarized-theme-20150725.730/solarized-theme-autoloads.el
+++ elpa/solarized-theme-20150725.730/solarized-theme-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "solarized" "solarized.el" (21930 18478 0 0))
+;;;### (autoloads nil "solarized" "solarized.el" (21968 62204 0 0))
 ;;; Generated autoloads from solarized.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
@@ -12,7 +12,7 @@
 
 ;;;### (autoloads nil nil ("solarized-dark-theme.el" "solarized-light-theme.el"
 ;;;;;;  "solarized-theme-pkg.el" "solarized-theme-utils.el" "solarized-theme.el")
-;;;;;;  (21930 18478 931641 0))
+;;;;;;  (21968 62204 986489 0))
 
 ;;;***
 

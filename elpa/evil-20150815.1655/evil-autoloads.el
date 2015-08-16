d30488dfbf4ade4e5160575ec6e8946b7b3c21dc
Nate Eagleson <nate@nateeag.com>
2015-08-16 16:25:40 -0400

    Update package evil-20150706.711

--- elpa/evil-20150815.1655/evil-autoloads.el
+++ elpa/evil-20150815.1655/evil-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "evil" "evil.el" (21915 45929 0 0))
+;;;### (autoloads nil "evil" "evil.el" (21968 61867 0 0))
 ;;; Generated autoloads from evil.el
 
 (defvar evil-mode nil "\
@@ -33,7 +33,7 @@ See `evil-local-mode' for more information on Evil-Local mode.
 ;;;;;;  "evil-common.el" "evil-core.el" "evil-digraphs.el" "evil-ex.el"
 ;;;;;;  "evil-integration.el" "evil-macros.el" "evil-maps.el" "evil-pkg.el"
 ;;;;;;  "evil-repeat.el" "evil-search.el" "evil-states.el" "evil-types.el"
-;;;;;;  "evil-vars.el") (21915 45929 951408 0))
+;;;;;;  "evil-vars.el") (21968 61868 215338 0))
 
 ;;;***
 

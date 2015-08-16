6d4754ef2d2038efdbb17c5964944c768a9186f9
Nate Eagleson <nate@nateeag.com>
2015-08-16 16:26:43 -0400

    Update package frame-cmds-20150104.2207

--- elpa/frame-cmds-20150104.2207/frame-cmds-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "frame-cmds" "20150104.2207" "Frame and window commands (interactive functions)." '((frame-fns "0")) :url "http://www.emacswiki.org/frame-cmds.el" :keywords '("internal" "extensions" "mouse" "frames" "windows" "convenience"))

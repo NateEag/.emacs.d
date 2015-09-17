774e3e4e1abfa34592895673215ceda764840009
Nate Eagleson <nate@nateeag.com>
2015-09-16 21:17:49 -0400

    Update package notmuch-20150807.1233

--- elpa/notmuch-20150907.527/notmuch-autoloads.el
+++ elpa/notmuch-20150907.527/notmuch-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "notmuch" "notmuch.el" (21968 62149 0 0))
+;;;### (autoloads nil "notmuch" "notmuch.el" (22010 5307 0 0))
 ;;; Generated autoloads from notmuch.el
 
 (put 'notmuch-search 'notmuch-doc "Search for messages.")
@@ -23,7 +23,7 @@ notmuch buffers exist, run `notmuch'.
 
 ;;;***
 
-;;;### (autoloads nil "notmuch-hello" "notmuch-hello.el" (21968 62149
+;;;### (autoloads nil "notmuch-hello" "notmuch-hello.el" (22010 5307
 ;;;;;;  0 0))
 ;;; Generated autoloads from notmuch-hello.el
 
@@ -34,7 +34,7 @@ Run notmuch and display saved searches, known tags, etc.
 
 ;;;***
 
-;;;### (autoloads nil "notmuch-jump" "notmuch-jump.el" (21968 62149
+;;;### (autoloads nil "notmuch-jump" "notmuch-jump.el" (22010 5307
 ;;;;;;  0 0))
 ;;; Generated autoloads from notmuch-jump.el
 
@@ -50,7 +50,7 @@ fast way to jump to a saved search from anywhere in Notmuch.
 
 ;;;***
 
-;;;### (autoloads nil "notmuch-show" "notmuch-show.el" (21968 62149
+;;;### (autoloads nil "notmuch-show" "notmuch-show.el" (22010 5307
 ;;;;;;  0 0))
 ;;; Generated autoloads from notmuch-show.el
 
@@ -81,8 +81,7 @@ function is used.
 ;;;;;;  "notmuch-crypto.el" "notmuch-lib.el" "notmuch-maildir-fcc.el"
 ;;;;;;  "notmuch-message.el" "notmuch-mua.el" "notmuch-parser.el"
 ;;;;;;  "notmuch-pkg.el" "notmuch-print.el" "notmuch-query.el" "notmuch-tag.el"
-;;;;;;  "notmuch-tree.el" "notmuch-wash.el") (21968 62149 621521
-;;;;;;  0))
+;;;;;;  "notmuch-tree.el" "notmuch-wash.el") (22010 5307 142602 0))
 
 ;;;***
 
---
+++ elpa/notmuch-20150907.527/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20150907.527" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

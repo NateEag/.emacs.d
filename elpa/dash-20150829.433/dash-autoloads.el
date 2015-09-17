c9fb5d557a7bc0015e28233b9488e0ba6f6912eb
Nate Eagleson <nate@nateeag.com>
2015-09-16 21:16:14 -0400

    Update package dash-20150804.1354

--- elpa/dash-20150829.433/dash-autoloads.el
+++ elpa/dash-20150829.433/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash.el") (21968 61839 508628 0))
+;;;### (autoloads nil nil ("dash.el") (22010 5213 985105 0))
 
 ;;;***
 

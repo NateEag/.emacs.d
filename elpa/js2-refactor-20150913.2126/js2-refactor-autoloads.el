e5adab48ec747b59cc569e895a090f94ee150576
Nate Eagleson <nate@nateeag.com>
2015-09-16 21:17:01 -0400

    Update package js2-refactor-20150806.159

--- elpa/js2-refactor-20150913.2126/js2-refactor-autoloads.el
+++ elpa/js2-refactor-20150913.2126/js2-refactor-autoloads.el
@@ -3,9 +3,14 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (21968 61970
+;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (22010 5259
 ;;;;;;  0 0))
 ;;; Generated autoloads from js2-refactor.el
+
+(autoload 'js2-refactor-mode "js2-refactor" "\
+Minor mode providing JavaScript refactorings.
+
+\(fn &optional ARG)" t nil)
 
 (autoload 'js2r-add-keybindings-with-prefix "js2-refactor" "\
 Add js2r keybindings using the prefix PREFIX.
@@ -19,7 +24,7 @@ Add js2r keybindings using the modifier MODIFIER.
 
 ;;;***
 
-;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (21968 61970 0 0))
+;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (22010 5259 0 0))
 ;;; Generated autoloads from js2r-vars.el
 
 (autoload 'js2r-extract-var "js2r-vars" "\
@@ -32,7 +37,7 @@ Add js2r keybindings using the modifier MODIFIER.
 ;;;### (autoloads nil nil ("js2-refactor-pkg.el" "js2r-conditionals.el"
 ;;;;;;  "js2r-conveniences.el" "js2r-formatting.el" "js2r-functions.el"
 ;;;;;;  "js2r-helpers.el" "js2r-iife.el" "js2r-paredit.el" "js2r-wrapping.el")
-;;;;;;  (21968 61970 936214 0))
+;;;;;;  (22010 5259 982353 0))
 
 ;;;***
 

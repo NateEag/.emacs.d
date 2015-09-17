67f562f1cc68ada57fd72e30b4087354ff749f8a
Nate Eagleson <nate@nateeag.com>
2015-09-16 21:17:23 -0400

    Update package key-chord-20150808.1705

---
+++ elpa/key-chord-20150808.1005/key-chord-pkg.el
@@ -0,0 +1,1 @@
+(define-package "key-chord" "20150808.1005" "map pairs of simultaneously pressed keys to commands" 'nil :keywords '("keyboard" "chord" "input"))
--- elpa/key-chord-20150808.1005/key-chord.el
+++ elpa/key-chord-20150808.1005/key-chord.el
@@ -25,7 +25,7 @@
 ;; Author: David Andersson <l.david.andersson(at)sverige.nu>
 ;; Created: 27 April 2003
 ;; Version: 0.6 (2012-10-23)
-;; Package-Version: 20150808.1705
+;; Package-Version: 20150808.1005
 ;; Keywords: keyboard chord input
 
 ;;; Commentary:

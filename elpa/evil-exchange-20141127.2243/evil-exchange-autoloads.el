d090bfda412690cf3700a5f328e58505f7052f4d
Nate Eagleson <nate@nateeag.com>
2015-09-26 13:35:34 -0400

    Install evil-exchange from melpa

---
+++ elpa/evil-exchange-20141127.2243/evil-exchange-autoloads.el
@@ -0,0 +1,30 @@
+;;; evil-exchange-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "evil-exchange" "evil-exchange.el" (22022 55079
+;;;;;;  0 0))
+;;; Generated autoloads from evil-exchange.el
+
+(autoload 'evil-exchange "evil-exchange" "\
+Exchange two regions with evil motion." t)
+
+(autoload 'evil-exchange-cancel "evil-exchange" "\
+Cancel current pending exchange.
+
+\(fn)" t nil)
+
+(autoload 'evil-exchange-install "evil-exchange" "\
+Setting evil-exchange key bindings.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-exchange-autoloads.el ends here

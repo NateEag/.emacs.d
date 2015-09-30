5841d15fc7613494eacd069b0dd98f36ae1c8768
Nate Eagleson <nate@nateeag.com>
2015-09-30 17:50:54 -0400

    Update package php-mode-20150911.1923

--- elpa/php-mode-20150926.816/php-mode.el
+++ elpa/php-mode-20150926.816/php-mode.el
@@ -11,7 +11,7 @@
 (defconst php-mode-version-number "1.17.0"
   "PHP Mode version number.")
 
-(defconst php-mode-modified "2015-06-23"
+(defconst php-mode-modified "2015-09-27"
   "PHP Mode build date.")
 
 ;;; License
@@ -517,7 +517,8 @@ PHP does not have an \"enum\"-like keyword."
         "null"))
 
 (c-lang-defconst c-lambda-kwds
-  php '("function"))
+  php '("function"
+        "use"))
 
 (c-lang-defconst c-other-kwds
   "Keywords not accounted for by any other `*-kwds' language constant."
@@ -604,6 +605,7 @@ but only if the setting is enabled"
                        (class-open . -)
                        (comment-intro . 0)
                        (inlambda . 0)
+                       (lambda-intro-cont . +)
                        (inline-open . 0)
                        (label . +)
                        (statement-cont . (first php-lineup-cascaded-calls php-lineup-string-cont +))
@@ -1301,7 +1303,8 @@ With a prefix argument, prompt (with completion) for a word to search for."
       t)))
 
 (defsubst php-search-web-documentation (word)
-  (php-browse-documentation-url (concat php-search-url word)))
+  (php-browse-documentation-url (concat php-search-url
+                                        (replace-regexp-in-string "_" "-" (downcase word)))))
 
 ;; Define function documentation function
 (defun php-search-documentation (word)

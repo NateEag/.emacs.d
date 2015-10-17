74d33db962d530a91244dc1ca0d1804c5809b810
Nate Eagleson <nate@nateeag.com>
2015-10-17 07:59:58 -0400

    Update package dash-20151006.221

--- elpa/dash-20151010.929/dash-autoloads.el
+++ elpa/dash-20151010.929/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash.el") (22041 7465 255516 0))
+;;;### (autoloads nil nil ("dash.el") (22050 14397 360482 0))
 
 ;;;***
 
---
+++ elpa/dash-20151010.929/dash-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dash" "20151010.929" "A modern list library for Emacs" 'nil :keywords '("lists"))

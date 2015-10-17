0049d2de42ce8cd41d231a29219d899a254d18e3
Nate Eagleson <nate@nateeag.com>
2015-10-17 08:00:48 -0400

    Update package helm-swoop-20150922.842

--- elpa/helm-swoop-20151015.2143/helm-swoop-autoloads.el
+++ elpa/helm-swoop-20151015.2143/helm-swoop-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm-swoop" "helm-swoop.el" (22028 22806 0
+;;;### (autoloads nil "helm-swoop" "helm-swoop.el" (22050 14445 0
 ;;;;;;  0))
 ;;; Generated autoloads from helm-swoop.el
 

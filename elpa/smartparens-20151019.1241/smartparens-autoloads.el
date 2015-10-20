c4b84a807e3ec540da4e4440e4685c9688855122
Nate Eagleson <nate@nateeag.com>
2015-10-19 20:24:39 -0400

    Update package smartparens-20151016.824

--- elpa/smartparens-20151019.1241/smartparens-autoloads.el
+++ elpa/smartparens-20151019.1241/smartparens-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22050 14576
+;;;### (autoloads nil "smartparens" "smartparens.el" (22053 35258
 ;;;;;;  0 0))
 ;;; Generated autoloads from smartparens.el
 
@@ -181,7 +181,7 @@ Turn off `show-smartparens-mode'.
 ;;;### (autoloads nil nil ("smartparens-config.el" "smartparens-haskell.el"
 ;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
 ;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-ruby.el"
-;;;;;;  "smartparens-scala.el") (22050 14576 203536 0))
+;;;;;;  "smartparens-scala.el") (22053 35258 475159 0))
 
 ;;;***
 

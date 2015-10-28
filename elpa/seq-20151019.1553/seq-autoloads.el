c1ecd7dae0ee003acb8918d7cfdd95957918d2f6
Nate Eagleson <nate@nateeag.com>
2015-10-28 06:07:11 -0400

    Add beacon package from MELPA

---
+++ elpa/seq-20151019.1553/seq-autoloads.el
@@ -0,0 +1,15 @@
+;;; seq-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("seq.el") (22064 40057 110748 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; seq-autoloads.el ends here

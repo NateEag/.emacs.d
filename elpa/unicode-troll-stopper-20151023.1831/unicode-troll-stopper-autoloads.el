83e8697243f9887f58c1702d8e6b7de90eb4c5ea
Nate Eagleson <nate@nateeag.com>
2015-10-28 06:06:28 -0400

    Add unicode-troll-stopper package from MELPA

---
+++ elpa/unicode-troll-stopper-20151023.1831/unicode-troll-stopper-autoloads.el
@@ -0,0 +1,22 @@
+;;; unicode-troll-stopper-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "unicode-troll-stopper" "unicode-troll-stopper.el"
+;;;;;;  (22062 46839 0 0))
+;;; Generated autoloads from unicode-troll-stopper.el
+
+(autoload 'unicode-troll-stopper-mode "unicode-troll-stopper" "\
+Highlight Unicode homoglyphs in the current buffer.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; unicode-troll-stopper-autoloads.el ends here
---
+++ elpa/unicode-troll-stopper-20151023.1831/unicode-troll-stopper-pkg.el
@@ -0,0 +1,1 @@
+(define-package "unicode-troll-stopper" "20151023.1831" "Minor mode for Highlighting Unicode homoglyphs" 'nil :url "https://github.com/camsaul/emacs-unicode-troll-stopper" :keywords '("unicode"))

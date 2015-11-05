619086eec5541f902a1af288db8bbc08b2474153
Nate Eagleson <nate@nateeag.com>
2015-11-04 20:33:53 -0500

    Update package async-20150909.2257

--- elpa/async-20151103.2309/async-pkg.el
+++ elpa/async-20151103.2309/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20150909.2257" "Asynchronous processing in Emacs"
+(define-package "async" "20151103.2309" "Asynchronous processing in Emacs"
   '((emacs "24")
     (cl-lib "0.5"))
   :url "https://github.com/jwiegley/emacs-async")

e063abe222cf9c43cacaca84a04fa14cdb050c38
Nate Eagleson <nate@nateeag.com>
2015-12-18 22:44:47 -0500

    Update package key-chord-20150808.1005

--- elpa/key-chord-20150808.1005/key-chord-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "key-chord" "20150808.1005" "map pairs of simultaneously pressed keys to commands" 'nil :keywords '("keyboard" "chord" "input"))
--- elpa/key-chord-20151209.104/key-chord-autoloads.el
+++ elpa/key-chord-20151209.104/key-chord-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "key-chord" "key-chord.el" (22010 5283 0 0))
+;;;### (autoloads nil "key-chord" "key-chord.el" (22132 53934 0 0))
 ;;; Generated autoloads from key-chord.el
 
 (autoload 'key-chord-mode "key-chord" "\

36d527f0e87980681d6d538947292ff173be0027
Nate Eagleson <nate@nateeag.com>
2015-12-18 22:44:24 -0500

    Update package evil-surround-20150605.2306

--- elpa/evil-surround-20151210.1218/evil-surround-autoloads.el
+++ elpa/evil-surround-20151210.1218/evil-surround-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "evil-surround" "evil-surround.el" (21915 45979
+;;;### (autoloads nil "evil-surround" "evil-surround.el" (22132 53911
 ;;;;;;  0 0))
 ;;; Generated autoloads from evil-surround.el
 

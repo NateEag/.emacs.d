0d7f82d016d50c5a5a55262151c88b5b490bdc7e
Nate Eagleson <nate@nateeag.com>
2015-12-18 22:44:02 -0500

    Update package auto-compile-20150731.545

--- elpa/auto-compile-20151107.1408/auto-compile-autoloads.el
+++ elpa/auto-compile-20151107.1408/auto-compile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-compile" "auto-compile.el" (21968 61831
+;;;### (autoloads nil "auto-compile" "auto-compile.el" (22132 53889
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-compile.el
 

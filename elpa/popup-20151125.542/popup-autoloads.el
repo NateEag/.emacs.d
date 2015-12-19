0fb91e4d11b523d8d864a87e5aee17ebf7a9432d
Nate Eagleson <nate@nateeag.com>
2015-12-18 22:43:45 -0500

    Update package popup-20150626.711

--- elpa/popup-20151125.542/popup-autoloads.el
+++ elpa/popup-20151125.542/popup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("popup.el") (21915 45792 721740 0))
+;;;### (autoloads nil nil ("popup.el") (22132 53873 25601 0))
 
 ;;;***
 

cdf22caa93ca284f6c2db190345dd0ea16310d99
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:12:56 -0400

    Update package mocha-snippets-20150116.800

--- elpa/mocha-snippets-20160211.832/mocha-snippets-autoloads.el
+++ elpa/mocha-snippets-20160211.832/mocha-snippets-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "mocha-snippets" "mocha-snippets.el" (21694
-;;;;;;  30892 0 0))
+;;;### (autoloads nil "mocha-snippets" "mocha-snippets.el" (22276
+;;;;;;  14439 0 0))
 ;;; Generated autoloads from mocha-snippets.el
 
 (autoload 'mocha-snippets-initialize "mocha-snippets" "\
@@ -16,8 +16,8 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("mocha-snippets-pkg.el") (21694 30892
-;;;;;;  827405 0))
+;;;### (autoloads nil nil ("mocha-snippets-pkg.el") (22276 14439
+;;;;;;  930723 0))
 
 ;;;***
 

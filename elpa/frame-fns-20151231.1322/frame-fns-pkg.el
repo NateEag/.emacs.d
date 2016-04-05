bef71d3f21e028a007e0bbb815978553877a04c4
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:12:16 -0400

    Update package frame-fns-20150104.1408

---
+++ elpa/frame-fns-20151231.1322/frame-fns-pkg.el
@@ -0,0 +1,1 @@
+(define-package "frame-fns" "20151231.1322" "Non-interactive frame and window functions." 'nil :url "http://www.emacswiki.org/frame-fns.el" :keywords '("internal" "extensions" "local" "frames"))

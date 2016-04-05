d3c1dd42d1cc952aed36a4bf69bea578b93d2208
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:11:44 -0400

    Update package helm-core-20160401.1145

--- elpa/helm-core-20160403.2307/helm-core-autoloads.el
+++ elpa/helm-core-20160403.2307/helm-core-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm" "helm.el" (22271 48924 0 0))
+;;;### (autoloads nil "helm" "helm.el" (22276 14368 0 0))
 ;;; Generated autoloads from helm.el
 
 (autoload 'helm-define-multi-key "helm" "\
@@ -190,7 +190,7 @@ Enable/disable helm debugging from outside of helm session.
 ;;;***
 
 ;;;### (autoloads nil nil ("helm-core-pkg.el" "helm-lib.el" "helm-multi-match.el"
-;;;;;;  "helm-source.el") (22271 48924 613898 0))
+;;;;;;  "helm-source.el") (22276 14368 287902 0))
 
 ;;;***
 

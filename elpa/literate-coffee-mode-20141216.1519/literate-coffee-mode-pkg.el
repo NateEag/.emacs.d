6de6a03120d2008b9476e3bcd8d0ae9f9f5f1a68
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:12:50 -0400

    Update package literate-coffee-mode-20141216.1519

--- elpa/literate-coffee-mode-20141216.1519/literate-coffee-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "literate-coffee-mode" "20141216.1519" "major-mode for Literate CoffeeScript" '((coffee-mode "0.5.0")) :url "https://github.com/syohex/emacs-literate-coffee-mode")
--- elpa/literate-coffee-mode-20160114.434/literate-coffee-mode-autoloads.el
+++ elpa/literate-coffee-mode-20160114.434/literate-coffee-mode-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "literate-coffee-mode" "literate-coffee-mode.el"
-;;;;;;  (21649 34840 0 0))
+;;;;;;  (22276 14433 0 0))
 ;;; Generated autoloads from literate-coffee-mode.el
 
 (autoload 'litcoffee-mode "literate-coffee-mode" "\

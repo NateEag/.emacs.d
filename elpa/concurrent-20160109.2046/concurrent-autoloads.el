95afb20725c14f832a51ca0b15e28205dafd478f
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:12:26 -0400

    Update package concurrent-20150309.2052

--- elpa/concurrent-20160109.2046/concurrent-autoloads.el
+++ elpa/concurrent-20160109.2046/concurrent-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("concurrent.el") (21758 53178 236770 0))
+;;;### (autoloads nil nil ("concurrent.el") (22276 14409 370899 0))
 
 ;;;***
 

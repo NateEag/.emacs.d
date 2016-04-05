539b2d7f602d1c285e1ac0586db2ae5ebfe05f97
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:11:46 -0400

    Update package elisp-slime-nav-20150805.1248

--- elpa/elisp-slime-nav-20160128.1109/elisp-slime-nav-autoloads.el
+++ elpa/elisp-slime-nav-20160128.1109/elisp-slime-nav-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "elisp-slime-nav" "elisp-slime-nav.el" (21968
-;;;;;;  61846 0 0))
+;;;### (autoloads nil "elisp-slime-nav" "elisp-slime-nav.el" (22276
+;;;;;;  14370 0 0))
 ;;; Generated autoloads from elisp-slime-nav.el
 
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav" "\

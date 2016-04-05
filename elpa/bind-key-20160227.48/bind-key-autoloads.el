1b9124e458d8c852f009110dcfa0ec7800fa0a5f
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:13:31 -0400

    Update package bind-key-20150321.213

--- elpa/bind-key-20160227.48/bind-key-autoloads.el
+++ elpa/bind-key-20160227.48/bind-key-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "bind-key" "bind-key.el" (21776 31201 0 0))
+;;;### (autoloads nil "bind-key" "bind-key.el" (22276 14474 0 0))
 ;;; Generated autoloads from bind-key.el
 
 (autoload 'bind-key "bind-key" "\
@@ -14,28 +14,37 @@ KEY-NAME may be a vector, in which case it is passed straight to
 spelled-out keystrokes, e.g., \"C-c C-z\". See documentation of
 `edmacro-mode' for details.
 
-\(fn KEY-NAME COMMAND &optional KEYMAP)" nil t)
+If PREDICATE is non-nil, it is a form evaluated to determine when
+a key should be bound. It must return non-nil in such cases.
+Emacs can evaluate this form at any time that it does redisplay
+or operates on menu data structures, so you should write it so it
+can safely be called at any time.
+
+\(fn KEY-NAME COMMAND &optional KEYMAP PREDICATE)" nil t)
 
 (autoload 'unbind-key "bind-key" "\
-
+Unbind the given KEY-NAME, within the KEYMAP (if specified).
+See `bind-key' for more details.
 
 \(fn KEY-NAME &optional KEYMAP)" nil t)
 
 (autoload 'bind-key* "bind-key" "\
+Similar to `bind-key', but overrides any mode-specific bindings.
 
-
-\(fn KEY-NAME COMMAND)" nil t)
+\(fn KEY-NAME COMMAND &optional PREDICATE)" nil t)
 
 (autoload 'bind-keys "bind-key" "\
 Bind multiple keys at once.
 
 Accepts keyword arguments:
-:map - a keymap into which the keybindings should be added
-:prefix-map - name of the prefix map that should be created for
-              these bindings
-:prefix - prefix key for these bindings
-:prefix-docstring - docstring for the prefix-map variable
-:menu-name - optional menu string for prefix map
+:map MAP               - a keymap into which the keybindings should be
+                         added
+:prefix KEY            - prefix key for these bindings
+:prefix-map MAP        - name of the prefix map that should be created
+                         for these bindings
+:prefix-docstring STR  - docstring for the prefix-map variable
+:menu-name NAME        - optional menu string for prefix map
+:filter FORM           - optional form to determine when bindings apply
 
 The rest of the arguments are conses of keybinding string and a
 function symbol (unquoted).

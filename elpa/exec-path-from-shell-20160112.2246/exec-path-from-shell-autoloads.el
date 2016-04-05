63daea2efadb244c93affdf42ea21abe5e9f113d
Nate Eagleson <nate@nateeag.com>
2016-04-05 18:12:09 -0400

    Update package exec-path-from-shell-20151125.1133

--- elpa/exec-path-from-shell-20160112.2246/exec-path-from-shell-autoloads.el
+++ elpa/exec-path-from-shell-20160112.2246/exec-path-from-shell-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "exec-path-from-shell" "exec-path-from-shell.el"
-;;;;;;  (22132 53912 0 0))
+;;;;;;  (22276 14392 0 0))
 ;;; Generated autoloads from exec-path-from-shell.el
 
 (autoload 'exec-path-from-shell-copy-envs "exec-path-from-shell" "\

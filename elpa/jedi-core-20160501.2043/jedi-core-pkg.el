fb27123e29dc0c0ef4aaa4a0c07f923862401c73
Nate Eagleson <nate@nateeag.com>
2016-05-17 21:52:52 -0400

    Update package jedi-core-20151214.705

--- elpa/jedi-core-20160501.2043/jedi-core-pkg.el
+++ elpa/jedi-core-20160501.2043/jedi-core-pkg.el
@@ -1,4 +1,4 @@
-(define-package "jedi-core" "20151214.705" "Common code of jedi.el and company-jedi.el"
+(define-package "jedi-core" "20160501.2043" "Common code of jedi.el and company-jedi.el"
   '((emacs "24")
     (epc "0.1.0")
     (python-environment "0.0.2")

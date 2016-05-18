0531e1f8bee0c0ec6f906f5c24af1f8f7882039d
Nate Eagleson <nate@nateeag.com>
2016-05-17 21:51:54 -0400

    Update package auto-compile-20160321.932

--- elpa/auto-compile-20160424.418/auto-compile-autoloads.el
+++ elpa/auto-compile-20160424.418/auto-compile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-compile" "auto-compile.el" (22271 48937
+;;;### (autoloads nil "auto-compile" "auto-compile.el" (22331 51897
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-compile.el
 
--- elpa/auto-compile-20160424.418/auto-compile.el
+++ elpa/auto-compile-20160424.418/auto-compile.el
@@ -4,10 +4,10 @@
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Created: 20080830
-;; Package-Requires: ((emacs "24.3") (dash "2.1") (packed "0.3.5"))
+;; Package-Requires: ((emacs "24.3") (dash "2.12.1") (packed "0.5.3"))
+;; Package-Version: 20160424.418
 ;; Homepage: https://github.com/tarsius/auto-compile
 ;; Keywords: compile, convenience, lisp
-;; Package-Version: 20160321.932
 
 ;; This file is not part of GNU Emacs.
 

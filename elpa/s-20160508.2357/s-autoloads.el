69299234ec28aa60768fa42fe4625430e3ed9cfa
Nate Eagleson <nate@nateeag.com>
2016-05-17 21:52:12 -0400

    Update package s-20160405.920

--- elpa/s-20160508.2357/s-autoloads.el
+++ elpa/s-20160508.2357/s-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("s.el") (22276 14375 64827 0))
+;;;### (autoloads nil nil ("s.el") (22331 51915 443987 0))
 
 ;;;***
 

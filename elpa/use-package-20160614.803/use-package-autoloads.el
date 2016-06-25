dfdc05df985b14a086760ef81466397eda78c6dd
Nate Eagleson <nate@nateeag.com>
2016-06-25 13:06:59 -0400

    Update package use-package-20160403.1129

--- elpa/use-package-20160614.803/use-package-autoloads.el
+++ elpa/use-package-20160614.803/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22276 14473
+;;;### (autoloads nil "use-package" "use-package.el" (22382 47666
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 

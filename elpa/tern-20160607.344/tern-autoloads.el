bc57dc9fa15dceca55d4fb6120d013b0070fa558
Nate Eagleson <nate@nateeag.com>
2016-06-25 13:06:57 -0400

    Update package tern-20160425.659

--- elpa/tern-20160607.344/tern-autoloads.el
+++ elpa/tern-20160607.344/tern-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "tern" "tern.el" (22331 52028 0 0))
+;;;### (autoloads nil "tern" "tern.el" (22382 47664 0 0))
 ;;; Generated autoloads from tern.el
 
 (autoload 'tern-use-server "tern" "\

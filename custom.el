98846212aee99e5080b09113b673d742926b4379
Nate Eagleson <nate@nateeag.com>
2016-07-05 08:11:17 -0400

    Remove helm-git-files from my helm-for-files config
    Because I have projectile now, which covers this case, and more, rather
    more elegantly.
    
    I can't believe it took me so long to actually try that thing out.

--- custom.el
+++ custom.el
@@ -37,7 +37,7 @@
      (js2-basic-offset . guess-style-guess-indent))))
  '(helm-for-files-preferred-list
    (quote
-    (helm-source-buffers-list helm-source-recentf helm-source-bookmarks helm-source-file-cache helm-source-files-in-current-dir helm-git-files:all-source)))
+    (helm-source-buffers-list helm-source-recentf helm-source-bookmarks helm-source-file-cache helm-source-files-in-current-dir)))
  '(indent-tabs-mode nil nil nil "I dislike using tabs for indentation. Spaces are a simpler way to indent.")
  '(inhibit-startup-screen t nil nil "The emacs startup message is a needless annoyance.")
  '(ispell-extra-args nil)

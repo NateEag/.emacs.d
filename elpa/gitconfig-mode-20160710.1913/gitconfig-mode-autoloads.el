4ef109a3cce204ef5e765a0e2c52a18277cafb85
Nate Eagleson <nate@nateeag.com>
2016-07-16 06:54:52 -0400

    Update package gitconfig-mode-20160319.302

--- elpa/gitconfig-mode-20160710.1913/gitconfig-mode-autoloads.el
+++ elpa/gitconfig-mode-20160710.1913/gitconfig-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "gitconfig-mode" "gitconfig-mode.el" (22276
-;;;;;;  14402 0 0))
+;;;### (autoloads nil "gitconfig-mode" "gitconfig-mode.el" (22410
+;;;;;;  4731 0 0))
 ;;; Generated autoloads from gitconfig-mode.el
 
 (autoload 'gitconfig-mode "gitconfig-mode" "\

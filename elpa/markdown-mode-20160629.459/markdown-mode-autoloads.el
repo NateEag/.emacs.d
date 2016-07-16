8c1860be49edcf573de71edf7d491f57f37cf86a
Nate Eagleson <nate@nateeag.com>
2016-07-16 06:55:25 -0400

    Update package markdown-mode-20160610.1745

--- elpa/markdown-mode-20160629.459/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20160629.459/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22382 47620
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22410 4762
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 

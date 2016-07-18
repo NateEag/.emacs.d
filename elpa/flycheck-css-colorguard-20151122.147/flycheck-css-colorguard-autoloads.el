74ea3dfc03199881a088640ed82aebcdff5741ff
Nate Eagleson <nate@nateeag.com>
2016-07-18 18:24:11 -0400

    Update package helm-20160715.2117

---
+++ elpa/flycheck-css-colorguard-20151122.147/flycheck-css-colorguard-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-css-colorguard-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flycheck-css-colorguard.el") (22410 5570
+;;;;;;  376337 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-css-colorguard-autoloads.el ends here

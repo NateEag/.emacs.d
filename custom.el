c97e4f9e9cf9ee7c1c3d83f5cb909a3983d47eea
Nate Eagleson <nate@nateeag.com>
2016-07-25 16:35:47 -0400

    Tell auto-complete-mode I prefer case-sensitivity
    because most programming languages are case-sensitive.
    
    PHP function names notwithstanding.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-ignore-case (quote smart) nil nil "Use the case I used, blast it. Case-insensitivity should be opt-in. :P")
  '(ac-max-width 0.5)
  '(ac-trigger-commands-on-completing
    (quote

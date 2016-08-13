1505cb4655aced4165343913f976a8322a4cfde6
Nate Eagleson <nate@nateeag.com>
2016-08-12 21:25:33 -0400

    Update package csharp-mode-20160615.1119

--- elpa/csharp-mode-20160808.1235/csharp-mode-autoloads.el
+++ elpa/csharp-mode-20160808.1235/csharp-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "csharp-mode" "csharp-mode.el" (22382 47633
+;;;### (autoloads nil "csharp-mode" "csharp-mode.el" (22446 30474
 ;;;;;;  0 0))
 ;;; Generated autoloads from csharp-mode.el
 
---
+++ elpa/csharp-mode-20160808.1235/csharp-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "csharp-mode" "20160808.1235" "C# mode derived mode" 'nil :url "https://github.com/josteink/csharp-mode" :keywords '("c#" "languages" "oop" "mode"))

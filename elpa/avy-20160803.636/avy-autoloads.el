1fc73c3d6d512b7eb9f9af395f8873a4723c9636
Nate Eagleson <nate@nateeag.com>
2016-08-12 21:23:32 -0400

    Update package avy-20160711.858

--- elpa/avy-20160803.636/avy-autoloads.el
+++ elpa/avy-20160803.636/avy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "avy" "avy.el" (22410 4708 0 0))
+;;;### (autoloads nil "avy" "avy.el" (22446 30353 0 0))
 ;;; Generated autoloads from avy.el
 
 (autoload 'avy-goto-char "avy" "\

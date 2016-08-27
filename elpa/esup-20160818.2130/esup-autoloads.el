6d2d020f7d1649c7aab05a5f296f410db033f7d2
Nate Eagleson <nate@nateeag.com>
2016-08-26 23:05:28 -0400

    Update package esup-20160716.23

--- elpa/esup-20160818.2130/esup-autoloads.el
+++ elpa/esup-20160818.2130/esup-autoloads.el
@@ -3,18 +3,19 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "esup" "esup.el" (22410 4715 0 0))
+;;;### (autoloads nil "esup" "esup.el" (22465 886 0 0))
 ;;; Generated autoloads from esup.el
 
 (autoload 'esup "esup" "\
 Profile the startup time of Emacs in the background.
+If INIT-FILE is non-nil, profile that instead of USER-INIT-FILE.
 
 \(fn &optional INIT-FILE)" t nil)
 
 ;;;***
 
-;;;### (autoloads nil nil ("esup-child.el" "esup-pkg.el") (22410
-;;;;;;  4715 78192 0))
+;;;### (autoloads nil nil ("esup-child.el" "esup-pkg.el") (22465
+;;;;;;  886 121706 0))
 
 ;;;***
 

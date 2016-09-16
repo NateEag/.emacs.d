64d9e05a2267cb409b1c03b6bc8773cebf91cea2
Nate Eagleson <nate@nateeag.com>
2016-09-16 16:02:24 -0400

    Update package simple-httpd-20150430.1755

--- elpa/simple-httpd-20160902.1800/simple-httpd-autoloads.el
+++ elpa/simple-httpd-20160902.1800/simple-httpd-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "simple-httpd" "simple-httpd.el" (21850 30574
+;;;### (autoloads nil "simple-httpd" "simple-httpd.el" (22492 20429
 ;;;;;;  0 0))
 ;;; Generated autoloads from simple-httpd.el
 
---
+++ elpa/simple-httpd-20160902.1800/simple-httpd-pkg.el
@@ -0,0 +1,1 @@
+(define-package "simple-httpd" "20160902.1800" "pure elisp HTTP server" '((cl-lib "0.3")) :url "https://github.com/skeeto/emacs-http-server")

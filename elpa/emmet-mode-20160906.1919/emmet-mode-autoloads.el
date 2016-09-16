3f56117c4939f5533375fddbf259cc68193961f5
Nate Eagleson <nate@nateeag.com>
2016-09-16 16:04:11 -0400

    Update package emmet-mode-20160501.1151

--- elpa/emmet-mode-20160906.1919/emmet-mode-autoloads.el
+++ elpa/emmet-mode-20160906.1919/emmet-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "emmet-mode" "emmet-mode.el" (22331 51870 0
+;;;### (autoloads nil "emmet-mode" "emmet-mode.el" (22492 20535 0
 ;;;;;;  0))
 ;;; Generated autoloads from emmet-mode.el
 
---
+++ elpa/emmet-mode-20160906.1919/emmet-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "emmet-mode" "20160906.1919" "Unofficial Emmet's support for emacs" 'nil :url "https://github.com/smihica/emmet-mode" :keywords '("convenience"))

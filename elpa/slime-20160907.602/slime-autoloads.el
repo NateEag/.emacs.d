a9448b29b8e39d6ef4b19e326e4d32da9196e843
Nate Eagleson <nate@nateeag.com>
2016-09-16 16:03:52 -0400

    Update package slime-20160821.1352

--- elpa/slime-20160907.602/slime-autoloads.el
+++ elpa/slime-20160907.602/slime-autoloads.el
@@ -48,6 +48,6 @@
 ;; End:
 
 ;;;### (autoloads nil nil ("slime-pkg.el" "slime-tests.el" "slime.el")
-;;;;;;  (22465 866 103009 0))
+;;;;;;  (22492 20490 654106 0))
 
 ;;;***
--- elpa/slime-20160907.602/slime-pkg.el
+++ elpa/slime-20160907.602/slime-pkg.el
@@ -1,4 +1,4 @@
-(define-package "slime" "20160821.1352" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20160907.602" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
   :url "https://github.com/slime/slime" :keywords

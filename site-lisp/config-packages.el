b8027d08b6fe9313c66a4dbba70cd02bd64a92ab
Nate Eagleson <nate@nateeag.com>
2016-09-30 08:43:16 -0400

    Tell helm-find-files I'd rather keep my own keybindings
    Getting sick of Helm's stupidity. It's a brilliant package, but it does
    a lot of dumb stuff on the surface.
    
    Maybe I'll have to get off my butt and investigate Ivy...

--- site-lisp/config-packages.el
+++ site-lisp/config-packages.el
@@ -74,6 +74,7 @@
   (progn
     (helm-mode 1)
 
+    (define-key helm-find-files-map (kbd "C-c d") 'insert-date)
     (global-set-key (kbd "M-x") 'helm-M-x)
     (global-set-key (kbd "C-x C-f") 'helm-find-files)))
 

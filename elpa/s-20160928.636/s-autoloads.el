a8da22f0f46d8d09b777ab6ff0ae056af0830476
Nate Eagleson <nate@nateeag.com>
2016-10-21 23:29:43 -0400

    Update package s-20160711.525

--- elpa/s-20160928.636/s-autoloads.el
+++ elpa/s-20160928.636/s-autoloads.el
@@ -1,9 +1,9 @@
 ;;; s-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("s.el") (22410 4711 153611 0))
+;;;### (autoloads nil nil ("s.el") (22538 56615 0 0))
 
 ;;;***
 

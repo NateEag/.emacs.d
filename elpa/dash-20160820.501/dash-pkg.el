4dc6c2d204c4e8e7a5e194f8d28ed1679c86906f
Nate Eagleson <nate@nateeag.com>
2016-10-21 23:29:48 -0400

    Update package dash-20160820.501

--- elpa/dash-20160820.501/dash-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dash" "20160820.501" "A modern list library for Emacs" 'nil :keywords '("lists"))
--- elpa/dash-20161018.136/dash-autoloads.el
+++ elpa/dash-20161018.136/dash-autoloads.el
@@ -1,9 +1,9 @@
 ;;; dash-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("dash.el") (22465 863 307112 0))
+;;;### (autoloads nil nil ("dash.el") (22538 56619 0 0))
 
 ;;;***
 

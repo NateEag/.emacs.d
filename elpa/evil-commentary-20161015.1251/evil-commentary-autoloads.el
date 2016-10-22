fb55ade1ce2b7f40ebefe154736cfc62389d4316
Nate Eagleson <nate@nateeag.com>
2016-10-21 23:28:33 -0400

    Update package evil-commentary-20160221.1230

--- elpa/evil-commentary-20161015.1251/evil-commentary-autoloads.el
+++ elpa/evil-commentary-20161015.1251/evil-commentary-autoloads.el
@@ -1,15 +1,16 @@
 ;;; evil-commentary-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "evil-commentary" "evil-commentary.el" (22276
-;;;;;;  14375 0 0))
+;;;### (autoloads nil "evil-commentary" "evil-commentary.el" (22538
+;;;;;;  56545 0 0))
 ;;; Generated autoloads from evil-commentary.el
 
 (defvar evil-commentary-mode nil "\
 Non-nil if Evil-Commentary mode is enabled.
-See the command `evil-commentary-mode' for a description of this minor mode.")
+See the `evil-commentary-mode' command
+for a description of this minor mode.")
 
 (custom-autoload 'evil-commentary-mode "evil-commentary" nil)
 
@@ -21,7 +22,7 @@ Commentary mode.
 ;;;***
 
 ;;;### (autoloads nil "evil-commentary-integration" "evil-commentary-integration.el"
-;;;;;;  (22276 14375 0 0))
+;;;;;;  (22538 56545 0 0))
 ;;; Generated autoloads from evil-commentary-integration.el
 
 (autoload 'evil-commentary/org-comment-or-uncomment-region "evil-commentary-integration" "\
@@ -31,8 +32,8 @@ Comment function for `org-mode'.
 
 ;;;***
 
-;;;### (autoloads nil nil ("evil-commentary-pkg.el") (22276 14375
-;;;;;;  811167 0))
+;;;### (autoloads nil nil ("evil-commentary-pkg.el") (22538 56545
+;;;;;;  0 0))
 
 ;;;***
 

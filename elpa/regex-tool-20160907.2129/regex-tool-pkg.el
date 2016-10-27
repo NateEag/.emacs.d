a5bc14f8f163a15f05b10918d1651689d6fefc2c
Nate Eagleson <nate@nateeag.com>
2016-10-27 08:27:36 -0400

    Add regex-tool package from MELPA

---
+++ elpa/regex-tool-20160907.2129/regex-tool-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "regex-tool" "20160907.2129" "A regular expression evaluation tool for programmers" 'nil :url "http://www.newartisans.com/" :keywords '("regex" "languages" "programming" "development"))

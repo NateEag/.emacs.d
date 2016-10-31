cca171f10f4f2259300087775632984f1d1785e1
Nate Eagleson <nate@nateeag.com>
2016-10-31 16:13:54 -0400

    Update package seq-2.16

--- elpa/seq-2.16/seq-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
-;;;;;;  (22382 47560 796227 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here

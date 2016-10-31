ad66e2c917ca0ad77fff28fdfd9b2cbe815d4bd0
Nate Eagleson <nate@nateeag.com>
2016-10-31 16:13:49 -0400

    Update package nginx-mode-20150824.1411

--- elpa/nginx-mode-20161023.223/nginx-mode-autoloads.el
+++ elpa/nginx-mode-20161023.223/nginx-mode-autoloads.el
@@ -1,9 +1,9 @@
 ;;; nginx-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "nginx-mode" "nginx-mode.el" (22010 5305 0
+;;;### (autoloads nil "nginx-mode" "nginx-mode.el" (22551 42493 0
 ;;;;;;  0))
 ;;; Generated autoloads from nginx-mode.el
 

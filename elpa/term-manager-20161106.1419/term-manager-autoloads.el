8af7e34fe6f51871a27c08324101f388172739bc
Nate Eagleson <nate@nateeag.com>
2016-11-10 14:06:11 -0500

    Update package term-manager-20161013.127

--- elpa/term-manager-20161106.1419/term-manager-autoloads.el
+++ elpa/term-manager-20161106.1419/term-manager-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("term-manager-indexed-mapping.el" "term-manager-pkg.el"
-;;;;;;  "term-manager.el") (22538 56610 0 0))
+;;;;;;  "term-manager.el") (22564 50467 0 0))
 
 ;;;***
 

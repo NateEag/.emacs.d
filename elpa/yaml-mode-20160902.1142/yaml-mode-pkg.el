80c1eb48779f279592b9fe909d0248231e6cc4b0
Nate Eagleson <nate@nateeag.com>
2016-11-10 14:06:18 -0500

    Update package yaml-mode-20160902.1142

--- elpa/yaml-mode-20160902.1142/yaml-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yaml-mode" "20160902.1142" "Major mode for editing YAML files" '((emacs "24.1")) :keywords '("data" "yaml"))
--- elpa/yaml-mode-20161105.814/yaml-mode-autoloads.el
+++ elpa/yaml-mode-20161105.814/yaml-mode-autoloads.el
@@ -1,9 +1,9 @@
 ;;; yaml-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22492 20645 0 0))
+;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22564 50474 0 0))
 ;;; Generated autoloads from yaml-mode.el
 
 (let ((loads (get 'yaml 'custom-loads))) (if (member '"yaml-mode" loads) nil (put 'yaml 'custom-loads (cons '"yaml-mode" loads))))

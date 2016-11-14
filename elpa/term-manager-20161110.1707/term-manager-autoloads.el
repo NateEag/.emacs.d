1febacf48ecb615dd6eee72833d40cebabc5210c
Nate Eagleson <nate@nateeag.com>
2016-11-14 08:45:30 -0500

    Update package term-manager-20161106.1419

--- elpa/term-manager-20161110.1707/term-manager-autoloads.el
+++ elpa/term-manager-20161110.1707/term-manager-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("term-manager-indexed-mapping.el" "term-manager-pkg.el"
-;;;;;;  "term-manager.el") (22564 50467 0 0))
+;;;;;;  "term-manager.el") (22569 49146 0 0))
 
 ;;;***
 
--- elpa/term-manager-20161110.1707/term-manager-pkg.el
+++ elpa/term-manager-20161110.1707/term-manager-pkg.el
@@ -1,4 +1,4 @@
-(define-package "term-manager" "20161106.1419" "Contextual terminal management"
+(define-package "term-manager" "20161110.1707" "Contextual terminal management"
   '((dash "2.12.0")
     (emacs "24.4"))
   :url "https://www.github.com/IvanMalison/term-manager" :keywords

3a2a437189e0fb96459e24e3f7a22d23e2c88aba
Nate Eagleson <nate@nateeag.com>
2016-11-14 08:39:56 -0500

    Add web-mode-edit-element package
    It looks like it should give web-mode the features I was trying to get
    from tagedit.
    
    At some point I'll have to play with it and see how it does.

---
+++ elpa/web-mode-edit-element-20161109.734/web-mode-edit-element-autoloads.el
@@ -0,0 +1,18 @@
+;;; web-mode-edit-element-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("web-mode-edit-element-attributes.el"
+;;;;;;  "web-mode-edit-element-elements.el" "web-mode-edit-element-pkg.el"
+;;;;;;  "web-mode-edit-element-utils.el" "web-mode-edit-element.el")
+;;;;;;  (22569 48451 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; web-mode-edit-element-autoloads.el ends here
---
+++ elpa/web-mode-edit-element-20161109.734/web-mode-edit-element-pkg.el
@@ -0,0 +1,8 @@
+(define-package "web-mode-edit-element" "20161109.734" "Helper-functions for attribute- and element-handling"
+  '((emacs "24.4")
+    (web-mode "14"))
+  :url "https://github.com/jtkDvlp/web-mode-edit-element" :keywords
+  '("languages" "convenience"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

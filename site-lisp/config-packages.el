96419af1a049358227e0f5edd84b45db68c26947
Nate Eagleson <nate@nateeag.com>
2016-12-08 14:37:56 -0500

    Prefer locally-installed eslint executable, if available
    because that's the way it ought to be.

--- site-lisp/config-packages.el
+++ site-lisp/config-packages.el
@@ -92,7 +92,24 @@
                         (message) line-end))
               :modes (text-mode markdown-mode gfm-mode rst-mode))
 
-            (add-to-list 'flycheck-checkers 'proselint)))
+            (add-to-list 'flycheck-checkers 'proselint)
+
+            ;; Prefer locally-installed eslint, if available.
+            ;;
+            ;; Yanked from lunaryorn himself:
+            ;;
+            ;; http://emacs.stackexchange.com/a/21207/351
+            (defun my/use-eslint-from-node-modules ()
+              (let* ((root (locate-dominating-file
+                            (or (buffer-file-name) default-directory)
+                            "node_modules"))
+                     (eslint (and root
+                                  (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                                    root))))
+                (when (and eslint (file-executable-p eslint))
+                  (setq-local flycheck-javascript-eslint-executable eslint))))
+
+            (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)))
 
 ;; Since emacs 24.4 made revert undoable, this option is perfectly safe and
 ;; pretty convenient.

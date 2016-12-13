3bf55de324a89900037db29f5e5c53b2931b665b
Nate Eagleson <nate@nateeag.com>
2016-12-13 10:56:48 -0500

    Install flycheck-objc-clang package from MELPA
    There's a decent chance I'll be writing Objective-C on a Cordova project
    at $DAYJOB within the next few months.
    
    Since this one popped up, I figured I'd just install it now.

---
+++ elpa/flycheck-objc-clang-20161211.2048/flycheck-objc-clang-autoloads.el
@@ -0,0 +1,22 @@
+;;; flycheck-objc-clang-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "flycheck-objc-clang" "flycheck-objc-clang.el"
+;;;;;;  (22608 6210 0 0))
+;;; Generated autoloads from flycheck-objc-clang.el
+
+(autoload 'flycheck-objc-clang-setup "flycheck-objc-clang" "\
+Set up Flycheck for Objective-C using Clang.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-objc-clang-autoloads.el ends here
---
+++ elpa/flycheck-objc-clang-20161211.2048/flycheck-objc-clang-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-objc-clang" "20161211.2048" "Flycheck: Objective-C support using Clang" '((emacs "24.4") (flycheck "26")) :url "https://github.com/GyazSquare/flycheck-objc-clang" :keywords '("convenience" "languages" "tools"))

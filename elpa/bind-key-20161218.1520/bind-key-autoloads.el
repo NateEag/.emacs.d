e73847daeac818a5c0ec82efaa81bdb0632f2cb1
Nate Eagleson <nate@nateeag.com>
2016-12-19 08:44:07 -0500

    Update package bind-key-20160227.48

--- elpa/bind-key-20161218.1520/bind-key-autoloads.el
+++ elpa/bind-key-20161218.1520/bind-key-autoloads.el
@@ -1,9 +1,9 @@
 ;;; bind-key-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "bind-key" "bind-key.el" (22276 14474 0 0))
+;;;### (autoloads nil "bind-key" "bind-key.el" (22615 58407 0 0))
 ;;; Generated autoloads from bind-key.el
 
 (autoload 'bind-key "bind-key" "\

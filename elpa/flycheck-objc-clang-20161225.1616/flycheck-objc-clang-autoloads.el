fe5cc70f677e6d8d53b567de2cd5e64dc460a426
Nate Eagleson <nate@nateeag.com>
2016-12-27 09:40:03 -0500

    Update package flycheck-objc-clang-20161214.509

--- elpa/flycheck-objc-clang-20161225.1616/flycheck-objc-clang-autoloads.el
+++ elpa/flycheck-objc-clang-20161225.1616/flycheck-objc-clang-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "flycheck-objc-clang" "flycheck-objc-clang.el"
-;;;;;;  (22609 21814 0 0))
+;;;;;;  (22626 32067 0 0))
 ;;; Generated autoloads from flycheck-objc-clang.el
 
 (autoload 'flycheck-objc-clang-setup "flycheck-objc-clang" "\

4c62090067639c9f03b76e758902c9cd564eabdb
Nate Eagleson <nate@nateeag.com>
2017-01-03 09:38:15 -0500

    Update package concurrent-20160109.2046

--- elpa/concurrent-20161228.1930/concurrent-autoloads.el
+++ elpa/concurrent-20161228.1930/concurrent-autoloads.el
@@ -1,9 +1,9 @@
 ;;; concurrent-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("concurrent.el") (22276 14409 370899 0))
+;;;### (autoloads nil nil ("concurrent.el") (22635 46935 0 0))
 
 ;;;***
 

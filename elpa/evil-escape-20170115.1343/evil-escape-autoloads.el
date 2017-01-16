8c6a3208fb5f6431078f58db607f1dc523344f04
Nate Eagleson <nate@nateeag.com>
2017-01-16 08:47:25 -0500

    Update package evil-escape-20160607.1015

--- elpa/evil-escape-20170115.1343/evil-escape-autoloads.el
+++ elpa/evil-escape-20170115.1343/evil-escape-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "evil-escape" "evil-escape.el" (22517 9034
+;;;### (autoloads nil "evil-escape" "evil-escape.el" (22652 52973
 ;;;;;;  0 0))
 ;;; Generated autoloads from evil-escape.el
 

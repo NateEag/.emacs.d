67d18ac35f62342a5349f5d23bd7087b0fbb60d0
Nate Eagleson <nate@nateeag.com>
2017-01-16 08:47:27 -0500

    Update package helm-projectile-20161213.2311

--- elpa/helm-projectile-20170113.209/helm-projectile-autoloads.el
+++ elpa/helm-projectile-20170113.209/helm-projectile-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (22609
-;;;;;;  21815 0 0))
+;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (22652
+;;;;;;  52974 0 0))
 ;;; Generated autoloads from helm-projectile.el
 
 (defvar helm-projectile-fuzzy-match t "\

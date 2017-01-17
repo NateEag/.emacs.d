2bfe9f849a101f83792a283e18853c1070f44070
Nate Eagleson <nate@nateeag.com>
2017-01-17 10:04:04 -0500

    Install eldoc-overlay-mode from MELPA

---
+++ elpa/eldoc-overlay-mode-20170114.2125/eldoc-overlay-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; eldoc-overlay-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "eldoc-overlay-mode" "eldoc-overlay-mode.el"
+;;;;;;  (22654 11521 0 0))
+;;; Generated autoloads from eldoc-overlay-mode.el
+
+(defvar eldoc-overlay-mode t "\
+Non-nil if Eldoc-Overlay mode is enabled.
+See the `eldoc-overlay-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `eldoc-overlay-mode'.")
+
+(custom-autoload 'eldoc-overlay-mode "eldoc-overlay-mode" nil)
+
+(autoload 'eldoc-overlay-mode "eldoc-overlay-mode" "\
+Minor mode for displaying eldoc with contextual documentation overlay.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; eldoc-overlay-mode-autoloads.el ends here

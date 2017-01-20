7f04e11f0eef8b62335209aada48f801e8b6a4fe
Nate Eagleson <nate@nateeag.com>
2017-01-20 10:29:10 -0500

    Update package eldoc-overlay-mode-20170114.2125

--- elpa/eldoc-overlay-mode-20170114.2125/eldoc-overlay-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "eldoc-overlay-mode" "20170114.2125" "Display eldoc with contextual documentation overlay." '((emacs "24.4")) :url "https://github.com/stardiviner/eldoc-overlay-mode" :keywords '("eldoc" "overlay"))

49331c47c1ea023851dc760365bf78d1c8bad711
Nate Eagleson <nate@nateeag.com>
2017-02-01 10:45:33 -0500

    Update package yasnippet-20170108.1830

--- elpa/yasnippet-20170127.2128/yasnippet-pkg.el
+++ elpa/yasnippet-20170127.2128/yasnippet-pkg.el
@@ -1,4 +1,4 @@
-(define-package "yasnippet" "20170108.1830" "Yet another snippet extension for Emacs."
+(define-package "yasnippet" "20170127.2128" "Yet another snippet extension for Emacs."
   '((cl-lib "0.5"))
   :url "http://github.com/joaotavora/yasnippet" :keywords
   '("convenience" "emulation"))

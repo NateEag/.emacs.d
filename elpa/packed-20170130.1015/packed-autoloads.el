92e19ccd589f013b943274bb4765563e5784ef7a
Nate Eagleson <nate@nateeag.com>
2017-02-01 10:45:22 -0500

    Update package packed-20160523.600

--- elpa/packed-20170130.1015/packed-autoloads.el
+++ elpa/packed-20170130.1015/packed-autoloads.el
@@ -1,9 +1,9 @@
 ;;; packed-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("packed.el") (22382 47558 140035 0))
+;;;### (autoloads nil nil ("packed.el") (22674 658 0 0))
 
 ;;;***
 

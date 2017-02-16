ef2cae4f51f6a771b16b97db986e1cdca8e48d6b
Nate Eagleson <nate@nateeag.com>
2017-02-16 10:59:59 -0500

    Update package web-mode-20170131.1400

--- elpa/web-mode-20170215.1023/web-mode-autoloads.el
+++ elpa/web-mode-20170215.1023/web-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (22674 664 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (22693 52350 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\

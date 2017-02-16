2d70a3b98ae430acaa12eab7036d87840f35e539
Nate Eagleson <nate@nateeag.com>
2017-02-16 10:59:40 -0500

    Update package puppet-mode-20170120.1813

--- elpa/puppet-mode-20170213.207/puppet-mode-autoloads.el
+++ elpa/puppet-mode-20170213.207/puppet-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (22663 33899
+;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (22693 52332
 ;;;;;;  0 0))
 ;;; Generated autoloads from puppet-mode.el
 

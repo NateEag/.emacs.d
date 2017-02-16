4a7c2a8e8d9d0aaa329c874ff5cf70d09158969e
Nate Eagleson <nate@nateeag.com>
2017-02-16 10:59:21 -0500

    Update package avy-20160814.250

--- elpa/avy-20170208.148/avy-autoloads.el
+++ elpa/avy-20170208.148/avy-autoloads.el
@@ -1,9 +1,9 @@
 ;;; avy-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "avy" "avy.el" (22465 882 0 0))
+;;;### (autoloads nil "avy" "avy.el" (22693 52312 0 0))
 ;;; Generated autoloads from avy.el
 
 (autoload 'avy-goto-char "avy" "\

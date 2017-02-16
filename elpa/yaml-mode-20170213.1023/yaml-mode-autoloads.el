dc15b583fafc880e5ae2a0e53b4fb90e408407dc
Nate Eagleson <nate@nateeag.com>
2017-02-16 10:59:59 -0500

    Update package yaml-mode-20161105.814

--- elpa/yaml-mode-20170213.1023/yaml-mode-autoloads.el
+++ elpa/yaml-mode-20170213.1023/yaml-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22564 50474 0 0))
+;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (22693 52351 0 0))
 ;;; Generated autoloads from yaml-mode.el
 
 (let ((loads (get 'yaml 'custom-loads))) (if (member '"yaml-mode" loads) nil (put 'yaml 'custom-loads (cons '"yaml-mode" loads))))

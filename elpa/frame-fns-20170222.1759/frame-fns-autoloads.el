6e442e4c4ebc18b9e0662d480ef894938286c91a
Nate Eagleson <nate@nateeag.com>
2017-03-02 11:19:01 -0500

    Update package frame-fns-20170101.951

--- elpa/frame-fns-20170222.1759/frame-fns-autoloads.el
+++ elpa/frame-fns-20170222.1759/frame-fns-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("frame-fns.el") (22635 46932 0 0))
+;;;### (autoloads nil nil ("frame-fns.el") (22712 17909 0 0))
 
 ;;;***
 
---
+++ elpa/frame-fns-20170222.1759/frame-fns-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "frame-fns" "20170222.1759" "Non-interactive frame and window functions." 'nil :url "https://www.emacswiki.org/emacs/download/frame-fns.el" :keywords '("internal" "extensions" "local" "frames"))

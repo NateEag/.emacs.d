8c04584b241902de442f11821f743703bb07a555
Nate Eagleson <nate@nateeag.com>
2017-03-14 08:33:59 -0400

    Update package projectile-20170225.2320

--- elpa/projectile-20170309.30/projectile-autoloads.el
+++ elpa/projectile-20170309.30/projectile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "projectile" "projectile.el" (22712 17938 0
+;;;### (autoloads nil "projectile" "projectile.el" (22727 58167 0
 ;;;;;;  0))
 ;;; Generated autoloads from projectile.el
 

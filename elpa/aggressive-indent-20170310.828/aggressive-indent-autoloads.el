9ab9b9a16daccd9d243285ce1da3adf461271602
Nate Eagleson <nate@nateeag.com>
2017-03-14 08:33:36 -0400

    Update package aggressive-indent-20161201.1001

--- elpa/aggressive-indent-20170310.828/aggressive-indent-autoloads.el
+++ elpa/aggressive-indent-20170310.828/aggressive-indent-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "aggressive-indent" "aggressive-indent.el"
-;;;;;;  (22600 4643 0 0))
+;;;;;;  (22727 58143 0 0))
 ;;; Generated autoloads from aggressive-indent.el
 
 (autoload 'aggressive-indent-indent-defun "aggressive-indent" "\

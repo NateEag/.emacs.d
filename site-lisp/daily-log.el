c136b98ef532d514f7e0c645ec7295f8d8fcbae9
Nate Eagleson <nate@nateeag.com>
2017-03-17 17:08:35 -0400

    Add function for showing hours worked so far this week

--- site-lisp/daily-log.el
+++ site-lisp/daily-log.el
@@ -29,19 +29,70 @@ activity it records, in the format 'YYYY-MM-DD.txt'."
       (goto-char (point-min))
 
       (while (re-search-forward (concat "^Started: +" daily-log-time-regex) nil t)
-        (setq start-time (date-to-time  (concat (file-name-base)
+        (setq start-time (date-to-time (concat (file-name-base)
                                                 " "
                                                 (match-string 1))))
+        ;; Apparently date-to-time ignores AM/PM?
+        ;;
+        ;; TODO Find a less-crazy way to do this. Emacs *must* have a way.
+        (when (and (s-ends-with? "PM" (match-string 1))
+                   (not (equal "12" (format-time-string "%H" start-time))))
+          (setq start-time (time-add start-time (days-to-time 0.5))))
 
         (re-search-forward (concat "^Stopped: +" daily-log-time-regex))
         (setq stop-time (date-to-time (concat (file-name-base)
                                               " "
                                               (match-string 1))))
+        ;; Apparently date-to-time ignores AM/PM?
+        ;;
+        ;; TODO Find a less-crazy way to do this. Emacs *must* have a way.
+        (when (and (s-ends-with? "PM" (match-string 1))
+                   (not (equal "12" (format-time-string "%H" stop-time))))
+          (setq stop-time (time-add stop-time (days-to-time 0.5))))
 
         (setq time-delta (time-subtract stop-time start-time))
 
         (setq times (cons time-delta times)))
+
       times)))
+
+(defun daily-log-get-file-time-deltas (path)
+  "Return a list of time deltas from the file in PATH."
+
+ (let ((target-buffer (find-file-noselect path)))
+   (with-current-buffer target-buffer
+    (daily-log-get-time-deltas))
+   ))
+
+(defun daily-log-show-current-week-time ()
+  "Show the amount of time logged so far this week."
+
+  (interactive)
+
+  ;; For each log file that exists so far this week:
+  (let* ((now (decode-time))
+         (current-day-of-week (nth 6 now))
+         (cur-time (current-time))
+         (total-time (seconds-to-time 0)))
+
+    (dotimes (i (+ current-day-of-week 1))
+      (let* ((target-date (format-time-string
+                           "%Y-%m-%d"
+                           (time-subtract cur-time
+                                          (days-to-time i))))
+             (log-file-path (concat "~/daily-log/" target-date ".txt"))
+             (time-deltas (daily-log-get-file-time-deltas log-file-path)))
+
+        (dolist (time-delta time-deltas)
+          (setq total-time (time-add time-delta total-time)))
+        ))
+
+    (setq total-time (time-to-seconds total-time))
+    (message "Total time worked this week: %s" (format-seconds
+                                                "%h hours, %m minutes"
+                                                total-time))
+    )
+  )
 
 (defun daily-log-show-total-time ()
   "Show the amount of time logged in current buffer."

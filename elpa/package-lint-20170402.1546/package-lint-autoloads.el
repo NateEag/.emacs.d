814f3004f4707a79646b5e83c2fd8dcd81f0d5c8
Nate Eagleson <nate@nateeag.com>
2017-04-03 16:23:21 -0400

    Update package package-lint-20170324.1727

--- elpa/package-lint-20170402.1546/package-lint-autoloads.el
+++ elpa/package-lint-20170402.1546/package-lint-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "package-lint" "package-lint.el" (22746 26702
+;;;### (autoloads nil "package-lint" "package-lint.el" (22754 44856
 ;;;;;;  0 0))
 ;;; Generated autoloads from package-lint.el
 
--- elpa/package-lint-20170402.1546/package-lint.el
+++ elpa/package-lint-20170402.1546/package-lint.el
@@ -5,7 +5,7 @@
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;;         Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/purcell/package-lint
-;; Package-Version: 20170324.1727
+;; Package-Version: 20170402.1546
 ;; Keywords: lisp
 ;; Version: 0
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
@@ -242,6 +242,7 @@ This is bound dynamically while the checks run.")
             (package-lint--check-lexical-binding-requires-emacs-24 deps)
             (package-lint--check-libraries-available-in-emacs deps)
             (package-lint--check-macros-functions-available-in-emacs deps))
+          (package-lint--check-for-literal-emacs-path)
           (let ((definitions (package-lint--get-defs)))
             (package-lint--check-defs-prefix definitions)
             (package-lint--check-symbol-separators definitions)))))
@@ -253,6 +254,15 @@ This is bound dynamically while the checks run.")
 
 
 ;;; Checks
+
+(defun package-lint--check-for-literal-emacs-path ()
+  "Verify package does not refer to \"\.emacs\.d\" literally.
+Instead it should use `user-emacs-directory' or `locate-user-emacs-file'."
+  (goto-char (point-min))
+  (while (re-search-forward (rx (syntax string-quote) (0+ (not (syntax string-quote))) (or "/" "\\") ".emacs.d") nil t)
+    (package-lint--error
+     (line-number-at-pos) (current-column) 'warning
+     "Use variable `user-emacs-directory' or function `locate-user-emacs-file' instead of a literal path to the Emacs user directory or files.")))
 
 (defun package-lint--check-keywords-list ()
   "Verify that package keywords are listed in `finder-known-keywords'."

11c96d454fb230565db7415654316c6d22d90266
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:57 -0400

    Update package csharp-mode-20170203.1122

--- elpa/csharp-mode-20170502.158/csharp-mode-autoloads.el
+++ elpa/csharp-mode-20170502.158/csharp-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "csharp-mode" "csharp-mode.el" (22680 33975
+;;;### (autoloads nil "csharp-mode" "csharp-mode.el" (22809 42452
 ;;;;;;  0 0))
 ;;; Generated autoloads from csharp-mode.el
 

ef02d7029bd1eeccf7e4f2da949319408f3dd126
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:26 -0400

    Update package buttercup-20170401.703

--- elpa/buttercup-20170421.245/buttercup-autoloads.el
+++ elpa/buttercup-20170421.245/buttercup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "buttercup" "buttercup.el" (22754 44846 0 0))
+;;;### (autoloads nil "buttercup" "buttercup.el" (22809 42421 0 0))
 ;;; Generated autoloads from buttercup.el
 
 (autoload 'buttercup-run-at-point "buttercup" "\
@@ -24,10 +24,22 @@ current directory.
 
 \(fn)" nil nil)
 
+(autoload 'buttercup-minor-mode "buttercup" "\
+Activate buttercup minor mode.
+
+With buttercup minor mode active the following is activated:
+
+- `describe' and `it' forms are fontified with
+  `font-lock-keyword-face'.
+- `describe' and `it' forms are available from `imenu' for
+  quicker access.
+
+\(fn &optional ARG)" t nil)
+
 ;;;***
 
 ;;;### (autoloads nil nil ("buttercup-compat.el" "buttercup-pkg.el")
-;;;;;;  (22754 44846 0 0))
+;;;;;;  (22809 42421 0 0))
 
 ;;;***
 

b9f0468613ae85140759bc35327c6af91e82c3d6
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:36 -0400

    Update package frame-cmds-20170222.1758

---
+++ elpa/frame-cmds-20170506.945/frame-cmds-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "frame-cmds" "20170506.945" "Frame and window commands (interactive functions)." '((frame-fns "0")) :url "https://www.emacswiki.org/emacs/download/frame-cmds.el" :keywords '("internal" "extensions" "mouse" "frames" "windows" "convenience"))

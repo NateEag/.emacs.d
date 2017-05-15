7532af8bcd786d8497b407c1864e6863fda7e309
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:29 -0400

    Update package evil-exchange-20160812.843

--- elpa/evil-exchange-20160812.843/evil-exchange-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-exchange" "20160812.843" "Exchange text more easily within Evil" '((evil "1.2.8") (cl-lib "0.3")) :url "http://github.com/Dewdrops/evil-exchange" :keywords '("evil" "plugin"))
--- elpa/evil-exchange-20170510.1959/evil-exchange-autoloads.el
+++ elpa/evil-exchange-20170510.1959/evil-exchange-autoloads.el
@@ -1,9 +1,9 @@
 ;;; evil-exchange-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "evil-exchange" "evil-exchange.el" (22446 30368
+;;;### (autoloads nil "evil-exchange" "evil-exchange.el" (22809 42424
 ;;;;;;  0 0))
 ;;; Generated autoloads from evil-exchange.el
 

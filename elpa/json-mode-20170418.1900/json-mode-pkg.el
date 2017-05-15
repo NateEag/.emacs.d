03462f208f2697f8d6538d62fd68b7e0595bbe6a
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:44 -0400

    Update package json-mode-20170402.1808

---
+++ elpa/json-mode-20170418.1900/json-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "json-mode" "20170418.1900" "Major mode for editing JSON files." '((json-reformat "0.0.5") (json-snatcher "1.0.0")) :commit "c3a7ab350533dddf6d18b4c55423bc0d1b40f3fb" :url "https://github.com/joshwnj/json-mode")

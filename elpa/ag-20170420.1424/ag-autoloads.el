b7f9dd88b360a8cbfec73643b697fc2f9c417745
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:24 -0400

    Update package ag-20161228.1641

--- elpa/ag-20170420.1424/ag-autoloads.el
+++ elpa/ag-20170420.1424/ag-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ag" "ag.el" (22635 46930 0 0))
+;;;### (autoloads nil "ag" "ag.el" (22809 42420 0 0))
 ;;; Generated autoloads from ag.el
 
 (autoload 'ag "ag" "\

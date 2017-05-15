1d8cf591adb08eccec90499f0e6070d50d839b27
Nate Eagleson <nate@nateeag.com>
2017-05-15 08:57:56 -0400

    Update package omnisharp-20170404.2224

--- elpa/omnisharp-20170509.104/omnisharp-pkg.el
+++ elpa/omnisharp-20170509.104/omnisharp-pkg.el
@@ -1,7 +1,7 @@
-(define-package "omnisharp" "20170404.2224" "Omnicompletion (intellisense) and more for C#"
-  '((json "1.2")
+(define-package "omnisharp" "20170509.104" "Omnicompletion (intellisense) and more for C#"
+  '((emacs "24")
     (flycheck "30")
-    (dash "2.12.1")
+    (dash "2.12.0")
     (auto-complete "1.4")
     (popup "0.5.1")
     (csharp-mode "0.8.7")
@@ -9,7 +9,7 @@
     (s "1.10.0")
     (shut-up "0.3.2"))
   :url "https://github.com/Omnisharp/omnisharp-emacs" :keywords
-  '("csharp" "c#" "ide" "auto-complete" "intellisense"))
+  '("languages" "csharp" "c#" "ide" "auto-complete" "intellisense"))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:

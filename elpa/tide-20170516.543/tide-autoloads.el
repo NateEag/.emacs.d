19a1d73bb756fd5f4af6cd9aa2dc12d6823112fe
Nate Eagleson <nate@nateeag.com>
2017-05-22 08:56:17 -0400

    Update package tide-20170513.1211

--- elpa/tide-20170516.543/tide-autoloads.el
+++ elpa/tide-20170516.543/tide-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "tide" "tide.el" (22809 42492 0 0))
+;;;### (autoloads nil "tide" "tide.el" (22818 57328 0 0))
 ;;; Generated autoloads from tide.el
 
 (autoload 'company-tide "tide" "\
@@ -57,7 +57,7 @@ timeout.
 
 ;;;***
 
-;;;### (autoloads nil nil ("tide-pkg.el") (22809 42492 0 0))
+;;;### (autoloads nil nil ("tide-pkg.el") (22818 57328 0 0))
 
 ;;;***
 
--- elpa/tide-20170516.543/tide-pkg.el
+++ elpa/tide-20170516.543/tide-pkg.el
@@ -1,4 +1,4 @@
-(define-package "tide" "20170513.1211" "Typescript Interactive Development Environment"
+(define-package "tide" "20170516.543" "Typescript Interactive Development Environment"
   '((dash "2.10.0")
     (flycheck "27")
     (typescript-mode "0.1")

3af4c2d19cec65df81b033ad71e7be6d32725a37
Nate Eagleson <nate@nateeag.com>
2017-05-30 11:24:52 -0400

    Update package smartparens-20170511.743

---
+++ elpa/smartparens-20170524.1533/smartparens-pkg.el
@@ -0,0 +1,6 @@
+(define-package "smartparens" "20170524.1533" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
+  '((dash "2.13.0")
+    (cl-lib "0.3")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

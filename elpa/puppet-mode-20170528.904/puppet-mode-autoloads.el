bcd3151a46b3af5486a17ebd502fab50d1355893
Nate Eagleson <nate@nateeag.com>
2017-05-30 11:24:48 -0400

    Update package puppet-mode-20170421.2255

--- elpa/puppet-mode-20170528.904/puppet-mode-autoloads.el
+++ elpa/puppet-mode-20170528.904/puppet-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (22809 42456
+;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (22829 36544
 ;;;;;;  0 0))
 ;;; Generated autoloads from puppet-mode.el
 
---
+++ elpa/puppet-mode-20170528.904/puppet-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "puppet-mode" "20170528.904" "Major mode for Puppet manifests" '((emacs "24.1") (pkg-info "0.4")) :commit "c5a0fc884d0f33c3140585c9c7699ce0bc969f63" :url "https://github.com/voxpupuli/puppet-mode" :keywords '("languages"))

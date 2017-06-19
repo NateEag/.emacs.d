17a7401dc90b704f1fd55948d02f55e3984e0364
Nate Eagleson <nate@nateeag.com>
2017-06-19 09:50:25 -0400

    Update package async-20170502.2343

--- elpa/async-20170610.2241/async-pkg.el
+++ elpa/async-20170610.2241/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20170502.2343" "Asynchronous processing in Emacs" 'nil :keywords
+(define-package "async" "20170610.2241" "Asynchronous processing in Emacs" 'nil :keywords
   '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:

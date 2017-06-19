e5fe67b08b39b5aee11708f8b415104efa67bbc8
Nate Eagleson <nate@nateeag.com>
2017-06-19 09:50:10 -0400

    Update package package-lint-20170604.1626

--- elpa/package-lint-20170604.1626/package-lint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "package-lint" "20170604.1626" "A linting library for elisp package authors" '((cl-lib "0.5") (emacs "24")) :commit "79a62f3abe812fbc7f233c9e80b0cf81b3fa292c" :url "https://github.com/purcell/package-lint" :keywords '("lisp"))
--- elpa/package-lint-20170617.2227/package-lint-autoloads.el
+++ elpa/package-lint-20170617.2227/package-lint-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "package-lint" "package-lint.el" (22838 44461
+;;;### (autoloads nil "package-lint" "package-lint.el" (22855 54930
 ;;;;;;  0 0))
 ;;; Generated autoloads from package-lint.el
 

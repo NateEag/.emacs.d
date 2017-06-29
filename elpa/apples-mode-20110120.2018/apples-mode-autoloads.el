07836741ecd66080836d627bc7bfd383b0195e4c
Nate Eagleson <nate@nateeag.com>
2017-06-29 13:51:42 -0400

    Install apples-mode from MELPA
    Sometimes you have to write AppleScript.
    
    *shudders*

---
+++ elpa/apples-mode-20110120.2018/apples-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; apples-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "apples-mode" "apples-mode.el" (22869 3469
+;;;;;;  0 0))
+;;; Generated autoloads from apples-mode.el
+
+(autoload 'apples-open-scratch "apples-mode" "\
+Open scratch buffer for AppleScript.
+
+\(fn)" t nil)
+
+(autoload 'apples-mode "apples-mode" "\
+Happy AppleScripting!
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("apples-mode-pkg.el") (22869 3469 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; apples-mode-autoloads.el ends here
---
+++ elpa/apples-mode-20110120.2018/apples-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "apples-mode" "20110120.2018" "Major mode for editing and executing AppleScript code" 'nil :keywords
+  '("applescript" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

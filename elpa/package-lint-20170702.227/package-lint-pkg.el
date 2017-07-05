4c46f5600833d51cbc9ca9bb851a521f4c7aed67
Nate Eagleson <nate@nateeag.com>
2017-07-05 08:53:32 -0400

    Update package package-lint-20170624.1712

---
+++ elpa/package-lint-20170702.227/package-lint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package-lint" "20170702.227" "A linting library for elisp package authors" '((cl-lib "0.5") (emacs "24")) :commit "416d1cba6664b0783722129d24f4a748bd71e057" :url "https://github.com/purcell/package-lint" :keywords '("lisp"))

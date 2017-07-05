f9e0f3e54881048d4cf478d27827928efae1e73b
Nate Eagleson <nate@nateeag.com>
2017-07-05 08:53:42 -0400

    Update package use-package-20170616.1454

--- elpa/use-package-20170616.1454/use-package-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "use-package" "20170616.1454" "A use-package declaration for simplifying your .emacs" '((bind-key "1.0") (diminish "0.44")) :commit "5bd87be2c99196c5342de7ffd56dd9e703a7e400" :url "https://github.com/jwiegley/use-package" :keywords '("dotemacs" "startup" "speed" "config" "package"))

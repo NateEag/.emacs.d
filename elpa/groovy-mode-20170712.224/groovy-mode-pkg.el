c7d7dbd35d3f10d838358f088cd7290fad780ba5
Nate Eagleson <nate@nateeag.com>
2017-07-12 08:53:06 -0400

    Update package groovy-mode-20170703.426

--- elpa/groovy-mode-20170712.224/groovy-mode-pkg.el
+++ elpa/groovy-mode-20170712.224/groovy-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "groovy-mode" "20170703.426" "Major mode for Groovy source files"
+(define-package "groovy-mode" "20170712.224" "Major mode for Groovy source files"
   '((s "1.11.0"))
   :keywords
   '("languages"))

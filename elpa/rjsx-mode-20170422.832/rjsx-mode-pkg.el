d13b1a398834ddffe7e85431a3fd95a5bd7ec12d
Nate Eagleson <nate@nateeag.com>
2017-07-12 08:53:19 -0400

    Update package rjsx-mode-20170422.832

--- elpa/rjsx-mode-20170422.832/rjsx-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rjsx-mode" "20170422.832" "Real support for JSX" '((emacs "24.4") (js2-mode "20160623")) :commit "b41de6c1b2f6668b674f8e5bf880f697c9ffb749" :url "https://github.com/felipeochoa/rjsx-mode/" :keywords '("languages"))

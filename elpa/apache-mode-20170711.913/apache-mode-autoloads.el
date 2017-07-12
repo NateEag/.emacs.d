f3c33c4c1fcbe958b208d6e978a8c0f73eef9b23
Nate Eagleson <nate@nateeag.com>
2017-07-12 08:53:01 -0400

    Update package apache-mode-20170407.1140

--- elpa/apache-mode-20170711.913/apache-mode-autoloads.el
+++ elpa/apache-mode-20170711.913/apache-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "apache-mode" "apache-mode.el" (22772 50498
+;;;### (autoloads nil "apache-mode" "apache-mode.el" (22886 7085
 ;;;;;;  0 0))
 ;;; Generated autoloads from apache-mode.el
 
@@ -11,12 +11,15 @@
 Major mode for editing Apache configuration files.
 
 \(fn)" t nil)
-(add-to-list 'auto-mode-alist '("/\\.htaccess\\'"   . apache-mode))
-(add-to-list 'auto-mode-alist '("/httpd\\.conf\\'"  . apache-mode))
-(add-to-list 'auto-mode-alist '("/srm\\.conf\\'"    . apache-mode))
-(add-to-list 'auto-mode-alist '("/access\\.conf\\'" . apache-mode))
+
+(add-to-list 'auto-mode-alist '("/\\.htaccess\\'" . apache-mode))
+
+(add-to-list 'auto-mode-alist '("/\\(?:access\\|httpd\\|srm\\)\\.conf\\'" . apache-mode))
+
 (add-to-list 'auto-mode-alist '("/apache2/.+\\.conf\\'" . apache-mode))
+
 (add-to-list 'auto-mode-alist '("/httpd/conf/.+\\.conf\\'" . apache-mode))
+
 (add-to-list 'auto-mode-alist '("/apache2/sites-\\(?:available\\|enabled\\)/" . apache-mode))
 
 ;;;***

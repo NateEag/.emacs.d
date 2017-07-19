c305b927eb11efe02a40eaf89466f9d4be7b062c
Nate Eagleson <nate@nateeag.com>
2017-07-19 12:58:18 -0400

    Update package helm-ls-git-20170601.2200

--- elpa/helm-ls-git-20170716.109/helm-ls-git-autoloads.el
+++ elpa/helm-ls-git-20170716.109/helm-ls-git-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "helm-ls-git" "helm-ls-git.el" (22838 44445
+;;;### (autoloads nil "helm-ls-git" "helm-ls-git.el" (22895 36778
 ;;;;;;  0 0))
 ;;; Generated autoloads from helm-ls-git.el
 

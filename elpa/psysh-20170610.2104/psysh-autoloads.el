6286c52c606433b184f971b00eb8b8d5af3464a0
Nate Eagleson <nate@nateeag.com>
2017-07-19 12:58:13 -0400

    Update package ag-20170420.1424

---
+++ elpa/psysh-20170610.2104/psysh-autoloads.el
@@ -0,0 +1,46 @@
+;;; psysh-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "psysh" "psysh.el" (22894 21665 0 0))
+;;; Generated autoloads from psysh.el
+
+(autoload 'psysh-doc-buffer "psysh" "\
+Execute PsySH Doc `TARGET' and Return PsySH buffer `BUF'.
+
+\(fn TARGET &optional BUF)" nil nil)
+
+(autoload 'psysh-doc-mode "psysh" "\
+Major mode for viewing PsySH Doc.
+
+\(fn)" t nil)
+
+(autoload 'psysh-doc-string "psysh" "\
+Return string of PsySH Doc `TARGET'.
+
+\(fn TARGET)" nil nil)
+
+(autoload 'psysh-doc "psysh" "\
+Display PsySH doc `TARGET'.
+
+\(fn TARGET)" t nil)
+
+(autoload 'psysh "psysh" "\
+Run PsySH interactive shell.
+
+\(fn)" t nil)
+
+(autoload 'psysh-run "psysh" "\
+Run PsySH interactive-shell in `BUFFER-NAME' and `PROCESS'.
+
+\(fn BUFFER-NAME PROCESS)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; psysh-autoloads.el ends here

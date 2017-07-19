37fbc1fe2f475e702fedc6fb0751a784c832d411
Nate Eagleson <nate@nateeag.com>
2017-07-19 12:58:18 -0400

    Update package groovy-mode-20170712.224

--- elpa/groovy-mode-20170718.234/groovy-mode-autoloads.el
+++ elpa/groovy-mode-20170718.234/groovy-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "groovy-electric" "groovy-electric.el" (22886
-;;;;;;  7089 0 0))
+;;;### (autoloads nil "groovy-electric" "groovy-electric.el" (22895
+;;;;;;  36777 0 0))
 ;;; Generated autoloads from groovy-electric.el
 
 (autoload 'groovy-electric-mode "groovy-electric" "\
@@ -22,7 +22,7 @@ have Font Lock enabled. ${ } is expanded when in a GString
 
 ;;;***
 
-;;;### (autoloads nil "groovy-mode" "groovy-mode.el" (22886 7089
+;;;### (autoloads nil "groovy-mode" "groovy-mode.el" (22895 36777
 ;;;;;;  0 0))
 ;;; Generated autoloads from groovy-mode.el
  (add-to-list 'auto-mode-alist '("\\.g\\(?:ant\\|roovy\\|radle\\)\\'\\|Jenkinsfile\\'" . groovy-mode))
@@ -41,7 +41,7 @@ Key bindings:
 
 ;;;***
 
-;;;### (autoloads nil "inf-groovy" "inf-groovy.el" (22886 7089 0
+;;;### (autoloads nil "inf-groovy" "inf-groovy.el" (22895 36777 0
 ;;;;;;  0))
 ;;; Generated autoloads from inf-groovy.el
 
@@ -103,7 +103,7 @@ process buffer for a list of commands.
 
 ;;;***
 
-;;;### (autoloads nil nil ("groovy-mode-pkg.el") (22886 7089 0 0))
+;;;### (autoloads nil nil ("groovy-mode-pkg.el") (22895 36777 0 0))
 
 ;;;***
 

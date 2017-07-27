9f6aad57d6ff4c559ad8b4f39b959fe75c54961c
Nate Eagleson <nate@nateeag.com>
2017-07-27 15:52:53 -0400

    Update package skewer-mode-20170709.939

--- elpa/skewer-mode-20170722.1745/skewer-mode-pkg.el
+++ elpa/skewer-mode-20170722.1745/skewer-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "skewer-mode" "20170709.939" "live browser JavaScript, CSS, and HTML interaction"
+(define-package "skewer-mode" "20170722.1745" "live browser JavaScript, CSS, and HTML interaction"
   '((simple-httpd "1.4.0")
     (js2-mode "20090723")
     (emacs "24")))

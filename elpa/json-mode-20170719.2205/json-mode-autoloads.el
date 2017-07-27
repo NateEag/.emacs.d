b781c0f97f9ba66b48ea09aa0fb2f9d135b54004
Nate Eagleson <nate@nateeag.com>
2017-07-27 15:52:39 -0400

    Update package json-mode-20170619.1701

--- elpa/json-mode-20170719.2205/json-mode-autoloads.el
+++ elpa/json-mode-20170719.2205/json-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "json-mode" "json-mode.el" (22858 26556 0 0))
+;;;### (autoloads nil "json-mode" "json-mode.el" (22906 17543 0 0))
 ;;; Generated autoloads from json-mode.el
 
 (defconst json-mode-standard-file-ext '(".json" ".jsonld") "\
@@ -33,6 +33,11 @@ Major mode for editing JSON files
 \(fn)" t nil)
 
 (autoload 'json-mode-show-path "json-mode" "\
+
+
+\(fn)" t nil)
+
+(autoload 'json-mode-kill-path "json-mode" "\
 
 
 \(fn)" t nil)

c1413860a63434d41f654d5f2895054f1ef9b5b0
Nate Eagleson <nate@nateeag.com>
2017-08-04 09:55:44 -0400

    Update package esup-20170725.2158

--- elpa/esup-20170729.1742/esup-autoloads.el
+++ elpa/esup-20170729.1742/esup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "esup" "esup.el" (22906 17537 0 0))
+;;;### (autoloads nil "esup" "esup.el" (22916 31968 0 0))
 ;;; Generated autoloads from esup.el
 
 (autoload 'esup "esup" "\
@@ -14,8 +14,8 @@ If INIT-FILE is non-nil, profile that instead of USER-INIT-FILE.
 
 ;;;***
 
-;;;### (autoloads nil nil ("esup-child.el" "esup-pkg.el") (22906
-;;;;;;  17537 0 0))
+;;;### (autoloads nil nil ("esup-child.el" "esup-pkg.el") (22916
+;;;;;;  31968 0 0))
 
 ;;;***
 
--- elpa/esup-20170729.1742/esup-pkg.el
+++ elpa/esup-20170729.1742/esup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "esup" "20170725.2158" "the Emacs StartUp Profiler (ESUP)"
+(define-package "esup" "20170729.1742" "the Emacs StartUp Profiler (ESUP)"
   '((cl-lib "0.5")
     (emacs "24"))
   :url "http://github.com/jschaf/esup" :keywords

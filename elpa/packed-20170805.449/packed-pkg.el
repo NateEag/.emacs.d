86d9c5ed741c85d40d8fc6e6c673ab615dba6160
Nate Eagleson <nate@nateeag.com>
2017-08-24 10:38:57 -0400

    Update package packed-20170805.449

--- elpa/packed-20170805.449/packed-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "packed" "20170805.449" "package manager agnostic Emacs Lisp package utilities" '((emacs "24.3") (dash "2.12.1")) :commit "e8b20e287222dbf08ebc46e405412c3213102a7e" :url "https://github.com/emacscollective/packed" :keywords '("compile" "convenience" "lisp" "package" "library"))

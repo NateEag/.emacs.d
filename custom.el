388fc89aa21dd765c267b4d8809bcfba769f2139
Nate Eagleson <nate@nateeag.com>
2017-09-13 10:14:54 -0400

    Do not debug when I press Control-G
    at least, not by default.
    
    I can see how it would be useful sometimes, but generally it's not going
    to be what I want.

--- custom.el
+++ custom.el
@@ -33,6 +33,7 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(debug-on-quit nil)
  '(delete-selection-mode t)
  '(evil-want-fine-undo t)
  '(exec-path-from-shell-arguments (quote ("-l")))

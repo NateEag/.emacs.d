7591c4bc1148ed746eca4b057061488658c43eaa
Nate Eagleson <nate@nateeag.com>
2017-09-18 13:49:38 -0400

    Update package goto-chg-20131228.659

--- elpa/goto-chg-20170917.1200/goto-chg-autoloads.el
+++ elpa/goto-chg-20170917.1200/goto-chg-autoloads.el
@@ -1,9 +1,9 @@
 ;;; goto-chg-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "goto-chg" "goto-chg.el" (22010 5232 0 0))
+;;;### (autoloads nil "goto-chg" "goto-chg.el" (22976 1842 0 0))
 ;;; Generated autoloads from goto-chg.el
 
 (autoload 'goto-last-change "goto-chg" "\

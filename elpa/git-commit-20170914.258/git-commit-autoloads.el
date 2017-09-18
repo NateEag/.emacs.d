482c5943c2372fb4a7953c0c3f60e6b2683300cf
Nate Eagleson <nate@nateeag.com>
2017-09-18 13:49:46 -0400

    Update package git-commit-20170823.2103

--- elpa/git-commit-20170914.258/git-commit-autoloads.el
+++ elpa/git-commit-20170914.258/git-commit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22942 58617 0
+;;;### (autoloads nil "git-commit" "git-commit.el" (22976 1849 0
 ;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 

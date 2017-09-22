8f2a4dcb9aba63125e062da562f92ba5c0a2a0a5
Nate Eagleson <nate@nateeag.com>
2017-09-22 16:07:37 -0400

    Ask lsp-mode to print io into the *Messages* buffer
    I'm hoping it will prove useful in improving the PHP editing experience
    I have at present.

--- custom.el
+++ custom.el
@@ -67,6 +67,7 @@
  '(js2-mode-show-parse-errors nil)
  '(js2-mode-show-strict-warnings nil)
  '(line-number-mode t)
+ '(lsp-print-io t nil nil "I'm turning this on to help me debug the occasional issues I have with the php-language-server.")
  '(magit-diff-refine-hunk (quote all))
  '(magit-diff-use-overlays nil)
  '(magit-push-always-verify nil)

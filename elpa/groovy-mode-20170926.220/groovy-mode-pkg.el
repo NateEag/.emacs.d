8aed9871d99d919a523a94b919a79058336ac549
Nate Eagleson <nate@nateeag.com>
2017-09-27 18:32:50 -0400

    Update package groovy-mode-20170828.1026

---
+++ elpa/groovy-mode-20170926.220/groovy-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "groovy-mode" "20170926.220" "Major mode for Groovy source files"
+  '((s "1.12.0"))
+  :keywords
+  '("languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

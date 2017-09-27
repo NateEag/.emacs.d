05c8f53150235e80ef7807618626dce2c0f5a766
Nate Eagleson <nate@nateeag.com>
2017-09-27 18:32:48 -0400

    Update package editorconfig-20170912.2223

--- elpa/editorconfig-20170919.1146/editorconfig-pkg.el
+++ elpa/editorconfig-20170919.1146/editorconfig-pkg.el
@@ -1,4 +1,4 @@
-(define-package "editorconfig" "20170912.2223" "EditorConfig Emacs Plugin"
+(define-package "editorconfig" "20170919.1146" "EditorConfig Emacs Plugin"
   '((cl-lib "0.5"))
   :url "https://github.com/editorconfig/editorconfig-emacs#readme")
 ;; Local Variables:

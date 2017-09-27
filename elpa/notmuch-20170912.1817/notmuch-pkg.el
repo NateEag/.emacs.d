58dc24972cb652d1ddc13fea258a28b573d337c0
Nate Eagleson <nate@nateeag.com>
2017-09-27 18:33:04 -0400

    Update package notmuch-20170912.1817

--- elpa/notmuch-20170912.1817/notmuch-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "notmuch" "20170912.1817" "run notmuch within emacs" 'nil :url "https://notmuchmail.org/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/notmuch-20170927.415/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20170927.415" "run notmuch within emacs" 'nil :url "https://notmuchmail.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

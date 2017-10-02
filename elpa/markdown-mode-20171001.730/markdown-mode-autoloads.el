75fcf1868af7e3a1d2c09b3c38c6571b73083604
Nate Eagleson <nate@nateeag.com>
2017-10-02 09:17:38 -0400

    Update package markdown-mode-20170925.912

--- elpa/markdown-mode-20171001.730/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20171001.730/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22988 10013
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22994 15473
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
---
+++ elpa/markdown-mode-20171001.730/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20171001.730" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "bc79248c605a5ebd01d96b26e5d5dba39bddcdc7" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))

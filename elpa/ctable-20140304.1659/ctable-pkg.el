9bf938c5bd2c3377b6372fe5202f0d6e2fab7202
Nate Eagleson <nate@nateeag.com>
2017-10-11 11:22:11 -0400

    Update package ctable-20140304.1659

--- elpa/ctable-20140304.1659/ctable-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ctable" "20140304.1659" "Table component for Emacs Lisp" 'nil :url "https://github.com/kiwanami/emacs-ctable" :keywords ("table"))
--- elpa/ctable-20171005.1711/ctable-autoloads.el
+++ elpa/ctable-20171005.1711/ctable-autoloads.el
@@ -1,9 +1,9 @@
 ;;; ctable-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("ctable.el") (21354 57380 857892 0))
+;;;### (autoloads nil nil ("ctable.el") (23006 14115 0 0))
 
 ;;;***
 

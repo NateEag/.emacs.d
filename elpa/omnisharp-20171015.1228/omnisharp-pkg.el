4b024bf2220c765f8938ef3b72aca0fb70a850c9
Nate Eagleson <nate@nateeag.com>
2017-10-16 09:06:32 -0400

    Update package omnisharp-20171007.416

--- elpa/omnisharp-20171015.1228/omnisharp-pkg.el
+++ elpa/omnisharp-20171015.1228/omnisharp-pkg.el
@@ -1,4 +1,4 @@
-(define-package "omnisharp" "20171007.416" "Omnicompletion (intellisense) and more for C#"
+(define-package "omnisharp" "20171015.1228" "Omnicompletion (intellisense) and more for C#"
   '((emacs "24")
     (flycheck "30")
     (dash "2.12.0")

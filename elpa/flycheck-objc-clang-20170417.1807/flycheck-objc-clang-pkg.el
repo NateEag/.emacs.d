d0c6cbecdf481020a40ca4b601d98796aaeb021f
Nate Eagleson <nate@nateeag.com>
2017-10-16 09:06:14 -0400

    Update package flycheck-objc-clang-20170417.1807

--- elpa/flycheck-objc-clang-20170417.1807/flycheck-objc-clang-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-objc-clang" "20170417.1807" "Flycheck: Objective-C support using Clang" '((emacs "24.4") (flycheck "26")) :commit "b16b77f95e4e53a8951ebee6209a5de311748447" :url "https://github.com/GyazSquare/flycheck-objc-clang" :keywords '("convenience" "languages" "tools"))

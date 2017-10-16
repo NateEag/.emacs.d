4f2b87f38b6cc5a65cf6e00f0dcd6c715fb23d7a
Nate Eagleson <nate@nateeag.com>
2017-10-16 08:48:42 -0400

    Install js-auto-format-mode from MELPA
    I'm not sure I'll ever actually use it, but I figured I might as well
    install it.

---
+++ elpa/js-auto-format-mode-20171014.911/js-auto-format-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; js-auto-format-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "js-auto-format-mode" "js-auto-format-mode.el"
+;;;;;;  (23012 43515 0 0))
+;;; Generated autoloads from js-auto-format-mode.el
+
+(autoload 'js-auto-format-execute "js-auto-format-mode" "\
+Format JavaScript source code.
+
+\(fn)" t nil)
+
+(autoload 'js-auto-format-mode "js-auto-format-mode" "\
+Minor mode for auto-formatting JavaScript code
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; js-auto-format-mode-autoloads.el ends here
---
+++ elpa/js-auto-format-mode-20171014.911/js-auto-format-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "js-auto-format-mode" "20171014.911" "Minor mode for auto-formatting JavaScript code" '((emacs "24")) :commit "309ff861059c724182398bdddcc1c58982fa41c7" :url "https://github.com/ybiquitous/js-auto-format-mode" :keywords '("languages"))

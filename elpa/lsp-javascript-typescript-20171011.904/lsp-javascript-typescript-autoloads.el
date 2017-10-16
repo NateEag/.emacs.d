41aec61204d4ede9d54e0ea96600a5c413a3df5e
Nate Eagleson <nate@nateeag.com>
2017-10-16 08:49:11 -0400

    Install lsp-javascript-typescript from MELPA
    Again, I'm not sure I'll ever actually use it, but at some point in
    principle I should compare this with Tern, consider which one's more
    likely long-term to get meaningful updates and be usable, and make an
    intelligent decision about which one to use.

---
+++ elpa/lsp-javascript-typescript-20171011.904/lsp-javascript-typescript-autoloads.el
@@ -0,0 +1,16 @@
+;;; lsp-javascript-typescript-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lsp-javascript-typescript.el") (23012
+;;;;;;  43515 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-javascript-typescript-autoloads.el ends here
---
+++ elpa/lsp-javascript-typescript-20171011.904/lsp-javascript-typescript-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-javascript-typescript" "20171011.904" "Javascript/Typescript support for lsp-mode" '((lsp-mode "2.0")) :commit "2339bca13cd5e13f7954991653402c6bba330bd2" :url "https://github.com/emacs-lsp/lsp-javascript" :keywords '("javascript" "typescript" "lsp"))

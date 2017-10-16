c4166e3051b0242f09b785bda0b089ef52a86bde
Nate Eagleson <nate@nateeag.com>
2017-10-16 09:06:08 -0400

    Update package avy-20170819.1039

---
+++ elpa/avy-20171014.20/avy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "avy" "20171014.20" "tree-based completion" '((emacs "24.1") (cl-lib "0.5")) :commit "36f768d9e320d300b81799e0fcc097cb89aad780" :url "https://github.com/abo-abo/avy" :keywords '("point" "location"))

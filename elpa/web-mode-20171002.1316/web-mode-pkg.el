cbf21b3485b814f6a3f35d47baef2bab2c989c72
Nate Eagleson <nate@nateeag.com>
2017-10-20 10:08:57 -0400

    Update package web-mode-20171002.1316

--- elpa/web-mode-20171002.1316/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20171002.1316" "major mode for editing web templates" '((emacs "23.1")) :commit "c15743454d0de944e5872a2ac4c9111d8cd61940" :url "http://web-mode.org" :keywords '("languages"))
--- elpa/web-mode-20171017.1214/web-mode-autoloads.el
+++ elpa/web-mode-20171017.1214/web-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (23006 14139 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (23018 887 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\

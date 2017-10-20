cf6582eef930d863423196adf121c89b284893d8
Nate Eagleson <nate@nateeag.com>
2017-10-20 10:08:46 -0400

    Update package magit-popup-20171014.202

--- elpa/magit-popup-20171020.28/magit-popup-pkg.el
+++ elpa/magit-popup-20171020.28/magit-popup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit-popup" "20171014.202" "Define prefix-infix-suffix command combos"
+(define-package "magit-popup" "20171020.28" "Define prefix-infix-suffix command combos"
   '((emacs "24.4")
     (async "20170823")
     (dash "20170810"))

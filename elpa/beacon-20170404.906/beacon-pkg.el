46c30791cda10126c9fb9107f793c6594fd99794
Nate Eagleson <nate@nateeag.com>
2017-10-20 10:08:33 -0400

    Update package beacon-20170404.906

--- elpa/beacon-20170404.906/beacon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beacon" "20170404.906" "Highlight the cursor whenever the window scrolls" '((seq "2.14")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))

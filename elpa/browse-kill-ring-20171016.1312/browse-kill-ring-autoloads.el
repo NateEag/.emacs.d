a8501ac7e28226d3da9747c713100acbcac742aa
Nate Eagleson <nate@nateeag.com>
2017-10-20 10:08:34 -0400

    Update package browse-kill-ring-20160125.9

--- elpa/browse-kill-ring-20171016.1312/browse-kill-ring-autoloads.el
+++ elpa/browse-kill-ring-20171016.1312/browse-kill-ring-autoloads.el
@@ -1,10 +1,10 @@
 ;;; browse-kill-ring-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "browse-kill-ring" "browse-kill-ring.el" (22271
-;;;;;;  48941 0 0))
+;;;### (autoloads nil "browse-kill-ring" "browse-kill-ring.el" (23018
+;;;;;;  865 0 0))
 ;;; Generated autoloads from browse-kill-ring.el
 
 (autoload 'browse-kill-ring-default-keybindings "browse-kill-ring" "\

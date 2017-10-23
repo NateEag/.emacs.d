247805da5aef27536fb3e557ee72086c95171ba7
Nate Eagleson <nate@nateeag.com>
2017-10-23 09:26:56 -0400

    Update package helm-20171019.2338

--- elpa/helm-20171021.2119/helm-pkg.el
+++ elpa/helm-20171021.2119/helm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm" "20171019.2338" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20171021.2119" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9.2")
     (popup "0.5.3")

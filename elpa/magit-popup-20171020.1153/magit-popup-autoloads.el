436eebf9e0837018bad77ff49e2f5d364fbb254e
Nate Eagleson <nate@nateeag.com>
2017-10-23 09:27:03 -0400

    Update package magit-popup-20171020.28

--- elpa/magit-popup-20171020.1153/magit-popup-autoloads.el
+++ elpa/magit-popup-20171020.1153/magit-popup-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (23018 878 0 0))
+;;;;;;  (23021 60967 0 0))
 
 ;;;***
 

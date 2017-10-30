ff58b8866f9c2bce597808b58df3e83690d6412b
Nate Eagleson <nate@nateeag.com>
2017-10-30 10:13:43 -0400

    Update package ac-slime-20150729.2035

--- elpa/ac-slime-20171027.1400/ac-slime-autoloads.el
+++ elpa/ac-slime-20171027.1400/ac-slime-autoloads.el
@@ -1,9 +1,9 @@
 ;;; ac-slime-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ac-slime" "ac-slime.el" (21968 61761 0 0))
+;;;### (autoloads nil "ac-slime" "ac-slime.el" (23031 13207 0 0))
 ;;; Generated autoloads from ac-slime.el
 
 (defface ac-slime-menu-face '((t (:inherit ac-candidate-face))) "\

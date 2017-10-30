f30310673f18ecc4f8beb558b0032b949c470c15
Nate Eagleson <nate@nateeag.com>
2017-10-30 10:13:53 -0400

    Update package js-auto-format-mode-20171019.2142

--- elpa/js-auto-format-mode-20171024.827/js-auto-format-mode-autoloads.el
+++ elpa/js-auto-format-mode-20171024.827/js-auto-format-mode-autoloads.el
@@ -4,8 +4,13 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "js-auto-format-mode" "js-auto-format-mode.el"
-;;;;;;  (23018 873 0 0))
+;;;;;;  (23031 13217 0 0))
 ;;; Generated autoloads from js-auto-format-mode.el
+
+(autoload 'js-auto-format-enabled-p "js-auto-format-mode" "\
+Test whether js-auto-format-mode is enabled.
+
+\(fn)" nil nil)
 
 (autoload 'js-auto-format-execute "js-auto-format-mode" "\
 Format JavaScript source code.

e566f85217e4dfbf213efc24fc68e71e59c4f2b2
Nate Eagleson <nate@nateeag.com>
2017-11-08 15:35:21 -0500

    Update package f-20170404.1039

---
+++ elpa/f-20171107.1248/f-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "f" "20171107.1248" "Modern API for working with files and directories" '((s "1.7.0") (dash "2.2.0")) :commit "3623a4577c89740cfad8d961f3201d3222c79ff6" :url "http://github.com/rejeep/f.el" :keywords '("files" "directories"))

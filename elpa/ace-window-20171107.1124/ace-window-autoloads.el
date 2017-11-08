6c6ae0b98c9d1849b6acca84c196b5fa065859f7
Nate Eagleson <nate@nateeag.com>
2017-11-08 15:35:12 -0500

    Update package ace-window-20170801.1334

--- elpa/ace-window-20171107.1124/ace-window-autoloads.el
+++ elpa/ace-window-20171107.1124/ace-window-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ace-window" "ace-window.el" (22916 31917 0
+;;;### (autoloads nil "ace-window" "ace-window.el" (23043 27264 0
 ;;;;;;  0))
 ;;; Generated autoloads from ace-window.el
 
---
+++ elpa/ace-window-20171107.1124/ace-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-window" "20171107.1124" "Quickly switch windows." '((avy "0.2.0")) :commit "056abfee87c9898011959d3992f8c97805e1d39a" :url "https://github.com/abo-abo/ace-window" :keywords '("window" "location"))

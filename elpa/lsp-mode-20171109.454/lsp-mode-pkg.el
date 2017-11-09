b0289d61a83efe39cd6792800302439115978cc6
Nate Eagleson <nate@nateeag.com>
2017-11-09 10:54:42 -0500

    Update package lsp-mode-20171108.439

--- elpa/lsp-mode-20171109.454/lsp-mode-pkg.el
+++ elpa/lsp-mode-20171109.454/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20171108.439" "Minor mode for interacting with Language Servers"
+(define-package "lsp-mode" "20171109.454" "Minor mode for interacting with Language Servers"
   '((emacs "25.1")
     (flycheck "30"))
   :url "https://github.com/emacs-lsp/lsp-mode")

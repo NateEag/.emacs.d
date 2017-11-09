7d947eb31aa3469769808d6c66fc8af9efb24740
Nate Eagleson <nate@nateeag.com>
2017-11-09 10:54:45 -0500

    Update package projectile-20171102.55

--- elpa/projectile-20171102.55/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile" "20171102.55" "Manage and navigate projects in Emacs easily" '((emacs "24.1") (pkg-info "0.4")) :commit "48457fb32b7ff31e5dd30d38e576b8e6c8d3ceba" :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
--- elpa/projectile-20171109.240/projectile-autoloads.el
+++ elpa/projectile-20171109.240/projectile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "projectile" "projectile.el" (23035 10809 0
+;;;### (autoloads nil "projectile" "projectile.el" (23044 31300 0
 ;;;;;;  0))
 ;;; Generated autoloads from projectile.el
 

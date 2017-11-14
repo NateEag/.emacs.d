4221479c69298217b649a538076ebd112594aedc
Nate Eagleson <nate@nateeag.com>
2017-11-14 10:52:08 -0500

    Update package websocket-20170829.457

--- elpa/websocket-20171113.2045/websocket-autoloads.el
+++ elpa/websocket-20171113.2045/websocket-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("websocket.el") (23022 14048 0 0))
+;;;### (autoloads nil nil ("websocket.el") (23051 4392 0 0))
 
 ;;;***
 

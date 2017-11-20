a77327e48a862bd16a350a9d890648b65073023c
Nate Eagleson <nate@nateeag.com>
2017-11-20 09:36:25 -0500

    Install thingatpt+ from MELPA
    It's apparently a dependency of one of the three packages I just
    installed... not really sure which one.

---
+++ elpa/thingatpt+-20170307.1539/thingatpt+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "thingatpt+" "20170307.1539" "Extensions to `thingatpt.el'." 'nil :url "https://www.emacswiki.org/emacs/download/thingatpt%2b.el" :keywords '("extensions" "matching" "mouse"))

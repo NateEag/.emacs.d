c90520ee454da798e5e6522d35141957a13b1782
Nate Eagleson <nate@nateeag.com>
2017-11-20 09:35:41 -0500

    Install exato from MELPA

---
+++ elpa/exato-20171119.1149/exato-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "exato" "20171119.1149" "EXATO: Evil XML/HTML Attributes Text Object" '((evil "1.2.13") (thingatpt+ "0")) :commit "a5bf46c1aa7462b13501c73ad17cddfbeaa51a88" :url "https://github.com/ninrod/exato")

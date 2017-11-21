08501a2cbd35d287ea31f932b34e0f287609cc70
Nate Eagleson <nate@nateeag.com>
2017-11-21 09:02:22 -0500

    Update package string-inflection-20170419.1638

---
+++ elpa/string-inflection-20171117.1731/string-inflection-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "string-inflection" "20171117.1731" "underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names" 'nil :commit "e644c8bf2a5e36ddadcfe4de657588c00fd368dd" :keywords '("elisp"))

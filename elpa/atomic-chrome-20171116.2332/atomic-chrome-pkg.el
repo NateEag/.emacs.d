f2d37765d253462020d48e71b72d5cba0052cfb4
Nate Eagleson <nate@nateeag.com>
2017-11-21 09:02:09 -0500

    Update package atomic-chrome-20171022.107

---
+++ elpa/atomic-chrome-20171116.2332/atomic-chrome-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "atomic-chrome" "20171116.2332" "Edit Chrome text area with Emacs using Atomic Chrome" '((emacs "24.3") (let-alist "1.0.4") (websocket "1.4")) :commit "7dd8a45ebccb9b5bbbfd2e822bd914b38dad6008" :url "https://github.com/alpha22jp/atomic-chrome" :keywords '("chrome" "edit" "textarea"))

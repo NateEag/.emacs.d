eed769daa1488de91067e398833688976e94c605
Nate Eagleson <nate@nateeag.com>
2017-11-27 11:36:09 -0500

    Update package avy-20171111.921

---
+++ elpa/avy-20171126.815/avy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "avy" "20171126.815" "Jump to arbitrary positions in visible text and select text quickly." '((emacs "24.1") (cl-lib "0.5")) :commit "c08fc7c1c6997f7e843175e632d3d812e9f5a5b5" :url "https://github.com/abo-abo/avy" :keywords '("point" "location"))

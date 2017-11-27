fd77d4d9211f8a7f74e391e88d551ee3c2f264af
Nate Eagleson <nate@nateeag.com>
2017-11-27 11:36:15 -0500

    Update package with-editor-20171006.352

--- elpa/with-editor-20171127.635/with-editor-autoloads.el
+++ elpa/with-editor-20171127.635/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (23006 14140 0 0))
+;;;;;;  (23068 16126 0 0))
 
 ;;;***
 

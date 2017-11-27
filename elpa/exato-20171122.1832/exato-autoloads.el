3c77b1a1c0aae3fec6158b09b562a44f7d095e13
Nate Eagleson <nate@nateeag.com>
2017-11-27 11:36:06 -0500

    Update package exato-20171119.1149

--- elpa/exato-20171122.1832/exato-autoloads.el
+++ elpa/exato-20171122.1832/exato-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("exato.el") (23058 59071 0 0))
+;;;### (autoloads nil nil ("exato.el") (23068 16118 0 0))
 
 ;;;***
 
---
+++ elpa/exato-20171122.1832/exato-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "exato" "20171122.1832" "EXATO: Evil XML/HTML Attributes Text Object" '((evil "1.2.13") (thingatpt+ "0")) :commit "0b1c5cd0cdaefba88a62c5e007c4d9762483a9cc" :url "https://github.com/ninrod/exato")

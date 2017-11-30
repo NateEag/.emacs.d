975cbc526aaecae13f7fff66dc8065cd9d8e1c32
Nate Eagleson <nate@nateeag.com>
2017-11-30 11:33:33 -0500

    Update package evil-20171126.659

--- elpa/evil-20171129.651/evil-pkg.el
+++ elpa/evil-20171129.651/evil-pkg.el
@@ -1,4 +1,4 @@
-(define-package "evil" "20171126.659" "Extensible Vi layer for Emacs."
+(define-package "evil" "20171129.651" "Extensible Vi layer for Emacs."
   '((emacs "24.1")
     (undo-tree "0.6.3")
     (goto-chg "1.6")

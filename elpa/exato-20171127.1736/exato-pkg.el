4bae7ad256dea1b99d7c3cccf23774d2770bc959
Nate Eagleson <nate@nateeag.com>
2017-11-30 11:33:30 -0500

    Update package exato-20171122.1832

---
+++ elpa/exato-20171127.1736/exato-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "exato" "20171127.1736" "EXATO: Evil XML/HTML Attributes Text Object" '((evil "1.2.13") (thingatpt+ "0")) :commit "ba21cd2c8d0588e1c70ba89ebad6df247605e03f" :url "https://github.com/ninrod/exato")

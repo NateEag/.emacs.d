e4be8adbd67241ee888d8876122e46540bb89eb4
Nate Eagleson <nate@nateeag.com>
2017-11-30 11:33:30 -0500

    Update package evil-surround-20171004.600

--- elpa/evil-surround-20171127.910/evil-surround-autoloads.el
+++ elpa/evil-surround-20171127.910/evil-surround-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "evil-surround" "evil-surround.el" (23006 14109
+;;;### (autoloads nil "evil-surround" "evil-surround.el" (23072 13018
 ;;;;;;  0 0))
 ;;; Generated autoloads from evil-surround.el
 
---
+++ elpa/evil-surround-20171127.910/evil-surround-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-surround" "20171127.910" "emulate surround.vim from Vim" '((evil "1.2.12")) :commit "55c820083a5f28d5361baeb9cd7da92549e5b3f5" :keywords '("emulation" "vi" "evil"))

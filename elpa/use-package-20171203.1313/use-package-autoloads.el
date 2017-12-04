1683cb6f39812be9f568bf04ec72a2f8532ff5ef
Nate Eagleson <nate@nateeag.com>
2017-12-04 09:28:51 -0500

    Update package use-package-20171129.2308

---
+++ elpa/use-package-20171203.1313/use-package-autoloads.el
@@ -0,0 +1,15 @@
+;;; use-package-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("use-package.el") (23077 23459 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; use-package-autoloads.el ends here

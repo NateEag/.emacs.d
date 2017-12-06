82e2312fc5e8236cc24cfd5e9ec53698027d2fcf
Nate Eagleson <nate@nateeag.com>
2017-12-06 09:42:14 -0500

    Update package typescript-mode-20171109.727

---
+++ elpa/typescript-mode-20171205.529/typescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typescript-mode" "20171205.529" "Major mode for editing typescript" 'nil :commit "257326695531eb3320403a8624b7179b71fd1103" :url "http://github.com/ananthakumaran/typescript.el" :keywords '("typescript" "languages"))

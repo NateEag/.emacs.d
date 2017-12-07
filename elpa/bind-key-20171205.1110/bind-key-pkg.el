10a3e964b48f222aa2b129e33eeb449cb724ab21
Nate Eagleson <nate@nateeag.com>
2017-12-07 11:48:15 -0500

    Update package bind-key-20171205.1110

--- elpa/bind-key-20171205.1110/bind-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bind-key" "20171205.1110" "A simple way to manage personal keybindings" 'nil :commit "9ce9c048a14967b4764872107c0ae9260febc44b" :url "https://github.com/jwiegley/use-package" :keywords '("keys" "keybinding" "config" "dotemacs"))

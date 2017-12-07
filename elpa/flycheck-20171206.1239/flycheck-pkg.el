8db7fbdf9cf616895c3058fa906a6806e745f538
Nate Eagleson <nate@nateeag.com>
2017-12-07 11:48:13 -0500

    Update package flycheck-20171205.111

--- elpa/flycheck-20171206.1239/flycheck-pkg.el
+++ elpa/flycheck-20171206.1239/flycheck-pkg.el
@@ -1,4 +1,4 @@
-(define-package "flycheck" "20171205.111" "On-the-fly syntax checking"
+(define-package "flycheck" "20171206.1239" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")

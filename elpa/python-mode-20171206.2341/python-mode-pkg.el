35c60cb519457727184bd938558a4fe4cb31c551
Nate Eagleson <nate@nateeag.com>
2017-12-08 08:48:27 -0500

    Update package python-mode-20171206.2341

--- elpa/python-mode-20171206.2341/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20171206.2341" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/python-mode-20171208.218/python-mode-autoloads.el
+++ elpa/python-mode-20171208.218/python-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "python-mode" "python-mode.el" (23081 28868
+;;;### (autoloads nil "python-mode" "python-mode.el" (23082 38952
 ;;;;;;  0 0))
 ;;; Generated autoloads from python-mode.el
 
@@ -224,7 +224,7 @@ Sets basic comint variables, see also versions-related stuff in `py-shell'.
 
 ;;;***
 
-;;;### (autoloads nil nil ("python-mode-pkg.el") (23081 28868 0 0))
+;;;### (autoloads nil nil ("python-mode-pkg.el") (23082 38952 0 0))
 
 ;;;***
 

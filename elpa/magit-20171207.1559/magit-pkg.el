00686865553c3ff24d0d0f97295c0e12fa508600
Nate Eagleson <nate@nateeag.com>
2017-12-08 08:48:24 -0500

    Update package magit-20171207.339

--- elpa/magit-20171207.1559/magit-pkg.el
+++ elpa/magit-20171207.1559/magit-pkg.el
@@ -1,7 +1,9 @@
-(define-package "magit" "20171207.339" "A Git porcelain inside Emacs"
+(define-package "magit" "20171207.1559" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20170823")
     (dash "20170810")
+    (ghub "20171204")
+    (let-alist "1.0.5")
     (with-editor "20170817")
     (git-commit "20170823")
     (magit-popup "20171121"))

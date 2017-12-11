2a32a377132fa33756fc87854f7794b668eccc0c
Nate Eagleson <nate@nateeag.com>
2017-12-11 09:44:38 -0500

    Update package use-package-20171208.44

--- elpa/use-package-20171210.2136/use-package-pkg.el
+++ elpa/use-package-20171210.2136/use-package-pkg.el
@@ -1,4 +1,4 @@
-(define-package "use-package" "20171208.44" "A configuration macro for simplifying your .emacs"
+(define-package "use-package" "20171210.2136" "A configuration macro for simplifying your .emacs"
   '((emacs "24.3")
     (bind-key "2.4"))
   :url "https://github.com/jwiegley/use-package" :keywords

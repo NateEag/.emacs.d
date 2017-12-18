1a0abe2d59143f5aaf709e28a83e6ead9e4fecfd
Nate Eagleson <nate@nateeag.com>
2017-12-18 08:40:37 -0500

    Update package typescript-mode-20171212.440

--- elpa/typescript-mode-20171213.541/typescript-mode-autoloads.el
+++ elpa/typescript-mode-20171213.541/typescript-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "typescript-mode" "typescript-mode.el" (23087
-;;;;;;  63311 0 0))
+;;;### (autoloads nil "typescript-mode" "typescript-mode.el" (23095
+;;;;;;  50516 0 0))
 ;;; Generated autoloads from typescript-mode.el
 
 (autoload 'typescript-mode "typescript-mode" "\
---
+++ elpa/typescript-mode-20171213.541/typescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typescript-mode" "20171213.541" "Major mode for editing typescript" 'nil :commit "861d5983b6894a101b6417ef3f4bd2999ea17ea1" :url "http://github.com/ananthakumaran/typescript.el" :keywords '("typescript" "languages"))

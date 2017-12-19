8bcbdbef057bf784bf4fa614dcac46d48295bef8
Nate Eagleson <nate@nateeag.com>
2017-12-19 09:02:44 -0500

    Install ac-capf package from MELPA

---
+++ elpa/ac-capf-20151031.1917/ac-capf-autoloads.el
@@ -0,0 +1,21 @@
+;;; ac-capf-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ac-capf" "ac-capf.el" (23097 6958 0 0))
+;;; Generated autoloads from ac-capf.el
+
+(autoload 'ac-capf-setup "ac-capf" "\
+Add `ac-source-capf' to `ac-sources' and enable `auto-complete' mode
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-capf-autoloads.el ends here

06de9619f6de9c82fa15d3d00515520b770f503c
Nate Eagleson <nate@nateeag.com>
2017-12-20 14:21:05 -0500

    Update package helm-core-20171130.2340

--- elpa/helm-core-20171220.105/helm-core-pkg.el
+++ elpa/helm-core-20171220.105/helm-core-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm-core" "20171130.2340" "Development files for Helm"
+(define-package "helm-core" "20171220.105" "Development files for Helm"
   '((emacs "24.4")
     (async "1.9.2"))
   :url "https://emacs-helm.github.io/helm/")

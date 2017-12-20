bcb7460b82fbd8d59d9a41b2d39fe07980e710d8
Nate Eagleson <nate@nateeag.com>
2017-12-20 14:21:04 -0500

    Update package browse-kill-ring-20171218.1410

--- elpa/browse-kill-ring-20171218.1410/browse-kill-ring-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "browse-kill-ring" "20171218.1410" "interactively insert items from kill-ring" 'nil :commit "b36d5a9e0f7dbf9cad147e52067e3bfe757cd271" :url "https://github.com/browse-kill-ring/browse-kill-ring" :keywords '("convenience"))

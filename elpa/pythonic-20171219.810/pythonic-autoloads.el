abb0e39d76ae2251369f4bcd6d5677eb0841ab55
Nate Eagleson <nate@nateeag.com>
2017-12-20 14:21:12 -0500

    Update package pythonic-20160221.1123

--- elpa/pythonic-20171219.810/pythonic-autoloads.el
+++ elpa/pythonic-20171219.810/pythonic-autoloads.el
@@ -1,9 +1,9 @@
 ;;; pythonic-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "pythonic" "pythonic.el" (22276 14454 0 0))
+;;;### (autoloads nil "pythonic" "pythonic.el" (23098 47144 0 0))
 ;;; Generated autoloads from pythonic.el
 
 (autoload 'pythonic-activate "pythonic" "\

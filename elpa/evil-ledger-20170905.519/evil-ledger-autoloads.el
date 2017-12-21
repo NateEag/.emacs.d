d4891f8753b920f1584a967e54f4c02e28210d26
Nate Eagleson <nate@nateeag.com>
2017-12-20 23:18:20 -0500

    Add evil-ledger package from MELPA

---
+++ elpa/evil-ledger-20170905.519/evil-ledger-autoloads.el
@@ -0,0 +1,26 @@
+;;; evil-ledger-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "evil-ledger" "evil-ledger.el" (23099 13821
+;;;;;;  0 0))
+;;; Generated autoloads from evil-ledger.el
+
+(autoload 'evil-ledger-mode "evil-ledger" "\
+Minor mode for more evil in `ledger-mode'.
+
+The following keys are available in `evil-ledger-mode':
+
+\\{evil-ledger-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-ledger-autoloads.el ends here

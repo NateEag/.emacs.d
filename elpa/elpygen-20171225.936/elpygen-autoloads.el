432880736ae9a63526958d047ee63ae5de7ec53a
Nate Eagleson <nate@nateeag.com>
2018-01-02 11:29:29 -0500

    Install elpygen from MELPA

---
+++ elpa/elpygen-20171225.936/elpygen-autoloads.el
@@ -0,0 +1,21 @@
+;;; elpygen-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "elpygen" "elpygen.el" (23115 45391 0 0))
+;;; Generated autoloads from elpygen.el
+
+(autoload 'elpygen-implement "elpygen" "\
+Implement a function or a method using the symbol name and call arguments under the point.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; elpygen-autoloads.el ends here
---
+++ elpa/elpygen-20171225.936/elpygen-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elpygen" "20171225.936" "Generate a Python function/method using a symbol under point" '((emacs "25") (yasnippet "0.8.0")) :commit "21929c997a05968f9eefe52b85a76ceaab3b0d81" :url "https://github.com/vkazanov/elpygen" :keywords '("python" "languages" "tools"))

4a411967d755810f539f7a9ecb6ebe478aa05a2b
Nate Eagleson <nate@nateeag.com>
2018-01-02 11:36:46 -0500

    Update package ace-window-20171211.921

---
+++ elpa/ace-window-20171228.1109/ace-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-window" "20171228.1109" "Quickly switch windows." '((avy "0.2.0")) :commit "c0fbdd51be8fdd43fd7a89140ebd4b4c311d45fc" :url "https://github.com/abo-abo/ace-window" :keywords '("window" "location"))

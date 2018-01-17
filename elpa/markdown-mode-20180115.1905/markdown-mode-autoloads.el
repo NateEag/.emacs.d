4e2ce1de2347ad4d64530dd39beca3e0b6277cec
Nate Eagleson <nate@nateeag.com>
2018-01-17 11:08:11 -0500

    Update package markdown-mode-20180101.503

--- elpa/markdown-mode-20180115.1905/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20180115.1905/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (23115 46389
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (23135 29930
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
@@ -20,6 +20,21 @@ Major mode for editing Markdown files.
 Major mode for editing GitHub Flavored Markdown files.
 
 \(fn)" t nil)
+
+(autoload 'markdown-view-mode "markdown-mode" "\
+Major mode for viewing Markdown content.
+
+\(fn)" t nil)
+
+(autoload 'gfm-view-mode "markdown-mode" "\
+Major mode for viewing GitHub Flavored Markdown content.
+
+\(fn)" t nil)
+
+(autoload 'markdown-live-preview-mode "markdown-mode" "\
+Toggle native previewing on save for a specific markdown file.
+
+\(fn &optional ARG)" t nil)
 
 ;;;***
 

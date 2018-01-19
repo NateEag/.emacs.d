6036de023067bc47370e827be9d378370bf2938b
Nate Eagleson <nate@nateeag.com>
2018-01-19 09:57:07 -0500

    Install lsp-php from MELPA
    I've been meaning to move my hack out to its own project for months now.
    
    Lo and behold, someone else has done it.

---
+++ elpa/lsp-php-20180104.152/lsp-php-autoloads.el
@@ -0,0 +1,22 @@
+;;; lsp-php-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "lsp-php" "lsp-php.el" (23138 842 0 0))
+;;; Generated autoloads from lsp-php.el
+
+(defvar lsp-php-server-install-dir (file-truename (locate-user-emacs-file "php-language-server/")) "\
+Install directory for php-language-server.
+The slash is expected at the end.")
+
+(custom-autoload 'lsp-php-server-install-dir "lsp-php" t)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-php-autoloads.el ends here

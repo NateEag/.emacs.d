dddb3c129d7d8dd9da91c7d2bfddfc8645b70f7f
Nate Eagleson <nate@nateeag.com>
2018-01-24 09:21:30 -0500

    Update package ace-window-20180119.1226

--- elpa/ace-window-20180123.1111/ace-window-autoloads.el
+++ elpa/ace-window-20180123.1111/ace-window-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ace-window" "ace-window.el" (23143 23475 0
+;;;### (autoloads nil "ace-window" "ace-window.el" (23144 38506 0
 ;;;;;;  0))
 ;;; Generated autoloads from ace-window.el
 

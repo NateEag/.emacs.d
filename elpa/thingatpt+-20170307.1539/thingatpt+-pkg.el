d25cf298510122b926532e1f7a89cb4c025f0a2f
Nate Eagleson <nate@nateeag.com>
2018-01-25 12:39:47 -0500

    Move thingatpt+.el from elpa/ to site-lisp/
    Another EmacsWiki package, and one I'm not even sure I use at all.
    
    I guess we'll find out.

--- elpa/thingatpt+-20170307.1539/thingatpt+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "thingatpt+" "20170307.1539" "Extensions to `thingatpt.el'." 'nil :url "https://www.emacswiki.org/emacs/download/thingatpt%2b.el" :keywords '("extensions" "matching" "mouse"))

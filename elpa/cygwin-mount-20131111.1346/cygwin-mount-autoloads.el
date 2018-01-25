6920d25747978644714ad43947d51dcb0ce156fc
Nate Eagleson <nate@nateeag.com>
2018-01-25 11:29:25 -0500

    Move cygwin-mount.el from elpa/ to site-lisp/
    Again, it was an EmacsWiki package, so it no longer exists in MELPA.

--- elpa/cygwin-mount-20131111.1346/cygwin-mount-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; cygwin-mount-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("cygwin-mount.el") (22010 5218 783942
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; cygwin-mount-autoloads.el ends here
--- elpa/cygwin-mount-20131111.1346/cygwin-mount-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cygwin-mount" "20131111.1346" "Teach EMACS about cygwin styles and mount points." 'nil :keywords '("files" "mount" "cygwin"))

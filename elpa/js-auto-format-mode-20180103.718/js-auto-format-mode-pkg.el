31531c9e820ff0dcbbbfd694915edf3086e7310e
Nate Eagleson <nate@nateeag.com>
2018-01-25 12:22:14 -0500

    Update package js-auto-format-mode-20180103.718

--- elpa/js-auto-format-mode-20180103.718/js-auto-format-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "js-auto-format-mode" "20180103.718" "Minor mode for auto-formatting JavaScript code" '((emacs "24")) :commit "37e83641fd5eab45e813e4bc74a835fe7229c160" :url "https://github.com/ybiquitous/js-auto-format-mode" :keywords '("languages"))

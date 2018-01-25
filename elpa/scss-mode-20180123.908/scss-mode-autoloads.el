7e98427d51e62a97d7ae34a0c74a66e7994a1317
Nate Eagleson <nate@nateeag.com>
2018-01-25 12:22:23 -0500

    Update package scss-mode-20150107.1400

--- elpa/scss-mode-20180123.908/scss-mode-autoloads.el
+++ elpa/scss-mode-20180123.908/scss-mode-autoloads.el
@@ -1,9 +1,9 @@
 ;;; scss-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "scss-mode" "scss-mode.el" (22446 30501 0 0))
+;;;### (autoloads nil "scss-mode" "scss-mode.el" (23146 4686 0 0))
 ;;; Generated autoloads from scss-mode.el
 
 (autoload 'scss-mode "scss-mode" "\

dc4e1c8900b04e67ab5fb715986235d6ff689337
Nate Eagleson <nate@nateeag.com>
2018-01-26 09:10:04 -0500

    Update package php-mode-20180123.752

--- elpa/php-mode-20180125.2352/php-mode-autoloads.el
+++ elpa/php-mode-20180125.2352/php-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "php-mode" "php-mode.el" (23146 4684 0 0))
+;;;### (autoloads nil "php-mode" "php-mode.el" (23147 14010 0 0))
 ;;; Generated autoloads from php-mode.el
 
 (let ((loads (get 'php 'custom-loads))) (if (member '"php-mode" loads) nil (put 'php 'custom-loads (cons '"php-mode" loads))))
@@ -61,7 +61,7 @@ Insert current namespace if cursor in namespace context.
 ;;;;;;  "php-exif.el" "php-ext.el" "php-filesystem.el" "php-gd.el"
 ;;;;;;  "php-math.el" "php-mode-pkg.el" "php-pcre.el" "php-regex.el"
 ;;;;;;  "php-simplexml.el" "php-strings.el" "php-var.el" "php-xmlparser.el"
-;;;;;;  "php-xmlreader.el") (23146 4684 0 0))
+;;;;;;  "php-xmlreader.el") (23147 14010 0 0))
 
 ;;;***
 

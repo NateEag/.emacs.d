d199d0004c89a56f22cd58dfa624ad7a441c4e7b
Nate Eagleson <nate@nateeag.com>
2018-01-29 14:49:49 -0500

    Install prog-fill from MELPA

---
+++ elpa/prog-fill-20180128.2019/prog-fill-autoloads.el
@@ -0,0 +1,24 @@
+;;; prog-fill-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "prog-fill" "prog-fill.el" (23151 27453 0 0))
+;;; Generated autoloads from prog-fill.el
+
+(autoload 'prog-fill "prog-fill" "\
+Split multi-argument call into one per line.
+
+TODO: Handle string quotations (do not break them apart).
+TODO: Handle different arg separators (Lisp style).
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; prog-fill-autoloads.el ends here
---
+++ elpa/prog-fill-20180128.2019/prog-fill-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "prog-fill" "20180128.2019" "Smartly format lines to use vertical space." '((emacs "25.1") (cl-lib "0.6.1")) :commit "ad38e2f6a45a8dd7eb08c407506256dd2e045de1" :url "https://github.com/ahungry/prog-fill" :keywords '("ahungry" "convenience" "c" "formatting" "editing"))

d48c9801eb713776e29ee09a901c778ede39d56e
Nate Eagleson <nate@nateeag.com>
2018-01-30 14:05:16 -0500

    Update package epl-20150517.433

--- elpa/epl-20180127.351/epl-autoloads.el
+++ elpa/epl-20180127.351/epl-autoloads.el
@@ -1,9 +1,9 @@
 ;;; epl-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("epl.el") (21850 30396 347676 0))
+;;;### (autoloads nil nil ("epl.el") (23152 49644 0 0))
 
 ;;;***
 

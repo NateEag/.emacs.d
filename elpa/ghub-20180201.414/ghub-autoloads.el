b45b2351222a72f3afc7f98a32575db32a496901
Nate Eagleson <nate@nateeag.com>
2018-02-01 09:39:49 -0500

    Update package ghub-20180130.807

--- elpa/ghub-20180201.414/ghub-autoloads.el
+++ elpa/ghub-20180201.414/ghub-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ghub" "ghub.el" (23152 49633 0 0))
+;;;### (autoloads nil "ghub" "ghub.el" (23155 9908 0 0))
 ;;; Generated autoloads from ghub.el
 
 (autoload 'ghub-create-token "ghub" "\
@@ -25,7 +25,7 @@ has to provide several values including their password.
 
 ;;;***
 
-;;;### (autoloads nil nil ("ghub-pkg.el") (23152 49633 0 0))
+;;;### (autoloads nil nil ("ghub-pkg.el") (23155 9908 0 0))
 
 ;;;***
 

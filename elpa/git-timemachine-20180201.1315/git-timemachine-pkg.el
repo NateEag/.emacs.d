70de6b27b13cc4514c4fff123857b916839a33a4
Nate Eagleson <nate@nateeag.com>
2018-02-05 09:22:31 -0500

    Update package git-timemachine-20170325.220

---
+++ elpa/git-timemachine-20180201.1315/git-timemachine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-timemachine" "20180201.1315" "Walk through git revisions of a file" '((emacs "24.3")) :commit "020d02cd77df6bf6f0efd4d4c597aad2083b6302" :url "https://github.com/pidu/git-timemachine" :keywords '("git"))

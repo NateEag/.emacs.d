bdbb0263e45e41023931352435b250b5550b2fb8
Nate Eagleson <nate@nateeag.com>
2018-02-12 09:16:51 -0500

    Add helm-rg package from MELPA
    I've been vaguely wondering if ripgrep would be a noticeably
    faster/better experience than The Silver Searcher, since it's
    technically faster.
    
    Initial tests say "no, not really", but I figure it's worth pulling the
    package in, anyway.

---
+++ elpa/helm-rg-20180205.2231/helm-rg-autoloads.el
@@ -0,0 +1,23 @@
+;;; helm-rg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-rg" "helm-rg.el" (23169 41187 0 0))
+;;; Generated autoloads from helm-rg.el
+
+(autoload 'helm-rg "helm-rg" "\
+Search for the perl regexp RG-PATTERN extremely quickly with ripgrep.
+
+\\{helm-rg-map}
+
+\(fn RG-PATTERN &optional PFX)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-rg-autoloads.el ends here

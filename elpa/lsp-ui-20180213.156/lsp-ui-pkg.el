27c9b880967de72361d40a460753611eb53e9385
Nate Eagleson <nate@nateeag.com>
2018-02-13 11:18:05 -0500

    Update package lsp-ui-20180209.1302

--- elpa/lsp-ui-20180213.156/lsp-ui-pkg.el
+++ elpa/lsp-ui-20180213.156/lsp-ui-pkg.el
@@ -1,8 +1,9 @@
-(define-package "lsp-ui" "20180209.1302" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20180213.156" "UI modules for lsp-mode"
   '((emacs "25.1")
     (dash "2.13")
     (flycheck "31")
-    (lsp-mode "3.4"))
+    (lsp-mode "3.4")
+    (markdown-mode "2.3"))
   :url "https://github.com/emacs-lsp/lsp-ui" :keywords
   '("lsp"))
 ;; Local Variables:

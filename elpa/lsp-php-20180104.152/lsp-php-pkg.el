d1bd5255c086db75d02c65f95b5c1d43fb65d6e7
Nate Eagleson <nate@nateeag.com>
2018-02-19 11:43:18 -0500

    Update package lsp-php-20180104.152

--- elpa/lsp-php-20180104.152/lsp-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-php" "20180104.152" "php support for lsp-mode" '((emacs "25.1") (lsp-mode "3.0")) :commit "64637be7c5c16a55660e208e2e8e8977ed6e22ef" :url "https://github.com/tszg/lsp-php" :keywords '("convenience" "php"))

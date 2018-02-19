9c2cf167a37a6952ddf44f042d2e18c8970809cc
Nate Eagleson <nate@nateeag.com>
2018-02-19 11:43:30 -0500

    Update package simple-httpd-20180205.1031

--- elpa/simple-httpd-20180218.538/simple-httpd-autoloads.el
+++ elpa/simple-httpd-20180218.538/simple-httpd-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "simple-httpd" "simple-httpd.el" (23161 45595
+;;;### (autoloads nil "simple-httpd" "simple-httpd.el" (23178 65202
 ;;;;;;  0 0))
 ;;; Generated autoloads from simple-httpd.el
 

682860bd1503ce118ff2d528c56505253359e287
Nate Eagleson <nate@nateeag.com>
2018-02-21 12:01:08 -0500

    Remove current evil-state from modeline

--- custom.el
+++ custom.el
@@ -37,6 +37,7 @@
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(debug-on-quit nil)
  '(delete-selection-mode t)
+ '(evil-mode-line-format nil nil nil "Since I use a block cursor in normal mode and a bar for insert, I don't need to see my evil-state in the modeline.")
  '(evil-want-fine-undo t)
  '(exec-path-from-shell-arguments (quote ("-l")))
  '(exec-path-from-shell-variables (quote ("PATH" "MANPATH" "EMAIL")) nil nil "Since I use notmuch and mbsync at work and home, it's convenient to have Emacs pick up my EMAIL var.")

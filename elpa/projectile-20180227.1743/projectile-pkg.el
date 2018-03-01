521c6edb60e4554db8cbc976cd09d84fb2c63098
Nate Eagleson <nate@nateeag.com>
2018-03-01 09:59:24 -0500

    Update package projectile-20180128.655

---
+++ elpa/projectile-20180227.1743/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile" "20180227.1743" "Manage and navigate projects in Emacs easily" '((emacs "24.3") (pkg-info "0.4")) :commit "40b01b9d61ed310737d87e86b226bb4e1ed2e0cb" :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))

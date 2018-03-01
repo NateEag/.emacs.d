4ccf2574bdf8f1d24988c6b08861153550ea2630
Nate Eagleson <nate@nateeag.com>
2018-03-01 09:59:03 -0500

    Update package cquery-20180226.1038

--- elpa/cquery-20180227.2156/cquery-pkg.el
+++ elpa/cquery-20180227.2156/cquery-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cquery" "20180226.1038" "cquery client for lsp-mode"
+(define-package "cquery" "20180227.2156" "cquery client for lsp-mode"
   '((emacs "25.1")
     (lsp-mode "3.4")
     (dash "0.13"))

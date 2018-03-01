606896d0e5658fc2c5f57e4cc2893acb32d917c8
Nate Eagleson <nate@nateeag.com>
2018-03-01 09:59:28 -0500

    Update package tide-20180212.2229

--- elpa/tide-20180301.121/tide-pkg.el
+++ elpa/tide-20180301.121/tide-pkg.el
@@ -1,4 +1,4 @@
-(define-package "tide" "20180212.2229" "Typescript Interactive Development Environment"
+(define-package "tide" "20180301.121" "Typescript Interactive Development Environment"
   '((dash "2.10.0")
     (s "1.11.0")
     (flycheck "27")

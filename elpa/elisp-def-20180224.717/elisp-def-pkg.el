ee99013a3505578eae646f37dbba5536ea824b09
Nate Eagleson <nate@nateeag.com>
2018-03-02 09:41:01 -0500

    Install elisp-def-autoloads from MELPA

---
+++ elpa/elisp-def-20180224.717/elisp-def-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elisp-def" "20180224.717" "macro-aware go-to-definition for elisp" '((dash "2.12.0") (f "0.19.0") (s "1.11.0") (emacs "24.3")) :commit "120c4dd30e00dfcc9bb4552fc812c0f3be5a8dc8" :keywords '("lisp"))

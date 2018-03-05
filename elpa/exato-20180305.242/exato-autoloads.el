615f95b7484b03b359408b005b4331c935acbeb6
Nate Eagleson <nate@nateeag.com>
2018-03-05 10:31:29 -0500

    Install exato from MELPA

---
+++ elpa/exato-20180305.242/exato-autoloads.el
@@ -0,0 +1,15 @@
+;;; exato-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("exato.el") (23197 25281 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; exato-autoloads.el ends here

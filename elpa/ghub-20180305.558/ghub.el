a6fee65bd107e940b79aa5ae927215eec0ec6b42
Nate Eagleson <nate@nateeag.com>
2018-03-05 09:18:31 -0500

    Update package ghub-20180221.1319

--- elpa/ghub-20180305.558/ghub.el
+++ elpa/ghub-20180305.558/ghub.el
@@ -27,11 +27,11 @@
 ;; from Emacs packages.  It abstracts access to API resources using only
 ;; a handful of functions that are not resource-specific.
 
-;; It also handles the creation, storage and use of access tokens using a
-;; setup wizard, to make it easier for users to get started and to reduce
+;; Ghub handles the creation, storage and use of access tokens using a
+;; setup wizard to make it easier for users to get started and to reduce
 ;; the support burden imposed on package maintainers.  It also comes with
 ;; a comprehensive manual to address the cases when things don't just
-;; work as expected.
+;; work as expected or in case you don't want to use the wizard.
 
 ;; Ghub is intentionally limited to only provide these two essential
 ;; features — basic request functions and guided setup — to avoid being
@@ -369,7 +369,7 @@ behave like for `ghub-request' (which see)."
   (cl-assert (not (stringp variables)))
   (ghub-request "POST" "/graphql" nil :payload
                 (json-encode `(("query" . ,graphql)
-                               ,@(and variables `(("variables" ,variables)))))
+                               ,@(and variables `(("variables" ,@variables)))))
                 :username username :auth auth :host host))
 
 ;;;; Internal

1d1340ceb6ed8a4d74b0fc9ea4dad2af0bbe2bff
Nate Eagleson <nate@nateeag.com>
2018-03-05 09:18:17 -0500

    Update package direnv-20180227.1306

--- elpa/direnv-20180301.842/direnv-autoloads.el
+++ elpa/direnv-20180301.842/direnv-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "direnv" "direnv.el" (23192 5431 0 0))
+;;;### (autoloads nil "direnv" "direnv.el" (23197 20905 0 0))
 ;;; Generated autoloads from direnv.el
 
 (autoload 'direnv-update-environment "direnv" "\

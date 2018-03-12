4548c15f77fbb53b609963219a3004451a17706b
Nate Eagleson <nate@nateeag.com>
2018-03-12 09:44:50 -0400

    Update package use-package-20180305.1009

--- elpa/use-package-20180311.2137/use-package-pkg.el
+++ elpa/use-package-20180311.2137/use-package-pkg.el
@@ -1,4 +1,4 @@
-(define-package "use-package" "20180305.1009" "A configuration macro for simplifying your .emacs"
+(define-package "use-package" "20180311.2137" "A configuration macro for simplifying your .emacs"
   '((emacs "24.3")
     (bind-key "2.4"))
   :url "https://github.com/jwiegley/use-package" :keywords

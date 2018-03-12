96aac4411917f2c3be6ef6fe19477e4122545a2c
Nate Eagleson <nate@nateeag.com>
2018-03-12 09:44:37 -0400

    Update package pip-requirements-20171109.1439

---
+++ elpa/pip-requirements-20180306.1522/pip-requirements-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pip-requirements" "20180306.1522" "A major mode for editing pip requirements files." '((dash "2.8.0")) :commit "88c764ad5d988a3ed0d2b6f0b7dd22308b03a058")

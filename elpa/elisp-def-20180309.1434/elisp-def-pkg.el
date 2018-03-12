647ce175cfd9f808f3e68a634c4da6edfc5177ae
Nate Eagleson <nate@nateeag.com>
2018-03-12 09:44:14 -0400

    Update package elisp-def-20180302.1402

---
+++ elpa/elisp-def-20180309.1434/elisp-def-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elisp-def" "20180309.1434" "macro-aware go-to-definition for elisp" '((dash "2.12.0") (f "0.19.0") (s "1.11.0") (emacs "24.3")) :commit "4c300ea67594892171076f49df7c56f25e35d0ff" :keywords '("lisp"))

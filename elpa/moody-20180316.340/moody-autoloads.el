ae072fa4d956c1d4370d166e69c2fa0e03385135
Nate Eagleson <nate@nateeag.com>
2018-03-16 10:48:10 -0400

    Install moody package from MELPA

---
+++ elpa/moody-20180316.340/moody-autoloads.el
@@ -0,0 +1,31 @@
+;;; moody-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "moody" "moody.el" (23211 49300 0 0))
+;;; Generated autoloads from moody.el
+
+(autoload 'moody-replace-mode-line-buffer-identification "moody" "\
+
+
+\(fn &optional REVERSE)" t nil)
+
+(autoload 'moody-replace-sml/mode-line-buffer-identification "moody" "\
+
+
+\(fn &optional REVERSE)" t nil)
+
+(autoload 'moody-replace-vc-mode "moody" "\
+
+
+\(fn &optional REVERSE)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; moody-autoloads.el ends here
---
+++ elpa/moody-20180316.340/moody-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "moody" "20180316.340" "Tabs and ribbons for the mode line" '((emacs "25.3")) :commit "890218c6c175f6f3efbf584dd27b8702a16b8272" :url "https://github.com/tarsius/moody")

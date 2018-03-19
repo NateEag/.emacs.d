2b2cd5daa958c69e9fd5380dbcf8a2a5bc67247a
Nate Eagleson <nate@nateeag.com>
2018-03-19 09:43:39 -0400

    Update package gitattributes-mode-20170118.1613

--- elpa/gitattributes-mode-20170118.1613/gitattributes-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gitattributes-mode" "20170118.1613" "Major mode for editing .gitattributes files" 'nil :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))

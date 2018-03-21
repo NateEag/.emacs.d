bd2cf83b1a9da1f7724ee20c33febfad4bfbf2fb
Nate Eagleson <nate@nateeag.com>
2018-03-21 08:44:15 -0400

    Update package yasnippet-20180318.639

--- elpa/yasnippet-20180318.639/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet" "20180318.639" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :commit "a0d2cff79b188b7a23565ff2cfafbb8cf364616d" :url "http://github.com/joaotavora/yasnippet" :keywords '("convenience" "emulation"))
---
+++ elpa/yasnippet-20180319.607/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yasnippet" "20180319.607" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :commit "fa6a71c42de7fecd58193e2fd3681a0225b71dc5" :url "http://github.com/joaotavora/yasnippet" :keywords '("convenience" "emulation"))

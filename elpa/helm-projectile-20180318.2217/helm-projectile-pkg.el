b5a5adee9a25ef1821d2defa115243e85490e6f6
Nate Eagleson <nate@nateeag.com>
2018-03-21 08:44:02 -0400

    Update package helm-projectile-20180318.2217

--- elpa/helm-projectile-20180318.2217/helm-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-projectile" "20180318.2217" "Helm integration for Projectile" '((helm "1.7.7") (projectile "0.14.0") (cl-lib "0.3")) :commit "b934857b87b4c985cd07b0135322929d13cb9091" :url "https://github.com/bbatsov/helm-projectile" :keywords '("project" "convenience"))
---
+++ elpa/helm-projectile-20180319.1414/helm-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-projectile" "20180319.1414" "Helm integration for Projectile" '((helm "1.7.7") (projectile "0.14.0") (cl-lib "0.3")) :commit "c59dfda92776290947e855798286f4b46418ec9a" :url "https://github.com/bbatsov/helm-projectile" :keywords '("project" "convenience"))

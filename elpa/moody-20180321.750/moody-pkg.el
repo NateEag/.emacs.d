eda0a94340f591094ebc9c60a7161e1f9f8ad839
Nate Eagleson <nate@nateeag.com>
2018-03-23 10:27:17 -0400

    Update package moody-20180318.1329

---
+++ elpa/moody-20180321.750/moody-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "moody" "20180321.750" "Tabs and ribbons for the mode line" '((emacs "25.3")) :commit "781635d7c66199ee4c98107604cf2db90f1deec8" :url "https://github.com/tarsius/moody")

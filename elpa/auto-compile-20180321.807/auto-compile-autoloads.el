aa5f23e57a24ff6e96fa6f3b45048cfd3ddac156
Nate Eagleson <nate@nateeag.com>
2018-03-23 10:26:46 -0400

    Update package auto-compile-20180318.1221

--- elpa/auto-compile-20180321.807/auto-compile-autoloads.el
+++ elpa/auto-compile-20180321.807/auto-compile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "auto-compile" "auto-compile.el" (23215 48776
+;;;### (autoloads nil "auto-compile" "auto-compile.el" (23221 3749
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-compile.el
 

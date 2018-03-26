326f426d9f54fd1b69cf0b83b8abba05bcf1ce94
Nate Eagleson <nate@nateeag.com>
2018-03-26 09:50:05 -0400

    Add auth-source-pass package from MELPA

---
+++ elpa/auth-source-pass-20180326.8/auth-source-pass-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auth-source-pass" "20180326.8" "Integrate auth-source with password-store" '((emacs "25")) :commit "5690092e40c790384692d8e8da3451e6878d8c17" :url "https://github.com/DamienCassou/auth-password-store")

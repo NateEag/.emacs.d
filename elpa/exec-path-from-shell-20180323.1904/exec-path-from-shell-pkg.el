e8f9e137c06bbd445b251d6dbe52c23b21fdd854
Nate Eagleson <nate@nateeag.com>
2018-03-28 08:52:26 -0400

    Update package exec-path-from-shell-20180224.1916

---
+++ elpa/exec-path-from-shell-20180323.1904/exec-path-from-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "exec-path-from-shell" "20180323.1904" "Get environment variables such as $PATH from the shell" 'nil :commit "4d0af1274797ce61f5d8c209339d5b9cdc49aca6" :url "https://github.com/purcell/exec-path-from-shell" :keywords '("unix" "environment"))

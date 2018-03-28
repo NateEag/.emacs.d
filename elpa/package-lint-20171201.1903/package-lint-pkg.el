2cc1aebdeb8e1299276a2ab0ecb3ae46ccf3582f
Nate Eagleson <nate@nateeag.com>
2018-03-28 08:52:38 -0400

    Update package package-lint-20171201.1903

--- elpa/package-lint-20171201.1903/package-lint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "package-lint" "20171201.1903" "A linting library for elisp package authors" '((cl-lib "0.5") (emacs "24")) :commit "9abfb14d9ad903ef73895a27b9964b5e6023d752" :url "https://github.com/purcell/package-lint" :keywords '("lisp"))
---
+++ elpa/package-lint-20180323.1852/package-lint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package-lint" "20180323.1852" "A linting library for elisp package authors" '((cl-lib "0.5") (emacs "24")) :commit "5ac4122cf1e24649b387bec8630740cd4ec85b72" :url "https://github.com/purcell/package-lint" :keywords '("lisp"))

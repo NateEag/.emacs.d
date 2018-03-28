f9503c3df2f8fddf3e9ae7fc7eb2912f50562bdd
Nate Eagleson <nate@nateeag.com>
2018-03-28 08:52:49 -0400

    Update package web-mode-20180313.1458

---
+++ elpa/web-mode-20180326.451/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web-mode" "20180326.451" "major mode for editing web templates" '((emacs "23.1")) :commit "bb1f200d4d9ac19ea6997c8d52c5a6c329e6dd24" :url "http://web-mode.org" :keywords '("languages"))

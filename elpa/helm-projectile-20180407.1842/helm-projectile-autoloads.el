0d5a826293108694f85d62e3b16f168029b149d4
Nate Eagleson <nate@nateeag.com>
2018-04-09 11:28:57 -0400

    Update package helm-projectile-20180324.1632

--- elpa/helm-projectile-20180407.1842/helm-projectile-autoloads.el
+++ elpa/helm-projectile-20180407.1842/helm-projectile-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (23227
-;;;;;;  36875 0 0))
+;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (23243
+;;;;;;  34488 0 0))
 ;;; Generated autoloads from helm-projectile.el
 
 (defvar helm-projectile-fuzzy-match t "\
---
+++ elpa/helm-projectile-20180407.1842/helm-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-projectile" "20180407.1842" "Helm integration for Projectile" '((helm "1.9.9") (projectile "0.14.0") (cl-lib "0.3")) :commit "213339b896eebff6d61778b2b1816301ab583081" :keywords '("project" "convenience") :url "https://github.com/bbatsov/helm-projectile")

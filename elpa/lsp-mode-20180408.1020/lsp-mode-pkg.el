3041cefce3b3696e8e033586a6ee6865f1a8e7fc
Nate Eagleson <nate@nateeag.com>
2018-04-09 11:29:13 -0400

    Update package lsp-mode-20180330.2309

--- elpa/lsp-mode-20180408.1020/lsp-mode-pkg.el
+++ elpa/lsp-mode-20180408.1020/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20180330.2309" "Minor mode for interacting with Language Servers"
+(define-package "lsp-mode" "20180408.1020" "Minor mode for interacting with Language Servers"
   '((emacs "25.1"))
   :url "https://github.com/emacs-lsp/lsp-mode")
 ;; Local Variables:

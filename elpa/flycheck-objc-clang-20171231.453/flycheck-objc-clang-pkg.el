80583b0ce4e1941da772fd68f8d1232605921029
Nate Eagleson <nate@nateeag.com>
2018-04-12 09:32:03 -0400

    Update package flycheck-objc-clang-20171231.453

--- elpa/flycheck-objc-clang-20171231.453/flycheck-objc-clang-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-objc-clang" "20171231.453" "Flycheck: Objective-C support using Clang" '((emacs "24.4") (flycheck "26")) :commit "07f17d1dbe878fdcabac791a8916ddf643571a68" :url "https://github.com/GyazSquare/flycheck-objc-clang" :keywords '("convenience" "languages" "tools"))
--- elpa/flycheck-objc-clang-20180410.422/flycheck-objc-clang-autoloads.el
+++ elpa/flycheck-objc-clang-20180410.422/flycheck-objc-clang-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "flycheck-objc-clang" "flycheck-objc-clang.el"
-;;;;;;  (23115 46372 0 0))
+;;;;;;  (23247 24530 0 0))
 ;;; Generated autoloads from flycheck-objc-clang.el
 
 (autoload 'flycheck-objc-clang-setup "flycheck-objc-clang" "\

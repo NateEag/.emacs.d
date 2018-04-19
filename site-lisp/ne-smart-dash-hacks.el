30b8eb8dfa1f3fbc7bcd2fdde2ffec11154146cd
Nate Eagleson <nate@nateeag.com>
2018-04-19 16:15:54 -0400

    Fix painfully stupid typo in site-lisp/ne-smart-dash-hacks.el
    That wasn't my finest moment, for sure.
    
    ...I should consider writing some tests for this thing, maybe.

--- site-lisp/ne-smart-dash-hacks.el
+++ site-lisp/ne-smart-dash-hacks.el
@@ -38,11 +38,11 @@
           ;; something like that, though, if you want dashes to be used in
           ;; command names, because otherwise you can't distinguish between "I
           ;; am typing a command" and "I am typing a variable name."
-          (thing-at-point-looking-at (concat "[[:space]]*" bash-var-chars "*="))
+          (thing-at-point-looking-at (concat "[[:space:]]*" bash-var-chars "*="))
 
           ;; Return true when editing an established variable name. Alas,
           ;; smart-dash-mode itself seems to return a - in that case.
-          (thing-at-point-looking-at (concat "[[:space]]*" bash-var-chars "*=.*"))
+          (thing-at-point-looking-at (concat "[[:space:]]*" bash-var-chars "*=.*"))
 
           (thing-at-point-looking-at (concat "\\${?" bash-var-chars))
           ;; If thing-at-point has an underscore in it, we should probably keep

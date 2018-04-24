5024e33c44e3c2642db230f98bcb5b9ded666086
Nate Eagleson <nate@nateeag.com>
2018-04-24 09:11:32 -0400

    Update package atomic-chrome-20180322.301

---
+++ elpa/format-all-20180421.156/format-all-autoloads.el
@@ -0,0 +1,41 @@
+;;; format-all-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "format-all" "format-all.el" (23263 11392 0
+;;;;;;  0))
+;;; Generated autoloads from format-all.el
+
+(autoload 'format-all-buffer "format-all" "\
+Auto-format the source code in the current buffer.
+
+No disk files are touched - the buffer doesn't even need to be
+saved.  If you don't like the results of the formatting, you can
+use ordinary undo to get your code back to its previous state.
+
+You will need to install external programs to do the formatting.
+If the command can't find the program that it needs, it will try
+to tell you how you might be able to install it on your operating
+system.  Only Emacs Lisp is formatted without an external program.
+
+A suitable formatter is selected according to the `major-mode' of
+the buffer.  Many popular programming languages are supported,
+but not all of them by any means, so unfortunately it's still
+likely that your favorite language is missing.  It is fairly easy
+to add new languages that have an external formatter.
+
+Any errors/warnings encountered during formatting are shown in a
+buffer called *format-all-errors*.  If the formatter made any
+changes to the code, point is placed at the first change.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; format-all-autoloads.el ends here

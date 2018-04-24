49ed05a5483c07145afd017bf9eaaa5fb8f19960
Nate Eagleson <nate@nateeag.com>
2018-04-24 09:12:33 -0400

    Update package websocket-20180402.2042

--- elpa/websocket-20180402.2042/websocket-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "websocket" "20180402.2042" "Emacs WebSocket client and server" '((cl-lib "0.5")) :commit "e9d148fbb6425c35dc7f76cccdec70aba7fafacf" :keywords '("communication" "websocket" "server"))

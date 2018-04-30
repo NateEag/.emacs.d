343fe87fb4b52e63c6d2d1fb73ce1073b451d858
Nate Eagleson <nate@nateeag.com>
2018-04-30 09:47:10 -0400

    Update package format-all-20180421.156

--- elpa/format-all-20180421.156/format-all-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "format-all" "20180421.156" "Auto-format C, C++, JS, Python, Ruby, etc. source code" '((cl-lib "0.5")) :commit "406396b7ba006e26a23bd1b49a24c5866fad8bfd" :keywords '("languages" "util") :url "https://github.com/lassik/emacs-format-all-the-code")

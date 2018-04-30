70d0daee797e77c27d68fb9f76d35b56343f35fc
Nate Eagleson <nate@nateeag.com>
2018-04-30 09:47:27 -0400

    Update package pdf-tools-20180427.846

--- elpa/pdf-tools-20180428.827/pdf-tools-pkg.el
+++ elpa/pdf-tools-20180428.827/pdf-tools-pkg.el
@@ -1,4 +1,4 @@
-(define-package "pdf-tools" "20180427.846" "Support library for PDF documents."
+(define-package "pdf-tools" "20180428.827" "Support library for PDF documents."
   '((emacs "24.3")
     (tablist "0.70")
     (let-alist "1.0.4"))

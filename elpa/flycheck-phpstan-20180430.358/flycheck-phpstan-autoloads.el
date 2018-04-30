89fb78a673fa0e135a22d550ad15e70d8d201e51
Nate Eagleson <nate@nateeag.com>
2018-04-30 10:14:12 -0400

    Install flycheck-phpstan package

---
+++ elpa/flycheck-phpstan-20180430.358/flycheck-phpstan-autoloads.el
@@ -0,0 +1,15 @@
+;;; flycheck-phpstan-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-phpstan.el") (23271 9379 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-phpstan-autoloads.el ends here

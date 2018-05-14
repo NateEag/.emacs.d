74984f631838780e3c8db7770044a8a6dac1a6fc
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:25:23 -0400

    Install yasnippet-classic-snippets package

---
+++ elpa/yasnippet-classic-snippets-1.0.0/yasnippet-classic-snippets-autoloads.el
@@ -0,0 +1,19 @@
+;;; yasnippet-classic-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "yasnippet-classic-snippets" "yasnippet-classic-snippets.el"
+;;;;;;  (23289 35434 0 0))
+;;; Generated autoloads from yasnippet-classic-snippets.el
+
+(eval-after-load 'yasnippet '(unless (memq 'yasnippet-classic-snippets-dir yas-snippet-dirs) (add-to-list 'yas-snippet-dirs 'yasnippet-classic-snippets-dir t) (yas-load-directory yasnippet-classic-snippets-dir)))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-classic-snippets-autoloads.el ends here

7ba7598654ef66761e28ff3e8e2f1e782b33c5bb
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:19:47 -0400

    Update package direnv-20180410.203

--- elpa/direnv-20180410.203/direnv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "direnv" "20180410.203" "direnv support" '((emacs "24.4") (dash "2.12.0") (with-editor "2.5.10")) :commit "bb0de5e8f662c3f522b931c6e2c2340e4ed682af" :keywords '("direnv" "environment" "processes" "unix" "tools") :url "https://github.com/wbolster/emacs-direnv")

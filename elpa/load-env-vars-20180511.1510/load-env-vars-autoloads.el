3865943965f4240cb51a003df13ccdfa1aa9c1a8
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:25:56 -0400

    Install load-env-vars package

---
+++ elpa/load-env-vars-20180511.1510/load-env-vars-autoloads.el
@@ -0,0 +1,22 @@
+;;; load-env-vars-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "load-env-vars" "load-env-vars.el" (23289 35597
+;;;;;;  0 0))
+;;; Generated autoloads from load-env-vars.el
+
+(autoload 'load-env-vars "load-env-vars" "\
+Load environment variables found in FILE-PATH.
+
+\(fn FILE-PATH)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; load-env-vars-autoloads.el ends here

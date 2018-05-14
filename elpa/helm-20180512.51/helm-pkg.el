48f5438242ba00b7c7f684871ba5a8ea027ca383
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:20:10 -0400

    Update package helm-20180423.2200

--- elpa/helm-20180512.51/helm-pkg.el
+++ elpa/helm-20180512.51/helm-pkg.el
@@ -1,8 +1,8 @@
-(define-package "helm" "20180423.2200" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20180512.51" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9.3")
     (popup "0.5.3")
-    (helm-core "2.9.2"))
+    (helm-core "2.9.4"))
   :url "https://emacs-helm.github.io/helm/")
 ;; Local Variables:
 ;; no-byte-compile: t

1c433eac9498db37c9eb4c9ae794cde2c5a095ab
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:20:01 -0400

    Update package evil-20180428.1409

--- elpa/evil-20180506.1341/evil-autoloads.el
+++ elpa/evil-20180506.1341/evil-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "evil-core" "evil-core.el" (23271 7768 0 0))
+;;;### (autoloads nil "evil-core" "evil-core.el" (23289 36092 0 0))
 ;;; Generated autoloads from evil-core.el
  (autoload 'evil-mode "evil" nil t)
 
@@ -14,7 +14,7 @@
 ;;;;;;  "evil-ex.el" "evil-integration.el" "evil-jumps.el" "evil-macros.el"
 ;;;;;;  "evil-maps.el" "evil-pkg.el" "evil-repeat.el" "evil-search.el"
 ;;;;;;  "evil-states.el" "evil-types.el" "evil-vars.el" "evil.el")
-;;;;;;  (23271 7768 0 0))
+;;;;;;  (23289 36092 0 0))
 
 ;;;***
 

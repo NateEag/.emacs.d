1a660f719765e1c788d56700f443b26bab6b7b3e
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:59:03 -0400

    Uninstall yasnippet-classic-snippets package
    It doesn't work and causes a crash on my system.
    
    I looked for a place to file a bug but didn't find a place quickly so I
    gave up.

--- elpa/yasnippet-classic-snippets-1.0.0/yasnippet-classic-snippets-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet-classic-snippets" "1.0.0" "\"Classic\" yasnippet snippets" '((yasnippet "0.9.1")) :url "http://elpa.gnu.org/packages/yasnippet-classic-snippets.html" :keywords '("snippets"))

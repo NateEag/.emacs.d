7315170702a62067ad726a1c08b3db0537bac82f
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:20:03 -0400

    Update package helm-rg-20180220.731

---
+++ elpa/helm-rg-20180512.1323/helm-rg-autoloads.el
@@ -0,0 +1,48 @@
+;;; helm-rg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "helm-rg" "helm-rg.el" (23289 36098 0 0))
+;;; Generated autoloads from helm-rg.el
+
+(autoload 'helm-rg "helm-rg" "\
+Search for the PCRE regexp RG-PATTERN extremely quickly with ripgrep.
+
+When invoked interactively with a prefix argument, or when PFX is non-nil,
+set the cwd for the ripgrep process to `default-directory'. Otherwise use the
+cwd as described by `helm-rg-default-directory'.
+
+If PATHS is non-nil, ripgrep will search only those paths, relative to the
+process's cwd. Otherwise, the process's cwd will be searched.
+
+Note that ripgrep respects glob patterns from .gitignore, .rgignore, and .ignore
+files. This composes with the glob defined by `helm-rg-default-glob-string', or
+overridden with `helm-rg--set-glob', which is defined in `helm-rg-map'.
+
+The ripgrep command's help output can be printed into its own buffer for reference
+with the interactive command `helm-rg-display-help'.
+
+\\{helm-rg-map}
+
+\(fn RG-PATTERN &optional PFX PATHS)" t nil)
+
+(autoload 'helm-rg-display-help "helm-rg" "\
+Display a buffer with the ripgrep command's usage help.
+
+The help buffer will be reused if it was already created. A prefix argument when
+invoked interactively, or a non-nil value for PFX, will display the help buffer
+in the current window. Otherwise, if the help buffer is already being displayed
+in some window, select that window, or else display the help buffer with
+`pop-to-buffer'.
+
+\(fn &optional PFX)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-rg-autoloads.el ends here

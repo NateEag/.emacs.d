426beac5e6393725368c8c73cc9ac61f763a22a3
Nate Eagleson <nate@nateeag.com>
2018-05-14 09:20:51 -0400

    Update package vc-msg-20171106.1747

--- elpa/vc-msg-20171106.1747/vc-msg-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "vc-msg" "20171106.1747" "Show commit information of current line"
-  '((emacs "24.3")
-    (popup "0.5.0"))
-  :url "http://github.com/redguardtoo/vc-msg" :keywords
-  '("git" "vc" "svn" "hg" "messenger"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/vc-msg-20180507.1725/vc-msg-pkg.el
@@ -0,0 +1,9 @@
+(define-package "vc-msg" "20180507.1725" "Show commit information of current line"
+  '((emacs "24.3")
+    (popup "0.5.0"))
+  :keywords
+  '("git" "vc" "svn" "hg" "messenger")
+  :url "http://github.com/redguardtoo/vc-msg")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

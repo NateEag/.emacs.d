5460b1c72e7c9df3cd65e6090975acd45ca5a7af
Nate Eagleson <nate@nateeag.com>
2018-05-22 09:18:44 -0400

    Update package rainbow-mode-1.0

--- elpa/rainbow-mode-1.0.1/rainbow-mode-autoloads.el
+++ elpa/rainbow-mode-1.0.1/rainbow-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (23227 36889
+;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (23300 6323
 ;;;;;;  0 0))
 ;;; Generated autoloads from rainbow-mode.el
 

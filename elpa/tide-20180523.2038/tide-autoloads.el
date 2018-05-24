dd28c7a61e5e5ea006d39f650b2e9cff84b35301
Nate Eagleson <nate@nateeag.com>
2018-05-24 09:24:14 -0400

    Update package tide-20180515.1417

--- elpa/tide-20180523.2038/tide-autoloads.el
+++ elpa/tide-20180523.2038/tide-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "tide" "tide.el" (23296 4717 0 0))
+;;;### (autoloads nil "tide" "tide.el" (23302 48382 0 0))
 ;;; Generated autoloads from tide.el
 
 (autoload 'company-tide "tide" "\
@@ -57,7 +57,7 @@ timeout.
 
 ;;;***
 
-;;;### (autoloads nil nil ("tide-lv.el" "tide-pkg.el") (23296 4717
+;;;### (autoloads nil nil ("tide-lv.el" "tide-pkg.el") (23302 48382
 ;;;;;;  0 0))
 
 ;;;***

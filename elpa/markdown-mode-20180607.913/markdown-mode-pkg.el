35954a57e8b504979014997a88d6fe7f7207b7a9
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:58:08 -0400

    Update package markdown-mode-20180124.138

---
+++ elpa/markdown-mode-20180607.913/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20180607.913" "Major mode for Markdown-formatted text" '((emacs "24.4") (cl-lib "0.5")) :commit "13a4f8e68da4ea5ef6151f8f6852c176e145fdcc" :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")

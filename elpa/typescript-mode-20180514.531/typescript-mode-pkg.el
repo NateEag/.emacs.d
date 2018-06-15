c211c2abb346b4021b172f5de7f46da98b9ac79d
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:58:47 -0400

    Update package typescript-mode-20180514.531

--- elpa/typescript-mode-20180514.531/typescript-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "typescript-mode" "20180514.531" "Major mode for editing typescript" 'nil :commit "7a5c74d88e3c5513cc4431a837003736f905a75e" :keywords '("typescript" "languages") :url "http://github.com/ananthakumaran/typescript.el")

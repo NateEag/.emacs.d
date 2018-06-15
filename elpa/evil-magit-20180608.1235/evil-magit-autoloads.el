029ab6d06d8e8f29ec344ce4eefc56eb672b90c6
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:57:47 -0400

    Update package evil-magit-20180522.741

--- elpa/evil-magit-20180608.1235/evil-magit-autoloads.el
+++ elpa/evil-magit-20180608.1235/evil-magit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "evil-magit" "evil-magit.el" (23302 48364 0
+;;;### (autoloads nil "evil-magit" "evil-magit.el" (23332 10314 0
 ;;;;;;  0))
 ;;; Generated autoloads from evil-magit.el
 

7303329b04117b3cbeeb6b91dde65ce6c2afc702
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:58:27 -0400

    Update package simple-httpd-20180218.538

--- elpa/simple-httpd-20180218.538/simple-httpd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "simple-httpd" "20180218.538" "pure elisp HTTP server" '((cl-lib "0.3")) :commit "c252765de191441cc25068b35bdff6a912745268" :url "https://github.com/skeeto/emacs-http-server")

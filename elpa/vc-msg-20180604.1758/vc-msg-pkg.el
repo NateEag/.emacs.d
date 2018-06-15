11f3efceae7567dd5723cee47328759baa9c1069
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:58:50 -0400

    Update package vc-msg-20180507.1725

--- elpa/vc-msg-20180604.1758/vc-msg-pkg.el
+++ elpa/vc-msg-20180604.1758/vc-msg-pkg.el
@@ -1,4 +1,4 @@
-(define-package "vc-msg" "20180507.1725" "Show commit information of current line"
+(define-package "vc-msg" "20180604.1758" "Show commit information of current line"
   '((emacs "24.3")
     (popup "0.5.0"))
   :keywords

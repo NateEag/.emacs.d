81f2af78506203b387394b6ebcab9650813fc18b
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:57:57 -0400

    Update package lsp-javascript-typescript-20180521.2205

--- elpa/lsp-javascript-typescript-20180521.2205/lsp-javascript-typescript-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; lsp-javascript-typescript-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("lsp-javascript-typescript.el") (23300
-;;;;;;  6318 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lsp-javascript-typescript-autoloads.el ends here
--- elpa/lsp-javascript-typescript-20180521.2205/lsp-javascript-typescript-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-javascript-typescript" "20180521.2205" "Javascript/Typescript support for lsp-mode" '((lsp-mode "3.0") (typescript-mode "0.1") (emacs "25.1")) :commit "e570c890870e6179ed7ed580bcafd9cb72ad4d2c" :keywords '("languages" "tools") :url "https://github.com/emacs-lsp/lsp-javascript")

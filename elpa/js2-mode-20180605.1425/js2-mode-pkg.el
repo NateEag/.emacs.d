64e980f3a989479fadb62306482ad9749e41df70
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:58:26 -0400

    Update package js2-mode-20180521.1523

--- elpa/js2-mode-20180605.1425/js2-mode-pkg.el
+++ elpa/js2-mode-20180605.1425/js2-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "js2-mode" "20180521.1523" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20180605.1425" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
   :keywords

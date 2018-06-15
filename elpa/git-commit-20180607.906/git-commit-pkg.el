64a99b63aaaa1395aa0a31b14561dfd9e04f6c1d
Nate Eagleson <nate@nateeag.com>
2018-06-15 16:58:05 -0400

    Update package magit-20180523.748

---
+++ elpa/git-commit-20180607.906/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180607.906" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "33e95da320857f9a6e50cb39627e0267361c99e2" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")

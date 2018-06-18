84207044a7f65b084ca466687906092a9098ee7c
Nate Eagleson <nate@nateeag.com>
2018-06-18 09:38:07 -0400

    Update package rjsx-mode-20180418.647

--- elpa/rjsx-mode-20180418.647/rjsx-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rjsx-mode" "20180418.647" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "263a5fc3595dbc6caf4b66750772bef3e6de723a" :keywords '("languages") :url "https://github.com/felipeochoa/rjsx-mode/")
---
+++ elpa/rjsx-mode-20180616.647/rjsx-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rjsx-mode" "20180616.647" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "37e004b3001932815088a02a0afa5bcf431e1960" :keywords '("languages") :url "https://github.com/felipeochoa/rjsx-mode/")

5c8e26b44d0b5a67025324c21e93f6156b3bf0fb
Nate Eagleson <nate@nateeag.com>
2018-06-28 15:17:36 -0400

    Update package lsp-ui-20180617.1746

--- elpa/lsp-ui-20180618.1951/lsp-ui-pkg.el
+++ elpa/lsp-ui-20180618.1951/lsp-ui-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-ui" "20180617.1746" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20180618.1951" "UI modules for lsp-mode"
   '((emacs "25.1")
     (dash "2.13")
     (dash-functional "1.2.0")

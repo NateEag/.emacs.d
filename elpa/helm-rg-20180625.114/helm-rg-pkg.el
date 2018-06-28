1af7dc1205470bcd1b5aa66e3c2e91c4ff9845d9
Nate Eagleson <nate@nateeag.com>
2018-06-28 15:17:30 -0400

    Update package helm-rg-20180615.1556

---
+++ elpa/helm-rg-20180625.114/helm-rg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-rg" "20180625.114" "a helm interface to ripgrep" '((emacs "25") (helm "2.8.8") (cl-lib "0.5") (dash "2.13.0")) :commit "a7aee0db61d5edd24d540ed3cf438bb883c7c03b" :keywords '("find" "file" "files" "helm" "fast" "rg" "ripgrep" "grep" "search") :url "https://github.com/cosmicexplorer/helm-rg")

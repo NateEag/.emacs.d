ea40b1aba0b48856ba116ec900dc045c48a48d38
Nate Eagleson <nate@nateeag.com>
2018-06-28 15:17:37 -0400

    Update package lsp-mode-20180616.1442

--- elpa/lsp-mode-20180620.2311/lsp-mode-autoloads.el
+++ elpa/lsp-mode-20180620.2311/lsp-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "lsp-methods" "lsp-methods.el" (23335 46517
+;;;### (autoloads nil "lsp-methods" "lsp-methods.el" (23349 13392
 ;;;;;;  0 0))
 ;;; Generated autoloads from lsp-methods.el
 
@@ -27,6 +27,14 @@ whitelist, or does not match any pattern in the blacklist.")
 Enable `eldoc-mode' integration.")
 
 (custom-autoload 'lsp-enable-eldoc "lsp-methods" t)
+
+(defvar lsp-eldoc-render-all t "\
+Define whether all of the returned by document/onHover will be displayed.
+
+If `lsp-markup-display-all' is set to nil `eldoc' will show only
+the symbol information.")
+
+(custom-autoload 'lsp-eldoc-render-all "lsp-methods" t)
 
 (defvar lsp-highlight-symbol-at-point t "\
 Highlight the symbol under the point.")
@@ -75,7 +83,7 @@ Face used for highlighting symbols being written to." :group (quote lsp-faces))
 
 ;;;***
 
-;;;### (autoloads nil "lsp-mode" "lsp-mode.el" (23335 46517 0 0))
+;;;### (autoloads nil "lsp-mode" "lsp-mode.el" (23349 13392 0 0))
 ;;; Generated autoloads from lsp-mode.el
 
 (autoload 'lsp-mode "lsp-mode" "\
@@ -86,8 +94,8 @@ Face used for highlighting symbols being written to." :group (quote lsp-faces))
 ;;;***
 
 ;;;### (autoloads nil nil ("lsp-common.el" "lsp-flycheck.el" "lsp-imenu.el"
-;;;;;;  "lsp-io.el" "lsp-mode-pkg.el" "lsp-notifications.el") (23335
-;;;;;;  46517 0 0))
+;;;;;;  "lsp-io.el" "lsp-mode-pkg.el" "lsp-notifications.el") (23349
+;;;;;;  13392 0 0))
 
 ;;;***
 

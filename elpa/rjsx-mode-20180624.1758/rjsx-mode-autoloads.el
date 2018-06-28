45d257736e3205563befe52038e7a4447a54cb08
Nate Eagleson <nate@nateeag.com>
2018-06-28 15:17:48 -0400

    Update package rjsx-mode-20180616.647

--- elpa/rjsx-mode-20180624.1758/rjsx-mode-autoloads.el
+++ elpa/rjsx-mode-20180624.1758/rjsx-mode-autoloads.el
@@ -3,13 +3,18 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "rjsx-mode" "rjsx-mode.el" (23335 46526 0 0))
+;;;### (autoloads nil "rjsx-mode" "rjsx-mode.el" (23349 13404 0 0))
 ;;; Generated autoloads from rjsx-mode.el
 
 (autoload 'rjsx-mode "rjsx-mode" "\
 Major mode for editing JSX files.
 
 \(fn)" t nil)
+
+(autoload 'rjsx-minor-mode "rjsx-mode" "\
+Minor mode for parsing JSX syntax into an AST.
+
+\(fn &optional ARG)" t nil)
 
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
 

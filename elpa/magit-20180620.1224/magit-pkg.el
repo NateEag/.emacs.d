c7c306befe3282987ea540853fb582640ae7a12a
Nate Eagleson <nate@nateeag.com>
2018-06-28 15:17:41 -0400

    Update package magit-20180616.1530

--- elpa/magit-20180620.1224/magit-pkg.el
+++ elpa/magit-20180620.1224/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20180616.1530" "A Git porcelain inside Emacs."
+(define-package "magit" "20180620.1224" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180413")

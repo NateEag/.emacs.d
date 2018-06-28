e2aa77181fc76a376e1af211cb74539b1c8998a7
Nate Eagleson <nate@nateeag.com>
2018-06-28 15:17:55 -0400

    Update package yasnippet-20180606.1722

--- elpa/yasnippet-20180606.1722/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yasnippet" "20180606.1722" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :commit "e67592ce1bd4ccc533107765bdfebfa02e9fb945" :keywords '("convenience" "emulation") :url "http://github.com/joaotavora/yasnippet")

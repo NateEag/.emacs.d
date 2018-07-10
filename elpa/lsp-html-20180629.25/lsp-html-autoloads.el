e0a46415ae022eccd30990006ae01ded7833eedc
Nate Eagleson <nate@nateeag.com>
2018-07-10 09:36:04 -0400

    Install lsp-html package from MELPA

---
+++ elpa/lsp-html-20180629.25/lsp-html-autoloads.el
@@ -0,0 +1,15 @@
+;;; lsp-html-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lsp-html.el") (23364 45205 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-html-autoloads.el ends here
---
+++ elpa/lsp-html-20180629.25/lsp-html-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-html" "20180629.25" "HTML support for lsp-mode" '((lsp-mode "4.2")))

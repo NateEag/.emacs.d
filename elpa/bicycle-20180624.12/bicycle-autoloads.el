2d408654a205516834d07f50393f3e4d2b88d83f
Nate Eagleson <nate@nateeag.com>
2018-07-10 09:35:15 -0400

    Install bicycle-mode from MELPA
    I'm not really planning to use this right now. It's mainly a reference
    point to some other features I wish my editor had.

---
+++ elpa/bicycle-20180624.12/bicycle-autoloads.el
@@ -0,0 +1,39 @@
+;;; bicycle-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "bicycle" "bicycle.el" (23364 45205 0 0))
+;;; Generated autoloads from bicycle.el
+
+(autoload 'bicycle-cycle "bicycle" "\
+Cycle local or global visibility.
+
+With a prefix argument call `bicycle-cycle-global'.
+Without a prefix argument call `bicycle-cycle-local'.
+
+\(fn &optional GLOBAL)" t nil)
+
+(autoload 'bicycle-cycle-global "bicycle" "\
+Cycle visibility of all sections.
+
+1. OVERVIEW: Show only top-level heading.
+2. TOC:      Show all headings, without treating top-level
+             code blocks as sections.
+3. TREES:    Show all headings, treaing top-level code blocks
+             as sections (i.e. their first line is treated as
+             a heading).
+4. ALL:      Show everything, except code blocks that have been
+             collapsed individually (using a `hideshow' command
+             or function).
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bicycle-autoloads.el ends here
---
+++ elpa/bicycle-20180624.12/bicycle-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bicycle" "20180624.12" "cycle outline and code visibility" '((emacs "25.1")))

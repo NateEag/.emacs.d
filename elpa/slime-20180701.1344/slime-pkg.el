c3d7509e351df1490f26a8532a9bcc89a827143e
Nate Eagleson <nate@nateeag.com>
2018-07-11 09:10:14 -0400

    Update package slime-20180601.324

--- elpa/slime-20180701.1344/slime-pkg.el
+++ elpa/slime-20180701.1344/slime-pkg.el
@@ -1,4 +1,4 @@
-(define-package "slime" "20180601.324" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20180701.1344" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
   :keywords

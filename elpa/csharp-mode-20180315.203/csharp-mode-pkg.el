279ec9d12f23272b6edcb43c737acc4a6d48b027
Nate Eagleson <nate@nateeag.com>
2018-07-11 09:09:54 -0400

    Update package csharp-mode-20180315.203

--- elpa/csharp-mode-20180315.203/csharp-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "csharp-mode" "20180315.203" "C# mode derived mode" 'nil :commit "29dab33e0ef5927c9b654e79382e79c161cc78e0" :url "https://github.com/josteink/csharp-mode" :keywords '("c#" "languages" "oop" "mode"))
---
+++ elpa/csharp-mode-20180708.652/csharp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csharp-mode" "20180708.652" "C# mode derived mode" 'nil)

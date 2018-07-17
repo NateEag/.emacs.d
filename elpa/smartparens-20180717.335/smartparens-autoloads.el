8517c9a80535c428a9fa123608927fd49787a862
Nate Eagleson <nate@nateeag.com>
2018-07-17 13:50:18 -0400

    Update package smartparens-20180705.801

--- elpa/smartparens-20180717.335/smartparens-autoloads.el
+++ elpa/smartparens-20180717.335/smartparens-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (23366 438 0
-;;;;;;  0))
+;;;### (autoloads nil "smartparens" "smartparens.el" (23374 11352
+;;;;;;  0 0))
 ;;; Generated autoloads from smartparens.el
 
 (autoload 'sp-cheat-sheet "smartparens" "\
@@ -190,7 +190,7 @@ Turn off `show-smartparens-mode'.
 ;;;;;;  "smartparens-ml.el" "smartparens-org.el" "smartparens-pkg.el"
 ;;;;;;  "smartparens-python.el" "smartparens-racket.el" "smartparens-ruby.el"
 ;;;;;;  "smartparens-rust.el" "smartparens-scala.el" "smartparens-text.el")
-;;;;;;  (23366 438 0 0))
+;;;;;;  (23374 11352 0 0))
 
 ;;;***
 

b10751283255bfd7ae6be869e5633e2e123f3622
Nate Eagleson <nate@nateeag.com>
2018-07-30 21:38:47 -0400

    Update package ac-emmet-20131015.858

--- elpa/ac-emmet-20131015.1558/ac-emmet-autoloads.el
+++ elpa/ac-emmet-20131015.1558/ac-emmet-autoloads.el
@@ -1,10 +1,9 @@
 ;;; ac-emmet-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads (ac-emmet-css-setup ac-emmet-html-setup) "ac-emmet"
-;;;;;;  "ac-emmet.el" (21240 15515 0 0))
+;;;### (autoloads nil "ac-emmet" "ac-emmet.el" (23391 48551 0 0))
 ;;; Generated autoloads from ac-emmet.el
 
 (defface ac-emmet-candidate-face '((t (:inherit ac-candidate-face))) "\
@@ -39,16 +38,9 @@ This affects only the current buffer.
 
 ;;;***
 
-;;;### (autoloads nil nil ("ac-emmet-pkg.el") (21240 15515 560406
-;;;;;;  0))
-
-;;;***
-
-(provide 'ac-emmet-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; ac-emmet-autoloads.el ends here
---
+++ elpa/ac-emmet-20131015.1558/ac-emmet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ac-emmet" "20131015.1558" "auto-complete sources for emmet-mode's snippets" '((emmet-mode "1.0.2") (auto-complete "1.4")))

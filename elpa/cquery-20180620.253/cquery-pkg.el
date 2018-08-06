2b44bf98ad375a616f7e8929b84a6e34afa30246
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:18:30 -0400

    Update package cquery-20180619.1953

--- elpa/cquery-20180620.253/cquery-pkg.el
+++ elpa/cquery-20180620.253/cquery-pkg.el
@@ -1,9 +1,13 @@
-(define-package "cquery" "20180619.1953" "cquery client for lsp-mode"
+(define-package "cquery" "20180620.253" "cquery client for lsp-mode"
   '((emacs "25.1")
     (lsp-mode "3.4")
     (dash "0.13"))
   :keywords
   '("languages" "lsp" "c++")
+  :authors
+  '(("Tobias Pisani"))
+  :maintainer
+  '("Tobias Pisani")
   :url "https://github.com/jacobdufault/cquery")
 ;; Local Variables:
 ;; no-byte-compile: t

5624d32ade65c28b5cc6336453d0d16752ce9fa7
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:19:06 -0400

    Update package evil-matchit-20180526.557

---
+++ elpa/evil-matchit-20180526.1257/evil-matchit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil-matchit" "20180526.1257" "Vim matchit ported to Evil"
+  '((evil "1.0.7"))
+  :keywords
+  '("matchit" "vim" "evil")
+  :authors
+  '(("Chen Bin" . "chenbin.sh@gmail.com"))
+  :maintainer
+  '("Chen Bin" . "chenbin.sh@gmail.com")
+  :url "http://github.com/redguardtoo/evil-matchit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

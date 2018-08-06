86df5b12359a6a28950913a69099696c314569f0
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:21:32 -0400

    Update package php-auto-yasnippets-20170330.1814

--- elpa/php-auto-yasnippets-20170330.1814/php-auto-yasnippets-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "php-auto-yasnippets" "20170330.1814" "Creates snippets for PHP functions"
-  '((php-mode "1.11")
-    (yasnippet "0.8.0"))
-  :url "https://github.com/ejmr/php-auto-yasnippets")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/php-auto-yasnippets-20170331.114/php-auto-yasnippets-autoloads.el
+++ elpa/php-auto-yasnippets-20170331.114/php-auto-yasnippets-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "php-auto-yasnippets" "php-auto-yasnippets.el"
-;;;;;;  (22754 44857 0 0))
+;;;;;;  (23400 19291 0 0))
 ;;; Generated autoloads from php-auto-yasnippets.el
 
 (autoload 'yas/initialize "php-auto-yasnippets" "\
@@ -25,7 +25,7 @@ of a method for that class.
 
 ;;;***
 
-;;;### (autoloads nil nil ("php-auto-yasnippets-pkg.el") (22754 44857
+;;;### (autoloads nil nil ("php-auto-yasnippets-pkg.el") (23400 19291
 ;;;;;;  0 0))
 
 ;;;***

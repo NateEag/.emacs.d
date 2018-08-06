28d61c9203b3d7f970d88e9791d2a99d22c1e2eb
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:20:12 -0400

    Update package jedi-core-20170121.610

--- elpa/jedi-core-20170121.1410/jedi-core-autoloads.el
+++ elpa/jedi-core-20170121.1410/jedi-core-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "jedi-core" "jedi-core.el" (22663 33898 0 0))
+;;;### (autoloads nil "jedi-core" "jedi-core.el" (23400 19212 0 0))
 ;;; Generated autoloads from jedi-core.el
 
 (autoload 'jedi:start-dedicated-server "jedi-core" "\
@@ -99,7 +99,7 @@ Blocking version `jedi:install-server'.
 
 ;;;***
 
-;;;### (autoloads nil nil ("jedi-core-pkg.el") (22663 33898 0 0))
+;;;### (autoloads nil nil ("jedi-core-pkg.el") (23400 19212 0 0))
 
 ;;;***
 

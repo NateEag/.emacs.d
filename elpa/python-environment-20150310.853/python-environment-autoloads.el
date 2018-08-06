683b51dc1802bb9f7845fc4bbdd3cc8fa643c250
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:21:51 -0400

    Update package python-environment-20150310.153

--- elpa/python-environment-20150310.853/python-environment-autoloads.el
+++ elpa/python-environment-20150310.853/python-environment-autoloads.el
@@ -1,10 +1,10 @@
 ;;; python-environment-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("python-environment-pkg.el" "python-environment.el"
-;;;;;;  "test-python-environment.el") (21758 53168 530825 0))
+;;;;;;  "test-python-environment.el") (23400 19311 0 0))
 
 ;;;***
 

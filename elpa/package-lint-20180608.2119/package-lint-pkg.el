c88cd0e0f4a1accd1f5ae2046e97aacbeeba5867
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:21:16 -0400

    Update package package-lint-20180608.2119

--- elpa/package-lint-20180608.2119/package-lint-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "package-lint" "20180608.2119" "A linting library for elisp package authors"
-  '((cl-lib "0.5")
-    (emacs "24"))
-  :keywords
-  '("lisp")
-  :url "https://github.com/purcell/package-lint")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:

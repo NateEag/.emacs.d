062822a487e45b73b620bb0fff57e2f5491b1ef4
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:20:13 -0400

    Update package epc-20140609.2234

--- elpa/epc-20140610.534/epc-autoloads.el
+++ elpa/epc-20140610.534/epc-autoloads.el
@@ -1,10 +1,10 @@
 ;;; epc-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (21453
-;;;;;;  1978 565427 0))
+;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (23400
+;;;;;;  19213 0 0))
 
 ;;;***
 

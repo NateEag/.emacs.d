ef34985f334135990921df67771d1ea30e2e9790
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:22:53 -0400

    Update package string-inflection-20180518.1707

--- elpa/string-inflection-20180518.1707/string-inflection-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "string-inflection" "20180518.1707" "underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names" 'nil :commit "a9de404b2ece932da9b1c9aa1c29dbf7cf506e76" :keywords '("elisp"))
--- elpa/string-inflection-20180723.441/string-inflection-autoloads.el
+++ elpa/string-inflection-20180723.441/string-inflection-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "string-inflection" "string-inflection.el"
-;;;;;;  (23296 4713 0 0))
+;;;;;;  (23400 19372 0 0))
 ;;; Generated autoloads from string-inflection.el
 
 (autoload 'string-inflection-ruby-style-cycle "string-inflection" "\

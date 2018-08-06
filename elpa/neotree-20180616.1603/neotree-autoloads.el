63dfaeabcc8ed5bb16fce3833b553e63a00b0439
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:21:01 -0400

    Update package neotree-20180616.903

--- elpa/neotree-20180616.1603/neotree-autoloads.el
+++ elpa/neotree-20180616.1603/neotree-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "neotree" "neotree.el" (23335 46511 0 0))
+;;;### (autoloads nil "neotree" "neotree.el" (23400 19260 0 0))
 ;;; Generated autoloads from neotree.el
 
 (autoload 'neotree-find "neotree" "\
@@ -48,7 +48,7 @@ Show the NeoTree window.")
 
 ;;;***
 
-;;;### (autoloads nil nil ("neotree-pkg.el") (23335 46511 0 0))
+;;;### (autoloads nil nil ("neotree-pkg.el") (23400 19260 0 0))
 
 ;;;***
 

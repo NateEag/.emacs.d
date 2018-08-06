7cd38a25d5d1b626d4dc8409e1c4b8d3de6824ed
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:23:54 -0400

    Update package with-editor-20180618.1602

--- elpa/with-editor-20180726.2044/with-editor-autoloads.el
+++ elpa/with-editor-20180726.2044/with-editor-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "with-editor" "with-editor.el" (23349 13409
+;;;### (autoloads nil "with-editor" "with-editor.el" (23400 19433
 ;;;;;;  0 0))
 ;;; Generated autoloads from with-editor.el
 
@@ -82,7 +82,7 @@ else like the former.
 
 ;;;***
 
-;;;### (autoloads nil nil ("with-editor-pkg.el") (23349 13409 0 0))
+;;;### (autoloads nil nil ("with-editor-pkg.el") (23400 19433 0 0))
 
 ;;;***
 

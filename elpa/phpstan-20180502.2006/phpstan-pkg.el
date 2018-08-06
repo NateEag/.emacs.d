bc2cb8e8c0883542011447fe5304037ba113c98f
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:21:36 -0400

    Update package phpstan-20180502.2006

--- elpa/phpstan-20180502.2006/phpstan-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "phpstan" "20180502.2006" "Interface to PHPStan." '((emacs "24.3")) :commit "97f643518714ba3e76b5b64f72c938800b4dc43b" :keywords '("tools" "php") :url "https://github.com/emacs-php/phpstan.el")

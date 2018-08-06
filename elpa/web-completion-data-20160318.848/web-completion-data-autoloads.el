a73d3d947b92f40ac5367e6480ec4f7183eb2925
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:23:46 -0400

    Update package web-completion-data-20160318.148

--- elpa/web-completion-data-20160318.848/web-completion-data-autoloads.el
+++ elpa/web-completion-data-20160318.848/web-completion-data-autoloads.el
@@ -1,10 +1,10 @@
 ;;; web-completion-data-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("web-completion-data-pkg.el" "web-completion-data.el")
-;;;;;;  (22410 5572 388113 0))
+;;;;;;  (23400 19425 0 0))
 
 ;;;***
 

068069aa5f6525ca1bc7106334a40bbe839291f1
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:22:43 -0400

    Update package macrostep-20161120.1306

--- elpa/macrostep-20161120.2106/macrostep-autoloads.el
+++ elpa/macrostep-20161120.2106/macrostep-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "macrostep" "macrostep.el" (22600 4672 0 0))
+;;;### (autoloads nil "macrostep" "macrostep.el" (23400 19362 0 0))
 ;;; Generated autoloads from macrostep.el
 
 (autoload 'macrostep-mode "macrostep" "\
@@ -35,7 +35,7 @@ behaviors.
 
 ;;;***
 
-;;;### (autoloads nil "macrostep-c" "macrostep-c.el" (22600 4672
+;;;### (autoloads nil "macrostep-c" "macrostep-c.el" (23400 19362
 ;;;;;;  0 0))
 ;;; Generated autoloads from macrostep-c.el
 
@@ -48,7 +48,7 @@ behaviors.
 
 ;;;***
 
-;;;### (autoloads nil nil ("macrostep-pkg.el") (22600 4672 0 0))
+;;;### (autoloads nil nil ("macrostep-pkg.el") (23400 19362 0 0))
 
 ;;;***
 

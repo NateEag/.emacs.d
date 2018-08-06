676084726b44f0130ae9625ff403f788b8e4b17c
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:22:54 -0400

    Update package string-utils-20140508.1341

--- elpa/string-utils-20140508.2041/string-utils-autoloads.el
+++ elpa/string-utils-20140508.2041/string-utils-autoloads.el
@@ -1,9 +1,9 @@
 ;;; string-utils-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "string-utils" "string-utils.el" (21592 11308
+;;;### (autoloads nil "string-utils" "string-utils.el" (23400 19374
 ;;;;;;  0 0))
 ;;; Generated autoloads from string-utils.el
 
--- elpa/string-utils-20140508.2041/string-utils.el
+++ elpa/string-utils-20140508.2041/string-utils.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/string-utils
 ;; URL: http://raw.githubusercontent.com/rolandwalker/string-utils/master/string-utils.el
-;; Version: 20140508.1341
-;; X-Original-Version: 0.3.2
+;; Package-Version: 20140508.2041
+;; Version: 0.3.2
 ;; Last-Updated: 22 Oct 2013
 ;; Package-Requires: ((list-utils "0.4.2"))
 ;; EmacsWiki: StringUtils

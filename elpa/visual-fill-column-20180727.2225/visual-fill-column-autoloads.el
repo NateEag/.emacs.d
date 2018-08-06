6a4028fa826e77966f2f496b62feb98d797cb233
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:23:57 -0400

    Update package visual-fill-column-20180511.211

--- elpa/visual-fill-column-20180727.2225/visual-fill-column-autoloads.el
+++ elpa/visual-fill-column-20180727.2225/visual-fill-column-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "visual-fill-column" "visual-fill-column.el"
-;;;;;;  (23289 36149 0 0))
+;;;;;;  (23400 19437 0 0))
 ;;; Generated autoloads from visual-fill-column.el
 
 (autoload 'visual-fill-column-mode "visual-fill-column" "\

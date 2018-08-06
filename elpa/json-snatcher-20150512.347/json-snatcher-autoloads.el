ee8f6e1407d9eb0a77d5b00f5a55507f1de7b230
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:20:24 -0400

    Update package json-snatcher-20150511.2047

--- elpa/json-snatcher-20150512.347/json-snatcher-autoloads.el
+++ elpa/json-snatcher-20150512.347/json-snatcher-autoloads.el
@@ -1,9 +1,9 @@
 ;;; json-snatcher-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "json-snatcher" "json-snatcher.el" (22414 61284
+;;;### (autoloads nil "json-snatcher" "json-snatcher.el" (23400 19224
 ;;;;;;  0 0))
 ;;; Generated autoloads from json-snatcher.el
 
--- elpa/json-snatcher-20150512.347/json-snatcher.el
+++ elpa/json-snatcher-20150512.347/json-snatcher.el
@@ -4,7 +4,7 @@
 
 ;; Author: Sterling Graham <sterlingrgraham@gmail.com>
 ;; URL: http://github.com/sterlingg/json-snatcher
-;; Package-Version: 20150511.2047
+;; Package-Version: 20150512.347
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "24"))
 

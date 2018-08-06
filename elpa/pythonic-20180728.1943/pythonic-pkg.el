883d675e2ddcba26dac882661947d89fffcfbd7b
Nate Eagleson <nate@nateeag.com>
2018-08-06 09:22:03 -0400

    Update package pythonic-20180624.2212

---
+++ elpa/pythonic-20180728.1943/pythonic-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pythonic" "20180728.1943" "Utility functions for writing pythonic emacs package." '((emacs "25") (s "1.9") (f "0.17.2")) :commit "6e4824d218cfbd311f7159f3a049eacbdbf0eb69" :authors '(("Artem Malyshev" . "proofit404@gmail.com")) :maintainer '("Artem Malyshev" . "proofit404@gmail.com") :url "https://github.com/proofit404/pythonic")

cc311af76eaaada73fb2c83dfbf987d3a332459b
Nate Eagleson <nate@nateeag.com>
2018-09-21 08:56:29 -0400

    Update package polymode-20170307.1122

---
+++ elpa/polymode-20180912.1854/polymode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "polymode" "20180912.1854" "Extensible framework for multiple major modes"
+  '((emacs "25"))
+  :keywords
+  '("languages" "multi-modes" "processes")
+  :authors
+  '(("Vitalie Spinu"))
+  :maintainer
+  '("Vitalie Spinu")
+  :url "https://github.com/vitoshka/polymode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

bb4470aade79a20e1d79ea75f459d587f4fd8a7d
Nate Eagleson <nate@nateeag.com>
2018-10-10 14:30:46 -0400

    Update package ace-window-20180814.1516

---
+++ elpa/lsp-sh-20180913.158/lsp-sh-autoloads.el
@@ -0,0 +1,15 @@
+;;; lsp-sh-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lsp-sh.el") (23486 17736 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-sh-autoloads.el ends here
---
+++ elpa/lsp-sh-20180913.158/lsp-sh-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-sh" "20180913.158" "Shell support for lsp-mode" '((lsp-mode "3.0")) :commit "d9e94837ccbb54eafa381833d2a9a0089e01096b" :keywords '("sh" "shell" "bash") :authors '(("Mike Wilkerson" . "wilkystyle@gmail.com")) :maintainer '("Mike Wilkerson" . "wilkystyle@gmail.com") :url "https://github.com/wilkystyle/lsp-sh")

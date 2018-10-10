dc1d3a2c7a4f9689571a8361a6877d179e72f4ec
Nate Eagleson <nate@nateeag.com>
2018-10-10 15:10:06 -0400

    Update package ghub-20180924.713

--- elpa/ghub-20181003.1456/ghub-pkg.el
+++ elpa/ghub-20181003.1456/ghub-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ghub" "20180924.713" "Minuscule client libraries for Git forge APIs."
+(define-package "ghub" "20181003.1456" "Minuscule client libraries for Git forge APIs."
   '((emacs "24.4")
     (dash "2.14.1")
     (graphql "0.1")

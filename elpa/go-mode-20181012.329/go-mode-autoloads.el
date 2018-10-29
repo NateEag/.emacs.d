85b91ca106847a672c719e31ce22cbab35090f35
Nate Eagleson <nate@nateeag.com>
2018-10-29 09:32:53 -0400

    Update package go-mode-20180327.1530

--- elpa/go-mode-20181012.329/go-mode-autoloads.el
+++ elpa/go-mode-20181012.329/go-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "go-mode" "go-mode.el" (23400 19186 0 0))
+;;;### (autoloads nil "go-mode" "go-mode.el" (23511 3076 0 0))
 ;;; Generated autoloads from go-mode.el
 
 (autoload 'go-mode "go-mode" "\

fbbc190eeb3cd758f08302109b2b549e9d1b500f
Nate Eagleson <nate@nateeag.com>
2018-10-29 09:32:47 -0400

    Update package aggressive-indent-20180627.21

--- elpa/aggressive-indent-20181018.236/aggressive-indent-autoloads.el
+++ elpa/aggressive-indent-20181018.236/aggressive-indent-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "aggressive-indent" "aggressive-indent.el"
-;;;;;;  (23400 19091 0 0))
+;;;;;;  (23511 3070 0 0))
 ;;; Generated autoloads from aggressive-indent.el
 
 (autoload 'aggressive-indent-indent-defun "aggressive-indent" "\
--- elpa/aggressive-indent-20181018.236/aggressive-indent.el
+++ elpa/aggressive-indent-20181018.236/aggressive-indent.el
@@ -4,7 +4,7 @@
 
 ;; Author: Artur Malabarba <emacs@endlessparentheses.com>
 ;; URL: https://github.com/Malabarba/aggressive-indent-mode
-;; Package-Version: 20180627.21
+;; Package-Version: 20181018.236
 ;; Version: 1.8.4
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 ;; Keywords: indent lisp maint tools
@@ -413,7 +413,7 @@ If you feel aggressive-indent is causing Emacs to hang while
 typing, try tweaking this number."
   :type 'float)
 
-(defvar-local aggressive-indent--idle-timer nil
+(defvar aggressive-indent--idle-timer nil
   "Idle timer used for indentation")
 
 (defun aggressive-indent--indent-if-changed ()
@@ -422,18 +422,15 @@ typing, try tweaking this number."
     (save-excursion
       (save-selected-window
         (while-no-input
-          (aggressive-indent--proccess-changed-list-and-indent))))
-    (when (timerp aggressive-indent--idle-timer)
-      (cancel-timer aggressive-indent--idle-timer))))
+          (aggressive-indent--proccess-changed-list-and-indent))))))
 
 (defun aggressive-indent--keep-track-of-changes (l r &rest _)
   "Store the limits (L and R) of each change in the buffer."
   (when aggressive-indent-mode
     (push (list l r) aggressive-indent--changed-list)
-    (when (timerp aggressive-indent--idle-timer)
-      (cancel-timer aggressive-indent--idle-timer))
-    (setq aggressive-indent--idle-timer
-          (run-with-idle-timer aggressive-indent-sit-for-time t #'aggressive-indent--indent-if-changed))))
+    (unless (timerp aggressive-indent--idle-timer)
+      (setq aggressive-indent--idle-timer
+            (run-with-idle-timer aggressive-indent-sit-for-time t #'aggressive-indent--indent-if-changed)))))
 
 ;;; Minor modes
 ;;;###autoload
@@ -466,7 +463,8 @@ typing, try tweaking this number."
         (add-hook 'before-save-hook #'aggressive-indent--proccess-changed-list-and-indent nil 'local))
     ;; Clean the hooks
     (when (timerp aggressive-indent--idle-timer)
-      (cancel-timer aggressive-indent--idle-timer))
+      (cancel-timer aggressive-indent--idle-timer)
+      (setq aggressive-indent--idle-timer nil))
     (remove-hook 'after-change-functions #'aggressive-indent--keep-track-of-changes 'local)
     (remove-hook 'before-save-hook #'aggressive-indent--proccess-changed-list-and-indent 'local)
     (remove-hook 'post-command-hook #'aggressive-indent--softly-indent-defun 'local)))

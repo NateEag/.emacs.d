c563f466f17b08c1f25d3b3c120a35504e7d274c
Nate Eagleson <nate@nateeag.com>
2018-10-29 09:32:49 -0400

    Update package buttercup-20181005.1356

--- elpa/buttercup-20181005.1356/buttercup-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "buttercup" "20181005.1356" "Behavior-Driven Emacs Lisp Testing" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/buttercup-20181012.740/buttercup-pkg.el
@@ -0,0 +1,4 @@
+(define-package "buttercup" "20181012.740" "Behavior-Driven Emacs Lisp Testing" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

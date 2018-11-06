6f81ccd2644b929310e5250156da91fa73bce543
Nate Eagleson <nate@nateeag.com>
2018-11-06 14:14:30 -0500

    Update package solarized-theme-20181025.1251

--- elpa/solarized-theme-20181030.1912/solarized-theme-pkg.el
+++ elpa/solarized-theme-20181030.1912/solarized-theme-pkg.el
@@ -1,4 +1,4 @@
-(define-package "solarized-theme" "20181025.1251" "The Solarized color theme, ported to Emacs."
+(define-package "solarized-theme" "20181030.1912" "The Solarized color theme, ported to Emacs."
   '((emacs "24.1")
     (cl-lib "0.5")
     (dash "2.6.0")))

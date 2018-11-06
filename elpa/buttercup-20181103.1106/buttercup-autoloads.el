b273fc25d29b5631a8e1e688a472a6bbf6ae12db
Nate Eagleson <nate@nateeag.com>
2018-11-06 14:14:04 -0500

    Update package buttercup-20181012.740

--- elpa/buttercup-20181103.1106/buttercup-autoloads.el
+++ elpa/buttercup-20181103.1106/buttercup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "buttercup" "buttercup.el" (23511 3072 0 0))
+;;;### (autoloads nil "buttercup" "buttercup.el" (23521 59387 0 0))
 ;;; Generated autoloads from buttercup.el
 
 (autoload 'buttercup-run-at-point "buttercup" "\
@@ -53,7 +53,7 @@ With buttercup minor mode active the following is activated:
 ;;;***
 
 ;;;### (autoloads nil nil ("buttercup-compat.el" "buttercup-pkg.el")
-;;;;;;  (23511 3072 0 0))
+;;;;;;  (23521 59387 0 0))
 
 ;;;***
 

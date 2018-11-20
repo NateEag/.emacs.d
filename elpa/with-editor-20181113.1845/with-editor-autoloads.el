5e9e6150e41d914cddf7ab3d1b5f4632ef738f01
Nate Eagleson <nate@nateeag.com>
2018-11-20 10:34:24 -0500

    Update package with-editor-20181103.2149

--- elpa/with-editor-20181113.1845/with-editor-autoloads.el
+++ elpa/with-editor-20181113.1845/with-editor-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "with-editor" "with-editor.el" (23521 59420
+;;;### (autoloads nil "with-editor" "with-editor.el" (23540 10623
 ;;;;;;  0 0))
 ;;; Generated autoloads from with-editor.el
 
@@ -82,7 +82,7 @@ else like the former.
 
 ;;;***
 
-;;;### (autoloads nil nil ("with-editor-pkg.el") (23521 59420 0 0))
+;;;### (autoloads nil nil ("with-editor-pkg.el") (23540 10623 0 0))
 
 ;;;***
 
--- elpa/with-editor-20181113.1845/with-editor-pkg.el
+++ elpa/with-editor-20181113.1845/with-editor-pkg.el
@@ -1,4 +1,4 @@
-(define-package "with-editor" "20181103.2149" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20181113.1845" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4")
     (async "1.9"))
   :keywords

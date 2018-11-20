9e11e202b455ab6d63a8b13fe66e475cc55318c1
Nate Eagleson <nate@nateeag.com>
2018-11-20 10:33:32 -0500

    Update package groovy-mode-20180810.607

--- elpa/groovy-mode-20181111.1057/groovy-mode-pkg.el
+++ elpa/groovy-mode-20181111.1057/groovy-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "groovy-mode" "20180810.607" "Major mode for Groovy source files"
+(define-package "groovy-mode" "20181111.1057" "Major mode for Groovy source files"
   '((s "1.12.0")
     (emacs "24.3")
     (dash "2.13.0"))

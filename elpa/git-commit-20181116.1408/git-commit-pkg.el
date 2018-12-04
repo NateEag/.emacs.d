6c1d5f848df262d97243829523a6205ad058528f
Nate Eagleson <nate@nateeag.com>
2018-12-04 09:05:48 -0500

    Update package git-commit-20181116.1408

--- elpa/git-commit-20181116.1408/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20181116.1408" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "36d89c88e1337ec2b33c75c3d426289c66f86b10" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")

12b26967dc8cb6bd9586e0a66ae9296eb7682a90
Nate Eagleson <nate@nateeag.com>
2018-12-04 09:05:28 -0500

    Update package cquery-20180811.2131

--- elpa/cquery-20181203.1829/cquery-autoloads.el
+++ elpa/cquery-20181203.1829/cquery-autoloads.el
@@ -3,16 +3,10 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "cquery" "cquery.el" (23460 59971 0 0))
-;;; Generated autoloads from cquery.el
- (autoload 'lsp-cquery-enable "cquery")
-
-;;;***
-
 ;;;### (autoloads nil nil ("cquery-call-hierarchy.el" "cquery-code-lens.el"
 ;;;;;;  "cquery-common.el" "cquery-inheritance-hierarchy.el" "cquery-member-hierarchy.el"
-;;;;;;  "cquery-pkg.el" "cquery-semantic-highlighting.el" "cquery-tree.el")
-;;;;;;  (23460 59971 0 0))
+;;;;;;  "cquery-pkg.el" "cquery-semantic-highlighting.el" "cquery-tree.el"
+;;;;;;  "cquery.el") (23558 35239 0 0))
 
 ;;;***
 
--- elpa/cquery-20181203.1829/cquery-pkg.el
+++ elpa/cquery-20181203.1829/cquery-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cquery" "20180811.2131" "cquery client for lsp-mode"
+(define-package "cquery" "20181203.1829" "cquery client for lsp-mode"
   '((emacs "25.1")
     (lsp-mode "3.4")
     (dash "0.13"))

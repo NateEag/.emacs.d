60002a5641f994956a340a16d42b2e737d4ed47b
Nate Eagleson <nate@nateeag.com>
2018-12-04 09:06:16 -0500

    Update package use-package-20181110.1758

--- elpa/use-package-20181119.2350/use-package-pkg.el
+++ elpa/use-package-20181119.2350/use-package-pkg.el
@@ -1,4 +1,4 @@
-(define-package "use-package" "20181110.1758" "A configuration macro for simplifying your .emacs"
+(define-package "use-package" "20181119.2350" "A configuration macro for simplifying your .emacs"
   '((emacs "24.3")
     (bind-key "2.4"))
   :keywords

5912c6591b0a62b8e1a4e9495ee49d253c2b015e
Nate Eagleson <nate@nateeag.com>
2018-12-18 11:00:24 -0500

    Add lsp-php from MELPA
    It appears the package has finally changed hands, so I'll need to update
    my setup to get it working again.
    
    All in good time.

---
+++ elpa/lsp-php-20180331.1644/lsp-php-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-php" "20180331.1644" "PHP support for lsp-mode" '((emacs "25.1") (lsp-mode "3.4")) :commit "f96e23570120eca765132504df852a78d8b4d042" :authors '(("Declspeck" . "declspeck@declblog.com") ("zg" . "13853850881@163.com")) :maintainer '("Declspeck" . "declspeck@declblog.com") :url "https://github.com/emacs-lsp/lsp-php")

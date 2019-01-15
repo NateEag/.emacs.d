143f2203d0bb62644a4432f97dea61e2744b813f
Nate Eagleson <nate@nateeag.com>
2019-01-15 09:08:08 -0500

    Update package php-mode-20181206.825

--- elpa/php-mode-20190109.1320/php-mode.el
+++ elpa/php-mode-20190109.1320/php-mode.el
@@ -1,6 +1,6 @@
 ;;; php-mode.el --- Major mode for editing PHP code
 
-;; Copyright (C) 2018  Friends of Emacs-PHP development
+;; Copyright (C) 2018-2019  Friends of Emacs-PHP development
 ;; Copyright (C) 1999, 2000, 2001, 2003, 2004 Turadg Aleahmad
 ;;               2008 Aaron S. Hawley
 ;;               2011, 2012, 2013, 2014, 2015, 2016, 2017 Eric James Michael Ritz
@@ -9,14 +9,14 @@
 ;; Maintainer: USAMI Kenta <tadsan@zonu.me>
 ;; URL: https://github.com/emacs-php/php-mode
 ;; Keywords: languages php
-;; Version: 1.20.0
+;; Version: 1.21.0
 ;; Package-Requires: ((emacs "24.3") (cl-lib "0.5"))
 ;; License: GPL-3.0-or-later
 
-(defconst php-mode-version-number "1.20.0"
+(defconst php-mode-version-number "1.21.0"
   "PHP Mode version number.")
 
-(defconst php-mode-modified "2018-12-05"
+(defconst php-mode-modified "2019-01-09"
   "PHP Mode build date.")
 
 ;; This file is free software; you can redistribute it and/or
@@ -123,7 +123,7 @@
 
 ;; Local variables
 ;;;###autoload
-(defgroup php-mode nil
+(defgroup php nil
   "Language support for PHP."
   :tag "PHP"
   :group 'languages
@@ -418,6 +418,12 @@ This function may interfere with other hooks and other behaviors.
 In that case set to `NIL'."
   :type 'boolean)
 
+(define-obsolete-variable-alias 'php-mode-disable-parent-mode-hooks 'php-mode-disable-c-mode-hook "1.21.0")
+(defcustom php-mode-disable-c-mode-hook t
+  "When set to `T', do not run hooks of parent modes (`java-mode', `c-mode')."
+  :type 'boolean
+  :group 'php-mode)
+
 (defun php-mode-version ()
   "Display string describing the version of PHP Mode."
   (interactive)
@@ -429,9 +435,9 @@ In that case set to `NIL'."
 (define-obsolete-variable-alias 'php-available-project-root-files 'php-project-available-root-files "1.19.0")
 
 (defvar php-mode-map
-  (let ((map (make-sparse-keymap)))
-    ;; (define-key map [menu-bar php]
-    ;;   (cons "PHP" (make-sparse-keymap "PHP")))
+  (let ((map (make-sparse-keymap "PHP Mode")))
+    ;; Remove menu item for c-mode
+    (define-key map [menu-bar C] nil)
 
     ;; (define-key map [menu-bar php complete-function]
     ;;   '("Complete function name" . php-complete-function))
@@ -1258,7 +1264,11 @@ After setting the stylevars run hooks according to STYLENAME
   "Major mode for editing PHP code.
 
 \\{php-mode-map}"
-
+  ;; :after-hook (c-update-modeline)
+  ;; (setq abbrev-mode t)
+  (when php-mode-disable-c-mode-hook
+    (setq-local c-mode-hook nil)
+    (setq-local java-mode-hook nil))
   (c-initialize-cc-mode t)
   (c-init-language-vars php-mode)
   (c-common-init 'php-mode)
@@ -1333,7 +1343,6 @@ After setting the stylevars run hooks according to STYLENAME
     (with-silent-modifications
       (save-excursion
         (php-syntax-propertize-function (point-min) (point-max))))))
-
 
 (declare-function semantic-create-imenu-index "semantic/imenu" (&optional stream))
 

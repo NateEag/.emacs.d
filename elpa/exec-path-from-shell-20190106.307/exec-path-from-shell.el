26a4c864e34baf9fd1e105b83cb9e92152b5bb3b
Nate Eagleson <nate@nateeag.com>
2019-01-15 09:07:24 -0500

    Update package exec-path-from-shell-20180324.204

--- elpa/exec-path-from-shell-20190106.307/exec-path-from-shell.el
+++ elpa/exec-path-from-shell-20190106.307/exec-path-from-shell.el
@@ -5,7 +5,7 @@
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; Keywords: unix, environment
 ;; URL: https://github.com/purcell/exec-path-from-shell
-;; Package-Version: 20180324.204
+;; Package-Version: 20190106.307
 ;; Package-X-Original-Version: 0
 
 ;; This file is not part of GNU Emacs.
@@ -120,9 +120,12 @@ See documentation for `exec-path-from-shell-shell-name'."
    (error "SHELL environment variable is unset")))
 
 (defcustom exec-path-from-shell-arguments
-  (if (string-match-p "t?csh$" (exec-path-from-shell--shell))
-      (list "-d")
-    (list "-l" "-i"))
+  (let ((shell (exec-path-from-shell--shell)))
+    (if (string-match-p "t?csh$" shell)
+        (list "-d")
+      (if (string-match-p "fish" shell)
+          (list "-l")
+        (list "-l" "-i"))))
   "Additional arguments to pass to the shell.
 
 The default value denotes an interactive login shell."

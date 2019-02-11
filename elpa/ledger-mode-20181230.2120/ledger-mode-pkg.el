9b42e3353d9ef9a802f1a195ecc48f35b6f60aeb
Nate Eagleson <nate@nateeag.com>
2019-02-11 10:56:21 -0500

    Update package ledger-mode-20181230.2120

--- elpa/ledger-mode-20181230.2120/ledger-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ledger-mode" "20181230.2120" "Helper code for use with the \"ledger\" command-line tool"
-  '((emacs "24.3")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/ledger-mode-20190211.336/ledger-mode-autoloads.el
+++ elpa/ledger-mode-20190211.336/ledger-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ledger-flymake" "ledger-flymake.el" (23613
-;;;;;;  59685 0 0))
+;;;### (autoloads nil "ledger-flymake" "ledger-flymake.el" (23649
+;;;;;;  39714 674302 325000))
 ;;; Generated autoloads from ledger-flymake.el
 
 (autoload 'ledger-flymake-enable "ledger-flymake" "\
@@ -17,8 +17,8 @@ available.
 
 ;;;***
 
-;;;### (autoloads nil "ledger-mode" "ledger-mode.el" (23613 59685
-;;;;;;  0 0))
+;;;### (autoloads nil "ledger-mode" "ledger-mode.el" (23649 39714
+;;;;;;  683556 987000))
 ;;; Generated autoloads from ledger-mode.el
 
 (autoload 'ledger-mode "ledger-mode" "\
@@ -36,7 +36,7 @@ A mode for editing ledger data files.
 ;;;;;;  "ledger-navigate.el" "ledger-occur.el" "ledger-post.el" "ledger-reconcile.el"
 ;;;;;;  "ledger-regex.el" "ledger-report.el" "ledger-schedule.el"
 ;;;;;;  "ledger-sort.el" "ledger-state.el" "ledger-test.el" "ledger-texi.el"
-;;;;;;  "ledger-xact.el") (23613 59686 0 0))
+;;;;;;  "ledger-xact.el") (23649 39714 695115 545000))
 
 ;;;***
 

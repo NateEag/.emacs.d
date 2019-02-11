1b8f200f0dbd2f3b7b7150a6104a9a28a0d54f3d
Nate Eagleson <nate@nateeag.com>
2019-02-11 10:56:32 -0500

    Update package slime-20190206.1107

--- elpa/slime-20190210.1101/slime-autoloads.el
+++ elpa/slime-20190210.1101/slime-autoloads.el
@@ -51,6 +51,6 @@
 ;; End:
 
 ;;;### (autoloads nil nil ("slime-pkg.el" "slime-tests.el" "slime.el")
-;;;;;;  (23644 11313 0 0))
+;;;;;;  (23649 39723 862840 459000))
 
 ;;;***
--- elpa/slime-20190210.1101/slime-pkg.el
+++ elpa/slime-20190210.1101/slime-pkg.el
@@ -1,4 +1,4 @@
-(define-package "slime" "20190206.1107" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20190210.1101" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
   :keywords

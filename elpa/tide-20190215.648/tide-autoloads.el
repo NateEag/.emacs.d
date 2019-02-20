7cac1ac94ccb14e41d4bcda4e4c752736e4e4ff8
Nate Eagleson <nate@nateeag.com>
2019-02-20 08:24:22 -0500

    Update package tide-20190204.1106

--- elpa/tide-20190215.648/tide-autoloads.el
+++ elpa/tide-20190215.648/tide-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "tide" "tide.el" (23641 39474 0 0))
+;;;### (autoloads nil "tide" "tide.el" (23661 21765 88343 638000))
 ;;; Generated autoloads from tide.el
 
 (autoload 'company-tide "tide" "\
@@ -57,8 +57,8 @@ timeout.
 
 ;;;***
 
-;;;### (autoloads nil nil ("tide-lv.el" "tide-pkg.el") (23641 39474
-;;;;;;  0 0))
+;;;### (autoloads nil nil ("tide-lv.el" "tide-pkg.el") (23661 21765
+;;;;;;  87094 19000))
 
 ;;;***
 
--- elpa/tide-20190215.648/tide-pkg.el
+++ elpa/tide-20190215.648/tide-pkg.el
@@ -1,4 +1,4 @@
-(define-package "tide" "20190204.1106" "Typescript Interactive Development Environment"
+(define-package "tide" "20190215.648" "Typescript Interactive Development Environment"
   '((dash "2.10.0")
     (s "1.11.0")
     (flycheck "27")

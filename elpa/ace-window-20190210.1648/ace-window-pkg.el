bc6d004f74e1f4ca15f5b6279f6ef38b8c005af8
Nate Eagleson <nate@nateeag.com>
2019-02-20 08:23:43 -0500

    Update package ace-window-20190210.1648

--- elpa/ace-window-20190210.1648/ace-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ace-window" "20190210.1648" "Quickly switch windows." '((avy "0.2.0")) :commit "d7bdb5362275a8f82a45cad6eb3583f3f48f823a" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")

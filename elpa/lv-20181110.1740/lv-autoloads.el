2e6ad796006c4667307c6ad4df0fdc1865bb76c0
Nate Eagleson <nate@nateeag.com>
2019-02-20 08:24:01 -0500

    Update package magit-20190202.1535

---
+++ elpa/lv-20181110.1740/lv-autoloads.el
@@ -0,0 +1,15 @@
+;;; lv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lv.el") (23661 21739 842228 164000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lv-autoloads.el ends here

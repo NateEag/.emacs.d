23258efdcac2146a6c47794f31f12ae1892c5f35
Nate Eagleson <nate@nateeag.com>
2019-03-14 08:18:49 -0400

    Update package js2-mode-20190219.1100

--- elpa/js2-mode-20190307.1649/js2-mode-autoloads.el
+++ elpa/js2-mode-20190307.1649/js2-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (23661
-;;;;;;  21756 118807 291000))
+;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (23690
+;;;;;;  18086 5394 457000))
 ;;; Generated autoloads from js2-imenu-extras.el
 
 (autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
@@ -19,8 +19,8 @@ Toggle Imenu support for frameworks and structural patterns.
 
 ;;;***
 
-;;;### (autoloads nil "js2-mode" "js2-mode.el" (23661 21756 120227
-;;;;;;  6000))
+;;;### (autoloads nil "js2-mode" "js2-mode.el" (23690 18086 6713
+;;;;;;  222000))
 ;;; Generated autoloads from js2-mode.el
 
 (autoload 'js2-highlight-unused-variables-mode "js2-mode" "\
@@ -56,7 +56,7 @@ variables (`sgml-basic-offset' et al) locally, like so:
 ;;;***
 
 ;;;### (autoloads nil nil ("js2-mode-pkg.el" "js2-old-indent.el")
-;;;;;;  (23661 21756 121922 64000))
+;;;;;;  (23690 18086 8960 865000))
 
 ;;;***
 

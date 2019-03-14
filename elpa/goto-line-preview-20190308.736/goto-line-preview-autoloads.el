19f37cb47601027b239e0d5677d2b066a88c4215
Nate Eagleson <nate@nateeag.com>
2019-03-14 08:14:09 -0400

    Install goto-line-preview from MELPA

---
+++ elpa/goto-line-preview-20190308.736/goto-line-preview-autoloads.el
@@ -0,0 +1,25 @@
+;;; goto-line-preview-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "goto-line-preview" "goto-line-preview.el"
+;;;;;;  (23690 17704 919308 708000))
+;;; Generated autoloads from goto-line-preview.el
+
+(autoload 'goto-line-preview "goto-line-preview" "\
+Preview goto line.
+LINE-NUM : Target line number to navigate to.
+
+\(fn)" t nil)
+
+(define-obsolete-function-alias 'goto-line-preview-goto-line 'goto-line-preview)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; goto-line-preview-autoloads.el ends here

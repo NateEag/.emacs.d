d3ff3997c4fff6bdc9eeb42abc69305670927fbe
Nate Eagleson <nate@nateeag.com>
2019-03-26 08:42:28 -0400

    Update package expand-region-20190312.828

--- elpa/expand-region-20190326.727/expand-region-autoloads.el
+++ elpa/expand-region-20190326.727/expand-region-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "expand-region" "expand-region.el" (23690 18039
-;;;;;;  881513 590000))
+;;;### (autoloads nil "expand-region" "expand-region.el" (23706 7729
+;;;;;;  102252 295000))
 ;;; Generated autoloads from expand-region.el
 
 (autoload 'er/expand-region "expand-region" "\
@@ -20,7 +20,7 @@ before calling `er/expand-region' for the first time.
 ;;;***
 
 ;;;### (autoloads nil "expand-region-custom" "expand-region-custom.el"
-;;;;;;  (23690 18039 869946 434000))
+;;;;;;  (23706 7729 89925 184000))
 ;;; Generated autoloads from expand-region-custom.el
 
 (let ((loads (get 'expand-region 'custom-loads))) (if (member '"expand-region-custom" loads) nil (put 'expand-region 'custom-loads (cons '"expand-region-custom" loads))))
@@ -87,8 +87,8 @@ If set to nil, always place the cursor at the beginning of the region.")
 ;;;;;;  "latex-mode-expansions.el" "nxml-mode-expansions.el" "octave-expansions.el"
 ;;;;;;  "python-el-expansions.el" "python-el-fgallina-expansions.el"
 ;;;;;;  "python-mode-expansions.el" "ruby-mode-expansions.el" "sml-mode-expansions.el"
-;;;;;;  "subword-mode-expansions.el" "text-mode-expansions.el" "the-org-mode-expansions.el"
-;;;;;;  "web-mode-expansions.el") (23690 18039 883470 681000))
+;;;;;;  "text-mode-expansions.el" "the-org-mode-expansions.el" "web-mode-expansions.el")
+;;;;;;  (23706 7729 103166 191000))
 
 ;;;***
 

cec61f6ff7f4306113ff40c74f509f17c0e23a2a
Nate Eagleson <nate@nateeag.com>
2019-03-29 11:50:22 -0400

    Update package expand-region-20190326.727

--- elpa/expand-region-20190327.1228/expand-region-autoloads.el
+++ elpa/expand-region-20190327.1228/expand-region-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "expand-region" "expand-region.el" (23706 7729
-;;;;;;  102252 295000))
+;;;### (autoloads nil "expand-region" "expand-region.el" (23710 16058
+;;;;;;  885669 167000))
 ;;; Generated autoloads from expand-region.el
 
 (autoload 'er/expand-region "expand-region" "\
@@ -20,7 +20,7 @@ before calling `er/expand-region' for the first time.
 ;;;***
 
 ;;;### (autoloads nil "expand-region-custom" "expand-region-custom.el"
-;;;;;;  (23706 7729 89925 184000))
+;;;;;;  (23710 16058 874324 766000))
 ;;; Generated autoloads from expand-region-custom.el
 
 (let ((loads (get 'expand-region 'custom-loads))) (if (member '"expand-region-custom" loads) nil (put 'expand-region 'custom-loads (cons '"expand-region-custom" loads))))
@@ -77,6 +77,13 @@ If set to nil, always place the cursor at the beginning of the region.")
 
 (custom-autoload 'expand-region-smart-cursor "expand-region-custom" t)
 
+(define-obsolete-variable-alias 'er/enable-subword-mode\? 'expand-region-subword-enabled "2019-03-23")
+
+(defvar expand-region-subword-enabled nil "\
+Whether expand-region should use subword expansions.")
+
+(custom-autoload 'expand-region-subword-enabled "expand-region-custom" t)
+
 ;;;***
 
 ;;;### (autoloads nil nil ("cc-mode-expansions.el" "clojure-mode-expansions.el"
@@ -87,8 +94,8 @@ If set to nil, always place the cursor at the beginning of the region.")
 ;;;;;;  "latex-mode-expansions.el" "nxml-mode-expansions.el" "octave-expansions.el"
 ;;;;;;  "python-el-expansions.el" "python-el-fgallina-expansions.el"
 ;;;;;;  "python-mode-expansions.el" "ruby-mode-expansions.el" "sml-mode-expansions.el"
-;;;;;;  "text-mode-expansions.el" "the-org-mode-expansions.el" "web-mode-expansions.el")
-;;;;;;  (23706 7729 103166 191000))
+;;;;;;  "subword-mode-expansions.el" "text-mode-expansions.el" "the-org-mode-expansions.el"
+;;;;;;  "web-mode-expansions.el") (23710 16058 887748 655000))
 
 ;;;***
 

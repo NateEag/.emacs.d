9dae3d17cf41c4f13306649f112bad29af858b64
Nate Eagleson <nate@nateeag.com>
2019-04-22 11:46:29 -0400

    Update package auth-source-pass-20190408.1255

--- elpa/auth-source-pass-20190408.1255/auth-source-pass-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auth-source-pass" "20190408.1255" "Integrate auth-source with password-store" '((emacs "25")) :commit "96fd4f08cebf11310116fe057c6195e61512e71f" :authors '(("Damien Cassou" . "damien@cassou.me") ("Nicolas Petton" . "nicolas@petton.fr")) :maintainer '("Damien Cassou" . "damien@cassou.me") :url "https://github.com/DamienCassou/auth-password-store")

fe2ce4df7fd1e68aa7892c069aa5d20a5e522135
Nate Eagleson <nate@nateeag.com>
2019-04-22 11:46:51 -0400

    Update package lsp-ui-20190329.357

--- elpa/lsp-ui-20190329.357/lsp-ui-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; lsp-ui-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "lsp-ui" "lsp-ui.el" (23710 16063 631873 313000))
-;;; Generated autoloads from lsp-ui.el
-
-(autoload 'lsp-ui-mode "lsp-ui" "\
-Toggle language server UI mode on or off.
-‘lsp-ui-mode’ is a minor mode that contains a series of useful UI
-integrations for ‘lsp-mode’.  With a prefix argument ARG, enable
-language server UI mode if ARG is positive, and disable it
-otherwise.  If called from Lisp, enable the mode if ARG is
-omitted or nil, and toggle it if ARG is ‘toggle’.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("lsp-ui-doc.el" "lsp-ui-flycheck.el" "lsp-ui-imenu.el"
-;;;;;;  "lsp-ui-peek.el" "lsp-ui-pkg.el" "lsp-ui-sideline.el") (23710
-;;;;;;  16063 633762 695000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lsp-ui-autoloads.el ends here

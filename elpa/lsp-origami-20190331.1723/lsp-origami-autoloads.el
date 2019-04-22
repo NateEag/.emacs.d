7ba0b8a95754c61d614276ae3688eb9abc6faa11
Nate Eagleson <nate@nateeag.com>
2019-04-22 11:45:02 -0400

    Install lsp-origami mode from MELPA
    I'll have to actually try turning it on one of these days and seeing if
    I actually like it, but it's an interesting idea.

---
+++ elpa/lsp-origami-20190331.1723/lsp-origami-autoloads.el
@@ -0,0 +1,27 @@
+;;; lsp-origami-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lsp-origami" "lsp-origami.el" (0 0 0 0))
+;;; Generated autoloads from lsp-origami.el
+
+(autoload 'lsp-origami-mode "lsp-origami" "\
+Toggle code folding support for origami.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lsp-origami" '("lsp-origami--")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lsp-origami-autoloads.el ends here
---
+++ elpa/lsp-origami-20190331.1723/lsp-origami.el
@@ -0,0 +1,75 @@
+;;; lsp-origami.el --- origami.el support for lsp-mode    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Vibhav Pant <vibhavp@gmail.com>
+
+;; Author: Vibhav Pant
+;; Version: 0.1
+;; Package-Version: 20190331.1723
+;; Keywords: languages lsp-mode
+;; Package-Requires: ((origami "1.0") (lsp-mode "20190326.522"))
+;; URL: https://github.com/emacs-lsp/lsp-origami
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; lsp-origami provides support for origami.el using language server
+;; protocol's "textDocument/foldingRange" functionality.  It can be enabled
+;; with
+;; (require 'lsp-origami)
+;; (add-hook 'origami-mode-hook #'lsp-origami-mode)
+
+;;; Code:
+
+(require 'origami)
+(require 'lsp-mode)
+
+(defun lsp-origami--folding-range-to-fold (range create)
+  (funcall create
+	   (lsp--folding-range-beg range)
+	   (lsp--folding-range-end range)
+	   0
+	   (seq-map
+	    (lambda (range) (lsp-origami--folding-range-to-fold range create))
+	    (seq-remove (lambda (child-range)
+			  (or (eq (lsp--folding-range-beg child-range)
+				  (lsp--folding-range-beg range))
+			      (eq (lsp--folding-range-end child-range)
+				  (lsp--folding-range-end range))))
+			(lsp--folding-range-children range)))))
+
+(defun lsp-origami--parser (create)
+  "Get a list of Folding Ranges for the current buffer."
+  (lambda (_content)
+    (unless (lsp--capability "foldingRangeProvider")
+      (signal 'lsp-capability-not-supported (list "foldingRangeProvider")))
+    (seq-map (lambda (range)
+	       (lsp-origami--folding-range-to-fold range create))
+	     (lsp--get-nested-folding-ranges))))
+
+;;;###autoload
+(define-minor-mode lsp-origami-mode
+  "Toggle code folding support for origami."
+  :group 'lsp-origami
+  :global nil
+  (cond
+   (lsp-origami-mode
+    (setq-local origami-fold-style 'lsp-mode))
+   (t
+    (setq-local origami-fold-style nil))))
+
+(push '(lsp-mode . lsp-origami--parser) origami-parser-alist)
+
+(provide 'lsp-origami)
+;;; lsp-origami.el ends here

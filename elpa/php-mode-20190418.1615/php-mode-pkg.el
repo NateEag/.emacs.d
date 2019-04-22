9f3e24fe84d4665fe51811df6945e762aee1d34b
Nate Eagleson <nate@nateeag.com>
2019-04-22 11:47:17 -0400

    Update package php-mode-20190401.1742

--- elpa/php-mode-20190418.1615/php-mode-pkg.el
+++ elpa/php-mode-20190418.1615/php-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "php-mode" "20190401.1742" "Major mode for editing PHP code"
+(define-package "php-mode" "20190418.1615" "Major mode for editing PHP code"
   '((emacs "24.3")
     (cl-lib "0.5"))
   :keywords

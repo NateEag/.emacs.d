72747e86b9705744c6ac900e5a6f6a6dec4b1d04
Nate Eagleson <nate@nateeag.com>
2019-05-17 15:00:02 -0400

    Update package helm-core-20190422.835

--- elpa/helm-core-20190514.659/helm-core-pkg.el
+++ elpa/helm-core-20190514.659/helm-core-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm-core" "20190422.835" "Development files for Helm"
+(define-package "helm-core" "20190514.659" "Development files for Helm"
   '((emacs "24.4")
     (async "1.9.3"))
   :url "https://emacs-helm.github.io/helm/")

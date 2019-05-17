a8fb43a2f67793f0251ee97f0612eb50548cd501
Nate Eagleson <nate@nateeag.com>
2019-05-17 15:00:14 -0400

    Update package git-commit-20190413.1201

--- elpa/git-commit-20190501.1823/git-commit.el
+++ elpa/git-commit-20190501.1823/git-commit.el
@@ -12,7 +12,7 @@
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
 ;; Package-Requires: ((emacs "25.1") (dash "20180910") (with-editor "20181103"))
-;; Package-Version: 20190413.1201
+;; Package-Version: 20190501.1823
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 

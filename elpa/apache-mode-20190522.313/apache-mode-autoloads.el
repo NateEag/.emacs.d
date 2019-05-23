ad9378fe1c707427cb34fb682349fa0314747027
Nate Eagleson <nate@nateeag.com>
2019-05-23 08:51:07 -0400

    Update package apache-mode-20180724.351

--- elpa/apache-mode-20190522.313/apache-mode-autoloads.el
+++ elpa/apache-mode-20190522.313/apache-mode-autoloads.el
@@ -1,10 +1,12 @@
 ;;; apache-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "apache-mode" "apache-mode.el" (23400 19093
-;;;;;;  0 0))
+;;;### (autoloads nil "apache-mode" "apache-mode.el" (0 0 0 0))
 ;;; Generated autoloads from apache-mode.el
 
 (autoload 'apache-mode "apache-mode" "\
@@ -22,11 +24,14 @@ Major mode for editing Apache configuration files.
 
 (add-to-list 'auto-mode-alist '("/apache2/sites-\\(?:available\\|enabled\\)/" . apache-mode))
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "apache-mode" '("apache-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; apache-mode-autoloads.el ends here

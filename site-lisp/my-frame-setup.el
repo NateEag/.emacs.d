1d0b6c90061db48063e689dbf26d04b5a848ae08
Nate Eagleson <nate@nateeag.com>
2019-05-31 07:28:34 -0400

    Add a bunch of plumbing to my frame setup code
    It should be straightforward to compute window size correctly with the
    parts I've dropped in place here, I think.

--- site-lisp/my-frame-setup.el
+++ site-lisp/my-frame-setup.el
@@ -10,17 +10,28 @@
 
 ;;; Code:
 
+(defvar my-window-width 83
+  "The number of columns I like my emacs windows to have.
+
+The default value of 83 is a result of wrapping code at 80
+columns, having 1 extra column on each side for the fringe,
+and finally 1 more column of width for git status characters
+in the gutter.")
+
 (defun my-get-default-font-size ()
   "Return a good size for my default font based on monitor's pixel density."
 
   (let ((my-display-pixel-width (display-pixel-width))
         (my-display-mm-width (display-mm-width)))
     (if (and my-display-pixel-width my-display-mm-width)
-        ;; Guess a font size based on monitor resolution.
+        ;; Guess a font size based on monitor display density.
         (floor
          (*
           (/ (float (display-pixel-width)) (display-mm-width))
+          ;; Multiplier chosen experimentally - there's no smarts to this.
           4.44))
+
+      ;; If we can't compute display density, 14 pixels is a decent default.
       14)))
 
 (defun my-get-default-font-name ()
@@ -58,7 +69,7 @@
   ;; at once is handy.
   ;;
   ;; FIXME Take current window width into account. What I really care about is
-  ;; how many characters I can reasonably display on a given monitor.
+  ;; how many characters of width I can reasonably fit on a given monitor.
   (let* ((frame-screen-ratio
           (/ (float (frame-pixel-width (selected-frame))) (display-pixel-width)))
          (num-windows (if (> frame-screen-ratio 0.34)
@@ -67,14 +78,31 @@
     (my-set-frame-width-by-window-count num-windows)
     ))
 
+(defun default-font-width ()
+  "Return the width in pixels of a character in the current
+window's default font.
+
+More precisely, this returns the width of the letter ‘m’. If the
+font is mono-spaced, this will also be the width of all other
+printable characters.
+
+Yanked from https://emacs.stackexchange.com/a/5511/351."
+
+  (let ((window (selected-window))
+        (remapping face-remapping-alist))
+    (with-temp-buffer
+      (make-local-variable 'face-remapping-alist)
+      (setq face-remapping-alist remapping)
+      (set-window-buffer window (current-buffer))
+      (insert "m")
+      (aref (aref (font-get-glyphs (font-at 1) 1 2) 0) 4))))
+
 (defun my-set-frame-width-by-window-count (num-windows)
   "Set frame width by number of desired 80-char windows."
 
   (interactive "N")
 
-  ;; 2 extra chars for the fringe on either side, plus 1 for git
-  ;; status characters in the gutter.
-  (set-frame-width (selected-frame) (* 83 num-windows))
+  (set-frame-width (selected-frame) (* my-window-width num-windows))
 
   ;; HACK I never use more than two windows.
  (if (= 2 num-windows)

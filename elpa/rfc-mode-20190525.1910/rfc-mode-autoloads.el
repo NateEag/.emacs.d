50c7c7746f04f7348922556f8bcb8ef67d63ff83
Nate Eagleson <nate@nateeag.com>
2019-06-18 13:51:09 -0400

    Install rfc-mode from MELPA

---
+++ elpa/rfc-mode-20190525.1910/rfc-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; rfc-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "rfc-mode" "rfc-mode.el" (0 0 0 0))
+;;; Generated autoloads from rfc-mode.el
+
+(autoload 'rfc-mode "rfc-mode" "\
+Major mode to browse and read RFC documents.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("rfc[0-9]+\\.txt\\'" . rfc-mode))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rfc-mode" '("rfc-mode-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rfc-mode-autoloads.el ends here
---
+++ elpa/rfc-mode-20190525.1910/rfc-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rfc-mode" "20190525.1910" "RFC document browser and viewer" '((emacs "25.1") (helm "3.2")) :commit "e7d9e2f4cb9ff302231efe57af6fdc201e5b48f0" :authors '(("Nicolas Martyanoff" . "khaelin@gmail.com")) :maintainer '("Nicolas Martyanoff" . "khaelin@gmail.com") :url "https://github.com/galdor/rfc-mode")

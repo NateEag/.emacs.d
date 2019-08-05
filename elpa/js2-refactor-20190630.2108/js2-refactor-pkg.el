a3445b77362243ec8168a33fa5b1fa99e384528e
Nate Eagleson <nate@nateeag.com>
2019-08-05 12:37:13 -0400

    Update package js2-refactor-20180502.1042

--- elpa/js2-refactor-20190630.2108/js2-refactor-pkg.el
+++ elpa/js2-refactor-20190630.2108/js2-refactor-pkg.el
@@ -1,4 +1,4 @@
-(define-package "js2-refactor" "20180502.1042" "A JavaScript refactoring library for emacs."
+(define-package "js2-refactor" "20190630.2108" "A JavaScript refactoring library for emacs."
   '((js2-mode "20101228")
     (s "1.9.0")
     (multiple-cursors "1.0.0")

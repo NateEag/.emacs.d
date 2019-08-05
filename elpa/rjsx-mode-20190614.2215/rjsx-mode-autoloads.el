b59eb317d402038e344a5f44ce65c2a3b05ee721
Nate Eagleson <nate@nateeag.com>
2019-08-05 12:38:29 -0400

    Update package rjsx-mode-20190312.117

--- elpa/rjsx-mode-20190614.2215/rjsx-mode-autoloads.el
+++ elpa/rjsx-mode-20190614.2215/rjsx-mode-autoloads.el
@@ -1,10 +1,12 @@
 ;;; rjsx-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "rjsx-mode" "rjsx-mode.el" (23690 18084 271762
-;;;;;;  599000))
+;;;### (autoloads nil "rjsx-mode" "rjsx-mode.el" (0 0 0 0))
 ;;; Generated autoloads from rjsx-mode.el
 
 (autoload 'rjsx-mode "rjsx-mode" "\
@@ -33,11 +35,14 @@ instead.
 
 \(fn ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rjsx-mode" '("rjsx-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; rjsx-mode-autoloads.el ends here

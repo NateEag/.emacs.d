e3a5323b4449c6c0d7f4e1c1ab32b7673710b778
Nate Eagleson <nate@nateeag.com>
2019-08-05 12:36:35 -0400

    Update package evil-magit-20190224.2028

--- elpa/evil-magit-20190620.153/evil-magit-autoloads.el
+++ elpa/evil-magit-20190620.153/evil-magit-autoloads.el
@@ -1,10 +1,12 @@
 ;;; evil-magit-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "evil-magit" "evil-magit.el" (23667 61960 903827
-;;;;;;  206000))
+;;;### (autoloads nil "evil-magit" "evil-magit.el" (0 0 0 0))
 ;;; Generated autoloads from evil-magit.el
 
 (autoload 'evil-magit-init "evil-magit" "\
@@ -20,11 +22,14 @@ Revert changes by evil-magit that affect default evil+magit behavior.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-magit" '("evil-magit-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; evil-magit-autoloads.el ends here

3c95185254ddfae75a2321b2ef91fbbf05ec276d
Nate Eagleson <nate@nateeag.com>
2019-08-05 12:37:08 -0400

    Update package helm-projectile-20190408.1508

--- elpa/helm-projectile-20190731.1538/helm-projectile-autoloads.el
+++ elpa/helm-projectile-20190731.1538/helm-projectile-autoloads.el
@@ -1,10 +1,13 @@
 ;;; helm-projectile-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (23727
-;;;;;;  12866 34154 964000))
+;;;### (autoloads nil "helm-projectile" "helm-projectile.el" (0 0
+;;;;;;  0 0))
 ;;; Generated autoloads from helm-projectile.el
 
 (defvar helm-projectile-fuzzy-match t "\
@@ -71,11 +74,14 @@ If invoked outside of a project, displays a list of known projects to jump.
 
 (eval-after-load 'projectile '(progn (define-key projectile-command-map (kbd "h") #'helm-projectile)))
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-projectile" '("helm-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; helm-projectile-autoloads.el ends here

039db83004374efb585f556820b9dd5692e85d37
Nate Eagleson <nate@nateeag.com>
2019-08-13 13:04:31 -0400

    Install ts package from MELPA
    I'm not sure I'll ever use it, but heaven knows working with datetimes
    using Emacs' built-in libraries is a pain.
    
    Perhaps this could ease those pain points.

---
+++ elpa/ts-20190812.1655/ts-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ts" "20190812.1655" "Timestamp and date/time library" '((emacs "26.1") (dash "2.14.1") (s "1.12.0")) :commit "31bd5a86aa35f7b8143170892ffaf6425284f3fd" :keywords '("date" "time" "timestamp") :authors '((nil . "Adam Porter <adam@alphapapa.net")) :maintainer '(nil . "Adam Porter <adam@alphapapa.net") :url "http://github.com/alphapapa/ts.el")

2cff71360c2c0abb03a104e4b4cb5c44ddd5b3c7
Nate Eagleson <nate@nateeag.com>
2019-08-19 11:20:50 -0400

    Update package cider-20190813.1406

--- elpa/cider-20190817.800/cider-pkg.el
+++ elpa/cider-20190817.800/cider-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cider" "20190813.1406" "Clojure Interactive Development Environment that Rocks"
+(define-package "cider" "20190817.800" "Clojure Interactive Development Environment that Rocks"
   '((emacs "25")
     (clojure-mode "5.9")
     (parseedn "0.1")

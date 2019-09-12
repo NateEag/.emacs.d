dc504c8e70af11efa58e244d916ea18c6f56fce5
Nate Eagleson <nate@nateeag.com>
2019-09-12 12:06:16 -0400

    Update package crontab-mode-20190304.1423

--- elpa/crontab-mode-20190827.1300/crontab-mode.el
+++ elpa/crontab-mode-20190827.1300/crontab-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Mario Rodas <marsam@users.noreply.github.com>
 ;; URL: https://github.com/emacs-pe/crontab-mode
-;; Package-Version: 20190304.1423
+;; Package-Version: 20190827.1300
 ;; Keywords: languages
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "24"))
@@ -31,6 +31,7 @@
 ;; Major mode for crontab(5) files
 
 ;;; Code:
+(eval-when-compile (require 'cl-lib))
 (require 'sh-script)
 
 (defgroup crontab nil
@@ -68,6 +69,9 @@
   "Face to use for crontab predefined definitions."
   :group 'crontab)
 
+(defvar crontab-fields '("minute (0-59)" "hour (0-23)" "day (1-31)" "month (1-12)" "day-of-week (0-6)" "command")
+  "Fields used by `crontab-eldoc-function' to show the crontab information.")
+
 (eval-and-compile
   (defconst crontab-rx-constituents
     ;; https://en.wikipedia.org/wiki/Cron#CRON_expression
@@ -99,7 +103,7 @@
     ;;  │ │ │ │ │
     ;;  │ │ │ │ │
     ;;  * * * * *  command to execute
-    (,(crontab-rx line-start
+    (,(crontab-rx line-start (* space)
                   (group unit (? step)) (+ space) ; minutes
                   (group unit (? step)) (+ space) ; hours
                   (group (or (seq unit (? step)) "?" "L" "W")) (+ space) ; day of month
@@ -133,6 +137,22 @@
   (interactive)
   (indent-line-to 0))
 
+(defun crontab-eldoc-function ()
+  "`eldoc-documentation-function' for Crontab."
+  (let* ((point (point))
+         (end-of-line (point-at-eol))
+         (fields (copy-sequence crontab-fields))
+         (n (save-excursion
+              (beginning-of-line)
+              (cl-loop while (re-search-forward "[^[:space:]]+" end-of-line t)
+                       with field = -1
+                       do (cl-incf field)
+                       if (or (>= (point) point) (>= field 5))
+                       return field))))
+    (when n
+      (setcar (nthcdr n fields) (propertize (elt fields n) 'face 'font-lock-constant-face)))
+    (mapconcat 'identity fields "  ")))
+
 ;;;###autoload
 (define-derived-mode crontab-mode text-mode "Crontab"
   "Major mode for editing crontab file.
@@ -141,6 +161,12 @@
   :syntax-table sh-mode-syntax-table
   (set (make-local-variable 'comment-start) "# ")
   (set (make-local-variable 'comment-start-skip) "#+\\s-*")
+
+  (if (null eldoc-documentation-function) ; Emacs<25
+      (set (make-local-variable 'eldoc-documentation-function)
+           #'crontab-eldoc-function)
+    (add-function :before-until (local 'eldoc-documentation-function)
+                  #'crontab-eldoc-function))
 
   (set (make-local-variable 'font-lock-defaults)
        '(crontab-font-lock-keywords nil t))

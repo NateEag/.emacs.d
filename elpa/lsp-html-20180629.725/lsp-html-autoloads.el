8e4603f3103cdd9ea08b6db8f8beac81754fc357
Nate Eagleson <nate@nateeag.com>
2019-09-16 15:10:33 -0400

    Remove lsp-css and lsp-html packages
    They've been pulled into the core lsp-mode package and having the old
    ones installed was causing errors that prevented lsp-mode from starting
    in php-mode.
    
    So away they go.

--- elpa/lsp-html-20180629.725/lsp-html-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; lsp-html-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("lsp-html.el") (23400 19234 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lsp-html-autoloads.el ends here
--- elpa/lsp-html-20180629.725/lsp-html-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-html" "20180629.725" "HTML support for lsp-mode" '((lsp-mode "4.2")) :commit "53b3c30511cab7e5f1e4ad15094b407b27cdc7f5" :keywords '("languages" "html" "lsp") :authors '(("Vibhav Pant" . "vibhavp@gmail.com")) :maintainer '("Vibhav Pant" . "vibhavp@gmail.com") :url "https://github.com/emacs-lsp/lsp-html")

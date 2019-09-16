d5b29f0490de62e803469c7ab29b0b417c9a6153
Nate Eagleson <nate@nateeag.com>
2019-09-16 15:29:58 -0400

    Remove old lsp-php package
    I had it deleted when I confirmed that my PHP config was working, so
    obviously I don't need it any more.

--- elpa/lsp-php-20180331.1644/lsp-php-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; lsp-php-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("lsp-php.el") (23569 15770 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; lsp-php-autoloads.el ends here

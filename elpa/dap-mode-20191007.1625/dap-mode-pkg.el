8838643dbe59e11d6a1538a3ce07a19db3b116bf
Nate Eagleson <nate@nateeag.com>
2019-10-11 08:53:08 -0400

    Update package dap-mode-20190927.1418

--- elpa/dap-mode-20191007.1625/dap-mode-pkg.el
+++ elpa/dap-mode-20191007.1625/dap-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dap-mode" "20190927.1418" "Debug Adapter Protocol mode"
+(define-package "dap-mode" "20191007.1625" "Debug Adapter Protocol mode"
   '((emacs "25.1")
     (dash "2.14.1")
     (lsp-mode "6.0")

a14635d71b7e073c353113d9875636027e6bc9b7
Nate Eagleson <nate@nateeag.com>
2019-12-13 10:05:51 -0500

    Update package ghub-20191007.1420

--- elpa/ghub-20191212.2202/ghub-pkg.el
+++ elpa/ghub-20191212.2202/ghub-pkg.el
@@ -1,6 +1,5 @@
-(define-package "ghub" "20191007.1420" "Minuscule client libraries for Git forge APIs."
+(define-package "ghub" "20191212.2202" "Minuscule client libraries for Git forge APIs."
   '((emacs "25.1")
-    (dash "2.14.1")
     (let-alist "1.0.5")
     (treepy "0.1.1")))
 ;; Local Variables:

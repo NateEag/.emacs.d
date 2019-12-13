a62ec5bfeb04dc9fadb81defccc3c2562553bdf0
Nate Eagleson <nate@nateeag.com>
2019-12-13 11:09:41 -0500

    Update package async-20191009.1018

--- elpa/async-20191030.2138/async-pkg.el
+++ elpa/async-20191030.2138/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20191009.1018" "Asynchronous processing in Emacs" 'nil :keywords
+(define-package "async" "20191030.2138" "Asynchronous processing in Emacs" 'nil :keywords
   '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:

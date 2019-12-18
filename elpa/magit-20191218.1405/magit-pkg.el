bd671817b0309f6ff8835002d03a3ed3c112d42b
Nate Eagleson <nate@nateeag.com>
2019-12-18 14:50:07 -0500

    Update package magit-20191210.1617

--- elpa/magit-20191218.1405/magit-pkg.el
+++ elpa/magit-20191218.1405/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20191210.1617" "A Git porcelain inside Emacs."
+(define-package "magit" "20191218.1405" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")

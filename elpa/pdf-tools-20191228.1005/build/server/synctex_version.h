5dae3b806ec564a8d2ecbf1da6cc9d366b601d3b
Nate Eagleson <nate@nateeag.com>
2019-12-30 10:38:40 -0500

    Update package pdf-tools-20191128.1731

--- elpa/pdf-tools-20191228.1005/build/server/synctex_version.h
+++ elpa/pdf-tools-20191228.1005/build/server/synctex_version.h
@@ -1,16 +1,15 @@
-/* 
-Copyright (c) 2008, 2009, 2010 , 2011 jerome DOT laurens AT u-bourgogne DOT fr
+/*
+Copyright (c) 2008-2017 jerome DOT laurens AT u-bourgogne DOT fr
 
-This file is part of the SyncTeX package.
+This file is part of the __SyncTeX__ package.
 
-Latest Revision: Tue Jun 14 08:23:30 UTC 2011
+[//]: # (Latest Revision: Fri Jul 14 16:20:41 UTC 2017)
+[//]: # (Version: 1.21)
 
-Version: 1.18
+See `synctex_parser_readme.md` for more details
 
-See synctex_parser_readme.txt for more details
+## License
 
-License:
---------
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without
@@ -32,14 +31,29 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE
 
-Except as contained in this notice, the name of the copyright holder  
-shall not be used in advertising or otherwise to promote the sale,  
-use or other dealings in this Software without prior written  
+Except as contained in this notice, the name of the copyright holder
+shall not be used in advertising or otherwise to promote the sale,
+use or other dealings in this Software without prior written
 authorization from the copyright holder.
 
+## Acknowledgments:
+
+The author received useful remarks from the __pdfTeX__ developers, especially Hahn The Thanh,
+and significant help from __XeTeX__ developer Jonathan Kew.
+
+## Nota Bene:
+
+If you include or use a significant part of the __SyncTeX__ package into a software,
+I would appreciate to be listed as contributor and see "__SyncTeX__" highlighted.
 */
 
-/* This local header file is for TEXLIVE, use your own header to fit your system */
-#   include <w2c/c-auto.h> /* for inline && HAVE_xxx */
-/*	No inlining for synctex tool in texlive. */
-#	define SYNCTEX_INLINE
+#ifndef __SYNCTEX_VERSION__
+#   define __SYNCTEX_VERSION__
+
+#   define SYNCTEX_VERSION_MAJOR 1
+
+#   define SYNCTEX_VERSION_STRING "1.21"
+
+#   define SYNCTEX_CLI_VERSION_STRING "1.5"
+
+#endif

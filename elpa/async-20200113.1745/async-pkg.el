f07b80950ae274b50394304603055f9b5691c516
Nate Eagleson <nate@nateeag.com>
2020-01-20 22:07:28 -0500

    Update package async-20191226.1227

--- elpa/async-20200113.1745/async-pkg.el
+++ elpa/async-20200113.1745/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20191226.1227" "Asynchronous processing in Emacs"
+(define-package "async" "20200113.1745" "Asynchronous processing in Emacs"
   '((emacs "24.3"))
   :keywords
   '("async")

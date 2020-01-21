18b39cfd3be6385bcd0397385d44ffbc11c9b15b
Nate Eagleson <nate@nateeag.com>
2020-01-20 22:07:26 -0500

    Update package with-editor-20191222.1939

--- elpa/with-editor-20200102.2147/with-editor-pkg.el
+++ elpa/with-editor-20200102.2147/with-editor-pkg.el
@@ -1,4 +1,4 @@
-(define-package "with-editor" "20191222.1939" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20200102.2147" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4")
     (async "1.9"))
   :keywords

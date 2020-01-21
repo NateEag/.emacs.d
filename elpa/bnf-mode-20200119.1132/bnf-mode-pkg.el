2e01289a10f2bb69f723d09c764dfd321b475b80
Nate Eagleson <nate@nateeag.com>
2020-01-20 22:05:04 -0500

    Update package bnf-mode-20190611.519

--- elpa/bnf-mode-20200119.1132/bnf-mode-pkg.el
+++ elpa/bnf-mode-20200119.1132/bnf-mode-pkg.el
@@ -1,12 +1,12 @@
-(define-package "bnf-mode" "20190611.519" "Major mode for editing BNF grammars."
+(define-package "bnf-mode" "20200119.1132" "Major mode for editing BNF grammars."
   '((cl-lib "0.5")
     (emacs "24.3"))
   :keywords
   '("languages")
   :authors
-  '(("Serghei Iakovlev" . "sadhooklay@gmail.com"))
+  '(("Serghei Iakovlev" . "egrep@protonmail.ch"))
   :maintainer
-  '("Serghei Iakovlev" . "sadhooklay@gmail.com")
+  '("Serghei Iakovlev" . "egrep@protonmail.ch")
   :url "https://github.com/sergeyklay/bnf-mode")
 ;; Local Variables:
 ;; no-byte-compile: t

7fa8bf3273028076abf48eab61db901ff98c483a
Nate Eagleson <nate@nateeag.com>
2020-01-27 14:13:00 -0500

    Update package python-mode-20191220.1543

--- elpa/python-mode-20191220.1543/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20191220.1543" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/python-mode-20200125.1842/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20200125.1842" "Python major mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

ff046dd0b1dfcbcc8d2c5bfb6d32bb4fc16e9008
Nate Eagleson <nate@nateeag.com>
2020-01-29 09:29:15 -0500

    Update package dap-mode-20200126.1952

--- elpa/dap-mode-20200128.1942/dap-mode-pkg.el
+++ elpa/dap-mode-20200128.1942/dap-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dap-mode" "20200126.1952" "Debug Adapter Protocol mode"
+(define-package "dap-mode" "20200128.1942" "Debug Adapter Protocol mode"
   '((emacs "25.1")
     (dash "2.14.1")
     (lsp-mode "6.0")

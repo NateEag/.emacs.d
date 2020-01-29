141ec69184d1ab347511098b654ea343e9dda02d
Nate Eagleson <nate@nateeag.com>
2020-01-29 09:29:20 -0500

    Update package helm-20200126.1804

--- elpa/helm-20200127.1826/helm-pkg.el
+++ elpa/helm-20200127.1826/helm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm" "20200126.1804" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20200127.1826" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9.4")
     (popup "0.5.3")

a1cf4ca7642dbd3fac0f158be58fa355a990602d
Nate Eagleson <nate@nateeag.com>
2020-01-30 12:41:01 -0500

    Update package esup-20200127.1709

--- elpa/esup-20200129.1533/esup-pkg.el
+++ elpa/esup-20200129.1533/esup-pkg.el
@@ -1,4 +1,4 @@
-(define-package "esup" "20200127.1709" "the Emacs StartUp Profiler (ESUP)"
+(define-package "esup" "20200129.1533" "the Emacs StartUp Profiler (ESUP)"
   '((cl-lib "0.5")
     (emacs "25"))
   :keywords
@@ -6,7 +6,7 @@
   :authors
   '(("Joe Schafer" . "joe@jschaf.com"))
   :maintainer
-  '("Joe Schafer" . "joe@jschaf.com")
+  '("Serghei Iakovlev" . "egrep@protonmail.ch")
   :url "http://github.com/jschaf/esup")
 ;; Local Variables:
 ;; no-byte-compile: t

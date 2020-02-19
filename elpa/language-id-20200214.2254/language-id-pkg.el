2218c45f5b7ee7de981c31568d5472ebfbb602ae
Nate Eagleson <nate@nateeag.com>
2020-02-19 09:48:27 -0500

    Update package format-all-20200206.1440

---
+++ elpa/language-id-20200214.2254/language-id-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/neagleson/.emacs.d/elpa/language-id-20200214.2254/language-id.el  -*- no-byte-compile: t -*-
+(define-package "language-id" "20200214.2254" "Library to work with programming language identifiers" '((emacs "24") (cl-lib "0.5")) :commit "dc18099dd74a954601ab425ef6cfd3ed32003023" :keywords '("languages" "util") :authors '(("Lassi Kortela" . "lassi@lassi.io")) :maintainer '("Lassi Kortela" . "lassi@lassi.io") :url "https://github.com/lassik/emacs-language-id")

30977ff3e8403eed240a00b73561c2704dcad73a
Nate Eagleson <nate@nateeag.com>
2020-03-05 17:03:50 -0500

    Update package lv-20200122.1200

---
+++ elpa/lv-20200227.1301/lv-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/neagleson/.emacs.d/elpa/lv-20200227.1301/lv.el  -*- no-byte-compile: t -*-
+(define-package "lv" "20200227.1301" "Other echo area" 'nil :commit "370a5c613ef4b1b48dbd1bd2133c575990fd3093" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))

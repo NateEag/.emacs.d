88a3e0d82c4a351e9978067346956548c209d28d
Nate Eagleson <nate@nateeag.com>
2020-04-09 09:45:22 -0400

    Update package anzu-20190303.1701

---
+++ elpa/anzu-20200331.1717/anzu-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/neagleson/.emacs.d/elpa/anzu-20200331.1717/anzu.el  -*- no-byte-compile: t -*-
+(define-package "anzu" "20200331.1717" "Show number of matches in mode-line while searching" '((emacs "24.3")) :commit "3e34fb3df53c0c68e842fa179c327a7395d1901d" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Neil Okamoto" . "neil.okamoto+melpa@gmail.com") :url "https://github.com/emacsorphanage/anzu")

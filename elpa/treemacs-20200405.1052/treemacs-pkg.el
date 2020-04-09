090aca965b41b261d5b30cc53780bfa522351c9b
Nate Eagleson <nate@nateeag.com>
2020-04-09 09:48:38 -0400

    Update package treemacs-20200305.1135

--- elpa/treemacs-20200405.1052/treemacs-pkg.el
+++ elpa/treemacs-20200405.1052/treemacs-pkg.el
@@ -1,4 +1,4 @@
-(define-package "treemacs" "20200305.1135" "A tree style file explorer package"
+(define-package "treemacs" "20200405.1052" "A tree style file explorer package"
   '((emacs "25.2")
     (cl-lib "0.5")
     (dash "2.11.0")

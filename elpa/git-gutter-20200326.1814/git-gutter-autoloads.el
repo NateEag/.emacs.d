07b0eac6f45925f5d1433c35e8cd8cacbeb8f71d
Nate Eagleson <nate@nateeag.com>
2020-04-09 09:45:51 -0400

    Update package git-gutter-20161105.1356

--- elpa/git-gutter-20200326.1814/git-gutter-autoloads.el
+++ elpa/git-gutter-20200326.1814/git-gutter-autoloads.el
@@ -1,21 +1,28 @@
 ;;; git-gutter-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "git-gutter" "git-gutter.el" (23400 19180 0
-;;;;;;  0))
+;;;### (autoloads nil "git-gutter" "git-gutter.el" (0 0 0 0))
 ;;; Generated autoloads from git-gutter.el
 
 (autoload 'git-gutter:linum-setup "git-gutter" "\
-Setup for linum-mode.
-
-\(fn)" nil nil)
+Setup for linum-mode." nil nil)
 
 (autoload 'git-gutter-mode "git-gutter" "\
 Git-Gutter mode
 
+If called interactively, enable Git-Gutter mode if ARG is positive, and disable
+it if ARG is zero or negative.  If called from Lisp, also enable the mode if
+ARG is omitted or nil, and toggle it if ARG is `toggle'; disable the mode
+otherwise.
+
 \(fn &optional ARG)" t nil)
+
+(put 'global-git-gutter-mode 'globalized-minor-mode t)
 
 (defvar global-git-gutter-mode nil "\
 Non-nil if Global Git-Gutter mode is enabled.
@@ -40,14 +47,12 @@ See `git-gutter-mode' for more information on Git-Gutter mode.
 \(fn &optional ARG)" t nil)
 
 (autoload 'git-gutter "git-gutter" "\
-Show diff information in gutter
-
-\(fn)" t nil)
+Show diff information in gutter" t nil)
 
 (autoload 'git-gutter:toggle "git-gutter" "\
-Toggle to show diff information.
+Toggle to show diff information." t nil)
 
-\(fn)" t nil)
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "git-gutter" '("git-gutter")))
 
 ;;;***
 
@@ -55,5 +60,6 @@ Toggle to show diff information.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; git-gutter-autoloads.el ends here

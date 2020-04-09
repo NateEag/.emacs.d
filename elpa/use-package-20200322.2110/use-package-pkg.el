6269ad1bc3bdb4bea9faca6c5f99ca6ffaeddcec
Nate Eagleson <nate@nateeag.com>
2020-04-09 09:48:47 -0400

    Update package use-package-20191126.2034

--- elpa/use-package-20200322.2110/use-package-pkg.el
+++ elpa/use-package-20200322.2110/use-package-pkg.el
@@ -1,4 +1,4 @@
-(define-package "use-package" "20191126.2034" "A configuration macro for simplifying your .emacs"
+(define-package "use-package" "20200322.2110" "A configuration macro for simplifying your .emacs"
   '((emacs "24.3")
     (bind-key "2.4"))
   :keywords

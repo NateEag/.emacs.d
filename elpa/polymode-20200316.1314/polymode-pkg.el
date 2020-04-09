9ee3c95ef89274fdb3754938c0598a79382c2d61
Nate Eagleson <nate@nateeag.com>
2020-04-09 09:47:08 -0400

    Update package polymode-20200304.1118

--- elpa/polymode-20200316.1314/polymode-pkg.el
+++ elpa/polymode-20200316.1314/polymode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "polymode" "20200304.1118" "Extensible framework for multiple major modes"
+(define-package "polymode" "20200316.1314" "Extensible framework for multiple major modes"
   '((emacs "25"))
   :keywords
   '("languages" "multi-modes" "processes")

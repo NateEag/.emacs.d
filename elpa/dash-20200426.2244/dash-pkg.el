b978cbcd554d80f8e3ec291d7e959722c26cf59d
Nate Eagleson <nate@nateeag.com>
2020-04-28 13:40:26 -0400

    Update package dash-20200119.2310

--- elpa/dash-20200426.2244/dash-pkg.el
+++ elpa/dash-20200426.2244/dash-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dash" "20200119.2310" "A modern list library for Emacs" 'nil :keywords
+(define-package "dash" "20200426.2244" "A modern list library for Emacs" 'nil :keywords
   '("lists")
   :authors
   '(("Magnar Sveen" . "magnars@gmail.com"))

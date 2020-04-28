df23945952630db9a9ce534c768a2ed3ba1831b5
Nate Eagleson <nate@nateeag.com>
2020-04-28 13:36:33 -0400

    Update package lsp-treemacs-20200409.411

--- elpa/lsp-treemacs-20200414.407/lsp-treemacs-pkg.el
+++ elpa/lsp-treemacs-20200414.407/lsp-treemacs-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-treemacs" "20200409.411" "LSP treemacs"
+(define-package "lsp-treemacs" "20200414.407" "LSP treemacs"
   '((emacs "25.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")

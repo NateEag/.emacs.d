9e9c515a5a5fcb5d28e38ad657e3ce66dc126621
Nate Eagleson <nate@nateeag.com>
2020-04-28 13:37:04 -0400

    Update package notmuch-20200406.1017

--- elpa/notmuch-20200406.1017/notmuch-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "notmuch" "20200406.1017" "run notmuch within emacs" 'nil :url "https://notmuchmail.org/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/notmuch-20200427.1036/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20200427.1036" "run notmuch within emacs" 'nil :url "https://notmuchmail.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

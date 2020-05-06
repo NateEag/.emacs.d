fb1024e24b061cc15588ed2d7887207ee94eb340
Nate Eagleson <nate@nateeag.com>
2020-05-06 10:44:50 -0400

    Install message-attachment-reminder mode
    I have made this mistake occasionally, and wouldn't mind being stopped
    from making it again.

---
+++ elpa/message-attachment-reminder-20200428.124/message-attachment-reminder-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/neagleson/.emacs.d/elpa/message-attachment-reminder-20200428.124/message-attachment-reminder.el  -*- no-byte-compile: t -*-
+(define-package "message-attachment-reminder" "20200428.124" "Remind if missing attachment" '((emacs "24.1")) :commit "a8b761d665c17694a04eccf1c2bc135d35bdf482" :authors '(("Alex Murray" . "murray.alex@gmail.com")) :maintainer '("Alex Murray" . "murray.alex@gmail.com") :url "https://github.com/alexmurray/message-attachment-reminder")

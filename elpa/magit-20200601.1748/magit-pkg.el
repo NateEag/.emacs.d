3a36b5f23d4f40aff9a12844c860b55d4f24c26e
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:18:59 -0400

    Update magit-20200601.1748 from MELPA

---
+++ elpa/magit-20200601.1748/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20200601.1748" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20180527")
+    (dash "20180910")
+    (git-commit "20181104")
+    (transient "20190812")
+    (with-editor "20181103"))
+  :commit "035c24055c0e111187d554a0a214d6f0cbfb4bf1" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

f7ec2bc0bc037dbcc73d627d6f7b25377dfdeba7
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:28:51 -0400

    Update package haskell-mode-20200408.247

--- elpa/haskell-mode-20200531.2255/haskell-mode-pkg.el
+++ elpa/haskell-mode-20200531.2255/haskell-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "haskell-mode" "20200408.247" "A Haskell editing mode"
+(define-package "haskell-mode" "20200531.2255" "A Haskell editing mode"
   '((emacs "25.1"))
-  :keywords
+  :commit "41683c0e634bb3f54eac8747919a82132e1714fe" :keywords
   '("haskell" "cabal" "ghc" "repl")
   :url "https://github.com/haskell/haskell-mode")
 ;; Local Variables:

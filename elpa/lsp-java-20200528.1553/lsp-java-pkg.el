1f069d11040be8c1b16a321423bd685495c1863c
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:29:14 -0400

    Update package lsp-java-20200425.1842

--- elpa/lsp-java-20200528.1553/lsp-java-pkg.el
+++ elpa/lsp-java-20200528.1553/lsp-java-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-java" "20200425.1842" "Java support for lsp-mode"
+(define-package "lsp-java" "20200528.1553" "Java support for lsp-mode"
   '((emacs "25.1")
     (lsp-mode "6.0")
     (markdown-mode "2.3")
@@ -8,7 +8,7 @@
     (dash-functional "1.2.0")
     (request "0.3.0")
     (treemacs "2.5"))
-  :keywords
+  :commit "75d89ec07448b4fb479719a5ff613a9e69b2aaa4" :keywords
   '("languague" "tools")
   :url "https://github.com/emacs-lsp/lsp-java")
 ;; Local Variables:

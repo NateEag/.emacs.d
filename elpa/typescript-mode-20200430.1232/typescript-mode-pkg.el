2ec97416805993f3808a8fdb3d30fc1d1fb69536
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:33:12 -0400

    Update package typescript-mode-20200312.2235

--- elpa/typescript-mode-20200430.1232/typescript-mode-pkg.el
+++ elpa/typescript-mode-20200430.1232/typescript-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "typescript-mode" "20200312.2235" "Major mode for editing typescript"
+(define-package "typescript-mode" "20200430.1232" "Major mode for editing typescript"
   '((emacs "24.3"))
-  :keywords
+  :commit "0fc729787007b5111f3584034af0f3ef2389098f" :keywords
   '("typescript" "languages")
   :url "http://github.com/ananthakumaran/typescript.el")
 ;; Local Variables:

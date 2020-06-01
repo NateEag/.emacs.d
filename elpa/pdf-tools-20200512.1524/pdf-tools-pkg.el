773a731e8a4b2d084994b93f9b985cca95e10b84
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:30:07 -0400

    Update package pdf-tools-20200427.2136

--- elpa/pdf-tools-20200512.1524/pdf-tools-pkg.el
+++ elpa/pdf-tools-20200512.1524/pdf-tools-pkg.el
@@ -1,8 +1,8 @@
-(define-package "pdf-tools" "20200427.2136" "Support library for PDF documents."
+(define-package "pdf-tools" "20200512.1524" "Support library for PDF documents."
   '((emacs "24.3")
     (tablist "1.0")
     (let-alist "1.0.4"))
-  :keywords
+  :commit "c510442ab89c8a9e9881230eeb364f4663f59e76" :keywords
   '("files" "multimedia")
   :authors
   '(("Andreas Politz" . "politza@fh-trier.de"))

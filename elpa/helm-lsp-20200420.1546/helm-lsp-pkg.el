9af0ec1a6cd206c784d8ebc7f3db085f084b1fe0
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:28:57 -0400

    Update package helm-lsp-20200420.1546

--- elpa/helm-lsp-20200420.1546/helm-lsp-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/neagleson/.emacs.d/elpa/helm-lsp-20200420.1546/helm-lsp.el  -*- no-byte-compile: t -*-
-(define-package "helm-lsp" "20200420.1546" "LSP helm integration" '((emacs "25.1") (dash "2.14.1") (lsp-mode "5.0") (helm "2.0")) :commit "db243993ea70be6343949759b4f2c556d9130751" :keywords '("languages" "debug") :authors '(("Ivan Yonchovski" . "yyoncho@gmail.com")) :maintainer '("Ivan Yonchovski" . "yyoncho@gmail.com") :url "https://github.com/yyoncho/helm-lsp")

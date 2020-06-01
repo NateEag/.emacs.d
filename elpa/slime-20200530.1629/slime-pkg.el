d1af542fe49aaf75700177ad318df046fdfdf2a6
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:31:28 -0400

    Update package slime-20200414.1444

--- elpa/slime-20200530.1629/slime-pkg.el
+++ elpa/slime-20200530.1629/slime-pkg.el
@@ -1,7 +1,7 @@
-(define-package "slime" "20200414.1444" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20200530.1629" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
-  :keywords
+  :commit "840e013d449a9b14e77a659291a44b746a4f1601" :keywords
   '("languages" "lisp" "slime")
   :url "https://github.com/slime/slime")
 ;; Local Variables:

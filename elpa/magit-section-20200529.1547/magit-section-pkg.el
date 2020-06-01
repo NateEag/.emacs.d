799567a1da2e2f21fb3c8b7bf9d9284af7c3c0e4
Nate Eagleson <nate@nateeag.com>
2020-06-01 15:29:37 -0400

    Update package magit-section-20200425.2044

--- elpa/magit-section-20200529.1547/magit-section-pkg.el
+++ elpa/magit-section-20200529.1547/magit-section-pkg.el
@@ -1,7 +1,7 @@
-(define-package "magit-section" "20200425.2044" "Sections for read-only buffers"
+(define-package "magit-section" "20200529.1547" "Sections for read-only buffers"
   '((emacs "25.1")
     (dash "20180910"))
-  :keywords
+  :commit "035c24055c0e111187d554a0a214d6f0cbfb4bf1" :keywords
   '("tools")
   :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))

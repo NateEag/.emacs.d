a6ba72335a4edeb57f6ece1e99e06fc9175c66df
Nate Eagleson <nate@nateeag.com>
2020-06-16 10:41:05 -0400

    Update package js2-mode-20200601.1226

--- elpa/js2-mode-20200610.1339/js2-mode-pkg.el
+++ elpa/js2-mode-20200610.1339/js2-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "js2-mode" "20200601.1226" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20200610.1339" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
-  :commit "a6e130500f0bedce283173dcee7b06966fabc4c6" :keywords
+  :commit "90e1434146988e855ade79c5acefc156f6420d7a" :keywords
   '("languages" "javascript")
   :authors
   '(("Steve Yegge" . "steve.yegge@gmail.com")
--- elpa/js2-mode-20200610.1339/js2-mode.el
+++ elpa/js2-mode-20200610.1339/js2-mode.el
@@ -161,6 +161,21 @@
             window alert confirm document java navigator prompt screen
             self top requestAnimationFrame cancelAnimationFrame
 
+            ;; Window or WorkerGlobalScope with support in Chromium and Firefox:
+            ;; https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope
+            ;; properties
+            caches indexedDB isSecureContext origin
+            ;; methods
+            atob btoa clearInterval clearTimeout
+            createImageBitmap fetch queueMicrotask setInterval
+            setTimeout
+
+            ;; from jslint "browser"
+            history location
+
+            ;; HTML element constructors
+            Audio Image Option
+
             ;; W3C CSS
             CSSCharsetRule CSSFontFace CSSFontFaceRule
             CSSImportRule CSSMediaRule CSSPageRule
@@ -7687,7 +7702,8 @@ For instance, processing a nested scope requires a parent function node."
   (let (result fn parent-qname p elem)
     (dolist (entry js2-imenu-recorder)
       ;; function node goes first
-      (cl-destructuring-bind (current-fn &rest (&whole chain head &rest)) entry
+      (cl-destructuring-bind
+          (current-fn &rest (&whole chain head &rest rest)) entry
         ;; Examine head's defining scope:
         ;; Pre-processed chain, or top-level/external, keep as-is.
         (if (or (stringp head) (js2-node-top-level-decl-p head))
@@ -10963,8 +10979,8 @@ expression)."
       ;; Clear out any lookahead tokens (possibly wrong modifier).
       ;; FIXME: Deal with this problem in a more systematic fashion.
       ;; Perhaps by making this modifier affect not how the token
-      ;; struct is constructed, but when js2-get-token returns based
-      ;; on it.
+      ;; struct value is constructed, but what js2-get-token returns
+      ;; based on it.
       (when (> js2-ti-lookahead 0)
         (setq js2-ti-lookahead 0)
         (setq js2-ts-cursor (js2-current-token-end)))
@@ -11669,7 +11685,7 @@ highlighting features of `js2-mode'."
         (let ((inhibit-read-only t))
           (erase-buffer)
           (dolist (err all-errs)
-            (cl-destructuring-bind ((msg-key beg _end &rest) type line) err
+            (cl-destructuring-bind ((msg-key beg _end &rest rest) type line) err
               (insert-text-button
                (format "line %d: %s" line (js2-get-msg msg-key))
                'face type

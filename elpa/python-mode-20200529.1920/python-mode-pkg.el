47979291ea06b6dabb4080ee8af09b219ef527a5
Nate Eagleson <nate@nateeag.com>
2020-06-16 10:40:43 -0400

    Update package python-mode-20200529.1920

--- elpa/python-mode-20200529.1920/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20200529.1920" "Python major mode" 'nil :commit "c029de567c99e4b42458da2661b27556276f5cab")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/python-mode-20200608.737/python-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "python-mode" "20200608.737" "Python major mode" 'nil :commit "6658e5d768db0c0d8dbc6fdedff3a85b65327061")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

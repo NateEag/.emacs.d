ece88f8a75383f62d6aa2fc92e3399b19661f734
Nate Eagleson <nate@nateeag.com>
2020-06-16 10:39:45 -0400

    Update package jq-mode-20200516.1606

--- elpa/jq-mode-20200604.833/jq-mode-pkg.el
+++ elpa/jq-mode-20200604.833/jq-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "jq-mode" "20200516.1606" "Edit jq scripts."
+(define-package "jq-mode" "20200604.833" "Edit jq scripts."
   '((emacs "25.1"))
-  :commit "85214664a9f20faab7c45087b28bedd0feabb560" :authors
+  :commit "42ad0a99f0114233e2cb317585cb9af494d18a2f" :authors
   '(("Bjarte Johansen <Bjarte dot Johansen at gmail dot com>"))
   :maintainer
   '("Bjarte Johansen <Bjarte dot Johansen at gmail dot com>")

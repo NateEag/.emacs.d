fccd46919a0fb5ff9494ec2efb9546724aac4d10
Nate Eagleson <nate@nateeag.com>
2020-06-16 10:42:04 -0400

    Update package flycheck-20200527.1502

--- elpa/flycheck-20200610.1809/flycheck-pkg.el
+++ elpa/flycheck-20200610.1809/flycheck-pkg.el
@@ -1,10 +1,10 @@
-(define-package "flycheck" "20200527.1502" "On-the-fly syntax checking"
+(define-package "flycheck" "20200610.1809" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
     (seq "1.11")
     (emacs "24.3"))
-  :commit "ee0937ffe23ea859f5547f81756257c12e2c34d4" :keywords
+  :commit "c02cd773dded0215f9417ec04dfe8dabda63ef43" :keywords
   '("convenience" "languages" "tools")
   :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))

f1badf5e23b39b02f624b22291ac9a30eb2aa993
Nate Eagleson <nate@nateeag.com>
2020-07-08 14:28:25 -0400

    Update package ghub-20200425.2233

--- elpa/ghub-20200707.845/ghub-autoloads.el
+++ elpa/ghub-20200707.845/ghub-autoloads.el
@@ -16,23 +16,6 @@
 ;;;### (autoloads nil "ghub" "ghub.el" (0 0 0 0))
 ;;; Generated autoloads from ghub.el
 
-(autoload 'ghub-create-token "ghub" "\
-Create, store and return a new token.
-
-HOST is the Github instance, usually \"api.github.com\".
-USERNAME is the name of a user on that instance.
-PACKAGE is the package that will use the token.
-SCOPES are the scopes the token is given access to.
-
-\(fn HOST USERNAME PACKAGE SCOPES)" t nil)
-
-(autoload 'ghub-token-scopes "ghub" "\
-Return and echo the scopes of the specified token.
-This is intended for debugging purposes only.  The user
-has to provide several values including their password.
-
-\(fn HOST USERNAME PACKAGE)" t nil)
-
 (autoload 'ghub-clear-caches "ghub" "\
 Clear all caches that might negatively affect Ghub.
 

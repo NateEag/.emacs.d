5977fed5d983bb9f36ec841fad12a573481dbff6
Nate Eagleson <nate@nateeag.com>
2020-07-08 14:31:23 -0400

    Update package with-editor-20200612.2210

--- elpa/with-editor-20200617.1234/with-editor-pkg.el
+++ elpa/with-editor-20200617.1234/with-editor-pkg.el
@@ -1,7 +1,7 @@
-(define-package "with-editor" "20200612.2210" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20200617.1234" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4")
     (async "1.9"))
-  :commit "48ca9bb49a1a7a37e85606de9d327a14030d4380" :keywords
+  :commit "7c512887c6d69864fb600d32fb92857c51babcff" :keywords
   '("tools")
   :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))

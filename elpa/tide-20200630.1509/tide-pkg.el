8e2b991e97c2abd80201289f9db062be783fd07f
Nate Eagleson <nate@nateeag.com>
2020-07-08 14:31:00 -0400

    Update package tide-20200523.1218

--- elpa/tide-20200630.1509/tide-pkg.el
+++ elpa/tide-20200630.1509/tide-pkg.el
@@ -1,10 +1,10 @@
-(define-package "tide" "20200523.1218" "Typescript Interactive Development Environment"
+(define-package "tide" "20200630.1509" "Typescript Interactive Development Environment"
   '((dash "2.10.0")
     (s "1.11.0")
     (flycheck "27")
     (typescript-mode "0.1")
     (cl-lib "0.5"))
-  :commit "13e7af77b6867ffca6eecc5b3b3b5e315518f49c" :keywords
+  :commit "5717214a3bf647b35a30fd1513aac1f60b820cbb" :keywords
   '("typescript")
   :authors
   '(("Anantha kumaran" . "ananthakumaran@gmail.com"))

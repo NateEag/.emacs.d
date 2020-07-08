362a23280b24f4b597d767d64c94295dc5cd579c
Nate Eagleson <nate@nateeag.com>
2020-07-08 14:28:41 -0400

    Update package lsp-ui-20200607.2009

--- elpa/lsp-ui-20200703.448/lsp-ui-pkg.el
+++ elpa/lsp-ui-20200703.448/lsp-ui-pkg.el
@@ -1,10 +1,10 @@
-(define-package "lsp-ui" "20200607.2009" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20200703.448" "UI modules for lsp-mode"
   '((emacs "26.1")
     (dash "2.14")
     (dash-functional "1.2.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-  :commit "d92cf83d95c9ca177b735500ead88cf68dc2e2db" :keywords
+  :commit "7d5326430eb88a58e111cb22ffa42c7d131e5052" :keywords
   '("languages" "tools")
   :authors
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))

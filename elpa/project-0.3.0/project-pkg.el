b9e552392f85628b8cbf97ac47a27ebc719ee58a
Nate Eagleson <nate@nateeag.com>
2020-07-08 14:29:15 -0400

    Update package project-0.3.0

--- elpa/project-0.3.0/project-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/neagleson/.emacs.d/elpa/project-0.3.0/project.el  -*- no-byte-compile: t -*-
-(define-package "project" "0.3.0" "Operations on the current project" '((emacs "26.3")) :url "http://elpa.gnu.org/packages/project.html")

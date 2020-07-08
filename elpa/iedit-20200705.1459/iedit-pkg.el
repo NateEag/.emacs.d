caf109107beb21c1b823c925a12439cdbaeea9a1
Nate Eagleson <nate@nateeag.com>
2020-07-08 14:30:52 -0400

    Update package iedit-20200412.756

--- elpa/iedit-20200705.1459/iedit-pkg.el
+++ elpa/iedit-20200705.1459/iedit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "iedit" "20200412.756" "Edit multiple regions in the same way simultaneously." 'nil :keywords
+(define-package "iedit" "20200705.1459" "Edit multiple regions in the same way simultaneously." 'nil :commit "7eef34898f64ef57078a65737e12f8bd355ec389" :keywords
   '("occurrence" "region" "simultaneous" "refactoring")
   :authors
   '(("Victor Ren" . "victorhge@gmail.com"))

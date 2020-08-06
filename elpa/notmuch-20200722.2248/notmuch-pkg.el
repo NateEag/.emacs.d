e09fc9e1bd6903f7930f831bca4c71951574f3ed
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:33:28 -0400

    Update package notmuch-20200606.1055

---
+++ elpa/notmuch-20200722.2248/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20200722.2248" "run notmuch within emacs" 'nil :commit "0e4695abaa7bda20d92813571ad53fb141f3e38c" :url "https://notmuchmail.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

c5b9b2cb485ee2754a5603694106a2d164b9f641
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:33:43 -0400

    Update package prettier-20200704.858

--- elpa/prettier-20200714.859/prettier-pkg.el
+++ elpa/prettier-20200714.859/prettier-pkg.el
@@ -1,8 +1,8 @@
-(define-package "prettier" "20200704.858" "Code formatting with Prettier"
+(define-package "prettier" "20200714.859" "Code formatting with Prettier"
   '((emacs "26.1")
     (iter2 "0.9")
     (nvm "0.2"))
-  :commit "d5ccedc7a8ed84aff292bce688c73648dc8f567f" :keywords
+  :commit "000244b82ca9e2118376f6a44506bac8ab225677" :keywords
   '("convenience" "languages" "files")
   :authors
   '(("Julian Scheid" . "julians37@gmail.com"))

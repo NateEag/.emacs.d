2c76e1cde0ee75e63b9c0cda0f419b48a2958f8f
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:35:43 -0400

    Update package use-package-20200629.1856

--- elpa/use-package-20200721.2156/use-package-pkg.el
+++ elpa/use-package-20200721.2156/use-package-pkg.el
@@ -1,7 +1,7 @@
-(define-package "use-package" "20200629.1856" "A configuration macro for simplifying your .emacs"
+(define-package "use-package" "20200721.2156" "A configuration macro for simplifying your .emacs"
   '((emacs "24.3")
     (bind-key "2.4"))
-  :commit "bfc5ecd69cdf71fae0af722d90336c66a6415f03" :keywords
+  :commit "4fb1f9a68f1e7e7d614652afc017a6652fd029f1" :keywords
   '("dotemacs" "startup" "speed" "config" "package")
   :authors
   '(("John Wiegley" . "johnw@newartisans.com"))

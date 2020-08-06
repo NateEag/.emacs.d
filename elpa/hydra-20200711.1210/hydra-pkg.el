ffc48e58157859a4487f483c8d75a226d1528f46
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:35:40 -0400

    Update package hydra-20200608.1528

--- elpa/hydra-20200711.1210/hydra-pkg.el
+++ elpa/hydra-20200711.1210/hydra-pkg.el
@@ -1,7 +1,7 @@
-(define-package "hydra" "20200608.1528" "Make bindings that stick around."
+(define-package "hydra" "20200711.1210" "Make bindings that stick around."
   '((cl-lib "0.5")
     (lv "0"))
-  :commit "8a9124f80b6919ad5288172b3e9f46c5332763ca" :keywords
+  :commit "112e689f75b59398d8eca79ac6562dca12b8a959" :keywords
   '("bindings")
   :authors
   '(("Oleh Krehel" . "ohwoeowho@gmail.com"))

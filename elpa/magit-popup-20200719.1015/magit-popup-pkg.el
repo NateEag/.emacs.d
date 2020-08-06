7fd5c5261767d2261a59cc5ea24d18b827ad7ae2
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:33:02 -0400

    Update package magit-popup-20200306.223

--- elpa/magit-popup-20200719.1015/magit-popup-pkg.el
+++ elpa/magit-popup-20200719.1015/magit-popup-pkg.el
@@ -1,7 +1,7 @@
-(define-package "magit-popup" "20200306.223" "Define prefix-infix-suffix command combos"
+(define-package "magit-popup" "20200719.1015" "Define prefix-infix-suffix command combos"
   '((emacs "24.4")
     (dash "2.13.0"))
-  :commit "b8e886c4f2242d6c58f84d4549af712e86360db1" :keywords
+  :commit "d8585fa39f88956963d877b921322530257ba9f5" :keywords
   '("bindings")
   :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))

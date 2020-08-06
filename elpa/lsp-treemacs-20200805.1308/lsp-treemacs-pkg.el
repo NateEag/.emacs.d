2188037e3a3d635d605f6e3697eb954ff545d917
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:32:47 -0400

    Update package lsp-treemacs-20200701.2035

---
+++ elpa/lsp-treemacs-20200805.1308/lsp-treemacs-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-treemacs" "20200805.1308" "LSP treemacs"
+  '((emacs "25.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (treemacs "2.5")
+    (lsp-mode "6.0"))
+  :commit "57e49627e58f026de99ed84e394fb5eedbf06a93" :keywords
+  '("languages")
+  :authors
+  '(("Ivan Yonchovski"))
+  :maintainer
+  '("Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-treemacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

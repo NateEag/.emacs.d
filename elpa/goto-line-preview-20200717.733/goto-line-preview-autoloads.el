c7e4515d4cd6915ca41ee3f609c7411396e38ee4
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:32:34 -0400

    Update package goto-line-preview-20190308.736

--- elpa/goto-line-preview-20200717.733/goto-line-preview-autoloads.el
+++ elpa/goto-line-preview-20200717.733/goto-line-preview-autoloads.el
@@ -1,19 +1,21 @@
 ;;; goto-line-preview-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "goto-line-preview" "goto-line-preview.el"
-;;;;;;  (23690 17704 919308 708000))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from goto-line-preview.el
 
 (autoload 'goto-line-preview "goto-line-preview" "\
-Preview goto line.
-LINE-NUM : Target line number to navigate to.
-
-\(fn)" t nil)
+Preview goto line." t nil)
 
 (define-obsolete-function-alias 'goto-line-preview-goto-line 'goto-line-preview)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "goto-line-preview" '("goto-line-preview-")))
 
 ;;;***
 
@@ -21,5 +23,6 @@ LINE-NUM : Target line number to navigate to.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; goto-line-preview-autoloads.el ends here

83b44ffccad2ee650802de444b182f75b0b43447
Nate Eagleson <nate@nateeag.com>
2020-08-06 00:35:46 -0400

    Update package dash-20200524.1947

--- elpa/dash-20200803.1520/dash-pkg.el
+++ elpa/dash-20200803.1520/dash-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dash" "20200524.1947" "A modern list library for Emacs" 'nil :commit "ea4a4cc7cce7c3b93862a22df8bca8b83052ccbf" :keywords
+(define-package "dash" "20200803.1520" "A modern list library for Emacs" 'nil :commit "b92ab5a39b987e4fe69317b9d9fda452300baf20" :keywords
   '("lists")
   :authors
   '(("Magnar Sveen" . "magnars@gmail.com"))

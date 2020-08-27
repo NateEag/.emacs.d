64e351afb7b1f7743dcb61685e2206d4d86e29e6
Nate Eagleson <nate@nateeag.com>
2020-08-27 18:40:39 -0400

    Update package nix-mode-20200728.1945

--- elpa/nix-mode-20200811.1947/nix-mode-pkg.el
+++ elpa/nix-mode-20200811.1947/nix-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "nix-mode" "20200728.1945" "Major mode for editing .nix files"
+(define-package "nix-mode" "20200811.1947" "Major mode for editing .nix files"
   '((emacs "25"))
-  :commit "a00b3f776de65b5af7571976a73213ca2a5c683e" :keywords
+  :commit "0cf1ea1e0ed330b59f47056d927797e625ba8f53" :keywords
   '("nix" "languages" "tools" "unix")
   :maintainer
   '("Matthew Bauer" . "mjbauer95@gmail.com")

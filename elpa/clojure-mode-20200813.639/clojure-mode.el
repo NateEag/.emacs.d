a2499003f8e824f24a317b010ed8c7d785c83da7
Nate Eagleson <nate@nateeag.com>
2020-08-27 18:23:20 -0400

    Update package clojure-mode-20200419.559

--- elpa/clojure-mode-20200813.639/clojure-mode.el
+++ elpa/clojure-mode-20200813.639/clojure-mode.el
@@ -9,9 +9,10 @@
 ;;       Bozhidar Batsov <bozhidar@batsov.com>
 ;;       Artur Malabarba <bruce.connor.am@gmail.com>
 ;; URL: http://github.com/clojure-emacs/clojure-mode
-;; Package-Version: 20200419.559
+;; Package-Version: 20200813.639
+;; Package-Commit: 84ed16c5ddb6561620886485e20669d0c81f88a1
 ;; Keywords: languages clojure clojurescript lisp
-;; Version: 5.12.0-snapshot
+;; Version: 5.12.0
 ;; Package-Requires: ((emacs "25.1"))
 
 ;; This file is not part of GNU Emacs.

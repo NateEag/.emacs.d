9e0d75dfdb26ee0d1a389b91dcce2adbacec3f67
Nate Eagleson <nate@nateeag.com>
2020-08-27 18:11:40 -0400

    Update package auto-complete-20170125.245

--- elpa/auto-complete-20200823.1758/auto-complete-autoloads.el
+++ elpa/auto-complete-20200823.1758/auto-complete-autoloads.el
@@ -1,10 +1,12 @@
 ;;; auto-complete-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "auto-complete" "auto-complete.el" (23400 19389
-;;;;;;  0 0))
+;;;### (autoloads nil "auto-complete" "auto-complete.el" (0 0 0 0))
 ;;; Generated autoloads from auto-complete.el
 
 (autoload 'auto-complete "auto-complete" "\
@@ -15,7 +17,14 @@ Start auto-completion at current point.
 (autoload 'auto-complete-mode "auto-complete" "\
 AutoComplete mode
 
+If called interactively, enable Auto-Complete mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
+
+(put 'global-auto-complete-mode 'globalized-minor-mode t)
 
 (defvar global-auto-complete-mode nil "\
 Non-nil if Global Auto-Complete mode is enabled.
@@ -39,21 +48,21 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "auto-complete" '("ac-" "auto-complete-mode")))
+
 ;;;***
 
 ;;;### (autoloads nil "auto-complete-config" "auto-complete-config.el"
-;;;;;;  (23400 19389 0 0))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from auto-complete-config.el
 
-(autoload 'ac-config-default "auto-complete-config" "\
+(autoload 'ac-config-default "auto-complete-config" nil nil nil)
 
-
-\(fn)" nil nil)
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "auto-complete-config" '("ac-")))
 
 ;;;***
 
-;;;### (autoloads nil nil ("auto-complete-pkg.el") (23400 19390 0
-;;;;;;  0))
+;;;### (autoloads nil nil ("auto-complete-pkg.el") (0 0 0 0))
 
 ;;;***
 
@@ -61,5 +70,6 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; auto-complete-autoloads.el ends here

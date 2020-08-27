55983d250c181b8196f94f869fbc3915a8908cab
Nate Eagleson <nate@nateeag.com>
2020-08-27 19:00:59 -0400

    Update package evil-matchit-20200803.1205

--- elpa/evil-matchit-20200812.1050/evil-matchit-pkg.el
+++ elpa/evil-matchit-20200812.1050/evil-matchit-pkg.el
@@ -1,7 +1,7 @@
-(define-package "evil-matchit" "20200803.1205" "Vim matchit ported to Evil"
+(define-package "evil-matchit" "20200812.1050" "Vim matchit ported to Evil"
   '((evil "1.2.0")
     (emacs "24.4"))
-  :commit "ae8fddea392441229ed8c3038f7c0ea94dbac9dc" :keywords
+  :commit "574d3c4e4517d3bb10bf652cacf459376ae401fe" :keywords
   '("matchit" "vim" "evil")
   :authors
   '(("Chen Bin <chenbin DOT sh AT gmail DOT com>"))

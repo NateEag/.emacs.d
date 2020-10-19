6e63ac4323f74f811363d0ca6d5c2f912e363eca
Nate Eagleson <nate@nateeag.com>
2020-10-19 09:12:06 -0400

    Update package buttercup-20200817.2001

--- elpa/buttercup-20200817.2001/buttercup-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "buttercup" "20200817.2001" "Behavior-Driven Emacs Lisp Testing" 'nil :commit "0e5eae0766a33b5c8997e1477e3914d5c8ba3d29")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/buttercup-20201016.2014/buttercup-pkg.el
@@ -0,0 +1,4 @@
+(define-package "buttercup" "20201016.2014" "Behavior-Driven Emacs Lisp Testing" 'nil :commit "f6f93353405cda51ad2778ae9247f77265b7cbfb")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

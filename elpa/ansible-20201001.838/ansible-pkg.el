b0169e29ed944e3a4e67211774284221c8b17fd0
Nate Eagleson <nate@nateeag.com>
2020-10-19 09:08:52 -0400

    Update package ansible-20200704.1354

--- elpa/ansible-20201001.838/ansible-pkg.el
+++ elpa/ansible-20201001.838/ansible-pkg.el
@@ -1,7 +1,7 @@
-(define-package "ansible" "20200704.1354" "Ansible minor mode"
+(define-package "ansible" "20201001.838" "Ansible minor mode"
   '((s "1.9.0")
     (f "0.16.2"))
-  :commit "b5ef59406604bc5027f4d816d90e633feef0149c" :authors
+  :commit "cf6b8f06c2628357fc2a72ea9817a2c2d0ebf690" :authors
   '(("k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>"))
   :maintainer
   '("k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>")

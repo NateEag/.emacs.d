86a603a34a998a60eeaa1cd71f10a1c8549fa161
Nate Eagleson <nate@nateeag.com>
2020-10-19 09:06:51 -0400

    Update package lispy-20200818.1151

--- elpa/lispy-20201013.1250/lispy-pkg.el
+++ elpa/lispy-20201013.1250/lispy-pkg.el
@@ -1,11 +1,11 @@
-(define-package "lispy" "20200818.1151" "vi-like Paredit"
+(define-package "lispy" "20201013.1250" "vi-like Paredit"
   '((emacs "24.3")
     (ace-window "0.9.0")
     (iedit "0.9.9")
     (counsel "0.11.0")
     (hydra "0.14.0")
     (zoutline "0.1.0"))
-  :commit "0a9dcfdfbc20cadbb9cb29b224dc64b8efdd7b70")
+  :commit "8d994a0ef2182dba732f2b81d2b247bc8717b3e2")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:

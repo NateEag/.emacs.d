ce8d842ed10d61acfc47f9f0899a982a826aa029
Nate Eagleson <nate@nateeag.com>
2020-10-29 10:22:58 -0400

    Install tree-sitter-langs package

---
+++ elpa/tree-sitter-langs-20201026.602/tree-sitter-langs-pkg.el
@@ -0,0 +1,13 @@
+(define-package "tree-sitter-langs" "20201026.602" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.9.2"))
+  :commit "f5065c86b067577d162850af21598921128bd33b" :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :url "https://github.com/ubolonton/emacs-tree-sitter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

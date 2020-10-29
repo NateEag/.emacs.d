f21be03f8d45172a1b2369c30c854cbf7aef7743
Nate Eagleson <nate@nateeag.com>
2020-10-29 10:25:20 -0400

    Add tsc package

---
+++ elpa/tsc-20201014.1431/tsc-pkg.el
@@ -0,0 +1,13 @@
+(define-package "tsc" "20201014.1431" "Core Tree-sitter APIs"
+  '((emacs "25.1"))
+  :commit "f5065c86b067577d162850af21598921128bd33b" :keywords
+  '("languages" "tools" "parsers" "dynamic-modules" "tree-sitter")
+  :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+    ("Jorge Javier Araya Navarro" . "jorgejavieran@yahoo.com.mx"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :url "https://github.com/ubolonton/emacs-tree-sitter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

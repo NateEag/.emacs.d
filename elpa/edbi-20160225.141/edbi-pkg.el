22f35b2c2f92da6e34299e8407804e2c9812e611
Nate Eagleson <nate@nateeag.com>
2020-11-04 22:43:48 -0500

    Install edbi package

---
+++ elpa/edbi-20160225.141/edbi-pkg.el
@@ -0,0 +1,8 @@
+(define-package "edbi" "20160225.141" "Emacs Database Interface"
+  '((concurrent "0.3.1")
+    (ctable "0.1.2")
+    (epc "0.1.1"))
+  :commit "6f50aaf4bde75255221f2292c7a4ad3fa9d918c0")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

199cfa53c63b2a63a2cfdbe2c462f62eae9f1e52
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:04:52 -0500

    Update package cider-20201018.1453

---
+++ elpa/cider-20210104.915/cider-pkg.el
@@ -0,0 +1,24 @@
+(define-package "cider" "20210104.915" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "25")
+    (clojure-mode "5.12")
+    (parseedn "0.2")
+    (pkg-info "0.4")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.22")
+    (sesman "0.3.2"))
+  :commit "3fac28541e03812990c771bd774bf8ea65c228c9" :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.com")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.com")
+  :keywords
+  '("languages" "clojure" "cider")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

5346c5b94c2d3e258b80aadc94ffc3ab95bb3c8d
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:10:49 -0500

    Update package multiple-cursors-20201018.538

--- elpa/multiple-cursors-20201018.538/multiple-cursors-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "multiple-cursors" "20201018.538" "Multiple cursors for Emacs."
-  '((cl-lib "0.5"))
-  :commit "7763f4f9d7b9d2a4eb08a1f94699edaeb4f433ed")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:

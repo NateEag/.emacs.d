3f8ea9f176461db1f21bc2a4a07b7838f901a4a0
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:05:16 -0500

    Update package slime-20200810.224

--- elpa/slime-20210105.2148/slime-pkg.el
+++ elpa/slime-20210105.2148/slime-pkg.el
@@ -1,7 +1,7 @@
-(define-package "slime" "20200810.224" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20210105.2148" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
-  :commit "fb12bac676ab51b75be19197e21ab4674479d627" :keywords
+  :commit "48bfe6cccfdf879cd7137b00eb2ca160665a92f8" :keywords
   '("languages" "lisp" "slime")
   :url "https://github.com/slime/slime")
 ;; Local Variables:

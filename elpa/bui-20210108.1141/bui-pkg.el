a35a5d3fded358340d0e0db4c7a9262685512efd
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:14:11 -0500

    Update package bui-20200426.2219

--- elpa/bui-20210108.1141/bui-pkg.el
+++ elpa/bui-20210108.1141/bui-pkg.el
@@ -1,12 +1,12 @@
-(define-package "bui" "20200426.2219" "Buffer interface library"
+(define-package "bui" "20210108.1141" "Buffer interface library"
   '((emacs "24.3")
     (dash "2.11.0"))
-  :commit "28a9b0a36cb69b931cf32db7546ad5c6589752cf" :keywords
-  '("tools")
-  :authors
+  :commit "f3a137628e112a91910fd33c0cff0948fa58d470" :authors
   '(("Alex Kost" . "alezost@gmail.com"))
   :maintainer
   '("Alex Kost" . "alezost@gmail.com")
+  :keywords
+  '("tools")
   :url "https://github.com/alezost/bui.el")
 ;; Local Variables:
 ;; no-byte-compile: t

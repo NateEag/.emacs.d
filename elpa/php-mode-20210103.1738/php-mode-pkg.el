076b86a0dd1c72982348f563afd5dcb0f72af33d
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:08:42 -0500

    Update package php-mode-20200812.1129

--- elpa/php-mode-20210103.1738/php-mode-pkg.el
+++ elpa/php-mode-20210103.1738/php-mode-pkg.el
@@ -1,11 +1,11 @@
-(define-package "php-mode" "20200812.1129" "Major mode for editing PHP code"
+(define-package "php-mode" "20210103.1738" "Major mode for editing PHP code"
   '((emacs "24.3"))
-  :commit "f4c7c6995dadcdc6da5fefadfd362f8418b2eec1" :keywords
-  '("languages" "php")
-  :authors
+  :commit "8cdc727e6d4eef81655b90574784e9540f407cda" :authors
   '(("Eric James Michael Ritz"))
   :maintainer
   '("USAMI Kenta" . "tadsan@zonu.me")
+  :keywords
+  '("languages" "php")
   :url "https://github.com/emacs-php/php-mode")
 ;; Local Variables:
 ;; no-byte-compile: t

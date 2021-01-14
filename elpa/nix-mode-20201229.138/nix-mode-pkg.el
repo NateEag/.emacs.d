e4f34e65d1d63eaa716051708d4d4483538aec82
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:10:35 -0500

    Update package nix-mode-20200915.1537

---
+++ elpa/nix-mode-20201229.138/nix-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "nix-mode" "20201229.138" "Major mode for editing .nix files"
+  '((emacs "25.1"))
+  :commit "53ea839a52335d089699d3530bae8ea5914cdbb6" :maintainer
+  '("Matthew Bauer" . "mjbauer95@gmail.com")
+  :keywords
+  '("nix" "languages" "tools" "unix")
+  :url "https://github.com/NixOS/nix-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

d74a2c41bcb0fd4565a0bbecf0f7e2e32e5c96b4
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:15:53 -0500

    Update package haskell-mode-20200929.2119

--- elpa/haskell-mode-20201230.342/haskell-customize.el
+++ elpa/haskell-mode-20201230.342/haskell-customize.el
@@ -172,7 +172,7 @@ pass additional flags to `ghc'."
 
 (defcustom haskell-process-do-cabal-format-string
   ":!cd %s && %s"
-  "The way to run cabal comands. It takes two arguments -- the directory and the command.
+  "The way to run cabal commands. It takes two arguments -- the directory and the command.
 See `haskell-process-do-cabal' for more details."
   :group 'haskell-interactive
   :type 'string)
@@ -409,9 +409,9 @@ imports."
 presence of a *.cabal file or stack.yaml file or something similar.")
 
 (defun haskell-build-type ()
-  "Looks for cabal and stack spec files. 
-   When found, returns a pair (TAG . DIR) 
-   where TAG is 'cabal-project, 'cabal-sandbox. 'cabal, or 'stack; 
+  "Looks for cabal and stack spec files.
+   When found, returns a pair (TAG . DIR)
+   where TAG is 'cabal-project, 'cabal-sandbox. 'cabal, or 'stack;
    and DIR is the directory containing cabal or stack file.
    When none found, DIR is nil, and TAG is 'ghc"
   ;; REVIEW maybe just 'cabal is enough.

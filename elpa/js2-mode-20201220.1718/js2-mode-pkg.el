245b9fdabb6074800532fd5ecfa350da3b85218e
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:06:15 -0500

    Update package js2-mode-20200725.112

--- elpa/js2-mode-20201220.1718/js2-mode-pkg.el
+++ elpa/js2-mode-20201220.1718/js2-mode-pkg.el
@@ -1,14 +1,14 @@
-(define-package "js2-mode" "20200725.112" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20201220.1718" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
-  :commit "40aab27581279d0fdbfeb9afeb85f39d401a927f" :keywords
-  '("languages" "javascript")
-  :authors
+  :commit "29979e5f3301796ba606759e39ee0b1b6a2a24f3" :authors
   '(("Steve Yegge" . "steve.yegge@gmail.com")
     ("mooz" . "stillpedant@gmail.com")
     ("Dmitry Gutov" . "dgutov@yandex.ru"))
   :maintainer
   '("Steve Yegge" . "steve.yegge@gmail.com")
+  :keywords
+  '("languages" "javascript")
   :url "https://github.com/mooz/js2-mode/")
 ;; Local Variables:
 ;; no-byte-compile: t

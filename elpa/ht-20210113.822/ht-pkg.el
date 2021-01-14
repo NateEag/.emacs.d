acb956c8aa0191a642a39b403c3629c128fe371c
Nate Eagleson <nate@nateeag.com>
2021-01-14 10:59:26 -0500

    Update package ht-20200217.2331

---
+++ elpa/ht-20210113.822/ht-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/neagleson/.emacs.d/elpa/ht-20210113.822/ht.el  -*- no-byte-compile: t -*-
+(define-package "ht" "20210113.822" "The missing hash table library for Emacs" '((dash "2.12.0")) :commit "5876209c9712bd1acbbf1d31f6e3293e5ab88971" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :keywords '("hash table" "hash map" "hash"))

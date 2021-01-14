c8015c31c8f2726a6cc673a6a5acd2da04f56d94
Nate Eagleson <nate@nateeag.com>
2021-01-14 10:58:17 -0500

    Update package vc-msg-20191128.1010

--- elpa/vc-msg-20201210.157/vc-msg-pkg.el
+++ elpa/vc-msg-20201210.157/vc-msg-pkg.el
@@ -1,12 +1,12 @@
-(define-package "vc-msg" "20191128.1010" "Show commit information of current line"
+(define-package "vc-msg" "20201210.157" "Show commit information of current line"
   '((emacs "24.4")
     (popup "0.5.0"))
-  :keywords
-  '("git" "vc" "svn" "hg" "messenger")
-  :authors
+  :commit "6c94578b5c692ade27c8b34bf5d99333ee7b4680" :authors
   '(("Chen Bin <chenbin DOT sh AT gmail DOT com>"))
   :maintainer
   '("Chen Bin <chenbin DOT sh AT gmail DOT com>")
+  :keywords
+  '("git" "vc" "svn" "hg" "messenger")
   :url "http://github.com/redguardtoo/vc-msg")
 ;; Local Variables:
 ;; no-byte-compile: t

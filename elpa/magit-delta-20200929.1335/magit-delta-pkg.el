1dfabad3697bb8c3c95ba7bdd4d9ce4a455e60c0
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:12:44 -0500

    Update package magit-delta-20200929.1335

--- elpa/magit-delta-20200929.1335/magit-delta-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/neagleson/.emacs.d/elpa/magit-delta-20200929.1335/magit-delta.el  -*- no-byte-compile: t -*-
-(define-package "magit-delta" "20200929.1335" "Use Delta when displaying diffs in Magit" '((emacs "25.1") (magit "20200426") (xterm-color "2.0")) :commit "b8526f890415374822514e488341d2b706d6bc2f" :authors '(("Dan Davison" . "dandavison7@gmail.com")) :maintainer '("Dan Davison" . "dandavison7@gmail.com") :url "https://github.com/dandavison/magit-delta")
---
+++ elpa/magit-delta-20210104.1541/magit-delta-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/neagleson/.emacs.d/elpa/magit-delta-20210104.1541/magit-delta.el  -*- no-byte-compile: t -*-
+(define-package "magit-delta" "20210104.1541" "Use Delta when displaying diffs in Magit" '((emacs "25.1") (magit "20200426") (xterm-color "2.0")) :commit "fc4de96e3faa1c983728239c5e41cc9f074b73a2" :authors '(("Dan Davison" . "dandavison7@gmail.com")) :maintainer '("Dan Davison" . "dandavison7@gmail.com") :url "https://github.com/dandavison/magit-delta")

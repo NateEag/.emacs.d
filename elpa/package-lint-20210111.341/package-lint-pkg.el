dffce085935414f7ce46c15452ab997b5dd66a87
Nate Eagleson <nate@nateeag.com>
2021-01-14 11:09:36 -0500

    Update package package-lint-20200906.512

--- elpa/package-lint-20210111.341/package-lint-pkg.el
+++ elpa/package-lint-20210111.341/package-lint-pkg.el
@@ -1,14 +1,14 @@
-(define-package "package-lint" "20200906.512" "A linting library for elisp package authors"
+(define-package "package-lint" "20210111.341" "A linting library for elisp package authors"
   '((cl-lib "0.5")
     (emacs "24.1")
     (let-alist "1.0.6"))
-  :commit "05596996286089acc7693b700c7c31780439e39f" :keywords
-  '("lisp")
-  :authors
+  :commit "348b15a1da1d62dee378b33971e39758b96c6575" :authors
   '(("Steve Purcell" . "steve@sanityinc.com")
     ("Fanael Linithien" . "fanael4@gmail.com"))
   :maintainer
   '("Steve Purcell" . "steve@sanityinc.com")
+  :keywords
+  '("lisp")
   :url "https://github.com/purcell/package-lint")
 ;; Local Variables:
 ;; no-byte-compile: t

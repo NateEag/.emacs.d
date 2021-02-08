1a1fd585f21f792c967f8ede8f3c1e3b1855da6f
Nate Eagleson <nate@nateeag.com>
2021-02-08 17:50:47 -0500

    Install pair-tree package
    I can't actually use it yet because I haven't got Emacs building with
    SVG support, but in principle it's possible.

---
+++ elpa/pair-tree-20210122.1506/pair-tree-autoloads.el
@@ -0,0 +1,27 @@
+;;; pair-tree-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "pair-tree" "pair-tree.el" (0 0 0 0))
+;;; Generated autoloads from pair-tree.el
+
+(autoload 'pair-tree "pair-tree" "\
+Draw a pair tree for PAIR.
+
+\(fn PAIR)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "pair-tree" '("pair-tree-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pair-tree-autoloads.el ends here

f1aacfb2524b95852ac917001f8592d1789ae640
Nate Eagleson <nate@nateeag.com>
2021-02-17 22:34:10 -0500

    Update package lispy-20201226.1746

--- elpa/lispy-20210121.926/lispy-pkg.el
+++ elpa/lispy-20210121.926/lispy-pkg.el
@@ -1,11 +1,11 @@
-(define-package "lispy" "20201226.1746" "vi-like Paredit"
+(define-package "lispy" "20210121.926" "vi-like Paredit"
   '((emacs "24.3")
     (ace-window "0.9.0")
     (iedit "0.9.9")
     (counsel "0.11.0")
     (hydra "0.14.0")
     (zoutline "0.1.0"))
-  :commit "1ad128be0afc04b58967c1158439d99931becef4" :authors
+  :commit "38a7df4cbb16cfe3d62dc8ea98b50e2d9a572e58" :authors
   '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
   :maintainer
   '("Oleh Krehel" . "ohwoeowho@gmail.com")

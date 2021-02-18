c7daa619646ca49dbdc4d0722bac3cc7d9329e1c
Nate Eagleson <nate@nateeag.com>
2021-02-17 22:29:29 -0500

    Update package tree-sitter-langs-20210106.1552

---
+++ elpa/tree-sitter-langs-20210217.1044/tree-sitter-langs-pkg.el
@@ -0,0 +1,13 @@
+(define-package "tree-sitter-langs" "20210217.1044" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.12.2"))
+  :commit "a811525932756a1fbfb69043d42bedcd483e50fa" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :url "https://github.com/ubolonton/emacs-tree-sitter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

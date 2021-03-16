43c5b7d0a13594ed6ed0dc2e98142265c0072647
Nate Eagleson <nate@nateeag.com>
2021-03-15 21:58:50 -0400

    Update package jedi-core-20191011.1750

--- elpa/jedi-core-20210202.856/jedi-core-pkg.el
+++ elpa/jedi-core-20210202.856/jedi-core-pkg.el
@@ -1,9 +1,9 @@
-(define-package "jedi-core" "20191011.1750" "Common code of jedi.el and company-jedi.el"
+(define-package "jedi-core" "20210202.856" "Common code of jedi.el and company-jedi.el"
   '((emacs "24")
     (epc "0.1.0")
     (python-environment "0.0.2")
     (cl-lib "0.5"))
-  :authors
+  :commit "3a9c503c35359d6bca6ff90c384c104c8743cdab" :authors
   '(("Takafumi Arakaki <aka.tkf at gmail.com>"))
   :maintainer
   '("Takafumi Arakaki <aka.tkf at gmail.com>"))

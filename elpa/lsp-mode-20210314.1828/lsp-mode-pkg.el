7b1e0a0d4ca7821eb284229349bc34aca12ee486
Nate Eagleson <nate@nateeag.com>
2021-03-15 21:57:38 -0400

    Update package lsp-mode-20210113.2045

---
+++ elpa/lsp-mode-20210314.1828/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20210314.1828" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "d2b90afdc947e411b8ce971bf0f6a01e2283d5d4" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

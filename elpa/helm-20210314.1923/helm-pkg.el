c8e43737281207209ef4ea9e0e8c25931bd7aead
Nate Eagleson <nate@nateeag.com>
2021-03-15 21:59:10 -0400

    Update package helm-20210111.2036

---
+++ elpa/helm-20210314.1923/helm-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm" "20210314.1923" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.7.1"))
+  :commit "b50d6de3fe319ba7d3530384f703802941b754a1" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://github.com/emacs-helm/helm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

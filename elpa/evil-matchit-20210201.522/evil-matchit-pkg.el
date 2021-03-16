7b27f3bad5a4ffc68eac1bb920f161daa9d5d5a3
Nate Eagleson <nate@nateeag.com>
2021-03-15 22:00:50 -0400

    Update package evil-matchit-20210110.1011

--- elpa/evil-matchit-20210201.522/evil-matchit-pkg.el
+++ elpa/evil-matchit-20210201.522/evil-matchit-pkg.el
@@ -1,7 +1,7 @@
-(define-package "evil-matchit" "20210110.1011" "Vim matchit ported to Evil"
+(define-package "evil-matchit" "20210201.522" "Vim matchit ported to Evil"
   '((evil "1.2.0")
     (emacs "25.1"))
-  :commit "9cdaddd55d28b50d1319baee8038972796e8b178" :authors
+  :commit "cdb9b90381ac0a225b01fb99472f5b23b612eb6e" :authors
   '(("Chen Bin <chenbin DOT sh AT gmail DOT com>"))
   :maintainer
   '("Chen Bin <chenbin DOT sh AT gmail DOT com>")

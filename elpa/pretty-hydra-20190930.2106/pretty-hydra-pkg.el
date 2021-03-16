0c3fadad26de5ce9aea8704ad0b377dc0ef944e9
Nate Eagleson <nate@nateeag.com>
2021-03-15 21:55:56 -0400

    Update package pretty-hydra-20190930.2106

--- elpa/pretty-hydra-20190930.2106/pretty-hydra-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/neagleson/.emacs.d/elpa/pretty-hydra-20190930.2106/pretty-hydra.el  -*- no-byte-compile: t -*-
-(define-package "pretty-hydra" "20190930.2106" "A macro for creating nice-looking hydras" '((hydra "0.15.0") (s "1.12.0") (dash "2.15.0") (dash-functional "1.2.0") (emacs "24")) :commit "20362323f66883c1336ffe70be24f91509addf54" :authors '(("Jerry Peng" . "pr2jerry@gmail.com")) :maintainer '("Jerry Peng" . "pr2jerry@gmail.com") :url "https://github.com/jerrypnz/major-mode-hydra.el")

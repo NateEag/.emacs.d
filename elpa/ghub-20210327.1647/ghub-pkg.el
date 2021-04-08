12967a672c4f3dc8b9951da00309e379dd455976
Nate Eagleson <nate@nateeag.com>
2021-04-08 18:08:40 -0400

    Update package ghub-20210227.1925

--- elpa/ghub-20210327.1647/ghub-pkg.el
+++ elpa/ghub-20210327.1647/ghub-pkg.el
@@ -1,8 +1,8 @@
-(define-package "ghub" "20210227.1925" "Minuscule client libraries for Git forge APIs."
+(define-package "ghub" "20210327.1647" "Minuscule client libraries for Git forge APIs."
   '((emacs "25.1")
     (let-alist "1.0.5")
     (treepy "0.1.1"))
-  :commit "f26c1f7e575209da047e77c18d415f9dc11015e2" :authors
+  :commit "2273c3b49a08cde0498b3b2cfae6c764629a4c93" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")

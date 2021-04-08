cb5576a154958e5c37fd4a84971999ded6f299dc
Nate Eagleson <nate@nateeag.com>
2021-04-08 18:03:04 -0400

    Update package posframe-20210311.937

--- elpa/posframe-20210311.937/posframe-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/neagleson/.emacs.d/elpa/posframe-20210311.937/posframe.el  -*- no-byte-compile: t -*-
-(define-package "posframe" "20210311.937" "Pop a posframe (just a frame) at point" '((emacs "26")) :commit "fff21ccb706b576f4074883f9fa87d2bcc534096" :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :keywords '("convenience" "tooltip") :url "https://github.com/tumashu/posframe")

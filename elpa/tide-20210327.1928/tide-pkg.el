a701ac69c3307cf0cd022379746b27b172f45fa9
Nate Eagleson <nate@nateeag.com>
2021-04-08 18:03:52 -0400

    Update package tide-20210313.1524

---
+++ elpa/tide-20210327.1928/tide-pkg.el
@@ -0,0 +1,17 @@
+(define-package "tide" "20210327.1928" "Typescript Interactive Development Environment"
+  '((emacs "25.1")
+    (dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :commit "ad6fa78911d5d7e85c0851c0c1afc01f3cbde7c1" :authors
+  '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  '("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :keywords
+  '("typescript")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

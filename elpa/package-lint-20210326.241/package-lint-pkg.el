05d2ea6c1206a564f1f0f35785a8be010a7dcf9f
Nate Eagleson <nate@nateeag.com>
2021-04-08 18:05:50 -0400

    Update package package-lint-20210227.314

--- elpa/package-lint-20210326.241/package-lint-pkg.el
+++ elpa/package-lint-20210326.241/package-lint-pkg.el
@@ -1,8 +1,8 @@
-(define-package "package-lint" "20210227.314" "A linting library for elisp package authors"
+(define-package "package-lint" "20210326.241" "A linting library for elisp package authors"
   '((cl-lib "0.5")
     (emacs "24.1")
     (let-alist "1.0.6"))
-  :commit "82282a8f8b89969ad2671d095906e9bc61abbb04" :authors
+  :commit "16e589114cc1f2514d95a58d53e1ae7c2ce941b4" :authors
   '(("Steve Purcell" . "steve@sanityinc.com")
     ("Fanael Linithien" . "fanael4@gmail.com"))
   :maintainer

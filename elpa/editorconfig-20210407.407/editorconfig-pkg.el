323d413768445e935113265e2ee8c795d382622b
Nate Eagleson <nate@nateeag.com>
2021-04-09 11:46:54 -0400

    Update package editorconfig-20210112.901

--- elpa/editorconfig-20210407.407/editorconfig-pkg.el
+++ elpa/editorconfig-20210407.407/editorconfig-pkg.el
@@ -1,7 +1,8 @@
-(define-package "editorconfig" "20210112.901" "EditorConfig Emacs Plugin"
+(define-package "editorconfig" "20210407.407" "EditorConfig Emacs Plugin"
   '((cl-lib "0.5")
+    (nadvice "0.3")
     (emacs "24"))
-  :commit "9dd9789b77521eb9d128f9ebd4cfc7ef45072d0f" :authors
+  :commit "e10fa22b1304c5801b026c10cc5d65a97ccc0a48" :authors
   '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
   :maintainer
   '("EditorConfig Team" . "editorconfig@googlegroups.com")

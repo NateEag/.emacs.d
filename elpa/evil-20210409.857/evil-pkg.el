4ee8f3f690b4703c64022181303ae7a97581a020
Nate Eagleson <nate@nateeag.com>
2021-04-09 11:46:20 -0400

    Update package evil-20210407.2119

--- elpa/evil-20210409.857/evil-pkg.el
+++ elpa/evil-20210409.857/evil-pkg.el
@@ -1,8 +1,8 @@
-(define-package "evil" "20210407.2119" "Extensible Vi layer for Emacs."
+(define-package "evil" "20210409.857" "Extensible Vi layer for Emacs."
   '((emacs "24.1")
     (goto-chg "1.6")
     (cl-lib "0.5"))
-  :commit "d3cc85bf828f04256de4a36ddb7624064b0252c8" :keywords
+  :commit "7d00c23496c25a66f90ac7a6a354b1c7f9498162" :keywords
   '("emulation" "vim")
   :url "https://github.com/emacs-evil/evil")
 ;; Local Variables:

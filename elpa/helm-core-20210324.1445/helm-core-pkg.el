664a40db95ff8eac87280d432ba7dbecbb21a784
Nate Eagleson <nate@nateeag.com>
2021-04-26 20:02:39 -0400

    Uninstall helm packages since I mostly use Ivy now

--- elpa/helm-core-20210324.1445/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20210324.1445" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "45450d57d5ff97e1a6fbd2a9816ae912ac50edcb")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:

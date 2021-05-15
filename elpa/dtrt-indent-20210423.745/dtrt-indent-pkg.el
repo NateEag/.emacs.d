7b73fd1f241c39b35c60bea04989653d7bf85785
Nate Eagleson <nate@nateeag.com>
2021-05-15 16:17:08 -0400

    Update package dtrt-indent-20210307.2140

--- elpa/dtrt-indent-20210423.745/dtrt-indent-pkg.el
+++ elpa/dtrt-indent-20210423.745/dtrt-indent-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dtrt-indent" "20210307.2140" "Adapt to foreign indentation offsets" 'nil :commit "37529fc7a98564164c87103e5107a6dca32b0e44" :authors
+(define-package "dtrt-indent" "20210423.745" "Adapt to foreign indentation offsets" 'nil :commit "9714f2c5f1c9b7c21e732df8c15a870a88caba84" :authors
   '(("Julian Scheid" . "julians37@googlemail.com"))
   :maintainer
   '("Reuben Thomas" . "rrt@sc3d.org")
--- package-quickstart.el
+++ package-quickstart.el
@@ -21205,10 +21205,10 @@ See `dynamic-spaces-mode' for more information on Dynamic-Spaces mode.
 
 
 )
-(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/dtrt-indent-20210307.2140/dtrt-indent-autoloads.el"))
+(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/dtrt-indent-20210423.745/dtrt-indent-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/dtrt-indent-20210307.2140/dtrt-indent-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/dtrt-indent-20210423.745/dtrt-indent-autoloads.el") (car load-path))))
 
 
 

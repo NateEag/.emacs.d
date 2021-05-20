ad6606dda059ffa779e8e6ef74c8adfaa447311e
Nate Eagleson <nate@nateeag.com>
2021-05-20 10:06:07 -0400

    Update package racket-mode-20210510.1517

--- elpa/racket-mode-20210517.1613/racket-mode-pkg.el
+++ elpa/racket-mode-20210517.1613/racket-mode-pkg.el
@@ -1,8 +1,8 @@
-(define-package "racket-mode" "20210510.1517" "Racket editing, REPL, and more"
+(define-package "racket-mode" "20210517.1613" "Racket editing, REPL, and more"
   '((emacs "25.1")
     (faceup "0.0.2")
     (pos-tip "20191127.1028"))
-  :commit "fc7e8144f117ae0c1e61904e3ee6401853c83deb" :authors
+  :commit "abd59fd4538f09fb201957d87887e58e377f0255" :authors
   '(("Greg Hendershott"))
   :maintainer
   '("Greg Hendershott")

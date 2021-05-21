17a01e25da38e917ec229e9968ad79d6755f0c4a
Nate Eagleson <nate@nateeag.com>
2021-05-21 09:18:18 -0400

    Update package lsp-ui-20210330.428

--- elpa/lsp-ui-20210520.1518/lsp-ui-pkg.el
+++ elpa/lsp-ui-20210520.1518/lsp-ui-pkg.el
@@ -1,9 +1,9 @@
-(define-package "lsp-ui" "20210330.428" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20210520.1518" "UI modules for lsp-mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-  :commit "efae00eb6a733d5271cb33e9d92c3d8c2fa98dde" :authors
+  :commit "bd94da9348d9b4e814fdb55dae061826f48ffde9" :authors
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
   :maintainer
   '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
--- package-quickstart.el
+++ package-quickstart.el
@@ -15348,10 +15348,10 @@ and toggle it if ARG is `toggle'; disable the mode otherwise.
 
 
 )
-(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/lsp-ui-20210330.428/lsp-ui-autoloads.el"))
+(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/lsp-ui-20210520.1518/lsp-ui-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/lsp-ui-20210330.428/lsp-ui-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/lsp-ui-20210520.1518/lsp-ui-autoloads.el") (car load-path))))
 
 
 

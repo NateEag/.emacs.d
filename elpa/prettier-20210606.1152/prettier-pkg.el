74033150dbe324b8ac9d5b7efe508fd8bd5cf82d
Nate Eagleson <nate@nateeag.com>
2021-06-17 11:50:38 -0400

    Update package prettier-20210313.1047

--- elpa/prettier-20210606.1152/prettier-pkg.el
+++ elpa/prettier-20210606.1152/prettier-pkg.el
@@ -1,8 +1,8 @@
-(define-package "prettier" "20210313.1047" "Code formatting with Prettier"
+(define-package "prettier" "20210606.1152" "Code formatting with Prettier"
   '((emacs "26.1")
     (iter2 "0.9")
     (nvm "0.2"))
-  :commit "5232b886bb6a4fb3848d63db1bcfcb30487a180f" :authors
+  :commit "e38d21a885e234af9ea6b03f499c487175570571" :authors
   '(("Julian Scheid" . "julians37@gmail.com"))
   :maintainer
   '("Julian Scheid" . "julians37@gmail.com")

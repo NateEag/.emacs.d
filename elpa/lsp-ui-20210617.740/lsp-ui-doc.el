6cdd6094cabd039dac495c936d1b3d0fe87bbae0
Nate Eagleson <nate@nateeag.com>
2021-06-17 11:52:26 -0400

    Update package lsp-ui-20210520.1518

--- elpa/lsp-ui-20210617.740/lsp-ui-doc.el
+++ elpa/lsp-ui-20210617.740/lsp-ui-doc.el
@@ -599,7 +599,7 @@ FN is the function to call on click."
   "Make empty lines half normal lines."
   (progn  ; Customize line before header
     (goto-char 1)
-    (insert (propertize "\n" 'face '(:height 0.2))))
+    (insert (propertize "\n" 'face '(:height 0.3))))
   (progn  ; Customize line after header
     (forward-line 1)
     (insert (propertize " " 'face '(:height 0.1))))
@@ -738,7 +738,6 @@ FN is the function to call on click."
                    (-zip-with 'lsp-ui-doc--inline-zip buffer-strings it)
                    (string-join it "\n")
                    (concat it "\n"))))
-    (add-face-text-property 0 (length merged) 'default t merged)
     merged))
 
 (defun lsp-ui-doc--inline-pos-at (start lines)
@@ -774,6 +773,7 @@ HEIGHT is the documentation number of lines."
           (ov (if (overlayp lsp-ui-doc--inline-ov) lsp-ui-doc--inline-ov
                 (setq lsp-ui-doc--inline-ov (make-overlay start end)))))
     (move-overlay ov start end)
+    (overlay-put ov 'face 'default)
     (overlay-put ov 'display (lsp-ui-doc--inline-merge buffer-string))
     (overlay-put ov 'lsp-ui-doc-inline t)
     (overlay-put ov 'window (selected-window))))
--- elpa/lsp-ui-20210617.740/lsp-ui-pkg.el
+++ elpa/lsp-ui-20210617.740/lsp-ui-pkg.el
@@ -1,9 +1,9 @@
-(define-package "lsp-ui" "20210520.1518" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20210617.740" "UI modules for lsp-mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-  :commit "bd94da9348d9b4e814fdb55dae061826f48ffde9" :authors
+  :commit "29976bb24e7e9bd327b666583d809bfefda64015" :authors
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
   :maintainer
   '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")

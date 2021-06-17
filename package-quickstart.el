6ab590337a94c00f4e34cf036172e9e0d111160a
Nate Eagleson <nate@nateeag.com>
2021-06-17 11:53:14 -0400

    Update package git-timemachine-20200603.701

--- package-quickstart.el
+++ package-quickstart.el
@@ -18337,10 +18337,10 @@ If target path is not found in COMMITISH tree, go up path and try again until fo
 
 
 )
-(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/git-timemachine-20200603.701/git-timemachine-autoloads.el"))
+(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/git-timemachine-20210528.908/git-timemachine-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/git-timemachine-20200603.701/git-timemachine-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/git-timemachine-20210528.908/git-timemachine-autoloads.el") (car load-path))))
 
 
 

6658d4ac38574b0b8be225338cbbb8872b3f4377
Nate Eagleson <nate@nateeag.com>
2022-02-12 00:38:04 -0500

    Update package haskell-mode-20210507.2243

--- elpa/haskell-mode-20220208.1821/haskell-mode-pkg.el
+++ elpa/haskell-mode-20220208.1821/haskell-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "haskell-mode" "20210507.2243" "A Haskell editing mode"
+(define-package "haskell-mode" "20220208.1821" "A Haskell editing mode"
   '((emacs "25.1"))
-  :commit "b35e8547b67f3d2c4d38ec7e7e516e3c0925352f" :authors
+  :commit "f13b6db78bc880e680c320e015ae232f525e0a27" :authors
   '(("1992      Simon Marlow")
     ("1997-1998 Graeme E Moss" . "gem@cs.york.ac.uk")
     ("Tommy Thorn" . "thorn@irisa.fr")

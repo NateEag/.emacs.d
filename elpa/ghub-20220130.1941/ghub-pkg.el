80d14bf155759884957f42a2a0e46d0b3d9f01db
Nate Eagleson <nate@nateeag.com>
2022-02-11 23:40:14 -0500

    Update package ghub-20210615.1504

--- elpa/ghub-20220130.1941/ghub-pkg.el
+++ elpa/ghub-20220130.1941/ghub-pkg.el
@@ -1,8 +1,8 @@
-(define-package "ghub" "20210615.1504" "Minuscule client libraries for Git forge APIs."
+(define-package "ghub" "20220130.1941" "Minuscule client libraries for Git forge APIs."
   '((emacs "25.1")
     (let-alist "1.0.5")
     (treepy "0.1.1"))
-  :commit "f2901f027ecf85614631e6b4b1e9c045e8cf3db5" :authors
+  :commit "e0d0bc7d97d1410d2a1cfc32007215328df1c361" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
--- elpa/ghub-20220130.1941/ghub.el
+++ elpa/ghub-20220130.1941/ghub.el
@@ -1,6 +1,6 @@
 ;;; ghub.el --- minuscule client libraries for Git forge APIs  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016-2021  Jonas Bernoulli
+;; Copyright (C) 2016-2022  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/ghub
@@ -53,7 +53,6 @@
 (require 'auth-source)
 (require 'cl-lib)
 (require 'gnutls)
-(require 'json)
 (require 'let-alist)
 (require 'url)
 (require 'url-auth)
@@ -81,6 +80,19 @@ only serves as documentation.")
 
 (defvar ghub-insecure-hosts nil
   "List of hosts that use http instead of https.")
+
+(defvar ghub-json-use-jansson nil
+  "Whether to use the Jansson library, if available.
+This is experimental.  Only let-bind this but do not enable it
+globally because doing that is likely to break other packages
+that use `ghub'.  As a user also do not enable this yet.
+See https://github.com/magit/ghub/pull/149.")
+
+(defvar ghub-json-object-type 'alist
+  "The object type that is used for json payload decoding.")
+
+(defvar ghub-json-array-type 'list
+  "The array type that is used for json payload decoding.")
 
 ;;; Request
 ;;;; Object
@@ -471,8 +483,8 @@ kludge.
 - For any other non-nil value use the kludge, if and only if we
   believe that doing so is the correct thing to do.
 
-The default value of this variable is either nil or `forge'.  It
-is `forge' if using libgnutls >=3.6.3 (the version introducing
+The default value of this variable is either nil or `force'.  It
+is `force' if using libgnutls >=3.6.3 (the version introducing
 TLS1.3); AND also using Emacs < 26.3 and/or macOS (any version).
 
 If the value is any other non-nil value, then `ghub--retrieve'
@@ -625,13 +637,21 @@ and https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341.")
   (let ((raw (ghub--decode-payload)))
     (and raw
          (condition-case nil
-             (let ((json-object-type 'alist)
-                   (json-array-type  'list)
-                   (json-key-type    'symbol)
-                   (json-false       nil)
-                   (json-null        nil))
-               (json-read-from-string raw))
-           (json-readtable-error
+             (if (and ghub-json-use-jansson
+                      (fboundp 'json-parse-string))
+                 (json-parse-string
+                  raw
+                  :object-type  ghub-json-object-type
+                  :array-type   ghub-json-array-type
+                  :false-object nil
+                  :null-object  nil)
+               (require 'json)
+               (let ((json-object-type ghub-json-object-type)
+                     (json-array-type  ghub-json-array-type)
+                     (json-false       nil)
+                     (json-null        nil))
+                 (json-read-from-string raw)))
+           ((json-parse-error json-readtable-error)
             `((message
                . ,(if (looking-at "<!DOCTYPE html>")
                       (if (re-search-forward
@@ -652,10 +672,15 @@ and https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341.")
   (and payload
        (progn
          (unless (stringp payload)
-           ;; Unfortunately `json-encode' may modify the input.
-           ;; See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=40693.
-           ;; and https://github.com/magit/forge/issues/267
-           (setq payload (json-encode (copy-tree payload))))
+           (setq payload
+                 (if (and ghub-json-use-jansson
+                          (fboundp 'json-serialize))
+                     (json-serialize payload)
+                   ;; Unfortunately `json-encode' may modify the input.
+                   ;; See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=40693.
+                   ;; and https://github.com/magit/forge/issues/267
+                   (require 'json)
+                   (json-encode (copy-tree payload)))))
          (encode-coding-string payload 'utf-8))))
 
 (defun ghub--url-encode-params (params)
@@ -698,7 +723,7 @@ and call `auth-source-forget+'."
                 headers)
         (cons (ghub--auth host auth username forge) headers)))))
 
-(defun ghub--auth (host auth &optional username forge)
+(cl-defgeneric ghub--auth (host auth &optional username forge)
   (unless username
     (setq username (ghub--username host forge)))
   (if (eq auth 'basic)
@@ -758,7 +783,7 @@ and call `auth-source-forget+'."
                 (auth-source-forget (list :host host :user user :max 1))
                 (and (not nocreate)
                      (error "\
-Required %s token (%S for %S) does not exist.
+Required %s token (\"%s\" for \"%s\") does not exist.
 See https://magit.vc/manual/ghub/Getting-Started.html
 or (info \"(ghub)Getting Started\") for instructions.
 \(The setup wizard no longer exists.)"
@@ -766,7 +791,7 @@ or (info \"(ghub)Getting Started\") for instructions.
                             user host))))))
     (if (functionp token) (funcall token) token)))
 
-(cl-defmethod ghub--host (&optional forge)
+(cl-defgeneric ghub--host (&optional forge)
   (cl-ecase forge
     ((nil github)
      (or (ignore-errors (car (process-lines "git" "config" "github.host")))
@@ -784,7 +809,7 @@ or (info \"(ghub)Getting Started\") for instructions.
      (or (ignore-errors (car (process-lines "git" "config" "bitbucket.host")))
          (bound-and-true-p buck-default-host)))))
 
-(cl-defmethod ghub--username (host &optional forge)
+(cl-defgeneric ghub--username (host &optional forge)
   (let ((var
          (cl-ecase forge
            ((nil github)
@@ -848,18 +873,21 @@ won't see the secret from a line that is followed by a commented
 line."
     (save-match-data (funcall fn))))
 
-(advice-add 'url-http-handle-authentication :around
-            'url-http-handle-authentication@unauthorized-bugfix)
-(defun url-http-handle-authentication@unauthorized-bugfix (fn proxy)
-  "If authorization failed then don't try again but fail properly.
+(progn ; (when (< emacs-major-version 28)
+  ;; Fixed by Emacs commit 0b98ea5fbe276c67206896dca111c000f984ee0f,
+  ;; but keep this advice until 28.1 is released.
+  (advice-add 'url-http-handle-authentication :around
+              'url-http-handle-authentication@unauthorized-bugfix)
+  (defun url-http-handle-authentication@unauthorized-bugfix (fn proxy)
+    "If authorization failed then don't try again but fail properly.
 For Emacs 27.1 prevent a useful `http' error from being replaced
 by a generic one that omits all useful information.  For earlier
 releases prevent a new request from being made, which would
 either result in an infinite loop or (e.g. in the case of `ghub')
 the user being asked for their name."
-  (if (assoc "Authorization" url-http-extra-headers)
-      t ; Return "success", here also known as "successfully failed".
-    (funcall fn proxy)))
+    (if (assoc "Authorization" url-http-extra-headers)
+        t ; Return "success", here also known as "successfully failed".
+      (funcall fn proxy))))
 
 ;;; _
 (provide 'ghub)

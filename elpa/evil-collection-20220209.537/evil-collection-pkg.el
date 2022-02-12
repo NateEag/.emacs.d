fd6d7e8e714d2bd84f389a1c449e549072cfef21
Nate Eagleson <nate@nateeag.com>
2022-02-12 01:26:13 -0500

    Update package evil-collection-20210609.1647

---
+++ elpa/evil-collection-20220209.537/evil-collection-pkg.el
@@ -0,0 +1,14 @@
+(define-package "evil-collection" "20220209.537" "A set of keybindings for Evil mode"
+  '((emacs "25.1")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "1959625fca6c8bedc5394e4e75ef0cae60487505" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

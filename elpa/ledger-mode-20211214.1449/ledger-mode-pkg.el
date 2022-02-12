0e03f586e504aeb0f49a8e51cc29924f414dfd03
Nate Eagleson <nate@nateeag.com>
2022-02-12 00:36:40 -0500

    Update package ledger-mode-20210516.2045

---
+++ elpa/ledger-mode-20211214.1449/ledger-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ledger-mode" "20211214.1449" "Helper code for use with the \"ledger\" command-line tool"
+  '((emacs "25.1"))
+  :commit "f47df6a18060002b47352b9c4b502556204df61b")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

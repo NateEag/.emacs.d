750a5cb2efea0d9997afa77b3918a332f30b7c05
Nate Eagleson <nate@nateeag.com>
2022-02-11 23:41:24 -0500

    Update package writeroom-mode-20201229.2242

--- elpa/writeroom-mode-20210927.1301/writeroom-mode-pkg.el
+++ elpa/writeroom-mode-20210927.1301/writeroom-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "writeroom-mode" "20201229.2242" "Minor mode for distraction-free writing"
+(define-package "writeroom-mode" "20210927.1301" "Minor mode for distraction-free writing"
   '((emacs "25.1")
     (visual-fill-column "2.2"))
-  :commit "b648b340172ce4e44307375697e190bc723203e0" :authors
+  :commit "eac1da790f316f357ed76ed67fbb790d6a4d126a" :authors
   '(("Joost Kremers" . "joostkremers@fastmail.fm"))
   :maintainer
   '("Joost Kremers" . "joostkremers@fastmail.fm")

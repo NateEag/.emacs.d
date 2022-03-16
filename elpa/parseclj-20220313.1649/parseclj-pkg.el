2c4c4256b269c8d345a75dbefcb0ec90a414eb71
Nate Eagleson <nate@nateeag.com>
2022-03-16 10:01:46 -0400

    Update package parseclj-20220207.1351

--- elpa/parseclj-20220313.1649/parseclj-pkg.el
+++ elpa/parseclj-20220313.1649/parseclj-pkg.el
@@ -1,6 +1,6 @@
-(define-package "parseclj" "20220207.1351" "Clojure/EDN parser"
+(define-package "parseclj" "20220313.1649" "Clojure/EDN parser"
   '((emacs "25"))
-  :commit "90595049634549e6d8872f719b13e9555897d17b" :authors
+  :commit "f32c041f2b301e790429fd16677c8cab11645702" :authors
   '(("Arne Brasseur" . "arne@arnebrasseur.net"))
   :maintainer
   '("Arne Brasseur" . "arne@arnebrasseur.net")

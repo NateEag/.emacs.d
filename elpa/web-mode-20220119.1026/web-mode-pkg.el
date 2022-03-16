0fdb4e5dec8d0ece85f4846800d92fa60abea455
Nate Eagleson <nate@nateeag.com>
2022-03-16 09:55:29 -0400

    Update package web-mode-20220119.1026

--- elpa/web-mode-20220119.1026/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
-(define-package "web-mode" "20220119.1026" "major mode for editing web templates" '((emacs "23.1")) :commit "d95e0db1bd042d1a8c9bb6bf744eb07ecbf62d73" :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :keywords '("languages") :url "https://web-mode.org")
---
+++ elpa/web-mode-20220227.1557/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
+(define-package "web-mode" "20220227.1557" "major mode for editing web templates" '((emacs "23.1")) :commit "f70277774a725e177774cc81ecbd228792cd6656" :authors '(("François-Xavier Bois")) :maintainer '("François-Xavier Bois" . "fxbois@gmail.com") :keywords '("languages") :url "https://web-mode.org")

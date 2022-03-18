a9accee0af8d018f619854ca1cdd3d439728fad4
Nate Eagleson <nate@nateeag.com>
2022-03-18 14:58:38 -0400

    Fix broken ELPA installation of queue library
    It had apparently been broken for years. Looks like an HTTP fetch created the
    file but got no response to put into it, at a wild guess.
    
    I'm really not sure how my init.el was working all this time with an empty
    version of this library. I guess I must not have been immediately using any
    libraries that depended on it until my recent installation of a few new
    packages.

--- elpa/queue-0.2/queue-autoloads.el
+++ elpa/queue-0.2/queue-autoloads.el
@@ -6,6 +6,20 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
+;;;### (autoloads nil "queue" "queue.el" (0 0 0 0))
+;;; Generated autoloads from queue.el
+
+(defalias 'make-queue 'queue-create "\
+Create an empty queue data structure.")
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "queue" '("queue")))
+
+;;;***
+
+;;;### (autoloads nil nil ("queue-pkg.el") (0 0 0 0))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t

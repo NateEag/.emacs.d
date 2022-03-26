3ce05865e61d375209f056fec8468e914360ceae
Nate Eagleson <nate@nateeag.com>
2022-03-26 09:52:12 -0400

    Add transient history to the project
    I figure it probably couldn't hurt to have this preserved when I move to
    different machines. Not sure what transient uses it for, but the data
    appears to be generic, not specific to a given machine's contents.

---
+++ transient/history.el
@@ -0,0 +1,45 @@
+(("git-branch" nil)
+ ("git-commit" nil)
+ ("git-commit" nil)
+ ("git-push" nil)
+ ("git-svn" nil)
+ ("git-svn" nil)
+ (magit-am
+  ("--3way"))
+ (magit-blame
+  ("-w")
+  nil)
+ (magit-branch nil)
+ (magit-cherry-pick
+  ("--ff"))
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat")
+  nil)
+ (magit-dispatch nil)
+ (magit-ediff nil)
+ (magit-fetch nil)
+ (magit-gitignore nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate")
+  ("--graph" "--decorate"))
+ (magit-merge nil
+              ("--ff-only"))
+ (magit-pull nil
+             ("--rebase"))
+ (magit-push nil
+             ("--force-with-lease")
+             ("--force"))
+ (magit-rebase
+  ("--autosquash" "--autostash")
+  nil
+  ("--autosquash")
+  ("--interactive"))
+ (magit-remote
+  ("-f"))
+ (magit-reset nil)
+ (magit-stash nil
+              ("--include-untracked"))
+ (magit-submodule nil)
+ (magit-svn nil)
+ (magit-tag nil))

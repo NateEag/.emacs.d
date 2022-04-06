742e23a4353441e49b1b853e664e022ccb93020f
Nate Eagleson <nate@nateeag.com>
2022-04-06 15:32:47 -0400

    Update package code-review-20220107.1355

--- elpa/code-review-20220328.108/code-review-pkg.el
+++ elpa/code-review-20220328.108/code-review-pkg.el
@@ -1,4 +1,4 @@
-(define-package "code-review" "20220107.1355" "Perform code review from Github, Gitlab, and Bitbucket Cloud"
+(define-package "code-review" "20220328.108" "Perform code review from Github, Gitlab, and Bitbucket Cloud"
   '((emacs "25.1")
     (closql "1.2.0")
     (magit "3.0.0")
@@ -9,7 +9,7 @@
     (markdown-mode "2.4")
     (forge "0.3.0")
     (emojify "1.2"))
-  :commit "ccc3795a72554439f230969322c0e3239252c193" :authors
+  :commit "85ab2080e489b4ca01c787f5a316ade02a4ee877" :authors
   '(("Wanderson Ferreira <https://github.com/wandersoncferreira>"))
   :maintainer
   '("Wanderson Ferreira" . "wand@hey.com")
--- package-quickstart.el
+++ package-quickstart.el
@@ -23873,10 +23873,10 @@ See `compact-docstrings-mode' for more information on Compact-Docstrings mode.
 
 
 )
-(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/code-review-20220107.1355/code-review-autoloads.el"))
+(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/code-review-20220328.108/code-review-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/code-review-20220107.1355/code-review-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/code-review-20220328.108/code-review-autoloads.el") (car load-path))))
 
 
 

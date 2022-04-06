7487d2666f192806f202d30ff28d666fc7bec45a
Nate Eagleson <nate@nateeag.com>
2022-04-06 13:20:31 -0400

    Update package evil-20220309.2216

--- elpa/evil-20220403.1955/evil-pkg.el
+++ elpa/evil-20220403.1955/evil-pkg.el
@@ -1,8 +1,8 @@
-(define-package "evil" "20220309.2216" "Extensible Vi layer for Emacs."
+(define-package "evil" "20220403.1955" "Extensible Vi layer for Emacs."
   '((emacs "24.1")
     (goto-chg "1.6")
     (cl-lib "0.5"))
-  :commit "3e41a823334abbba9cf16e482855699054d9dfe0" :maintainer
+  :commit "067a29214d9d4e1fc4b4d29fc596ad4cefe7e492" :maintainer
   '("Tom Dalziel" . "tom.dalziel@gmail.com")
   :keywords
   '("emulation" "vim")

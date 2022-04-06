48feb9960a09e727f192885e1e78417808b91538
Nate Eagleson <nate@nateeag.com>
2022-04-06 15:21:24 -0400

    Update package lsp-java-20211124.605

--- elpa/lsp-java-20220325.547/lsp-java-pkg.el
+++ elpa/lsp-java-20220325.547/lsp-java-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-java" "20211124.605" "Java support for lsp-mode"
+(define-package "lsp-java" "20220325.547" "Java support for lsp-mode"
   '((emacs "25.1")
     (lsp-mode "6.0")
     (markdown-mode "2.3")
@@ -8,7 +8,7 @@
     (request "0.3.0")
     (treemacs "2.5")
     (dap-mode "0.5"))
-  :commit "ce03cb6574566e334c3ce5735458cc3ec1989486" :keywords
+  :commit "0968038b9aea52ef3bf499e597cf3800d913c059" :keywords
   '("languague" "tools")
   :url "https://github.com/emacs-lsp/lsp-java")
 ;; Local Variables:
--- package-quickstart.el
+++ package-quickstart.el
@@ -16710,10 +16710,10 @@ evaluate python in the context of the debugee, ...." t nil)
 
 
 )
-(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/lsp-java-20211124.605/lsp-java-autoloads.el"))
+(let ((load-file-name "/Users/neagleson/.emacs.d/elpa/lsp-java-20220325.547/lsp-java-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/lsp-java-20211124.605/lsp-java-autoloads.el") (car load-path))))
+                         (or (file-name-directory "/Users/neagleson/.emacs.d/elpa/lsp-java-20220325.547/lsp-java-autoloads.el") (car load-path))))
 
 
 (with-eval-after-load 'lsp-java (require 'dap-java))

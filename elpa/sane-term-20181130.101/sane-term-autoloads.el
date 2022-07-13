6360b68b02f026418fb4231fec358f5bc54f6eb5
Nate Eagleson <nate@nateeag.com>
2022-07-13 15:59:32 -0400

    Remove package sane-term

--- elpa/sane-term-20181130.101/sane-term-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; sane-term-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "sane-term" "sane-term.el" (23558 35268 0 0))
-;;; Generated autoloads from sane-term.el
-
-(autoload 'sane-term-create "sane-term" "\
-Create new term buffer.
-
-\(fn)" t nil)
-
-(autoload 'sane-term "sane-term" "\
-Cycle through term buffers, creating if necessary.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sane-term-autoloads.el ends here

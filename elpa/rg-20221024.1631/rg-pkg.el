4f86b3a8aae04a8f747a7d0300bccd22d8a970e5
Nate Eagleson <nate@nateeag.com>
2022-11-10 22:21:02 -0500

    Install rg and ripgrep packages

---
+++ elpa/rg-20221024.1631/rg-pkg.el
@@ -0,0 +1,15 @@
+(define-package "rg" "20221024.1631" "A search tool based on ripgrep"
+  '((emacs "25.1")
+    (transient "0.3.0")
+    (wgrep "2.1.10"))
+  :commit "3880e8c734c976a2d393aeacf60d510f56d8217d" :authors
+  '(("David Landell" . "david.landell@sunnyhill.email")
+    ("Roland McGrath" . "roland@gnu.org"))
+  :maintainer
+  '("David Landell" . "david.landell@sunnyhill.email")
+  :keywords
+  '("matching" "tools")
+  :url "https://github.com/dajva/rg.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

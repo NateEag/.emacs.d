e5d9b4d393a53cc0cd068477011318ba9a5dc8bb
Nate Eagleson <nate@nateeag.com>
2023-02-19 23:46:45 -0500

    Install package terraform-mode from ELPA

---
+++ elpa/terraform-doc-20211003.1333/terraform-doc-autoloads.el
@@ -0,0 +1,27 @@
+;;; terraform-doc-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "terraform-doc" "terraform-doc.el" (0 0 0 0))
+;;; Generated autoloads from terraform-doc.el
+
+(autoload 'terraform-doc "terraform-doc" "\
+Look up PROVIDER.
+
+\(fn &optional PROVIDER)" t nil)
+
+(register-definition-prefixes "terraform-doc" '("terraform-doc-"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; terraform-doc-autoloads.el ends here
---
+++ elpa/terraform-mode-20221208.2213/terraform-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from terraform-mode.el  -*- no-byte-compile: t -*-
+(define-package "terraform-mode" "20221208.2213" "Major mode for terraform configuration file" '((emacs "24.3") (hcl-mode "0.3") (dash "2.17.0")) :commit "e67459fefc871fdbf20e27be8f85b98b10b97b1b" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-terraform-mode")

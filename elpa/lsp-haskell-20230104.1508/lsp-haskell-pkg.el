c04a801197725391a31fa01c8e34dc39b3b572ba
Nate Eagleson <nate@nateeag.com>
2023-04-04 15:48:44 -0400

    Install lsp-haskell package

---
+++ elpa/lsp-haskell-20230104.1508/lsp-haskell-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from lsp-haskell.el  -*- no-byte-compile: t -*-
+(define-package "lsp-haskell" "20230104.1508" "Haskell support for lsp-mode" '((emacs "24.3") (lsp-mode "3.0") (haskell-mode "16.1")) :commit "3249cde75fb411f95fe173c222b848182fd0b752" :keywords '("haskell") :url "https://github.com/emacs-lsp/lsp-haskell")

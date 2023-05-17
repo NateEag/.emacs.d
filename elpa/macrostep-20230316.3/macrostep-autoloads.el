e49cb7fa37d7cdc78d8d3c20939eb09c66730601
Nate Eagleson <nate@nateeag.com>
2023-05-17 12:43:18 -0400

    Update package macrostep-20161120.2106

--- elpa/macrostep-20230316.3/macrostep-autoloads.el
+++ elpa/macrostep-20230316.3/macrostep-autoloads.el
@@ -1,13 +1,30 @@
-;;; macrostep-autoloads.el --- automatically extracted autoloads
+;;; macrostep-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "macrostep" "macrostep.el" (23400 19362 0 0))
+;;;### (autoloads nil "macrostep" "macrostep.el" (0 0 0 0))
 ;;; Generated autoloads from macrostep.el
 
 (autoload 'macrostep-mode "macrostep" "\
 Minor mode for inline expansion of macros in Emacs Lisp source buffers.
+
+This is a minor mode.  If called interactively, toggle the
+`Macrostep mode' mode.  If the prefix argument is positive,
+enable the mode, and if it is zero or negative, disable the mode.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `macrostep-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
 
 \\<macrostep-keymap>Progressively expand macro forms with \\[macrostep-expand], collapse them with \\[macrostep-collapse],
 and move back and forth with \\[macrostep-next-macro] and \\[macrostep-prev-macro].
@@ -22,7 +39,7 @@ quit and return to normal editing.
 Expand the macro form following point by one step.
 
 Enters `macrostep-mode' if it is not already active, making the
-buffer temporarily read-only. If macrostep-mode is active and the
+buffer temporarily read-only.  If `macrostep-mode' is active and the
 form following point is not a macro form, search forward in the
 buffer and expand the next macro form found, if any.
 
@@ -33,22 +50,22 @@ behaviors.
 
 \(fn &optional TOGGLE-SEPARATE-BUFFER)" t nil)
 
+(register-definition-prefixes "macrostep" '("macrostep-"))
+
 ;;;***
 
-;;;### (autoloads nil "macrostep-c" "macrostep-c.el" (23400 19362
-;;;;;;  0 0))
+;;;### (autoloads nil "macrostep-c" "macrostep-c.el" (0 0 0 0))
 ;;; Generated autoloads from macrostep-c.el
 
-(autoload 'macrostep-c-mode-hook "macrostep-c" "\
-
-
-\(fn)" nil nil)
+(autoload 'macrostep-c-mode-hook "macrostep-c" nil nil nil)
 
 (add-hook 'c-mode-hook #'macrostep-c-mode-hook)
 
+(register-definition-prefixes "macrostep-c" '("macrostep-c-"))
+
 ;;;***
 
-;;;### (autoloads nil nil ("macrostep-pkg.el") (23400 19362 0 0))
+;;;### (autoloads nil nil ("macrostep-pkg.el") (0 0 0 0))
 
 ;;;***
 
@@ -56,5 +73,6 @@ behaviors.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; macrostep-autoloads.el ends here

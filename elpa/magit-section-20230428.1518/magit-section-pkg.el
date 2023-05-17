34be0d847bf27b15d6029ae88d34e800af1c30bd
Nate Eagleson <nate@nateeag.com>
2023-05-17 13:02:27 -0400

    Update package magit-20220331.2308

---
+++ elpa/magit-section-20230428.1518/magit-section-pkg.el
@@ -0,0 +1,14 @@
+(define-package "magit-section" "20230428.1518" "Sections for read-only buffers."
+  '((emacs "25.1")
+    (compat "29.1.3.4")
+    (dash "20221013"))
+  :commit "c5ebbf86ed9fa0cc33d5e7e788a55fe8c7ab4b93" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

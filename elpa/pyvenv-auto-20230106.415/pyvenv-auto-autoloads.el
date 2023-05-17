6a2c7ae10b53289af9423f199283be862d6b036c
Nate Eagleson <nate@nateeag.com>
2023-05-17 12:49:09 -0400

    Update package pyvenv-auto-20220315.1606

---
+++ elpa/pyvenv-auto-20230106.415/pyvenv-auto-autoloads.el
@@ -0,0 +1,22 @@
+;;; pyvenv-auto-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "pyvenv-auto" "pyvenv-auto.el" (0 0 0 0))
+;;; Generated autoloads from pyvenv-auto.el
+
+(register-definition-prefixes "pyvenv-auto" '("pyvenv-auto-"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pyvenv-auto-autoloads.el ends here
---
+++ elpa/pyvenv-auto-20230106.415/pyvenv-auto-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from pyvenv-auto.el  -*- no-byte-compile: t -*-
+(define-package "pyvenv-auto" "20230106.415" "Automatically switch Python venvs" '((emacs "26.3") (pyvenv "1.21")) :commit "b4365e60e3ba747a5fec8ca909f64fe8c73d8db2" :url "https://github.com/nryotaro/pyvenv-auto")

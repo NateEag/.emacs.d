3995a3035ba4f921a05ce50aff10a3667a1446e1
Nate Eagleson <nate@nateeag.com>
2023-05-17 16:53:16 -0400

    Update package aggressive-indent-20210701.2224

--- elpa/aggressive-indent-20230112.1300/aggressive-indent-autoloads.el
+++ elpa/aggressive-indent-20230112.1300/aggressive-indent-autoloads.el
@@ -1,4 +1,4 @@
-;;; aggressive-indent-autoloads.el --- automatically extracted autoloads
+;;; aggressive-indent-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -25,14 +25,22 @@ then keep indenting until nothing more happens.
 \(fn L R)" t nil)
 
 (autoload 'aggressive-indent-mode "aggressive-indent" "\
-Toggle Aggressive-Indent mode on or off.
+Minor mode to keep your code always indented.
 
-If called interactively, enable Aggressive-Indent mode if ARG is
-positive, and disable it if ARG is zero or negative.  If called
-from Lisp, also enable the mode if ARG is omitted or nil, and
-toggle it if ARG is `toggle'; disable the mode otherwise.
+This is a minor mode.  If called interactively, toggle the
+`Aggressive-Indent mode' mode.  If the prefix argument is
+positive, enable the mode, and if it is zero or negative, disable
+the mode.
 
-\\{aggressive-indent-mode-map}
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `aggressive-indent-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
 
 \(fn &optional ARG)" t nil)
 
@@ -51,18 +59,22 @@ or call the function `global-aggressive-indent-mode'.")
 (autoload 'global-aggressive-indent-mode "aggressive-indent" "\
 Toggle Aggressive-Indent mode in all buffers.
 With prefix ARG, enable Global Aggressive-Indent mode if ARG is positive;
-otherwise, disable it.  If called from Lisp, enable the mode if
-ARG is omitted or nil.
+otherwise, disable it.
 
-Aggressive-Indent mode is enabled in all buffers where
-`aggressive-indent-mode' would do it.
+If called from Lisp, toggle the mode if ARG is `toggle'.
+Enable the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+Aggressive-Indent mode is enabled in all buffers where `aggressive-indent-mode'
+would do it.
+
 See `aggressive-indent-mode' for more information on Aggressive-Indent mode.
 
 \(fn &optional ARG)" t nil)
 
 (defalias 'aggressive-indent-global-mode #'global-aggressive-indent-mode)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "aggressive-indent" '("aggressive-indent-")))
+(register-definition-prefixes "aggressive-indent" '("aggressive-indent-"))
 
 ;;;***
 

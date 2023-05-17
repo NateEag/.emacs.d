e79318eb0e408322d755712e627aa8c479f1991c
Nate Eagleson <nate@nateeag.com>
2023-05-17 12:54:47 -0400

    Update package parseclj-20220328.558

--- elpa/parseclj-20220422.936/parseclj-autoloads.el
+++ elpa/parseclj-20220422.936/parseclj-autoloads.el
@@ -1,4 +1,4 @@
-;;; parseclj-autoloads.el --- automatically extracted autoloads
+;;; parseclj-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -9,7 +9,7 @@
 ;;;### (autoloads nil "parseclj" "parseclj.el" (0 0 0 0))
 ;;; Generated autoloads from parseclj.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parseclj" '("parseclj-")))
+(register-definition-prefixes "parseclj" '("parseclj-"))
 
 ;;;***
 
@@ -17,21 +17,21 @@
 ;;;;;;  0))
 ;;; Generated autoloads from parseclj-alist.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parseclj-alist" '("parseclj-")))
+(register-definition-prefixes "parseclj-alist" '("parseclj-"))
 
 ;;;***
 
 ;;;### (autoloads nil "parseclj-ast" "parseclj-ast.el" (0 0 0 0))
 ;;; Generated autoloads from parseclj-ast.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parseclj-ast" '("parseclj-ast-")))
+(register-definition-prefixes "parseclj-ast" '("parseclj-ast-"))
 
 ;;;***
 
 ;;;### (autoloads nil "parseclj-lex" "parseclj-lex.el" (0 0 0 0))
 ;;; Generated autoloads from parseclj-lex.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parseclj-lex" '("parseclj-lex-")))
+(register-definition-prefixes "parseclj-lex" '("parseclj-lex-"))
 
 ;;;***
 
@@ -39,7 +39,7 @@
 ;;;;;;  0 0))
 ;;; Generated autoloads from parseclj-parser.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "parseclj-parser" '("parseclj-")))
+(register-definition-prefixes "parseclj-parser" '("parseclj-"))
 
 ;;;***
 
--- elpa/parseclj-20220422.936/parseclj-pkg.el
+++ elpa/parseclj-20220422.936/parseclj-pkg.el
@@ -1,6 +1,8 @@
-(define-package "parseclj" "20220328.558" "Clojure/EDN parser"
+(define-package "parseclj" "20220422.936" "Clojure/EDN parser"
   '((emacs "25"))
-  :commit "b04eae67384c1d8181edf318fd62d422a8220724" :authors
+  :commit "4d0e780e00f1828b00c43099e6eebc6582998f72" :authors
+  '(("Arne Brasseur" . "arne@arnebrasseur.net"))
+  :maintainers
   '(("Arne Brasseur" . "arne@arnebrasseur.net"))
   :maintainer
   '("Arne Brasseur" . "arne@arnebrasseur.net")

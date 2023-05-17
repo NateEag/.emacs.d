c1d12ae448e935a39108605bc710f9435e838d0e
Nate Eagleson <nate@nateeag.com>
2023-05-17 11:17:49 -0400

    Update package swiper-20220430.2247

--- elpa/swiper-20220430.2247/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from swiper.el  -*- no-byte-compile: t -*-
-(define-package "swiper" "20220430.2247" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.4")) :commit "2051de5882706246da298ce5f60482cd7f469fe7" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :keywords '("matching") :url "https://github.com/abo-abo/swiper")

3e703918e165580d0510f045a22afd95ce8949ea
Nate Eagleson <nate@nateeag.com>
2023-05-17 11:36:08 -0400

    Update string-edit package to renamed string-edit-at-point

---
+++ elpa/string-edit-at-point-20220913.712/string-edit-at-point-autoloads.el
@@ -0,0 +1,27 @@
+;;; string-edit-at-point-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "string-edit-at-point" "string-edit-at-point.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from string-edit-at-point.el
+
+(autoload 'string-edit-at-point "string-edit-at-point" "\
+Pop up a buffer to edit the string at point.
+This saves you from needing to manually escape characters." t nil)
+
+(register-definition-prefixes "string-edit-at-point" '("se/" "string-edit-at-point-"))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; string-edit-at-point-autoloads.el ends here
---
+++ elpa/string-edit-at-point-20220913.712/string-edit-at-point-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from string-edit-at-point.el  -*- no-byte-compile: t -*-
+(define-package "string-edit-at-point" "20220913.712" "Avoid escape nightmares by editing string in separate buffer" '((dash "1.2.0")) :commit "e285ada415b975a9eb70a7e065123923d9bf8798" :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainers '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))

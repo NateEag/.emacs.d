11a425d6e2203214a778013feff7d3f218773f46
Nate Eagleson <nate@nateeag.com>
2023-05-17 09:28:41 -0400

    Update package xref-1.4.1

--- elpa/xref-1.4.1/xref-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from xref.el  -*- no-byte-compile: t -*-
-(define-package "xref" "1.4.1" "Cross-referencing commands" '((emacs "26.1")) :commit "890b08e41580a716c2f0b3f1a40ef9b52a19372c" :url "https://elpa.gnu.org/packages/xref.html")

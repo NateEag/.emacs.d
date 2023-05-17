5404c7d3b7f54ee6ed70f4f6edca2304b55cf0df
Nate Eagleson <nate@nateeag.com>
2023-05-17 11:16:31 -0400

    Update package tern-auto-complete-20170521.1935

--- elpa/tern-auto-complete-20191227.950/tern-auto-complete.el
+++ elpa/tern-auto-complete-20191227.950/tern-auto-complete.el
@@ -2,7 +2,8 @@
 
 ;; Author:  <m.sakurai at kiwanami.net>
 ;; Version: 0.0.1
-;; Package-Version: 20170521.1935
+;; Package-Version: 20191227.950
+;; Package-Commit: 0d19800db70a6348c627a69f444b91d21ad89629
 ;; Package-Requires: ((tern "0.0.1") (auto-complete "1.4") (cl-lib "0.5") (emacs "24"))
 
 ;;; Commentary:

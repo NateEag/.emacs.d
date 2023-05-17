10ae8008d9685bada5952ecd438fa0790a8720af
Nate Eagleson <nate@nateeag.com>
2023-05-17 13:24:54 -0400

    Update package corfu-0.20

--- elpa/corfu-0.20/corfu-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from corfu.el  -*- no-byte-compile: t -*-
-(define-package "corfu" "0.20" "Completion Overlay Region FUnction" '((emacs "27.1")) :commit "e2cc92b029bd9334f88d382def7066c35c31b3a9" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/corfu")
---
+++ elpa/corfu-0.36/corfu-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from corfu.el  -*- no-byte-compile: t -*-
+(define-package "corfu" "0.36" "Completion Overlay Region FUnction" '((emacs "27.1") (compat "29.1.4.0")) :commit "224f79b5f92d5a13a623a4cd7283a236b40dde81" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/corfu")

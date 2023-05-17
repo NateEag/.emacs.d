e3ec7d3ed67bbb3640f82ab3510bfdd6f4655413
Nate Eagleson <nate@nateeag.com>
2023-05-17 12:46:20 -0400

    Update package rjsx-mode-20200120.1446

--- elpa/rjsx-mode-20200224.2149/rjsx-mode-autoloads.el
+++ elpa/rjsx-mode-20200224.2149/rjsx-mode-autoloads.el
@@ -1,4 +1,4 @@
-;;; rjsx-mode-autoloads.el --- automatically extracted autoloads
+;;; rjsx-mode-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -17,10 +17,19 @@ Major mode for editing JSX files.
 (autoload 'rjsx-minor-mode "rjsx-mode" "\
 Minor mode for parsing JSX syntax into an AST.
 
-If called interactively, enable Rjsx minor mode if ARG is positive, and
-disable it if ARG is zero or negative.  If called from Lisp,
-also enable the mode if ARG is omitted or nil, and toggle it
-if ARG is `toggle'; disable the mode otherwise.
+This is a minor mode.  If called interactively, toggle the `rjsx
+minor mode' mode.  If the prefix argument is positive, enable the
+mode, and if it is zero or negative, disable the mode.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `rjsx-minor-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
 
 \(fn &optional ARG)" t nil)
 
@@ -40,7 +49,7 @@ instead.
 
 \(fn ARG)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rjsx-mode" '("rjsx-")))
+(register-definition-prefixes "rjsx-mode" '("rjsx-"))
 
 ;;;***
 
---
+++ elpa/rjsx-mode-20200224.2149/rjsx-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from rjsx-mode.el  -*- no-byte-compile: t -*-
+(define-package "rjsx-mode" "20200224.2149" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "0061587a06cdc2579a8d0e90863498d96bf982d8" :authors '(("Felipe Ochoa" . "felipe@fov.space")) :maintainers '(("Felipe Ochoa" . "felipe@fov.space")) :maintainer '("Felipe Ochoa" . "felipe@fov.space") :keywords '("languages") :url "https://github.com/felipeochoa/rjsx-mode/")

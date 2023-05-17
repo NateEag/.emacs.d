5715998503bbe24429b9248f57ed1479aa02c5ad
Nate Eagleson <nate@nateeag.com>
2023-05-17 13:14:25 -0400

    Update package add-node-modules-path-20220315.340

--- elpa/add-node-modules-path-20230307.655/add-node-modules-path-autoloads.el
+++ elpa/add-node-modules-path-20230307.655/add-node-modules-path-autoloads.el
@@ -1,4 +1,4 @@
-;;; add-node-modules-path-autoloads.el --- automatically extracted autoloads
+;;; add-node-modules-path-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -10,10 +10,11 @@
 ;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from add-node-modules-path.el
 
-(defvar add-node-modules-path-command "npm bin" "\
-Command to find the bin path.")
+(defvar add-node-modules-path-command '("npm bin") "\
+Command(s) to find the bin path. To add multiple bin paths, simply add
+multiple commands to the list, e.g. \\='(\"pnpm bin\" \"pnpm bin -w\")")
 
-(custom-autoload 'add-node-modules-path-command "add-node-modules-path" t)
+(custom-autoload 'add-node-modules-path-command "add-node-modules-path" nil)
 
 (defvar add-node-modules-path-debug nil "\
 Enable verbose output when non nil.")
@@ -23,6 +24,8 @@ Enable verbose output when non nil.")
 (autoload 'add-node-modules-path "add-node-modules-path" "\
 Run `npm bin` command and add the path to the `exec-path`.
 If `npm` command fails, it does nothing." t nil)
+
+(register-definition-prefixes "add-node-modules-path" '("add-node-modules-path/"))
 
 ;;;***
 
---
+++ elpa/add-node-modules-path-20230307.655/add-node-modules-path-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from add-node-modules-path.el  -*- no-byte-compile: t -*-
+(define-package "add-node-modules-path" "20230307.655" "Add node_modules to your exec-path" '((s "1.12.0")) :commit "841e93dfed50448da66c89a977c9182bb18796a1" :authors '(("Neri Marschik" . "marschik_neri@cyberagent.co.jp")) :maintainers '(("Neri Marschik" . "marschik_neri@cyberagent.co.jp")) :maintainer '("Neri Marschik" . "marschik_neri@cyberagent.co.jp") :keywords '("javascript" "node" "node_modules" "eslint") :url "https://github.com/codesuki/add-node-modules-path")

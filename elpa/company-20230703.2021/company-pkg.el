8c47aa89a0f6563d7ae4cc7743477c73a200b625
Nate Eagleson <nate@nateeag.com>
2023-08-30 12:09:07 -0400

    Update package company-20230416.2034

--- elpa/company-20230703.2021/company-pkg.el
+++ elpa/company-20230703.2021/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20230416.2034" "Modular text completion framework"
+(define-package "company" "20230703.2021" "Modular text completion framework"
   '((emacs "25.1"))
-  :commit "8a78f320019574bc35b5727f95b052b27918da20" :authors
+  :commit "3ec40b0a0ea751b6c48f24abd58c8304deb53014" :authors
   '(("Nikolaj Schumacher"))
   :maintainers
   '(("Dmitry Gutov" . "dgutov@yandex.ru"))
--- elpa/company-20230703.2021/company.el
+++ elpa/company-20230703.2021/company.el
@@ -1,6 +1,6 @@
 ;;; company.el --- Modular text completion framework  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2009-2022  Free Software Foundation, Inc.
+;; Copyright (C) 2009-2023  Free Software Foundation, Inc.
 
 ;; Author: Nikolaj Schumacher
 ;; Maintainer: Dmitry Gutov <dgutov@yandex.ru>
@@ -1061,6 +1061,10 @@ means that `company-mode' is always turned on except in `message-mode' buffers."
         (row (cdr (or (posn-actual-col-row posn)
                       ;; When position is non-visible for some reason.
                       (posn-col-row posn)))))
+    ;; posn-col-row return value relative to the left
+    (when (eq (current-bidi-paragraph-direction) 'right-to-left)
+      (let ((ww (window-body-width)))
+        (setq col (- ww col))))
     (when (bound-and-true-p display-line-numbers)
       (cl-decf col (+ 2 (line-number-display-width))))
     (cons (+ col (window-hscroll)) row)))

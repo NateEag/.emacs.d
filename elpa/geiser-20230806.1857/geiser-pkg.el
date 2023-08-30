690fc1bbbf068de4eb93ebde04b1b60498697580
Nate Eagleson <nate@nateeag.com>
2023-08-30 12:08:48 -0400

    Update package geiser-20230503.1044

--- elpa/geiser-20230806.1857/geiser-pkg.el
+++ elpa/geiser-20230806.1857/geiser-pkg.el
@@ -1,7 +1,7 @@
-(define-package "geiser" "20230503.1044" "GNU Emacs and Scheme talk to each other"
-  '((emacs "25.1")
+(define-package "geiser" "20230806.1857" "GNU Emacs and Scheme talk to each other"
+  '((emacs "27.1")
     (project "0.8.1"))
-  :commit "83305e6bb5f13d68f8e31d457605b02d2a10c014" :authors
+  :commit "d743a8cd1df4101a6dc2226ccfc3b4883ec761c5" :authors
   '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
   :maintainers
   '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))

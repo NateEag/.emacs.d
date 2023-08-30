230f85e8ccdd120e52bf55cca484cc7f114be6a6
Nate Eagleson <nate@nateeag.com>
2023-08-30 12:15:09 -0400

    Update package bufler-20221031.1852

--- elpa/bufler-20230830.518/bufler-pkg.el
+++ elpa/bufler-20230830.518/bufler-pkg.el
@@ -1,11 +1,11 @@
-(define-package "bufler" "20221031.1852" "Group buffers into workspaces with programmable rules"
+(define-package "bufler" "20230830.518" "Group buffers into workspaces with programmable rules"
   '((emacs "26.3")
     (dash "2.18")
     (f "0.17")
     (pretty-hydra "0.2.2")
     (magit-section "0.1")
     (map "2.1"))
-  :commit "bf5fdccbae6bb6dc51e31dc282805e32bb41e412" :authors
+  :commit "06d67144778f80a19db58aef1495ba18bbbba147" :authors
   '(("Adam Porter" . "adam@alphapapa.net"))
   :maintainers
   '(("Adam Porter" . "adam@alphapapa.net"))

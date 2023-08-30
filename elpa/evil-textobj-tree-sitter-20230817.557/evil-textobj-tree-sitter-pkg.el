5ce1c1f4302d19218066e769c334677aaa693f76
Nate Eagleson <nate@nateeag.com>
2023-08-30 12:10:27 -0400

    Update package evil-textobj-tree-sitter-20230515.519

---
+++ elpa/evil-textobj-tree-sitter-20230817.557/evil-textobj-tree-sitter-pkg.el
@@ -0,0 +1,8 @@
+(define-package "evil-textobj-tree-sitter" "20230817.557" "Provides evil textobjects using tree-sitter"
+  '((emacs "25.1"))
+  :commit "19979843f5fc437917f9a4dae977f5e6d4793726" :keywords
+  '("evil" "tree-sitter" "text-object" "convenience")
+  :url "https://github.com/meain/evil-textobj-tree-sitter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

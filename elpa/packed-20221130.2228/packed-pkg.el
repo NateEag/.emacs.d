22a7c3870502f298ef9265621d602c9c394832f6
Nate Eagleson <nate@nateeag.com>
2025-03-12 23:22:37 -0400

    Manually remove packed.el package
    It's no longer on Elpa, apparently.

--- elpa/packed-20221130.2228/packed-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from packed.el  -*- no-byte-compile: t -*-
-(define-package "packed" "20221130.2228" "[DEPRECATED] Package manager agnostic Emacs Lisp package utilities" '((emacs "25.1") (compat "28.1.1.0")) :commit "169064f7acfe198cc7dd43d02518b773691e1314" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainers '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("lisp") :url "https://github.com/emacscollective/packed")

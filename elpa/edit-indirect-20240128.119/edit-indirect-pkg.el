aabd3ec1e51d3f009a996e923d923e94b9f50ad5
Nate Eagleson <nate@nateeag.com>
2025-03-12 21:53:24 -0400

    Update package edit-indirect-20220511.1124

---
+++ elpa/edit-indirect-20240128.119/edit-indirect-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "edit-indirect" "20240128.119"
+  "Edit regions in separate buffers."
+  '((emacs "24.3"))
+  :url "https://github.com/Fanael/edit-indirect"
+  :commit "82a28d8a85277cfe453af464603ea330eae41c05"
+  :revdesc "82a28d8a8527"
+  :authors '(("Fanael Linithien" . "fanael4@gmail.com"))
+  :maintainers '(("Fanael Linithien" . "fanael4@gmail.com")))

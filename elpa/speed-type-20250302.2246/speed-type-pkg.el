e41ab15f9f96eb1c496dffa240d5ecedf1320208
Nate Eagleson <nate@nateeag.com>
2025-03-12 21:51:16 -0400

    Update package speed-type-20230206.1330

---
+++ elpa/speed-type-20250302.2246/speed-type-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "speed-type" "20250302.2246"
+  "Practice touch and speed typing."
+  '((emacs  "26.1")
+    (compat "29.1.3")
+    (dash   "2.19.1"))
+  :url "https://github.com/dakra/speed-type"
+  :commit "9602ac6a794ff16393f6665afe7096b2690e4393"
+  :revdesc "9602ac6a794f"
+  :keywords '("games")
+  :maintainers '(("Daniel Kraus" . "daniel@kraus.my")))

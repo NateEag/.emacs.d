1747aa934e5d896e16c967e8f6300ef1011dbe51
Nate Eagleson <nate@nateeag.com>
2025-03-13 00:55:06 -0400

    Update package key-chord-20230522.223

---
+++ elpa/key-chord-20240910.1441/key-chord-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "key-chord" "20240910.1441"
+  "Map pairs of simultaneously pressed keys to commands."
+  '((emacs "24"))
+  :url "https://github.com/emacsorphanage/key-chord"
+  :commit "fc75b1451759121601110da8ddfadcf5156318af"
+  :revdesc "fc75b1451759"
+  :keywords '("keyboard" "chord" "input")
+  :authors '(("David Andersson" . "l.david.anderssonsverige.nu"))
+  :maintainers '(("David Andersson" . "l.david.anderssonsverige.nu")))

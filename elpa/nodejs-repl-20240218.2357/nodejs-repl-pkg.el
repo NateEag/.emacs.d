821e27d047fbc75534ee740ceb80938c43ef389e
Nate Eagleson <nate@nateeag.com>
2025-03-12 23:30:44 -0400

    Update package nodejs-repl-20200802.1310

---
+++ elpa/nodejs-repl-20240218.2357/nodejs-repl-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "nodejs-repl" "20240218.2357"
+  "Run Node.js REPL."
+  ()
+  :url "https://github.com/abicky/nodejs-repl.el"
+  :commit "77a864ca72a6c30217085f1c4db5de72e47eb4da"
+  :revdesc "77a864ca72a6")

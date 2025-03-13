e6322e6bdcda71c8eb5edc23c42eb5d47480422e
Nate Eagleson <nate@nateeag.com>
2025-03-12 21:46:30 -0400

    Update package posframe-20230714.227

--- elpa/posframe-20250211.110/posframe-benchmark.el
+++ elpa/posframe-20250211.110/posframe-benchmark.el
@@ -5,7 +5,6 @@
 ;; Author: Feng Shu <tumashu@163.com>
 ;; Maintainer: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/posframe
-;; Version: 1.0.3
 ;; Keywords: convenience, tooltip
 ;; Package-Requires: ((emacs "26"))
 

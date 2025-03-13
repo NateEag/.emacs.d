094bd973e786ea3ee2b649001a6737e920e506fb
Nate Eagleson <nate@nateeag.com>
2025-03-12 21:48:54 -0400

    Update package term-projectile-20230727.2308

---
+++ elpa/term-projectile-20240602.2356/term-projectile-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "term-projectile" "20240602.2356"
+  "Projectile terminal management."
+  '((emacs        "24")
+    (term-manager "0.1.0")
+    (projectile   "0.13.0"))
+  :url "https://www.github.com/IvanMalison/term-manager"
+  :commit "25353734c65cd5cc952e4893b552629ca1d0d37f"
+  :revdesc "25353734c65c"
+  :keywords '("projectile" "tools" "terminals" "vc")
+  :authors '(("Ivan Malison" . "IvanMalison@gmail.com"))
+  :maintainers '(("Ivan Malison" . "IvanMalison@gmail.com")))

eaf66b3ef8606628d41d0f6404ab85a3a6c6b1b9
Nate Eagleson <nate@nateeag.com>
2025-10-19 10:07:41 -0400

    Delete several unused versions of package
    Judging by the conflict markers, they must have slipped past me in a
    botched merge lately.

--- elpa/ledger-mode-20250313.244/ledger-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "ledger-mode" "20250313.244"
-  "Helper code for use with the \"ledger\" command-line tool."
-  '((emacs "25.1"))
-  :url "https://github.com/ledger/ledger-mode"
-  :commit "5546f19567fb4043548a343a06d4b96b44d3bb11"
-  :revdesc "5546f19567fb")

967be02af6f763078567a091fba9f11b254d9b0c
Nate Eagleson <nate@nateeag.com>
2025-12-08 21:11:46 -0500

    Install git-gutter-fringe from MELPA

---
+++ elpa/git-gutter-fringe-20211003.2228/git-gutter-fringe-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "git-gutter-fringe" "20211003.2228"
+  "Fringe version of git-gutter.el."
+  '((git-gutter    "0.88")
+    (fringe-helper "0.1.1")
+    (cl-lib        "0.5")
+    (emacs         "24"))
+  :url "https://github.com/emacsorphanage/git-gutter-fringe"
+  :commit "648cb5b57faec55711803cdc9434e55a733c3eba"
+  :revdesc "648cb5b57fae"
+  :authors '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainers '(("Neil Okamoto" . "neil.okamoto+melpa@gmail.com")))

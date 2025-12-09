c43ff7ae6ef963d18bd434ab733da3ee3ff021d1
Nate Eagleson <nate@nateeag.com>
2025-12-08 21:11:56 -0500

    Install qml-mode package

---
+++ elpa/qml-mode-20161016.31/qml-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "qml-mode" "20161016.31"
+  "Major mode for editing QT Declarative (QML) code."
+  ()
+  :url "https://github.com/coldnew/qml-mode"
+  :commit "6c5f33ba88ae010bf201a80ee8095e20a724558c"
+  :revdesc "6c5f33ba88ae"
+  :keywords '("qml" "qt" "qt declarative")
+  :authors '(("Yen-Chin Lee" . "coldnew.tw@gmail.com"))
+  :maintainers '(("Yen-Chin Lee" . "coldnew.tw@gmail.com")))

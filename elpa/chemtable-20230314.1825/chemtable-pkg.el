c6c69994af266b7dbbe3005f584bbcc928557a2a
Nate Eagleson <nate@nateeag.com>
2026-05-05 12:00:02 -0400

    Install chemtable package
    I'm honestly surprised there isn't an Emacs built-in for this.
    
    ...no doubt in a year or three I'll discover it.

---
+++ elpa/chemtable-20230314.1825/chemtable-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "chemtable" "20230314.1825"
+  "Periodic table of the elements."
+  '((emacs "24.1"))
+  :url "https://github.com/sergiruiztrepat/chemtable"
+  :commit "ca0fea2f28162e90a93be242279ec6aee9046475"
+  :revdesc "ca0fea2f2816"
+  :keywords '("convenience" "chemistry"))

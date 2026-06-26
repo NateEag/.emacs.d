460a450032b2eb0d39224ccc651753870a2d52e0
Nate Eagleson <nate@nateeag.com>
2026-06-26 17:53:01 -0400

    Update package popup-20250101.843

--- elpa/popup-20251231.1622/popup-pkg.el
+++ elpa/popup-20251231.1622/popup-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "popup" "20250101.843"
+(define-package "popup" "20251231.1622"
   "Visual Popup User Interface."
   '((emacs "24.3"))
   :url "https://github.com/auto-complete/popup-el"
-  :commit "7a05700a37aae66d2b24f0cd8851f65383a5cf96"
-  :revdesc "7a05700a37aa"
+  :commit "45a0b759076ce4139aba36dde0a2904136282e73"
+  :revdesc "45a0b759076c"
   :keywords '("lisp")
   :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
   :maintainers '(("Jen-Chieh" . "jcs090218@gmail.com")))
--- elpa/popup-20251231.1622/popup.el
+++ elpa/popup-20251231.1622/popup.el
@@ -1,14 +1,14 @@
 ;;; popup.el --- Visual Popup User Interface  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2009-2015  Tomohiro Matsuyama
-;; Copyright (c) 2020-2025 Jen-Chieh Shen
+;; Copyright (c) 2020-2026 Jen-Chieh Shen
 
 ;; Author: Tomohiro Matsuyama <m2ym.pub@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/auto-complete/popup-el
 ;; Keywords: lisp
-;; Package-Version: 20250101.843
-;; Package-Revision: 7a05700a37aa
+;; Package-Version: 20251231.1622
+;; Package-Revision: 45a0b759076c
 ;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify

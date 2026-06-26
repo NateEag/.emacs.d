cedefa409b6cabd051e14f56d4eca2558fe9cdd7
Nate Eagleson <nate@nateeag.com>
2026-06-26 18:25:27 -0400

    Update package dap-mode-20251105.2320

---
+++ elpa/dap-mode-20260616.1526/dap-mode-pkg.el
@@ -0,0 +1,19 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dap-mode" "20260616.1526"
+  "Debug Adapter Protocol mode."
+  '((emacs        "29.1")
+    (dash         "2.18.0")
+    (lsp-mode     "6.0")
+    (bui          "1.1.0")
+    (f            "0.20.0")
+    (s            "1.12.0")
+    (lsp-treemacs "0.1")
+    (posframe     "0.7.0")
+    (ht           "2.3")
+    (lsp-docker   "1.0.0"))
+  :url "https://github.com/emacs-lsp/dap-mode"
+  :commit "c73a587d613788003986a11ffe393b46affe8322"
+  :revdesc "c73a587d6137"
+  :keywords '("languages" "debug")
+  :authors '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
+  :maintainers '(("Ivan Yonchovski" . "yyoncho@gmail.com")))

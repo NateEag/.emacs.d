6e3cb25ba4bf7d35d127cc6571b27fe6c7aa460a
Nate Eagleson <nate@nateeag.com>
2026-06-26 18:24:23 -0400

    Update package magit-20251202.2209

---
+++ elpa/magit-20260621.1253/magit-pkg.el
@@ -0,0 +1,19 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit" "20260621.1253"
+  "A Git porcelain inside Emacs."
+  '((emacs         "28.1")
+    (compat        "31.0")
+    (cond-let      "0.2")
+    (llama         "1.0")
+    (magit-section "4.5")
+    (seq           "2.24")
+    (transient     "0.13")
+    (with-editor   "3.4"))
+  :url "https://github.com/magit/magit"
+  :commit "20058311576bd47751681bdc2ae239da76dec2dc"
+  :revdesc "20058311576b"
+  :keywords '("git" "tools" "vc")
+  :authors '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+             ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+                 ("Kyle Meyer" . "kyle@kyleam.com")))

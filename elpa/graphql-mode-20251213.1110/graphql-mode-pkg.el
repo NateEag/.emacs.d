54b8b25b837de4f7300a7a62a9877dc162ed23b9
Nate Eagleson <nate@nateeag.com>
2026-06-26 18:26:20 -0400

    Update package graphql-mode-20240218.1603

---
+++ elpa/graphql-mode-20251213.1110/graphql-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "graphql-mode" "20251213.1110"
+  "Major mode for editing GraphQL schemas."
+  '((emacs "25.1"))
+  :url "https://github.com/davazp/graphql-mode"
+  :commit "ccec811d0f1cb13e63e330234e408d56ba0f4d8e"
+  :revdesc "ccec811d0f1c"
+  :keywords '("languages")
+  :authors '(("David Vazquez Pua" . "davazp@gmail.com"))
+  :maintainers '(("David Vazquez Pua" . "davazp@gmail.com")))

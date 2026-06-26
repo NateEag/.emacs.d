fe4062427c68f207a9cb57de4309b90481f32922
Nate Eagleson <nate@nateeag.com>
2026-06-26 17:54:19 -0400

    Update package flycheck-20251128.1706

--- elpa/flycheck-20260604.2002/flycheck-pkg.el
+++ elpa/flycheck-20260604.2002/flycheck-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "flycheck" "20251128.1706"
+(define-package "flycheck" "20260604.2002"
   "On-the-fly syntax checking."
   '((emacs "27.1")
     (seq   "2.24"))
-  :url "https://www.flycheck.org"
-  :commit "62570fafbedb8fa3f7d75a50a9364feca3b294ef"
-  :revdesc "62570fafbedb"
+  :url "https://github.com/flycheck/flycheck"
+  :commit "96f1852c7e352c969393e6e66176178177e933be"
+  :revdesc "96f1852c7e35"
   :keywords '("convenience" "languages" "tools")
   :authors '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainers '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com")

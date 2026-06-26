64ab259ed62164962c415c23a1f5e62ea9c44e9c
Nate Eagleson <nate@nateeag.com>
2026-06-26 18:09:30 -0400

    Update transient package

---
+++ elpa/transient-20260617.1137/transient-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20260617.1137"
+  "Transient commands."
+  '((emacs    "28.1")
+    (compat   "31.0")
+    (cond-let "1.1")
+    (llama    "1.0")
+    (seq      "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "9d103f338fb9fd4506a0f1651c483209ab838f49"
+  :revdesc "9d103f338fb9"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))

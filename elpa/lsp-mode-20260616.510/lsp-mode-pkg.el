22fbbb72ca0a7479fa54b4751e3daadd21afca62
Nate Eagleson <nate@nateeag.com>
2026-06-26 18:24:42 -0400

    Update package lsp-mode-20251204.1027

---
+++ elpa/lsp-mode-20260616.510/lsp-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-mode" "20260616.510"
+  "LSP mode."
+  '((emacs         "29.1")
+    (dash          "2.18.0")
+    (f             "0.21.0")
+    (ht            "2.3")
+    (spinner       "1.7.3")
+    (markdown-mode "2.3")
+    (lv            "0")
+    (eldoc         "1.11"))
+  :url "https://github.com/emacs-lsp/lsp-mode"
+  :commit "2a6ab7cd41e31e4a74b1a6ea670ac4e4356f4be9"
+  :revdesc "2a6ab7cd41e3"
+  :keywords '("languages"))

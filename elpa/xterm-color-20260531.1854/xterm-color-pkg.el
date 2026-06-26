f321090cc8ac412ea6df8b6b6595ef6537e386af
Nate Eagleson <nate@nateeag.com>
2026-06-26 17:52:33 -0400

    Update package xterm-color-20251128.1842

--- elpa/xterm-color-20260531.1854/xterm-color-pkg.el
+++ elpa/xterm-color-20260531.1854/xterm-color-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "xterm-color" "20251128.1842"
+(define-package "xterm-color" "20260531.1854"
   "ANSI, XTERM 256 and Truecolor support."
-  '((emacs "24.4"))
+  '((emacs "25.1"))
   :url "https://github.com/atomontage/xterm-color"
-  :commit "ce82e87ea3d277c7e4fc48ce390d540fbd78f6d1"
-  :revdesc "ce82e87ea3d2"
+  :commit "0b0d808f8bc5007037341dc5f63149cc32cf2c5b"
+  :revdesc "0b0d808f8bc5"
   :keywords '("faces")
   :authors '(("xristos" . "xristos@sdf.org"))
   :maintainers '(("xristos" . "xristos@sdf.org")))

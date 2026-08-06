2101b8e939584f888fea697c6d29a7e29e6ed34d
Nate Eagleson <nate@nateeag.com>
2026-08-06 02:59:53 -0400

    Add a few packages to mortgage conf

--- mortgage/site-lisp/package-conf.el
+++ mortgage/site-lisp/package-conf.el
@@ -16,13 +16,36 @@
 
 (use-package evil :ensure t :demand t)
 
-(use-package helpful :ensure t
+;; Make emacs' built-in help functions more helpful.
+;;
+;; They're already way better than those of any other program I use to begin
+;; with, but helpful does make them noticeably more useful.
+(use-package helpful
+  :ensure t
   :bind (("C-h f" . helpful-callable)
          ("C-h v" . helpful-variable)
          ("C-h k" . helpful-key)
          ("C-c C-d" . helpful-at-point)
          ("C-h F" . helpful-function)
          ("C-h C" . helpful-command)))
+
+;; Many minor modes don't need to be documented in the precious space of the
+;; modeline.
+;;
+;; ...and diminish.el is a lovely reflection on the nature of those things we
+;; take for granted and thus no longer notice, if you take the time to read the
+;; source.
+(use-package diminish
+  :ensure t)
+
+;; I love project-specific custom shell environments.
+;;
+;; Direnv is the best tool I've encountered for that.
+(use-package envrc
+  :ensure t
+  :diminish
+  :init
+  (envrc-global-mode))
 
 (provide 'package-conf)
 ;;; package-conf.el ends here
